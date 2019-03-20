550ac37b24fa625d44b0593e94d223a1819bcc9d
Neil Smith <github@nsmith.me.uk>
2019-03-20 11:15:17 +0000

    Added basic site to demo the local setup is working

--- service-viewer/app/public/index.php
+++ service-viewer/app/public/index.php
@@ -10,21 +10,4 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
-/**
- * Self-called anonymous function that creates its own scope and keep the global namespace clean.
- */
-(function () {
-    /** @var \Psr\Container\ContainerInterface $container */
-    $container = require 'config/container.php';
-
-    /** @var \Zend\Expressive\Application $app */
-    $app = $container->get(\Zend\Expressive\Application::class);
-    $factory = $container->get(\Zend\Expressive\MiddlewareFactory::class);
-
-    // Execute programmatic/declarative middleware pipeline and routing
-    // configuration statements
-    (require 'config/pipeline.php')($app, $factory, $container);
-    (require 'config/routes.php')($app, $factory, $container);
-
-    $app->run();
-})();
+echo "Viewer Container is working";
