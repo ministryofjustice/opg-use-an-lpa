94320474711e6244fefc5a88f1846fb655929f73
Tom <tomtucka@gmail.com>
2019-03-20 10:18:43 +0000

    UML-49 Added elb and alias to dns record

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -9,9 +9,9 @@ resource "aws_route53_record" "use-my-lpa" {
   name     = "${local.dns_prefix}"
   type     = "A"
 
-  #   alias {
-  #     evaluate_target_health = false
-  #     name                   = "${aws_lb.load_balancer.dns_name}"
-  #     zone_id                = "${aws_lb.load_balancer.zone_id}"
-  #   }
+  alias {
+    evaluate_target_health = false
+    name                   = "${aws_elb.view.dns_name}"
+    zone_id                = "${aws_elb.view.zone_id}"
+  }
 }
---
+++ terraform/load_balancer.tf
@@ -0,0 +1,11 @@
+resource "aws_elb" "view" {
+  name               = "view-${terraform.workspace}"
+  availability_zones = ["eu-west-2a"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+}
