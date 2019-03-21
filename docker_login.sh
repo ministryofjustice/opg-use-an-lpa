98ce6a4ed2b4444d910473f110e791cf48612439
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-03-21 17:00:46 +0000

    Remove newline from script

--- docker_login.sh
+++ docker_login.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Assume role in management account
-temp_role=$(/home/circleci/bin/aws sts assume-role --role-arn "arn:aws:iam::311462405659:role/ci"\
---role-session-name "ci")
+temp_role=$(/home/circleci/bin/aws sts assume-role --role-arn "arn:aws:iam::311462405659:role/ci" --role-session-name "ci")
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
