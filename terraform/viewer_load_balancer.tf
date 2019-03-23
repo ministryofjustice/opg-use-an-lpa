4694735c1f2a61bc2aa8f49b73d6dd6cba17e9ff
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-03-23 15:56:41 +0000

    Restore egress rule

--- terraform/viewer_load_balancer.tf
+++ terraform/viewer_load_balancer.tf
@@ -56,3 +56,12 @@ resource "aws_security_group_rule" "viewer_loadbalancer_ingress" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.viewer_loadbalancer.id}"
 }
+
+resource "aws_security_group_rule" "viewer_loadbalancer_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.viewer_loadbalancer.id}"
+}
