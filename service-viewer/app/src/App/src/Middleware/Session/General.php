8890f29b4aa96d39909b70ae5ba0f3d53d408f7d
Neil Smith <github@nsmith.me.uk>
2019-04-01 17:38:19 +0100

    Added session skeleton

---
+++ service-viewer/app/src/App/src/Middleware/Session/General.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware\Session;
+
+use App\Service\Session\Cookie as CookieSessionPersistence;
+
+use Zend\Expressive\Session\SessionMiddleware;
+
+class General extends SessionMiddleware
+{
+    public const SESSION_ATTRIBUTE = 's';
+
+    /**
+     * General constructor.
+     * @param CookieSessionPersistence $persistence
+     */
+    public function __construct(CookieSessionPersistence $persistence)
+    {
+        parent::__construct($persistence);
+
+        // Apply config
+    }
+}
