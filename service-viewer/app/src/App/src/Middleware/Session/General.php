cab57e0b1aafdb2570252cd40b46e7c254c1043c
Neil Smith <github@nsmith.me.uk>
2019-04-02 08:51:19 +0100

    Session updates

--- service-viewer/app/src/App/src/Middleware/Session/General.php
+++ service-viewer/app/src/App/src/Middleware/Session/General.php
@@ -6,11 +6,28 @@ namespace App\Middleware\Session;
 
 use App\Service\Session\Cookie as CookieSessionPersistence;
 
-use Zend\Expressive\Session\SessionMiddleware;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Expressive\Session\LazySession;
+use Zend\Expressive\Session\SessionPersistenceInterface;
 
-class General extends SessionMiddleware
+/**
+ * TODO: This could extent Zend\Expressive\Session\SessionMiddleware if/when self::SESSION_ATTRIBUTE is changed to static
+ *  https://github.com/zendframework/zend-expressive-session/pull/35
+ *
+ * Class General
+ * @package App\Middleware\Session
+ */
+class General implements MiddlewareInterface
 {
-    #public const SESSION_ATTRIBUTE = 'cookie';
+    public const SESSION_ATTRIBUTE = 'general';
+
+    /**
+     * @var SessionPersistenceInterface
+     */
+    private $persistence;
 
     /**
      * General constructor.
@@ -18,8 +35,14 @@ class General extends SessionMiddleware
      */
     public function __construct(CookieSessionPersistence $persistence)
     {
-        parent::__construct($persistence);
+        $this->persistence = $persistence;
 
-        # Apply config
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface
+    {
+        $session = new LazySession($this->persistence, $request);
+        $response = $handler->handle($request->withAttribute(static::SESSION_ATTRIBUTE, $session));
+        return $this->persistence->persistSession($session, $response);
     }
 }
