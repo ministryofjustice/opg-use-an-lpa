e3eb491fae2791ffe4a6cf0e0ef29634c6655da0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-04-02 23:20:31 +0100

    Run headless

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.headless = True
 
 def before_scenario(context, scenario):
   if 'web' in context.tags:
-    context.browser = webdriver.Firefox()
+    context.browser = webdriver.Firefox(options=options)
     context.browser.implicitly_wait(10)
  
 def after_scenario(context, scenario):
