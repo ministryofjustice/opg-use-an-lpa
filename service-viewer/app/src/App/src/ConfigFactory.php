a5a4e98d625eb40c9ba647280fc175519fccba8e
Neil Smith <github@nsmith.me.uk>
2019-04-03 13:15:16 +0100

    Moved Config object generation into a factory

---
+++ service-viewer/app/src/App/src/ConfigFactory.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+use DI\Factory\RequestedEntry;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Creates an instance of the Requested Entry, and injects all the config into it.
+ *
+ * Used in the generation of autowired 'Config' instances.
+ *
+ * Class ConfigFactory
+ * @package App
+ */
+class ConfigFactory
+{
+    public function __invoke(ContainerInterface $container, RequestedEntry $entityClass)
+    {
+        $class = $entityClass->getName();
+
+        return new $class(
+            $container->get('config')
+        );
+    }
+}
