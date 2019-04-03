ef791c656af4832c5b2b2f7a7744cff2e87d6dd9
Neil Smith <github@nsmith.me.uk>
2019-04-03 14:19:26 +0100

    Moved secret config into docker-compose

--- localstack-config/config.sh
+++ localstack-config/config.sh
@@ -9,8 +9,8 @@ if [ $? -ne 0 ]; then
     echo "Secrets Manager failed to start"
 else
     aws secretsmanager create-secret \
-        --name 'session-keys' \
-        --secret-string '{"1":"763C683D6F62536EC4A8694FEFBD1", "2":"DB63C574D8CEBBAD2EEACEA9E9D68", "3":null}' \
+        --name "$SECRET_SESSION_KEYS_KEY" \
+        --secret-string "$SECRET_SESSION_KEYS_VALUE" \
         --endpoint-url=http://localstack:4584 \
         --region eu-west-2
 fi
