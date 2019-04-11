ccb8e1bab95da68d9f5efa0424b46280789bd894
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-04-11 08:40:07 +0100

    Make state file for per-environment terraform configuration

---
+++ terraform_environment/credentials.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "s3" {
+    bucket         = "opg.terraform.state"
+    key            = "opg-use-my-lpa-environment/terraform.tfstate"
+    encrypt        = true
+    region         = "eu-west-1"
+    role_arn       = "arn:aws:iam::311462405659:role/state_write"
+    dynamodb_table = "remote_lock"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::${local.account}:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  alias  = "management"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.management_role}"
+    session_name = "terraform-session"
+  }
+}
