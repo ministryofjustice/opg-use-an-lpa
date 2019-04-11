060a9106e14003326527ff9c1d098a697b452608
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-04-11 23:41:58 +0100

    Make dns namespace variable

--- terraform_environment/locals.tf
+++ terraform_environment/locals.tf
@@ -1,13 +1,15 @@
 locals {
-  account    = "${lookup(var.accounts, terraform.workspace)}"
-  dns_prefix = "${lookup(var.dns_prefixes, terraform.workspace)}"
+  account_name = "${lookup(var.account_mapping, terraform.workspace, lookup(var.account_mapping, "development"))}"
+  account_id   = "${lookup(var.account_ids, local.account_name)}"
+
+  dns_namespace = "${terraform.workspace == "production" ? "": "${terraform.workspace}."}"
 
   mandatory_moj_tags = {
     business-unit    = "OPG"
     application      = "use-an-lpa"
     environment-name = "${terraform.workspace}"
     owner            = "Katie Gibbs: katie.gibbs@digital.justice.gov.uk"
-    is-production    = "${lookup(var.is_production, terraform.workspace)}"
+    is-production    = "${lookup(var.is_production, local.account_name)}"
   }
 
   optional_tags = {
@@ -17,14 +19,14 @@ locals {
   default_tags = "${merge(local.mandatory_moj_tags,local.optional_tags)}"
 }
 
-variable "dns_prefixes" {
-  type = "map"
-}
-
-variable "accounts" {
+variable "account_ids" {
   type = "map"
 }
 
 variable "is_production" {
+  type = "map"
+}
+
+variable "account_mapping" {
   type = "map"
 }
