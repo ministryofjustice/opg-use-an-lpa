718da4f11fff89c6ebec24796f1d7c6ef8683218
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-04-18 11:32:44 +0100

    Update get-frontend_url to correctly construct the URL

--- features/modules.py
+++ features/modules.py
@@ -6,11 +6,17 @@ def get_frontend_url( frontend ):
   The environment is based on the Terraform Workspace environment variable.
   """
   workspace = os.getenv('TF_WORKSPACE', 'development')
-  if workspace == "production":
-    dns_namespace = ""
-  else:
-    dns_namespace = workspace + "."
 
-  URL = 'https://{}.{}use-an-lpa.opg.service.justice.gov.uk'.format(frontend, dns_namespace)
+  account_namespace_mapping = {'production': "", 'preproduction': "preproduction."}
+
+  dns_account_namespace = account_namespace_mapping.get(workspace, "development.")
+
+  if dns_account_namespace == "development.":
+    dns_env_namespace = workspace + "."
+  else:
+    dns_env_namespace = ""
+
+  URL = 'https://{0}{1}.{2}use-an-lpa.opg.service.justice.gov.uk'.format(dns_env_namespace, frontend, dns_account_namespace)
   print(URL)
   return URL
+
