7fdcca619be3f2c8ed94aa3127961885421e9213
Neil Smith <github@nsmith.me.uk>
2019-05-02 18:18:32 +0100

    Testing sessions across multiple containers

--- terraform_environment/viewer_ecs.tf
+++ terraform_environment/viewer_ecs.tf
@@ -5,7 +5,7 @@ resource "aws_ecs_service" "viewer" {
   name            = "${terraform.workspace}-viewer"
   cluster         = "${aws_ecs_cluster.use-an-lpa.id}"
   task_definition = "${aws_ecs_task_definition.viewer.arn}"
-  desired_count   = 1
+  desired_count   = 6
   launch_type     = "FARGATE"
 
   network_configuration {
