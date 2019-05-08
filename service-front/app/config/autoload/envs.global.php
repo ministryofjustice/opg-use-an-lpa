868c93a0baf912c784babadc2b80f25d73d287ca
Neil Smith <github@nsmith.me.uk>
2019-05-08 19:26:34 +0100

    Moved config into global

--- service-front/app/config/autoload/envs.global.php
+++ service-front/app/config/autoload/envs.global.php
@@ -14,9 +14,67 @@ return [
     ],
 
     'session' => [
+
+        // Time in seconds after which a session will expire.
+        'expires' => 60 * 60 * 1,    // 1 hour
+
         'key' => [
+            // KMS alias to sue for data key generation.
             'alias' => getenv('KMS_SESSION_CMK_ALIAS') ?: null,
         ],
+
+        // The name of the session cookie. This name must comply with
+        // the syntax outlined in https://tools.ietf.org/html/rfc6265.html
+        'cookie_name' => 'session',
+
+        // The (sub)domain that the cookie is available to. Setting this
+        // to a subdomain (such as 'www.example.com') will make the cookie
+        // available to that subdomain and all other sub-domains of it
+        // (i.e. w2.www.example.com). To make the cookie available to the
+        // whole domain (including all subdomains of it), simply set the
+        // value to the domain name ('example.com', in this case).
+        // Leave this null to use browser default (current hostname).
+        'cookie_domain' => null,
+
+        // The path prefix of the cookie domain to which it applies.
+        'cookie_path' => '/',
+
+        // Indicates that the cookie should only be transmitted over a
+        // secure HTTPS connection from the client. When set to TRUE, the
+        // cookie will only be set if a secure connection exists.
+        'cookie_secure' => false,
+
+        // When TRUE the cookie will be made accessible only through the
+        // HTTP protocol. This means that the cookie won't be accessible
+        // by scripting languages, such as JavaScript.
+        'cookie_http_only' => true,
+
+        // Governs the various cache control headers emitted when
+        // a session cookie is provided to the client. Value may be one
+        // of "nocache", "public", "private", or "private_no_expire";
+        // semantics are the same as outlined in
+        // http://php.net/session_cache_limiter
+        'cache_limiter' => 'nocache',
+
+        // An integer value indicating when the resource to which the session
+        // applies was last modified. If not provided, it uses the last
+        // modified time of, in order,
+        // - the public/index.php file of the current working directory
+        // - the index.php file of the current working directory
+        // - the current working directory
+        'last_modified' => null,
+
+        // A boolean value indicating whether or not the session cookie
+        // should persist. By default, this is disabled (false); passing
+        // a boolean true value will enable the feature. When enabled, the
+        // cookie will be generated with an Expires directive equal to the
+        // the current time plus the cache_expire value as noted above.
+        //
+        // As of 1.2.0, developers may define the session TTL by calling the
+        // session instance's `persistSessionFor(int $duration)` method. When
+        // that method has been called, the engine will use that value even if
+        // the below flag is toggled off.
+        'persistent' => false,
     ],
 
 ];
--- service-front/app/src/Viewer/src/Service/Session/Config.php
+++ service-front/app/src/Viewer/src/Service/Session/Config.php
@@ -15,51 +15,91 @@ class Config
 
     public function __construct(array $config)
     {
+        if (!array_key_exists('session', $config)) {
+            throw new RuntimeException("Session configuration missing");
+        }
+
+        if (!array_key_exists('expires', $config['session'])) {
+            throw new RuntimeException("Missing session configuration: expires");
+        }
+
+        if (!array_key_exists('cookie_name', $config['session'])) {
+            throw new RuntimeException("Missing session configuration: cookie_name");
+        }
+
+        if (!array_key_exists('cookie_domain', $config['session'])) {
+            throw new RuntimeException("Missing session configuration: cookie_domain");
+        }
+
+        if (!array_key_exists('cookie_path', $config['session'])) {
+            throw new RuntimeException("Missing session configuration: cookie_path");
+        }
+
+        if (!array_key_exists('cookie_secure', $config['session'])) {
+            throw new RuntimeException("Missing session configuration: cookie_secure");
+        }
+
+        if (!array_key_exists('cookie_http_only', $config['session'])) {
+            throw new RuntimeException("Missing session configuration: cookie_http_only");
+        }
+
+        if (!array_key_exists('cache_limiter', $config['session'])) {
+            throw new RuntimeException("Missing session configuration: cache_limiter");
+        }
+
+        if (!array_key_exists('last_modified', $config['session'])) {
+            throw new RuntimeException("Missing session configuration: last_modified");
+        }
+
+        if (!array_key_exists('persistent', $config['session'])) {
+            throw new RuntimeException("Missing session configuration: persistent");
+        }
+
         $this->config = $config;
     }
 
     public function getCookieName() : string
     {
-        return 'session';
+        return $this->config['session']['cookie_name'];
     }
 
     public function getCookiePath() : string
     {
-        return '/';
+        return $this->config['session']['cookie_path'];
     }
 
     public function getCacheLimiter() : string
     {
-        return 'nocache';
+        return $this->config['session']['cache_limiter'];
     }
 
     public function getSessionExpired() : int
     {
-        return 60 * 60 * 1; // 1 hour
+        return $this->config['session']['expires'];
     }
 
     public function getLastModified() : ?int
     {
-        return null;
+        return $this->config['session']['last_modified'];
     }
 
     public function getPersistent() : bool
     {
-        return false;
+        return $this->config['session']['persistent'];
     }
 
     public function getCookieDomain() : ?string
     {
-        return null;
+        return $this->config['session']['cookie_domain'];
     }
 
     public function getCookieSecure() : bool
     {
-        return false;
+        return $this->config['session']['cookie_secure'];
     }
 
     public function getCookieHttpOnly() : bool
     {
-        return false;
+        return $this->config['session']['cookie_http_only'];
     }
 }
