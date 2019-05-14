341146cb3552f97d929ad2db7018b380413001ba
Neil Smith <github@nsmith.me.uk>
2019-05-14 09:54:59 +0100

    Added variable to correct layer
    API not Front.

--- terraform_environment/viewer_ecs.tf
+++ terraform_environment/viewer_ecs.tf
@@ -175,10 +175,6 @@ locals {
       "value": "${data.aws_kms_alias.sessions_viewer.name}"
     },
     {
-      "name": "DYNAMODB_TABLE_VIEWER_CODES",
-      "value": "${aws_dynamodb_table.viewer_codes_table.name}"
-    },
-    {
       "name": "CONTAINER_VERSION",
       "value": "${var.container_version}"
     }]
