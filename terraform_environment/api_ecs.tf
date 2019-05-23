c53a51a21ee538e1a9ea3956ec6a3a5e21b64ab7
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-05-23 22:37:54 +0100

    generate api_task_config file

--- terraform_environment/api_ecs.tf
+++ terraform_environment/api_ecs.tf
@@ -42,7 +42,7 @@ resource "aws_service_discovery_service" "api" {
 
 //
 locals {
-  
+
   api_service_fqdn = "${aws_service_discovery_service.api.name}.${aws_service_discovery_private_dns_namespace.internal.name}"
 }
 
@@ -224,4 +224,17 @@ output "api_web_deployed_version" {
 
 output "api_app_deployed_version" {
   value = "${data.aws_ecr_repository.use_an_lpa_api_app.repository_url}:${var.container_version}"
+}
+
+resource "local_file" "api_task_config" {
+  content  = "${jsonencode(local.api_task_config)}"
+  filename = "${path.module}/api_task_config.json"
+}
+
+locals {
+  api_task_config = {
+    cluster_name = "${aws_ecs_cluster.use-an-lpa.name}"
+    service_name = "${aws_ecs_service.api.name}"
+    account_id   = "${local.account_id}"
+  }
 }
