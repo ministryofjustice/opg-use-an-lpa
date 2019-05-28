36013617768579c51d609a64cf278c273eb567ef
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-05-28 17:37:19 +0100

    Fix up the test.

--- service-front/app/test/ViewerTest/Handler/HealthcheckHandlerTest.php
+++ service-front/app/test/ViewerTest/Handler/HealthcheckHandlerTest.php
@@ -8,18 +8,47 @@ use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
 use Viewer\Handler\HealthcheckHandler;
 use Zend\Diactoros\Response\JsonResponse;
+use Http\Client\HttpClient;
+use Psr\Http\Message\RequestInterface;
+use Prophecy\Argument\Token\CallbackToken;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 class HealthcheckHandlerTest extends TestCase
 {
-    public function testReturnsJsonResponse()
+    protected function apiHealthcheckResponse(string $response) : HttpClient
+    {
+        $bodyProphecy = $this->prophesize(StreamInterface::class);
+        $bodyProphecy->getContents()
+            ->willReturn($response);
+
+        $responseProphecy = $this->prophesize(ResponseInterface::class);
+        $responseProphecy->getBody()
+            ->willReturn($bodyProphecy->reveal());
+
+        $httpClientProphecy = $this->prophesize(HttpClient::class);
+        $httpClientProphecy->sendRequest(new CallbackToken(function($request) {
+            $this->assertInstanceOf(RequestInterface::class, $request);
+
+            return true;
+        }))
+            ->willReturn($responseProphecy->reveal());
+
+        return $httpClientProphecy->reveal();
+    }
+
+    /**
+     * @dataProvider responseDataProvider
+     */
+    public function testReturnsJsonResponse(string $response)
     {
         //  Set up the handler
-        $handler = new HealthcheckHandler();
+        $handler = new HealthcheckHandler($this->apiHealthcheckResponse($response));
 
         $requestProphecy = $this->prophesize(ServerRequestInterface::class);
 
         $response = $handler->handle($requestProphecy->reveal());
-        $json = json_decode((string) $response->getBody());
+        $json = json_decode((string) $response->getBody()->getContents());
 
         $this->assertInstanceOf(JsonResponse::class, $response);
         $this->assertObjectHasAttribute('healthy', $json);
@@ -32,5 +61,26 @@ class HealthcheckHandlerTest extends TestCase
         $api = $dependencies->api;
         $this->assertObjectHasAttribute('healthy', $api);
         $this->assertObjectHasAttribute('version', $api);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function responseDataProvider() : array
+    {
+        $allHealthyResponse = [
+            'healthy' => true,
+            'version' => 'dev',
+            'dependencies' => [
+                'api' => [
+                    'healthy' => true,
+                    'version' => 'dev'
+                ]
+            ]
+        ];
+
+        return [
+            [ json_encode($allHealthyResponse) ]
+        ];
     }
 }
