17e0f3205ddca8b9debbc351a886a518023a1295
Neil Smith <github@nsmith.me.uk>
2019-06-03 15:25:16 +0100

    Fixed logging

--- service-api/app/config/autoload/envs.global.php
+++ service-api/app/config/autoload/envs.global.php
@@ -14,9 +14,8 @@ return [
         'region'    => 'eu-west-1',
         'version'   => 'latest',
 
-        'dynamodb' => [
-            'region'    => 'eu-west-1',
-            'version'   => 'latest',
+        'DynamoDb' => [
+            'endpoint' => getenv('AWS_ENDPOINT_DYNAMODB') ?: null,
         ],
     ],
 
