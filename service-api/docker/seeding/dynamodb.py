ab26aec5c2b552d54fa8a4726f0805640ed04f03
Neil Smith <github@nsmith.me.uk>
2019-06-05 14:53:11 +0100

    Moved back to seeding with Python

--- service-api/docker/seeding/dynamodb.py
+++ service-api/docker/seeding/dynamodb.py
@@ -10,7 +10,7 @@ It will not be run in production.
 
 if 'AWS_ENDPOINT_DYNAMODB' in os.environ:
     # For local development
-    dynamodb_endpoint_url = os.environ['AWS_ENDPOINT_DYNAMODB']
+    dynamodb_endpoint_url = 'http://' + os.environ['AWS_ENDPOINT_DYNAMODB']
 else:
     # Should be none in AWS
     dynamodb_endpoint_url = None
@@ -19,31 +19,31 @@ dynamodb = boto3.resource('dynamodb', region_name='eu-west-1', endpoint_url=dyna
 
 table = dynamodb.Table(os.environ['DYNAMODB_TABLE_VIEWER_CODES'])
 
-response = table.put_item(
+table.put_item(
    Item={
         'ViewerCode': "123456789012",
         'SiriusId': "12345678901",
         'Expires': "2020-01-01 12:34:56",
     }
 )
-print(json.dumps(response, indent=4))
 
-response = table.put_item(
+table.put_item(
    Item={
         'ViewerCode': "987654321098",
         'SiriusId': "98765432109",
         'Expires': "2020-01-01 12:34:56",
     }
 )
-print(json.dumps(response, indent=4))
 
-response = table.put_item(
+table.put_item(
    Item={
         'ViewerCode': "222222222222",
         'SiriusId': "22222222222",
         'Expires': "2019-01-01 12:34:56",
     }
 )
-print(json.dumps(response, indent=4))
 
-print("Seeding finished")
+# Scan and output the table, so we can see what we've got in the logs.
+response = table.scan()
+for i in response['Items']:
+    print(json.dumps(i))
