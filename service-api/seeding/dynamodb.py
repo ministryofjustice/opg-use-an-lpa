6e3602b628e0256e69be7051ec467020c77a9e8e
Neil Smith <github@nsmith.me.uk>
2019-06-07 11:45:57 +0100

    Add the data

--- service-api/seeding/dynamodb.py
+++ service-api/seeding/dynamodb.py
@@ -13,7 +13,9 @@ import pprint
 if 'AWS_ENDPOINT_DYNAMODB' in os.environ:
     # For local development
     dynamodb_endpoint_url = 'http://' + os.environ['AWS_ENDPOINT_DYNAMODB']
-    dynamodb = boto3.client('dynamodb', region_name='eu-west-1', endpoint_url=dynamodb_endpoint_url)
+    dynamodb = boto3.resource('dynamodb', region_name='eu-west-1', endpoint_url=dynamodb_endpoint_url)
+
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE_VIEWER_CODES'])
 else:
     with open('/tmp/cluster_config.json') as json_file:
         parameters = json.load(json_file)
@@ -35,7 +37,7 @@ else:
     )
 
     # Create a authenticated client
-    dynamodb = boto3.client(
+    dynamodb = boto3.resource(
         'dynamodb',
         region_name='eu-west-1',
         aws_access_key_id=session['Credentials']['AccessKeyId'],
@@ -43,37 +45,38 @@ else:
         aws_session_token=session['Credentials']['SessionToken']
     )
 
+    table = dynamodb.Table(parameters['viewer_codes_table'])
 
-tables = dynamodb.list_tables()
-print(json.dumps(tables, indent=4, separators=(',', ': ')))
 
-# table = dynamodb.Table(os.environ['DYNAMODB_TABLE_VIEWER_CODES'])
-#
-# table.put_item(
-#    Item={
-#         'ViewerCode': "123456789012",
-#         'SiriusId': "12345678901",
-#         'Expires': "2020-01-01 12:34:56",
-#     }
-# )
-#
-# table.put_item(
-#    Item={
-#         'ViewerCode': "987654321098",
-#         'SiriusId': "98765432109",
-#         'Expires': "2020-01-01 12:34:56",
-#     }
-# )
-#
-# table.put_item(
-#    Item={
-#         'ViewerCode': "222222222222",
-#         'SiriusId': "22222222222",
-#         'Expires': "2019-01-01 12:34:56",
-#     }
-# )
-#
-# # Scan and output the table, so we can see what we've got in the logs.
-# response = table.scan()
-# for i in response['Items']:
-#     print(json.dumps(i, indent=4, separators=(',', ': ')))
+# tables = dynamodb.list_tables()
+# print(json.dumps(tables, indent=4, separators=(',', ': ')))
+
+
+table.put_item(
+   Item={
+        'ViewerCode': "123456789012",
+        'SiriusId': "12345678901",
+        'Expires': "2020-01-01 12:34:56",
+    }
+)
+
+table.put_item(
+   Item={
+        'ViewerCode': "987654321098",
+        'SiriusId': "98765432109",
+        'Expires': "2020-01-01 12:34:56",
+    }
+)
+
+table.put_item(
+   Item={
+        'ViewerCode': "222222222222",
+        'SiriusId': "22222222222",
+        'Expires': "2019-01-01 12:34:56",
+    }
+)
+
+# Scan and output the table, so we can see what we've got in the logs.
+response = table.scan()
+for i in response['Items']:
+    print(json.dumps(i, indent=4, separators=(',', ': ')))
