af2f5e3be317bd0466fe82b8351d874327b361a8
Neil Smith <github@nsmith.me.uk>
2019-06-07 11:37:01 +0100

    Output table name

--- terraform_environment/outputs.tf
+++ terraform_environment/outputs.tf
@@ -5,7 +5,8 @@ resource "local_file" "cluster_config" {
 
 locals {
   cluster_config = {
-    cluster_name = "${aws_ecs_cluster.use-an-lpa.name}"
-    account_id   = "${local.account_id}"
+    cluster_name         = "${aws_ecs_cluster.use-an-lpa.name}"
+    account_id           = "${local.account_id}"
+    viewer_codes_table   = "${aws_dynamodb_table.viewer_codes_table.name}"
   }
 }
