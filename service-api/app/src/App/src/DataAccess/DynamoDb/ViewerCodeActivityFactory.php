110c3be490b665e095b6a1f07de5b3caa51dfb83
Neil Smith <github@nsmith.me.uk>
2019-06-10 17:49:36 +0100

    Started logging successful Viewer code lookups

---
+++ service-api/app/src/App/src/DataAccess/DynamoDb/ViewerCodeActivityFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataAccess\DynamoDb;
+
+use Aws\DynamoDb\DynamoDbClient;
+use Psr\Container\ContainerInterface;
+
+class ViewerCodeActivityFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        $config = $container->get('config');
+
+        if ( !isset($config['repositories']['dynamodb']['viewer-activity-table'])) {
+            throw new \Exception('Viewer Activity table configuration not present');
+        }
+
+        return new ViewerCodeActivity(
+            $container->get(DynamoDbClient::class),
+            $config['repositories']['dynamodb']['viewer-activity-table']
+        );
+    }
+}
