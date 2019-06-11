fc58219f06fbaf090372b9088c758faa5d9aa71d
SeemaMenon <seema.workspace@gmail.com>
2019-06-11 17:09:55 +0100

    UML-144: Added new dynamo resource

--- terraform_environment/dynamodb.tf
+++ terraform_environment/dynamodb.tf
@@ -18,3 +18,31 @@ resource "aws_dynamodb_table" "viewer_codes_table" {
     prevent_destroy = false
   }
 }
+
+resource "aws_dynamodb_table" "viewer_activity_table" {
+  name         = "${terraform.workspace}-ViewerActivity"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "ViewerCode"
+  range_key    = "Viewed"
+
+  attribute = [{
+
+    name = "ViewerCode"
+    type = "S"
+  },
+  {
+    name = "Viewed"
+    type = "S"
+  }
+  ]
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  tags = "${local.default_tags}"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
