de991808f05dc90e83a5f806088187325b429ed8
Steven Taylor <taylorsj1980@gmail.com>
2019-06-14 17:10:58 +0100

    Added email address and password validators

---
+++ service-front/app/src/Common/src/Validator/EmailAddressValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Common\Validator;
+
+use Zend\Validator\EmailAddress as ZfEmailAddressValidator;
+
+class EmailAddressValidator extends ZfEmailAddressValidator
+{
+    /**
+     * Overridden function to translate error messages
+     *
+     * @param string $value
+     * @return bool
+     */
+    public function isValid($value)
+    {
+        $valid = parent::isValid($value);
+
+        if ($valid === false && count($this->getMessages()) > 0) {
+            $this->abstractOptions['messages'] = [
+                'Enter a valid email address'
+            ];
+        }
+
+        return $valid;
+    }
+}
---
+++ service-front/app/src/Common/src/Validator/PasswordValidator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Common\Validator;
+
+use Zend\Validator\AbstractValidator;
+use Zend\Validator\Regex;
+
+class PasswordValidator extends AbstractValidator
+{
+    const MUST_INCLUDE_DIGIT      = 'mustIncludeDigit';
+    const MUST_INCLUDE_LOWER_CASE = 'mustIncludeLowerCase';
+    const MUST_INCLUDE_UPPER_CASE = 'mustIncludeUpperCase';
+
+    protected $messageTemplates = [
+        self::MUST_INCLUDE_DIGIT      => 'Your password must include at least one digit (0-9)',
+        self::MUST_INCLUDE_LOWER_CASE => 'Your password must include at least one lower case letter (a-z)',
+        self::MUST_INCLUDE_UPPER_CASE => 'Your password must include at least one capital letter (A-Z)',
+    ];
+
+    public function isValid($value)
+    {
+        $isValid = true;
+
+        //  Check that a number has been provided
+        $regExValidator = new Regex('/.*[0-9].*/');
+
+        if (!$regExValidator->isValid($value)) {
+            $this->error(self::MUST_INCLUDE_DIGIT);
+            $isValid = false;
+        }
+
+        //  Check that a lower case letter has been provided
+        $regExValidator = new Regex('/.*[a-z].*/');
+
+        if (!$regExValidator->isValid($value)) {
+            $this->error(self::MUST_INCLUDE_LOWER_CASE);
+            $isValid = false;
+        }
+
+        //  Check that an upper case letter has been provided
+        $regExValidator = new Regex('/.*[A-Z].*/');
+
+        if (!$regExValidator->isValid($value)) {
+            $this->error(self::MUST_INCLUDE_UPPER_CASE);
+            $isValid = false;
+        }
+
+        return $isValid;
+    }
+}
