3e019dd6c8fd60303cee2e9d605977f4f01332df
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-06-19 18:13:25 +0100

    Get LpaService tested up to spec.

---
+++ service-api/app/test/AppTest/Service/Lpa/LpaServiceTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Lpa;
+
+use App\DataAccess\Repository\ViewerCodeActivityInterface;
+use App\DataAccess\Repository\ViewerCodesInterface;
+use App\Exception\GoneException;
+use App\Exception\NotFoundException;
+use DateInterval;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class LpaServiceTest extends TestCase
+{
+    public function testCanCreateAnInstance()
+    {
+        $viewerRepository = $this->prophesize(ViewerCodesInterface::class);
+
+        $activityRepository = $this->prophesize(ViewerCodeActivityInterface::class);
+
+        $instance = new LpaService($viewerRepository->reveal(), $activityRepository->reveal());
+
+        $this->assertInstanceOf(LpaService::class, $instance);
+    }
+
+    public function testGetByValidId()
+    {
+        $viewerRepository = $this->prophesize(ViewerCodesInterface::class);
+
+        $activityRepository = $this->prophesize(ViewerCodeActivityInterface::class);
+
+        $instance = new LpaService($viewerRepository->reveal(), $activityRepository->reveal());
+
+        $lpa = $instance->getById('12345678901');
+
+        $this->assertIsArray($lpa);
+        $this->assertArrayHasKey('id', $lpa);
+        $this->assertEquals($lpa['id'], '12345678901');
+    }
+
+    public function testGetByInvalidId()
+    {
+        $viewerRepository = $this->prophesize(ViewerCodesInterface::class);
+
+        $activityRepository = $this->prophesize(ViewerCodeActivityInterface::class);
+
+        $instance = new LpaService($viewerRepository->reveal(), $activityRepository->reveal());
+
+        $this->expectException(NotFoundException::class);
+
+        $lpa = $instance->getById('bad-id');
+    }
+
+    public function testGetByValidShareCode()
+    {
+        $viewerRepository = $this->prophesize(ViewerCodesInterface::class);
+        $viewerRepository->get('share-code')
+            ->willReturn([
+                'Expires' => new DateTime('tomorrow'),
+                'ViewerCode' => 'viewer-code',
+                'SiriusId' => '12345678901'
+            ]);
+
+        $activityRepository = $this->prophesize(ViewerCodeActivityInterface::class);
+        $activityRepository->recordSuccessfulLookupActivity('viewer-code')->shouldBeCalled();
+
+        $instance = new LpaService($viewerRepository->reveal(), $activityRepository->reveal());
+
+        $lpa = $instance->getByCode('share-code');
+
+        $this->assertIsArray($lpa);
+        $this->assertArrayHasKey('id', $lpa);
+        $this->assertEquals($lpa['id'], '12345678901');
+    }
+
+    public function testGetByExpiredShareCode()
+    {
+        $viewerRepository = $this->prophesize(ViewerCodesInterface::class);
+        $viewerRepository->get('share-code')
+            ->willReturn([
+                'Expires' => new DateTime('yesterday'),
+                'ViewerCode' => 'viewer-code',
+                'SiriusId' => '12345678901'
+            ]);
+
+        $activityRepository = $this->prophesize(ViewerCodeActivityInterface::class);
+
+        $instance = new LpaService($viewerRepository->reveal(), $activityRepository->reveal());
+
+        $this->expectException(GoneException::class);
+
+        $lpa = $instance->getByCode('share-code');
+    }
+}
