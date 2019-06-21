8aa93b982398fa4c95d82b69690d2c7ded7ba7d7
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-06-21 17:05:34 +0100

    Add a test for the config provider

--- service-front/app/test/ActorTest/ConfigProviderTest.php
+++ service-front/app/test/ActorTest/ConfigProviderTest.php
@@ -1,10 +1,22 @@
 <?php
-
+declare(strict_types=1);
 
 namespace ActorTest;
 
+use Actor\ConfigProvider;
 
-class ConfigProviderTest
+use PHPUnit\Framework\TestCase;
+
+class ConfigProviderTest extends TestCase
 {
+    public function testCreatesAWorkingConfigProvider()
+    {
+        $cp = new ConfigProvider();
 
+        $config = $cp();
+
+        $this->assertIsArray($config);
+        $this->assertArrayHasKey('dependencies', $config);
+        $this->assertArrayHasKey('templates', $config);
+    }
 }
