33d50ab8a61bd67462ab00986b17773e0d64e5fe
Steven Taylor <taylorsj1980@gmail.com>
2019-06-27 16:07:02 +0100

    Update terraform_shared/secretsmanager.tf
    Co-Authored-By: Andrew Pearce <andrew.pearce@digital.justice.gov.uk>

--- terraform_shared/secretsmanager.tf
+++ terraform_shared/secretsmanager.tf
@@ -3,6 +3,9 @@ resource "aws_secretsmanager_secret" "notify_api_key" {
 
   tags   = "${local.default_tags}"
 }
+data "aws_secretsmanager_secret" "notify_api_key" {
+  name = "notify-api-key"
+}
 
 data "aws_secretsmanager_secret_version" "notify_api_key" {
   secret_id = "${aws_secretsmanager_secret.notify_api_key.id}"
