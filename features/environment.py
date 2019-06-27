ec23e4670820da1a5bda1c379b84355be0ff5f97
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-06-27 12:51:33 +0100

    Behave tests take screenshots on scenario failure

--- features/environment.py
+++ features/environment.py
@@ -1,14 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import os
 
 options = Options()
 options.headless = True
 
+
 def before_tag(context, tag):
-  if tag == 'web':
-    context.browser = webdriver.Firefox(options=options)
-    context.browser.implicitly_wait(10)
+    if tag == 'web':
+        context.browser = webdriver.Firefox(options=options)
+        context.browser.implicitly_wait(20)
+
 
 def after_tag(context, tag):
-  if tag == 'web':
+    if tag == 'web':
+        context.browser.quit()
+
+
+def after_scenario(context, scenario):
+    print("scenario status" + scenario.status)
+    if scenario.status == "failed":
+        if not os.path.exists("failed_scenarios_screenshots"):
+            os.makedirs("failed_scenarios_screenshots")
+        os.chdir("failed_scenarios_screenshots")
+        context.browser.save_screenshot(scenario.name + "_failed.png")
     context.browser.quit()
