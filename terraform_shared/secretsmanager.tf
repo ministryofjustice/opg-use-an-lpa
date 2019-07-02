d5207ba8a15bb8d2398e2ff91c65212c5cdbea1c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-07-02 09:51:40 +0100

    remove secrests manager data sources

--- terraform_shared/secretsmanager.tf
+++ terraform_shared/secretsmanager.tf
@@ -1,13 +1,5 @@
 resource "aws_secretsmanager_secret" "notify_api_key" {
   name = "notify-api-key"
 
-  tags   = "${local.default_tags}"
-}
-
-data "aws_secretsmanager_secret" "notify_api_key" {
-  name = "notify-api-key"
-}
-
-data "aws_secretsmanager_secret_version" "notify_api_key" {
-  secret_id = "${data.aws_secretsmanager_secret.notify_api_key.id}"
+  tags = "${local.default_tags}"
 }
