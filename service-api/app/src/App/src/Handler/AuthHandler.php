30ec18b6b431f4d187c785dfafbefbca3066971a
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-07-16 15:09:03 +0100

    Add auth handler and appropriate tests.

--- service-api/app/src/App/src/Handler/AuthHandler.php
+++ service-api/app/src/App/src/Handler/AuthHandler.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
+use App\Exception\BadRequestException;
 use App\Service\User\UserService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Diactoros\Response\JsonResponse;
 
 class AuthHandler implements RequestHandlerInterface
 {
@@ -28,6 +30,14 @@ class AuthHandler implements RequestHandlerInterface
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $params = $request->getQueryParams();
 
+        if (!isset($params['email']) || !isset($params['password'])) {
+            throw new BadRequestException('Email address and password must be provided');
+        }
+
+        $user = $this->userService->authenticate($params['email'], $params['password']);
+
+        return new JsonResponse($user);
     }
 }
---
+++ service-api/app/test/AppTest/Handler/AuthHandlerTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Handler;
+
+use App\Exception\BadRequestException;
+use App\Exception\NotFoundException;
+use App\Handler\AuthHandler;
+use App\Service\User\UserService;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\JsonResponse;
+
+class AuthHandlerTest extends TestCase
+{
+    // Password hash for password 'test' generated using PASSWORD_DEFAULT
+    // 'test':
+    const PASS_HASH = '$2y$10$Ew4y5jzm6fGKAB16huUw6ugZbuhgW5cvBQ6DGVDFzuyBXsCw51dzq';
+
+    /** @test */
+    public function can_create_an_instance()
+    {
+        $userServiceProphecy = $this->prophesize(UserService::class);
+
+        $handler = new AuthHandler($userServiceProphecy->reveal());
+
+        $this->assertInstanceOf(AuthHandler::class, $handler);
+    }
+
+    /** @test */
+    public function returns_an_error_when_bad_parameters_received()
+    {
+        $userServiceProphecy = $this->prophesize(UserService::class);
+
+        $handler = new AuthHandler($userServiceProphecy->reveal());
+
+        $requestProphecy = $this->prophesize(ServerRequestInterface::class);
+        $requestProphecy->getQueryParams()
+            ->willReturn([
+            ]);
+
+        $this->expectException(BadRequestException::class);
+        $response = $handler->handle($requestProphecy->reveal());
+    }
+
+    /** @test */
+    public function returns_a_valid_user_when_correct_parameters_received()
+    {
+        $userServiceProphecy = $this->prophesize(UserService::class);
+        $userServiceProphecy->authenticate('a@b.com', 'test')
+            ->willReturn([
+                'Email' => 'a@b.com',
+                'Password' => self::PASS_HASH
+             ]);
+
+        $handler = new AuthHandler($userServiceProphecy->reveal());
+
+        $requestProphecy = $this->prophesize(ServerRequestInterface::class);
+        $requestProphecy->getQueryParams()
+            ->willReturn([
+                'email' => 'a@b.com',
+                'password' => 'test'
+            ]);
+
+        /** @var JsonResponse $response */
+        $response = $handler->handle($requestProphecy->reveal());
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(['Email' => 'a@b.com', 'Password' => self::PASS_HASH], $response->getPayload());
+    }
+
+    /** @test */
+    public function returns_a_not_found_when_credentials_incorrect()
+    {
+        $userServiceProphecy = $this->prophesize(UserService::class);
+        $userServiceProphecy->authenticate('b@c.com', 'test')
+            ->willThrow(new NotFoundException());
+
+        $handler = new AuthHandler($userServiceProphecy->reveal());
+
+        $requestProphecy = $this->prophesize(ServerRequestInterface::class);
+        $requestProphecy->getQueryParams()
+            ->willReturn([
+                'email' => 'b@c.com',
+                'password' => 'test'
+            ]);
+
+        $this->expectException(NotFoundException::class);
+        $response = $handler->handle($requestProphecy->reveal());
+    }
+}
