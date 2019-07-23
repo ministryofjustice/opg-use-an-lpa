cd7e7f8b6013753009cec014008bc46aeaf1ee16
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-07-23 15:11:04 +0100

    Break out the API's auth handler into separate functions.

--- service-api/app/src/App/src/Handler/UserHandler.php
+++ service-api/app/src/App/src/Handler/UserHandler.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App\Handler;
 
 use App\Exception\BadRequestException;
+use App\Exception\ConflictException;
+use App\Exception\NotFoundException;
 use App\Service\User\UserService;
+use \Exception;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -30,24 +33,25 @@ class UserHandler implements RequestHandlerInterface
     /**
      * @param ServerRequestInterface $request
      * @return ResponseInterface
-     * @throws \Exception
+     * @throws Exception
      */
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        if ($request->getMethod() === 'POST') {
-            //  This is an attempt to create a user
-            $requestData = $request->getParsedBody();
-
-            if (!isset($requestData['email']) || !isset($requestData['password'])) {
-                throw new BadRequestException('Email address and password must be provided');
-            }
-
-            $data = $this->userService->add($requestData);
-
-            return new JsonResponse($data, 201);
+        switch ($request->getMethod()) {
+            case 'POST':
+                return $this->handlePost($request);
+            default:
+                return $this->handleGet($request);
         }
+    }
 
-        //  This is an attempt to search for a user - must be done by email address
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     * @throws BadRequestException|NotFoundException
+     */
+    private function handleGet(ServerRequestInterface $request) : ResponseInterface
+    {
         $params = $request->getQueryParams();
 
         if (!isset($params['email'])) {
@@ -57,5 +61,24 @@ class UserHandler implements RequestHandlerInterface
         $data = $this->userService->get($params['email']);
 
         return new JsonResponse($data);
+    }
+
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     * @throws Exception|BadRequestException|ConflictException|NotFoundException
+     */
+    private function handlePost(ServerRequestInterface $request) : ResponseInterface
+    {
+        $requestData = $request->getParsedBody();
+
+        if (!isset($requestData['email']) || !isset($requestData['password'])) {
+            throw new BadRequestException('Email address and password must be provided');
+        }
+
+        $data = $this->userService->add($requestData);
+
+        return new JsonResponse($data, 201);
     }
 }
