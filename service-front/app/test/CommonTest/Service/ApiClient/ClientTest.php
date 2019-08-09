54d4cac61b7121ec49cb5915b6d3b10b14b07de6
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-08-09 18:11:36 +0100

    Further test coverage on apiclient in the frontend

--- service-front/app/test/CommonTest/Service/ApiClient/ClientTest.php
+++ service-front/app/test/CommonTest/Service/ApiClient/ClientTest.php
@@ -43,6 +43,10 @@ class ClientTest extends TestCase
         return $responseProphecy;
     }
 
+    // ============
+    // httpGet
+    // ============
+
     /** @test */
     public function can_get_a_simple_endpoint_returning_valid_json()
     {
@@ -97,8 +101,203 @@ class ClientTest extends TestCase
         $data = $client->httpGet('/simple_get', ['simple_query' => 'query_value']);
     }
 
+    // ============
+    // httpPost
+    // ============
+
+    /**
+     * @test
+     * @dataProvider validStatusCodes
+     */
+    public function can_post_to_an_endpoint_with_parameters_returning_valid_json(int $statusCode)
+    {
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($this->setupResponse('[]', $statusCode)->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $data = $client->httpPost('/simple_post', ['simple_query' => 'query_value']);
+
+        $this->assertIsArray($data);
+    }
+
     /** @test */
-    public function sets_appropriate_request_headers()
+    public function correctly_processes_a_non_2xx_response_to_a_post_request()
+    {
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($this->setupResponse('', StatusCodeInterface::STATUS_NOT_FOUND)->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $this->expectException(ApiException::class);
+        $this->expectExceptionCode(StatusCodeInterface::STATUS_NOT_FOUND);
+        $data = $client->httpPost('/simple_bad_post', ['simple_query' => 'query_value']);
+    }
+
+    /** @test */
+    public function client_throws_error_with_post_request()
+    {
+        $exceptionProphecy = $this->prophesize(ApiException::class);
+
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willThrow($exceptionProphecy->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $this->expectException(ApiException::class);
+        $data = $client->httpPost('/simple_post', ['simple_query' => 'query_value']);
+    }
+
+    // ============
+    // httpPut
+    // ============
+
+    /**
+     * @test
+     * @dataProvider validStatusCodes
+     */
+    public function can_put_to_an_endpoint_with_parameters_returning_valid_json(int $statusCode)
+    {
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($this->setupResponse('[]', $statusCode)->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $data = $client->httpPut('/simple_put', ['simple_query' => 'query_value']);
+
+        $this->assertIsArray($data);
+    }
+
+    /** @test */
+    public function correctly_processes_a_non_2xx_response_to_a_put_request()
+    {
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($this->setupResponse('', StatusCodeInterface::STATUS_NOT_FOUND)->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $this->expectException(ApiException::class);
+        $this->expectExceptionCode(StatusCodeInterface::STATUS_NOT_FOUND);
+        $data = $client->httpPut('/simple_bad_put', ['simple_query' => 'query_value']);
+    }
+
+    /** @test */
+    public function client_throws_error_with_put_request()
+    {
+        $exceptionProphecy = $this->prophesize(ApiException::class);
+
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willThrow($exceptionProphecy->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $this->expectException(ApiException::class);
+        $data = $client->httpPut('/simple_put', ['simple_query' => 'query_value']);
+    }
+
+    // ============
+    // httpPatch
+    // ============
+
+    /**
+     * @test
+     * @dataProvider validStatusCodes
+     */
+    public function can_patch_to_an_endpoint_with_parameters_returning_valid_json(int $statusCode)
+    {
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($this->setupResponse('[]', $statusCode)->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $data = $client->httpPatch('/simple_patch', ['simple_query' => 'query_value']);
+
+        $this->assertIsArray($data);
+    }
+
+    /** @test */
+    public function correctly_processes_a_non_2xx_response_to_a_patch_request()
+    {
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($this->setupResponse('', StatusCodeInterface::STATUS_NOT_FOUND)->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $this->expectException(ApiException::class);
+        $this->expectExceptionCode(StatusCodeInterface::STATUS_NOT_FOUND);
+        $data = $client->httpPatch('/simple_bad_patch', ['simple_query' => 'query_value']);
+    }
+
+    /** @test */
+    public function client_throws_error_with_patch_request()
+    {
+        $exceptionProphecy = $this->prophesize(ApiException::class);
+
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willThrow($exceptionProphecy->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $this->expectException(ApiException::class);
+        $data = $client->httpPatch('/simple_patch', ['simple_query' => 'query_value']);
+    }
+
+    // ============
+    // httpDelete
+    // ============
+
+    /**
+     * @test
+     * @dataProvider validStatusCodes
+     */
+    public function can_delete_to_an_endpoint_with_parameters_returning_valid_json(int $statusCode)
+    {
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($this->setupResponse('[]', $statusCode)->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $data = $client->httpDelete('/simple_delete', ['simple_query' => 'query_value']);
+
+        $this->assertIsArray($data);
+    }
+
+    /** @test */
+    public function correctly_processes_a_non_2xx_response_to_a_delete_request()
+    {
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($this->setupResponse('', StatusCodeInterface::STATUS_NOT_FOUND)->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $this->expectException(ApiException::class);
+        $this->expectExceptionCode(StatusCodeInterface::STATUS_NOT_FOUND);
+        $data = $client->httpDelete('/simple_bad_delete', ['simple_query' => 'query_value']);
+    }
+
+    /** @test */
+    public function client_throws_error_with_delete_request()
+    {
+        $exceptionProphecy = $this->prophesize(ApiException::class);
+
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willThrow($exceptionProphecy->reveal());
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        $this->expectException(ApiException::class);
+        $data = $client->httpDelete('/simple_delete', ['simple_query' => 'query_value']);
+    }
+
+    // ============
+    // All
+    // ============
+
+    // These tests operate on all request methods in the Client class but test identical
+    // expected functionality in each.
+
+    /** @test */
+    public function sets_appropriate_request_headers_for_request()
     {
         $this->apiClient->sendRequest(Argument::that(function($request) {
             $this->assertInstanceOf(RequestInterface::class, $request);
@@ -115,7 +314,18 @@ class ClientTest extends TestCase
         $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
 
         $data = $client->httpGet('/simple_get');
+        $this->assertIsArray($data);
 
+        $data = $client->httpPost('/simple_post', []);
+        $this->assertIsArray($data);
+
+        $data = $client->httpPut('/simple_put', []);
+        $this->assertIsArray($data);
+
+        $data = $client->httpPatch('/simple_patch', []);
+        $this->assertIsArray($data);
+
+        $data = $client->httpDelete('/simple_delete');
         $this->assertIsArray($data);
     }
 
@@ -135,7 +345,70 @@ class ClientTest extends TestCase
         $client = new Client($this->apiClient->reveal(), 'https://localhost', 'test_token');
 
         $data = $client->httpGet('/simple_get');
-
         $this->assertIsArray($data);
+
+        $data = $client->httpPost('/simple_post', []);
+        $this->assertIsArray($data);
+
+        $data = $client->httpPut('/simple_put', []);
+        $this->assertIsArray($data);
+
+        $data = $client->httpPatch('/simple_patch', []);
+        $this->assertIsArray($data);
+
+        $data = $client->httpDelete('/simple_delete');
+        $this->assertIsArray($data);
+    }
+
+    /** @test */
+    public function gracefully_handles_malformed_response_data()
+    {
+        $this->apiClient->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($this->setupResponse('<xml>we_dont_do_xml</xml>', StatusCodeInterface::STATUS_OK));
+
+        $client = new Client($this->apiClient->reveal(), 'https://localhost', null);
+
+        try {
+            $data = $client->httpGet('/simple_get');
+        } catch (\Exception $e) {
+            $this->assertInstanceOf(ApiException::class, $e);
+        }
+
+        try {
+            $data = $client->httpPost('/simple_post', []);
+        } catch (\Exception $e) {
+            $this->assertInstanceOf(ApiException::class, $e);
+        }
+
+        try {
+            $data = $client->httpPut('/simple_put', []);
+        } catch (\Exception $e) {
+            $this->assertInstanceOf(ApiException::class, $e);
+        }
+
+        try {
+            $data = $client->httpPatch('/simple_patch', []);
+        } catch (\Exception $e) {
+            $this->assertInstanceOf(ApiException::class, $e);
+        }
+
+        try {
+            $data = $client->httpDelete('/simple_delete');
+        } catch (\Exception $e) {
+            $this->assertInstanceOf(ApiException::class, $e);
+        }
+    }
+
+    /**
+     * Provides expected valid response codes that we know our methods should handle.
+     * @return array
+     */
+    public function validStatusCodes(): array {
+        return [
+            [ StatusCodeInterface::STATUS_OK ],
+            [ StatusCodeInterface::STATUS_CREATED],
+            [ StatusCodeInterface::STATUS_ACCEPTED],
+            [ StatusCodeInterface::STATUS_NO_CONTENT],
+        ];
     }
 }
