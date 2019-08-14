24a18915265a7d3c53279c7ace8b187221ccc1c3
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-08-14 10:57:13 +0100

    Increase test coverage

--- service-front/app/test/CommonTest/Service/User/UserServiceTest.php
+++ service-front/app/test/CommonTest/Service/User/UserServiceTest.php
@@ -9,10 +9,89 @@ use Common\Exception\ApiException;
 use Common\Service\ApiClient\Client;
 use Common\Service\User\UserService;
 use DateTime;
+use Fig\Http\Message\StatusCodeInterface;
 use PHPUnit\Framework\TestCase;
 
 class UserServiceTest extends TestCase
 {
+    /** @test */
+    public function can_create_a_new_user_account()
+    {
+        $apiClientProphecy = $this->prophesize(Client::class);
+        $apiClientProphecy->httpPost(
+            '/v1/user',
+            [
+                'email' => 'test@example.com',
+                'password' => 'test'
+            ])
+            ->willReturn([
+                'Email'           => 'a@b.com',
+                'ActivationToken' => 'activate1234567890',
+            ]);
+
+        $userFactoryCallable = function($identity, $roles, $details) {
+            // Not returning a user here since it shouldn't be called.
+            $this->fail('User should not be created');
+        };
+
+        $service = new UserService($apiClientProphecy->reveal(), $userFactoryCallable);
+
+        $return = $service->create('test@example.com', 'test');
+
+        $this->assertIsArray($return);
+        $this->assertArrayHasKey('Email', $return);
+        $this->assertArrayHasKey('ActivationToken', $return);
+    }
+
+    /** @test */
+    public function can_get_an_account_by_email()
+    {
+        $apiClientProphecy = $this->prophesize(Client::class);
+        $apiClientProphecy->httpGet(
+            '/v1/user',
+            [
+                'email' => 'test@example.com',
+            ])
+            ->willReturn([
+                'Email' => 'a@b.com',
+            ]);
+
+        $userFactoryCallable = function($identity, $roles, $details) {
+            // Not returning a user here since it shouldn't be called.
+            $this->fail('User should not be created');
+        };
+
+        $service = new UserService($apiClientProphecy->reveal(), $userFactoryCallable);
+
+        $return = $service->getByEmail('test@example.com');
+
+        $this->assertIsArray($return);
+        $this->assertArrayHasKey('Email', $return);
+    }
+
+    /** @test */
+    public function passes_exception_when_user_not_found_by_email()
+    {
+        $apiClientProphecy = $this->prophesize(Client::class);
+        $apiClientProphecy->httpGet(
+            '/v1/user',
+            [
+                'email' => 'test@example.com',
+            ])
+            ->willThrow(new ApiException('User not found', StatusCodeInterface::STATUS_NOT_FOUND));
+
+        $userFactoryCallable = function($identity, $roles, $details) {
+            // Not returning a user here since it shouldn't be called.
+            $this->fail('User should not be created');
+        };
+
+        $service = new UserService($apiClientProphecy->reveal(), $userFactoryCallable);
+
+        $this->expectExceptionCode(StatusCodeInterface::STATUS_NOT_FOUND);
+        $this->expectException(ApiException::class);
+        $return = $service->getByEmail('test@example.com');
+    }
+
     /** @test */
     public function can_authenticate_with_good_credentials()
     {
@@ -72,7 +151,7 @@ class UserServiceTest extends TestCase
     }
 
     /** @test */
-    public function bad_datetime_throws_exception()
+    public function bad_datetime_throws_exception_during_authentication()
     {
         $apiClientProphecy = $this->prophesize(Client::class);
         $apiClientProphecy->httpPatch(
@@ -95,5 +174,99 @@ class UserServiceTest extends TestCase
 
         $this->expectException(\RuntimeException::class);
         $return = $service->authenticate('test@example.com', 'test');
+    }
+
+    /** @test */
+    public function can_activate_a_user()
+    {
+        $apiClientProphecy = $this->prophesize(Client::class);
+        $apiClientProphecy->httpPatch(
+            '/v1/user-activation',
+            [
+                'activation_token' => 'activate1234567890',
+            ])
+            ->willReturn([
+                'Email' => 'test@example.com'
+            ]);
+
+        $userFactoryCallable = function($identity, $roles, $details) {
+            // Not returning a user here since it shouldn't be called.
+            $this->fail('User should not be created');
+        };
+
+        $service = new UserService($apiClientProphecy->reveal(), $userFactoryCallable);
+
+        $return = $service->activate('activate1234567890');
+
+        $this->assertTrue($return);
+    }
+
+    /** @test */
+    public function can_activate_a_user_and_return_empty_data()
+    {
+        $apiClientProphecy = $this->prophesize(Client::class);
+        $apiClientProphecy->httpPatch(
+            '/v1/user-activation',
+            [
+                'activation_token' => 'activate1234567890',
+            ])
+            ->willReturn([]);
+
+        $userFactoryCallable = function($identity, $roles, $details) {
+            // Not returning a user here since it shouldn't be called.
+            $this->fail('User should not be created');
+        };
+
+        $service = new UserService($apiClientProphecy->reveal(), $userFactoryCallable);
+
+        $return = $service->activate('activate1234567890');
+
+        $this->assertFalse($return);
+    }
+
+    /** @test */
+    public function whilst_activating_an_unknown_user_false_is_returned()
+    {
+        $apiClientProphecy = $this->prophesize(Client::class);
+        $apiClientProphecy->httpPatch(
+            '/v1/user-activation',
+            [
+                'activation_token' => 'activate1234567890',
+            ])
+            ->willThrow(new ApiException('User not found', StatusCodeInterface::STATUS_NOT_FOUND));
+
+        $userFactoryCallable = function($identity, $roles, $details) {
+            // Not returning a user here since it shouldn't be called.
+            $this->fail('User should not be created');
+        };
+
+        $service = new UserService($apiClientProphecy->reveal(), $userFactoryCallable);
+
+        $return = $service->activate('activate1234567890');
+
+        $this->assertFalse($return);
+    }
+
+    /** @test */
+    public function whilst_activating_a_user_an_exception_can_be_thrown()
+    {
+        $apiClientProphecy = $this->prophesize(Client::class);
+        $apiClientProphecy->httpPatch(
+            '/v1/user-activation',
+            [
+                'activation_token' => 'activate1234567890',
+            ])
+            ->willThrow(new ApiException('Activation exception', StatusCodeInterface::STATUS_INTERNAL_SERVER_ERROR));
+
+        $userFactoryCallable = function($identity, $roles, $details) {
+            // Not returning a user here since it shouldn't be called.
+            $this->fail('User should not be created');
+        };
+
+        $service = new UserService($apiClientProphecy->reveal(), $userFactoryCallable);
+
+        $this->expectExceptionCode(StatusCodeInterface::STATUS_INTERNAL_SERVER_ERROR);
+        $this->expectException(ApiException::class);
+        $return = $service->activate('activate1234567890');
     }
 }
