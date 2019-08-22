c61099d21d7b49b1cd430959d7d7c5518136ac03
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-08-22 10:29:36 +0100

    add after scenario method to take a screenshot when tests fail

--- features/environment.py
+++ features/environment.py
@@ -1,14 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import os
 
 options = Options()
 options.headless = True
 
+
 def before_tag(context, tag):
-  if tag == 'web':
-    context.browser = webdriver.Firefox(options=options)
-    context.browser.implicitly_wait(10)
+    if tag == 'web':
+        context.browser = webdriver.Firefox(options=options)
+        context.browser.implicitly_wait(10)
+
 
 def after_tag(context, tag):
-  if tag == 'web':
-    context.browser.quit()
+    if tag == 'web':
+        context.browser.quit()
+
+
+def after_scenario(context, scenario):
+    if scenario.status == "failed":
+        if not os.path.exists("failed_scenarios_screenshots"):
+            os.makedirs("failed_scenarios_screenshots")
+        os.chdir("failed_scenarios_screenshots")
+        context.browser.save_screenshot(scenario.name + "_failed.png")
