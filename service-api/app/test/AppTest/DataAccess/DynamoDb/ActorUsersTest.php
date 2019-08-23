ef7c55b654c21130c908bdc1c861fd9f334018fd
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-08-23 15:31:18 +0100

    Add test for NotFoundException

--- service-api/app/test/AppTest/DataAccess/DynamoDb/ActorUsersTest.php
+++ service-api/app/test/AppTest/DataAccess/DynamoDb/ActorUsersTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AppTest\DataAccess\DynamoDb;
 
 use App\DataAccess\DynamoDb\ActorUsers;
+use App\Exception\NotFoundException;
 use Aws\DynamoDb\DynamoDbClient;
 use Aws\Result;
 use DateTime;
@@ -77,7 +78,7 @@ class ActorUsersTest extends TestCase
                 return true;
             }))
             ->willReturn($this->createAWSResult([
-                'Item' => [
+                'Attributes' => [
                     'Email' => [
                         'S' => 'test@example.com',
                     ],
@@ -92,7 +93,37 @@ class ActorUsersTest extends TestCase
 
         $actorRepo = new ActorUsers($dynamoDbClientProphecy->reveal(), 'users-table');
 
-        $actorRepo->recordPasswordResetRequest('test@example.com', 'resetTokenAABBCCDDEE', $time);
+        $result = $actorRepo->recordPasswordResetRequest('test@example.com', 'resetTokenAABBCCDDEE', $time);
+
+        $this->assertIsArray($result);
+        $this->assertEquals('test@example.com', $result['Email']);
+        $this->assertEquals('resetTokenAABBCCDDEE', $result['PasswordResetToken']);
+        $this->assertEquals('12345678912', $result['PasswordResetExpiry']);
+    }
+
+    /** @test */
+    public function will_fail_to_record_a_password_reset_request_if_user_does_not_exist()
+    {
+        $dynamoDbClientProphecy = $this->prophesize(DynamoDbClient::class);
+        $dynamoDbClientProphecy
+            ->getItem(Argument::that(function(array $data) {
+                $this->assertIsArray($data);
+
+                // we don't care what the array looks like as it's specific to the AWS api and may change
+                // we do care that the data *at least* contains the items we want to affect
+                $this->assertStringContainsString('users-table', serialize($data));
+                $this->assertStringContainsString('test@example.com', serialize($data));
+
+                return true;
+            }))
+            ->willThrow(new NotFoundException("User not found"));
+
+        $actorRepo = new ActorUsers($dynamoDbClientProphecy->reveal(), 'users-table');
+
+        $time = time() + (60 * 60 * 24);
+
+        $this->expectException(NotFoundException::class);
+        $result = $actorRepo->recordPasswordResetRequest('test@example.com', 'resetTokenAABBCCDDEE', $time);
     }
 
     /** @test */
@@ -197,14 +228,14 @@ class ActorUsersTest extends TestCase
         // "return by reference" which is what `foreach` expects.
         $awsResult = $this->createMock(Result::class);
 
-        $awsResult->expects($this->once())
+        $awsResult
             ->method('offsetExists')
             ->with($this->isType('string'))
             ->will($this->returnCallback(function($index) use ($iterator) {
                 return $iterator->offsetExists($index);
             }));
 
-        $awsResult->expects($this->once())
+        $awsResult
             ->method('offsetGet')
             ->with($this->isType('string'))
             ->will($this->returnCallback(function($index) use ($iterator) {
