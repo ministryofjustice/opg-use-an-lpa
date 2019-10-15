ac2b6ccbfeab52cb7f4768ba0a208c316e95dd88
GemTay <gemma.taylor@digital.justice.gov.uk>
2019-10-15 11:09:46 +0100

    Setup view LPA handler & twig file

--- service-front/app/src/Actor/src/Handler/ViewLpaSummaryHandler.php
+++ service-front/app/src/Actor/src/Handler/ViewLpaSummaryHandler.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Actor\Handler;
 
+use ArrayObject;
+use Common\Exception\ApiException;
 use Common\Handler\AbstractHandler;
-use Common\Handler\CsrfGuardAware;
 use Common\Handler\Traits\CsrfGuard;
 use Common\Handler\Traits\Session as SessionTrait;
 use Common\Handler\Traits\User;
@@ -25,11 +26,10 @@ use Zend\Expressive\Template\TemplateRendererInterface;
  * Class ViewLpaSummaryHandler
  * @package Actor\Handler
  */
-class ViewLpaSummaryHandler extends AbstractHandler implements UserAware, CsrfGuardAware
+class ViewLpaSummaryHandler extends AbstractHandler
 {
     use SessionTrait;
     use User;
-    use CsrfGuard;
 
     /**
      * @var LpaService
@@ -39,13 +39,13 @@ class ViewLpaSummaryHandler extends AbstractHandler implements UserAware, CsrfGu
     public function __construct(
         TemplateRendererInterface $renderer,
         UrlHelper $urlHelper,
-        LpaService $lpaService,
-        AuthenticationInterface $authenticator)
+        AuthenticationInterface $authenticator,
+        LpaService $lpaService)
     {
         parent::__construct($renderer, $urlHelper);
 
-        $this->lpaService = $lpaService;
         $this->setAuthenticator($authenticator);
+        $this->lpaService = $lpaService;
     }
 
     /**
@@ -55,6 +55,58 @@ class ViewLpaSummaryHandler extends AbstractHandler implements UserAware, CsrfGu
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $session = $this->getSession($request,'session');
+
+        $passcode = $session->get('passcode');
+        $referenceNumber = $session->get('reference_number');
+        $dob = $session->get('dob');
+
+        if (isset($passcode) && isset($referenceNumber) && isset($dob)) {
+
+            try {
+                $lpaData = $this->lpaService->getLpaByPasscode($passcode, $referenceNumber, $dob);
+
+                // The lpa comes back as two concurrent records. An actor which has been pulled from the
+                // relevant attorneys section and the complete lpa record itself.
+                $lpa = $lpaData['lpa'];
+
+                if ($lpa instanceof ArrayObject) {
+                    //  Check the logged in user role for this LPA
+                    $user = null;
+                    $userRole = null;
+
+                    if (isset($lpa->donor->dob) && $lpa->donor->dob == $dob) {
+                        $user = $lpa->donor;
+                        $userRole = 'Donor';
+                    } elseif (isset($lpa->attorneys) && is_iterable($lpa->attorneys)) {
+                        //  Loop through the attorneys
+                        foreach ($lpa->attorneys as $attorney) {
+                            if (isset($attorney->dob) && $attorney->dob == $dob) {
+                                $user = $attorney;
+                                $userRole = 'Attorney';
+                            }
+                        }
+                    }
+
+                    return new HtmlResponse($this->renderer->render('actor::view-lpa-summary', [
+                        'lpa'      => $lpa,
+                        'user'     => $user,
+                        'userRole' => $userRole,
+                    ]));
+                }
+
+            } catch (ApiException $aex) {
+                if ($aex->getCode() == StatusCodeInterface::STATUS_NOT_FOUND) {
+                    //  Show LPA not found page
+                    return new HtmlResponse($this->renderer->render('actor::lpa-not-found', [
+                        'user' => $this->getUser($request)
+                    ]));
+                } else {
+                    throw $aex;
+                }
+            }
+
+        }
 
 //        $id = $this->getSession($request,'session')->get('reference_number');
 //
