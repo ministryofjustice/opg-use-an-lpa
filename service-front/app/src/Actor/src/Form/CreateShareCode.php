4fe8bcad2386e26ad33f34ead12d536068a39d66
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-10-24 16:27:09 +0100

    Create viewercode form

--- service-front/app/src/Actor/src/Form/CreateShareCode.php
+++ service-front/app/src/Actor/src/Form/CreateShareCode.php
@@ -6,8 +6,11 @@ namespace Actor\Form;
 
 use Common\Form\AbstractForm;
 use Zend\Expressive\Csrf\CsrfGuardInterface;
+use Zend\Filter\StringTrim;
+use Zend\InputFilter\InputFilterProviderInterface;
+use Zend\Validator\NotEmpty;
 
-class CreateShareCode extends AbstractForm
+class CreateShareCode extends AbstractForm implements InputFilterProviderInterface
 {
     const FORM_NAME = 'lpa_sharecode_create';
 
@@ -18,5 +21,40 @@ class CreateShareCode extends AbstractForm
     public function __construct(CsrfGuardInterface $csrfGuard)
     {
         parent::__construct(self::FORM_NAME, $csrfGuard);
+
+        $this->add([
+            'name' => 'org_name',
+            'type' => 'Text',
+        ]);
+        $this->add([
+            'name' => 'lpa_token',
+            'type' => 'Hidden',
+        ]);
+    }
+
+    public function getInputFilterSpecification() : array
+    {
+        return [
+            'org_name' => [
+                'required'   => true,
+                'filters'  => [
+                    ['name' => StringTrim::class],
+                ],
+                'validators' => [
+                    [
+                        'name'                   => NotEmpty::class,
+                        'break_chain_on_failure' => true,
+                        'options'                => [
+                            'messages'           => [
+                                NotEmpty::IS_EMPTY => 'Enter an organisation name',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'lpa_token' => [
+                'required'   => true,
+            ],
+        ];
     }
 }
