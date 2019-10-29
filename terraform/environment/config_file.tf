be85f2e77180ad600db7d8e883b2794b7f172b20
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-10-29 14:21:20 +0000

    tf fmt

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -5,14 +5,14 @@ resource "local_file" "cluster_config" {
 
 locals {
   cluster_config = {
-    cluster_name          = aws_ecs_cluster.use-an-lpa.name
-    account_id            = local.account_id
+    cluster_name = aws_ecs_cluster.use-an-lpa.name
+    account_id   = local.account_id
 
     actor_lpa_codes_table = aws_dynamodb_table.actor_codes_table.name
     viewer_codes_table    = aws_dynamodb_table.viewer_codes_table.name
 
-    actor_fqdn            = aws_route53_record.actor-use-my-lpa.fqdn
-    viewer_fqdn           = aws_route53_record.viewer-use-my-lpa.fqdn
+    actor_fqdn  = aws_route53_record.actor-use-my-lpa.fqdn
+    viewer_fqdn = aws_route53_record.viewer-use-my-lpa.fqdn
   }
 }
 
