8ff079cf285aaa3fe02504ac2c3ed5478e19ff2c
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-10-30 11:11:25 +0000

    updatee behave tests to use new dns pattern

--- features/modules.py
+++ features/modules.py
@@ -7,20 +7,15 @@ def get_frontend_url(frontend, path=''):
     # The environment is based on the Terraform Workspace environment variable.
     workspace = os.getenv('TF_WORKSPACE', 'localhost')
 
-    # match workspace to a value in the dict or return a default value "development."
-    account_namespace_mapping = defaultdict(
-        lambda: "development.", {'production': "", 'preproduction': "preproduction."})
-    dns_account_namespace = account_namespace_mapping[workspace]
-
-    if dns_account_namespace == "development.":
-        dns_env_namespace = workspace + "."
-    else:
+    if workspace == "production":
         dns_env_namespace = ""
+    else:
+        dns_env_namespace = workspace + "."
 
     if workspace == 'localhost':
         url = 'http://viewer-web'
     else:
-        url = 'https://{0}{1}.{2}lastingpowerofattorney.opg.service.justice.gov.uk'.format(
-            dns_env_namespace, frontend, dns_account_namespace)
+        url = 'https://{0}{1}.lastingpowerofattorney.opg.service.justice.gov.uk'.format(
+            dns_env_namespace, frontend)
 
     return url + path
