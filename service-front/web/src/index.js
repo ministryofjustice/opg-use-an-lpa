000d68d29d8d406020f81f7fc31a7535e42cd53b
John Nolan <john.nolan@digital.justice.gov.uk>
2019-11-06 14:49:22 +0000

    tests: Added Jest to web project with jsenabled functionality

--- service-front/web/src/index.js
+++ service-front/web/src/index.js
@@ -1,5 +1,9 @@
+/* istanbul ignore file */
+
 import './main.scss';
 import './scss/lpa.scss';
 import { initAll } from 'govuk-frontend';
+import jsEnabled from './javascript/jsEnabled';
 
 initAll();
+jsEnabled(document.body);
