f18d96cf57d8adc97116a9ed958f108f93aa307b
John Nolan <john.nolan@digital.justice.gov.uk>
2019-11-06 14:50:46 +0000

    tests: Removed unneeded async declaration from test

--- service-front/web/src/javascript/jsEnabled.test.js
+++ service-front/web/src/javascript/jsEnabled.test.js
@@ -1,7 +1,7 @@
 import jsEnabled from './jsEnabled';
 
 describe('Given you want to add a js-enabled class to the document.body', () => {
-  test('It should exist in the body classList', async () => {
+  test('It should exist in the body classList', () => {
     jsEnabled(document.body);
 
     expect(document.body.classList).toHaveLength(1);
