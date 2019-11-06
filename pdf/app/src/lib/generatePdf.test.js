cc1411987d5aefe244448777c54594914093e412
John Nolan <john.nolan@digital.justice.gov.uk>
2019-11-06 11:50:17 +0000

    test: Added describe to tests, covered catch lines in puppeteer

--- pdf/app/src/lib/generatePdf.test.js
+++ pdf/app/src/lib/generatePdf.test.js
@@ -2,20 +2,22 @@ import GeneratePdf from "./generatePdf";
 
 const testHtml = `<html><head></head><body><p><a href="/home" class="govuk-link">Test with no links</a></p></body></html>`;
 
-test("It should generate a PDF with stripping tags", async () => {
-  const result = await GeneratePdf(testHtml, {
-    stripTags: true
+describe("Given you have a HTML document you want to create a PDF Document of", () => {
+  test("It should generate a PDF with stripping tags", async () => {
+    const result = await GeneratePdf(testHtml, {
+      stripTags: true
+    });
+
+    expect(result).not.toBeNull();
+    expect(result).toBeInstanceOf(Uint8Array);
   });
 
-  expect(result).not.toBeNull();
-  expect(result).toBeInstanceOf(Uint8Array);
-});
+  test("It should generate a PDF without stripping tags", async () => {
+    const result = await GeneratePdf(testHtml, {
+      stripTags: false
+    });
 
-test("It should generate a PDF without stripping tags", async () => {
-  const result = await GeneratePdf(testHtml, {
-    stripTags: false
+    expect(result).not.toBeNull();
+    expect(result).toBeInstanceOf(Uint8Array);
   });
-
-  expect(result).not.toBeNull();
-  expect(result).toBeInstanceOf(Uint8Array);
 });
