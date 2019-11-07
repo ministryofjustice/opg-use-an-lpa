071be10c3b253a139e51b4fdcd224eda8b01f6b4
John Nolan <john.nolan@digital.justice.gov.uk>
2019-11-07 14:30:04 +0000

    build: test fix

--- service-front/web/src/javascript/jsEnabled.test.js
+++ service-front/web/src/javascript/jsEnabled.test.js
@@ -4,7 +4,7 @@ describe('Given you want to add a js-enabled class to the document.body', () => 
   test('It should exist in the body classList', () => {
     jsEnabled(document.body);
 
-    expect(document.body.classList).toHaveLength(2);
+    expect(document.body.classList).toHaveLength(1);
     expect(document.body.classList).toContain('js-enabled');
   });
 });
