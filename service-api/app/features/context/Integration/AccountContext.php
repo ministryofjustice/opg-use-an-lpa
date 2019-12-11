5446cc5e63c215dec87fdd70d2e11a9c2c637260
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-12-11 17:15:14 +0000

    Get integration test for password reset working on API

--- service-api/app/features/context/Integration/AccountContext.php
+++ service-api/app/features/context/Integration/AccountContext.php
@@ -5,24 +5,44 @@ declare(strict_types=1);
 namespace BehatTest\Context\Integration;
 
 use Acpr\Behat\Psr\Context\Psr11AwareContext;
+use App\Service\User\UserService;
+use Aws\DynamoDb\Marshaler;
+use Aws\MockHandler as AwsMockHandler;
+use Aws\Result;
 use Behat\Behat\Context\Context;
-use Behat\Behat\Tester\Exception\PendingException;
+use BehatTest\Context\SetupEnvTrait;
 use JSHayes\FakeRequests\MockHandler;
 use Psr\Container\ContainerInterface;
 
+require_once __DIR__ . '/../../../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
+
+/**
+ * Class AccountContext
+ *
+ * @package BehatTest\Context\Integration
+ *
+ * @property $userAccountEmail
+ * @property $passwordResetData
+ */
 class AccountContext implements Context, Psr11AwareContext
 {
+    use SetupEnvTrait;
+
     /** @var ContainerInterface */
     private $container;
 
     /** @var MockHandler */
     private $apiFixtures;
 
+    /** @var AwsMockHandler */
+    private $awsFixtures;
+
     public function setContainer(ContainerInterface $container): void
     {
         $this->container = $container;
 
         $this->apiFixtures = $this->container->get(MockHandler::class);
+        $this->awsFixtures = $this->container->get(AwsMockHandler::class);
     }
 
     /**
@@ -30,7 +50,7 @@ class AccountContext implements Context, Psr11AwareContext
      */
     public function iAmAUserOfTheLpaApplication()
     {
-        throw new PendingException();
+        $this->userAccountEmail = 'test@example.com';
     }
 
     /**
@@ -38,7 +58,7 @@ class AccountContext implements Context, Psr11AwareContext
      */
     public function iHaveForgottenMyPassword()
     {
-        throw new PendingException();
+        // Not needed for this context
     }
 
     /**
@@ -46,7 +66,29 @@ class AccountContext implements Context, Psr11AwareContext
      */
     public function iAskForMyPasswordToBeReset()
     {
-        throw new PendingException();
+        $resetToken = 'AAAABBBBCCCC';
+
+        // ActorUsers::getByEmail
+        $this->awsFixtures->append(new Result([
+            'Items' => [
+                $this->marshalAwsResultData([
+                    'Id'    => '123456789',
+                    'Email' => $this->userAccountEmail
+                ])
+            ]
+        ]));
+
+        // ActorUsers::requestPasswordReset
+        $this->awsFixtures->append(new Result([
+            'Attributes' => $this->marshalAwsResultData([
+                'Id'    => '123456789',
+                'PasswordResetToken' => $resetToken
+            ])
+        ]));
+
+        $us = $this->container->get(UserService::class);
+
+        $this->passwordResetData = $us->requestPasswordReset($this->userAccountEmail);
     }
 
     /**
@@ -54,6 +96,22 @@ class AccountContext implements Context, Psr11AwareContext
      */
     public function iReceiveUniqueInstructionsOnHowToResetMyPassword()
     {
-        throw new PendingException();
+        assertArrayHasKey('PasswordResetToken', $this->passwordResetData);
+    }
+
+    /**
+     * Convert a key/value array to a correctly marshaled AwsResult structure.
+     *
+     * AwsResult data is in a special array format that tells you
+     * what datatype things are. This function creates that data structure.
+     *
+     * @param array $input
+     * @return array
+     */
+    protected function marshalAwsResultData(array $input): array
+    {
+        $marshaler = new Marshaler();
+
+        return $marshaler->marshalItem($input);
     }
 }
