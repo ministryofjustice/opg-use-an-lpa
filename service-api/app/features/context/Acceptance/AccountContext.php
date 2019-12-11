869906c1c24148207454cb19e43fee4b68c4a264
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-12-11 17:28:40 +0000

    Add context steps for api acceptance

--- service-api/app/features/context/Acceptance/AccountContext.php
+++ service-api/app/features/context/Acceptance/AccountContext.php
@@ -5,8 +5,39 @@ declare(strict_types=1);
 namespace BehatTest\Context\Acceptance;
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 
 class AccountContext implements Context
 {
+    /**
+     * @Given I am a user of the lpa application
+     */
+    public function iAmAUserOfTheLpaApplication()
+    {
+        throw new PendingException();
+    }
 
+    /**
+     * @Given I have forgotten my password
+     */
+    public function iHaveForgottenMyPassword()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I ask for my password to be reset
+     */
+    public function iAskForMyPasswordToBeReset()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I receive unique instructions on how to reset my password
+     */
+    public function iReceiveUniqueInstructionsOnHowToResetMyPassword()
+    {
+        throw new PendingException();
+    }
 }
