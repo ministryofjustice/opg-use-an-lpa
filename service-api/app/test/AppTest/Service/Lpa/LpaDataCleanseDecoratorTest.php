fbff312fe8f5df629d81bc14982455e3fa1589b6
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-12-16 11:23:51 +0000

    Tighten up testing of decorator

--- service-api/app/test/AppTest/Service/Lpa/LpaDataCleanseDecoratorTest.php
+++ service-api/app/test/AppTest/Service/Lpa/LpaDataCleanseDecoratorTest.php
@@ -6,6 +6,7 @@ namespace AppTest\Service\Lpa;
 
 use App\DataAccess\Repository\Response\LpaInterface;
 use App\Service\Lpa\LpaDataCleanseDecorator;
+use Common\Entity\Lpa;
 use PHPUnit\Framework\TestCase;
 
 class LpaDataCleanseDecoratorTest extends TestCase
@@ -21,6 +22,7 @@ class LpaDataCleanseDecoratorTest extends TestCase
 
         $decoratedLpaInterface = new LpaDataCleanseDecorator($lpaResponseProphecy->reveal());
 
+        $this->assertInstanceOf(LpaInterface::class, $decoratedLpaInterface);
         $this->assertNotEquals($lpaResponseProphecy->reveal(), $decoratedLpaInterface);
         $this->assertArrayHasKey('id', $decoratedLpaInterface->getData());
     }
@@ -36,6 +38,11 @@ class LpaDataCleanseDecoratorTest extends TestCase
                         'id' => 0,
                         'firstname' => '',
                         'surname' => ''
+                    ],
+                    [
+                        'id' => 1,
+                        'firstname' => 'Test',
+                        'surname' => 'Testerson'
                     ]
                 ]
             ]);
@@ -43,6 +50,42 @@ class LpaDataCleanseDecoratorTest extends TestCase
         $decoratedLpaInterface = new LpaDataCleanseDecorator($lpaResponseProphecy->reveal());
         $lpa = $decoratedLpaInterface->getData();
 
+        $this->assertCount(1, $lpa['attorneys']);
+    }
+
+    /** @test */
+    public function it_can_cope_with_no_attorneys() {
+        $lpaResponseProphecy = $this->prophesize(LpaInterface::class);
+        $lpaResponseProphecy->getData()
+            ->willReturn([
+                'id' => '12345',
+                'attorneys' => []
+            ]);
+
+        $decoratedLpaInterface = new LpaDataCleanseDecorator($lpaResponseProphecy->reveal());
+        $lpa = $decoratedLpaInterface->getData();
+
         $this->assertCount(0, $lpa['attorneys']);
+    }
+
+    /** @test */
+    public function it_wont_alter_valid_data() {
+        $lpaResponseProphecy = $this->prophesize(LpaInterface::class);
+        $lpaResponseProphecy->getData()
+            ->willReturn([
+                'id' => '12345',
+                'attorneys' => [
+                    [
+                        'id' => 0,
+                        'firstname' => 'Test',
+                        'surname' => 'Testerson'
+                    ]
+                ]
+            ]);
+
+        $decoratedLpaInterface = new LpaDataCleanseDecorator($lpaResponseProphecy->reveal());
+        $lpa = $decoratedLpaInterface->getData();
+
+        $this->assertCount(1, $lpa['attorneys']);
     }
 }
