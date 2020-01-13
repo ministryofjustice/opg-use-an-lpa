e1dcad0f8514512d1e08b46ebdda263f2430bd31
Matt Machell <matt.machell@digital.justice.gov.uk>
2020-01-13 17:40:57 +0000

    Move to installed puppeteer

--- service-front/web/pa11y.js
+++ service-front/web/pa11y.js
@@ -1,5 +1,7 @@
 const pa11y = require('pa11y');
 const cli = require('pa11y-reporter-cli');
+const puppeteer = require('puppeteer');
+
 /*
 
 Pa11y checks for common accessibility issues in your pages' code. More documentation at https://github.com/pa11y/pa11y-ci
@@ -14,40 +16,58 @@ It checks code quality, so won't pick up content issues such as a picture of an 
 
 (async function () {
 
-    const config = {
-        hideElements: 'svg', //svg fallback images currently cause issues due to fallback images beneath a role=presentation
-    };    
+    try{
+        const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/chromium-browser',
+            args: ['--disable-dev-shm-usage','--no-sandbox']
+          });
 
-    const auth_config = {...config}; 
+        const config = {
+            hideElements: 'svg', //svg fallback images currently cause issues due to fallback images beneath a role=presentation
+            browser: browser //use the specified browser config
+        };    
 
-    //actions API is new so may change
-    auth_config.actions=[
-        'set field #username to test@test.com', //check user
-        'set field #password to password1', //check pwd
-        'click element button[type="submit"]',
-    ]
+        const auth_config = {...config}; 
 
-    //set of page tests to run, array of pa11y calls returning promises
-    const full_results = await Promise.all([
-        
-        //actor side local homepage
-        pa11y('http://localhost:9001', config),
+        //actions API is new so may change
+        auth_config.actions=[
+            'set field #email to test@test.com', //check email
+            'set field #password to password1', //check pwd
+            'click element button[type="submit"]',
+            'wait for path to not be /login',
+        ]
 
-        //actor side login page pre login
-        pa11y('http://localhost:9001/login', config),
+        //set of page tests to run, array of pa11y calls returning promises
+        const full_results = await Promise.all([
+            
+            //actor side local homepage
+            pa11y('http://actor-web', config),
 
-        //actor side initial page post login actions
-        pa11y('http://localhost:9001/login', auth_config),
+            //actor side login page pre login
+            pa11y('http://actor-web/login', config),
 
-        //view side local homepage
-        pa11y('http://localhost:9002', config)
+            //actor side initial page post login actions
+            pa11y('http://actor-web/login', auth_config),
 
-        //you can add additional page tests here
-    ]);
+            //view side local homepage
+            pa11y('http://viewer-web', config)
 
-    full_results.map(function(result){
-        const cliResults = cli.results(result);
-        console.log(cliResults);
-    });
+            //you can add additional page tests here
+        ]);
+
+        full_results.map(function(result){
+            const cliResults = cli.results(result);
+            console.log(cliResults);
+        });
+        browser.close();
+
+    }
+    catch(error){
+        console.log("Pa11y analysis failed\n", error)
+
+        if(browser){
+            browser.close();
+        }
+    }
 
 })();
