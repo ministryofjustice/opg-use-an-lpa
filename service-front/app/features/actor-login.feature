a4e70b9049d7bd39a743d31dd441b4fafde8dff8
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-01-24 17:53:32 +0000

    Add tests to 'bad path' check the login flow.

--- service-front/app/features/actor-login.feature
+++ service-front/app/features/actor-login.feature
@@ -4,16 +4,36 @@ Feature: A user of the system is able to login
   I can login using my credentials
   So that I can carry out operations within the application
 
+  Background:
+    Given I am a user of the lpa application
+
   @ui
   Scenario: A user can login
-    Given I am a user of the lpa application
-    And I access the login form
+    Given I access the login form
     When I enter correct credentials
     Then I am signed in
 
   @ui
+    Scenario: A user cannot login with an incorrect password
+    Given I access the login form
+    When I enter incorrect login password
+    Then I am told my credentials are incorrect
+
+  @ui
+  Scenario: An incorrect email will give the same message as an incorrect password
+    Given I access the login form
+    When I enter incorrect login password
+    Then I am told my credentials are incorrect
+
+  @ui
+  Scenario: A user cannot login if they have not activated their account
+    Given I have not activated my account
+    And I access the login form
+    When I enter correct credentials
+    Then I am told my account has not been activated
+
+  @ui
   Scenario: Visiting the login page when signed in will redirect to the dashboard
-    Given I am a user of the lpa application
-    And I am currently signed in
+    Given I am currently signed in
     When I attempt to sign in again
     Then I am directed to my dashboard
--- service-front/app/features/context/UI/AccountContext.php
+++ service-front/app/features/context/UI/AccountContext.php
@@ -17,6 +17,7 @@ use Psr\Http\Message\RequestInterface;
  *
  * @property $userEmail
  * @property $userPassword
+ * @property $userActive
  */
 class AccountContext extends BaseUIContext
 {
@@ -39,6 +40,7 @@ class AccountContext extends BaseUIContext
     {
         $this->userEmail = 'test@test.com';
         $this->userPassword = 'pa33w0rd';
+        $this->userActive = true;
 
         $this->ui->iAmOnHomepage();
 
@@ -50,6 +52,7 @@ class AccountContext extends BaseUIContext
      */
     public function iAmCurrentlySignedIn()
     {
+        // do all the steps to sign in
         $this->iAccessTheLoginForm();
         $this->iEnterCorrectCredentials();
         $this->iSignIn();
@@ -61,6 +64,26 @@ class AccountContext extends BaseUIContext
     public function iAmDirectedToMyPersonalDashboard()
     {
         $this->ui->assertPageAddress('/lpa/dashboard');
+    }
+
+    /**
+     * @Then /^I am told my account has not been activated$/
+     */
+    public function iAmToldMyAccountHasNotBeenActivated()
+    {
+        // TODO UML-501
+        //      When a user account has not been activated we will need a better user flow
+        //      around how this works. Probably taking them to a help page that details
+        //      what they need to do, including resending the email.
+        $this->ui->assertPageContainsText('Email and password combination not recognised');
+    }
+
+    /**
+     * @Then /^I am told my credentials are incorrect$/
+     */
+    public function iAmToldMyCredentialsAreIncorrect()
+    {
+        $this->ui->assertPageContainsText('Email and password combination not recognised');
     }
 
     /**
@@ -79,19 +102,55 @@ class AccountContext extends BaseUIContext
         $this->ui->fillField('email', $this->userEmail);
         $this->ui->fillField('password', $this->userPassword);
 
-        // API call for password reset request
-        $this->apiFixtures->patch('/v1/auth')
-            ->respondWith(new Response(StatusCodeInterface::STATUS_OK, [], json_encode(
-                [
-                    'Id'        => '123',
-                    'Email'     => $this->userEmail,
-                    'LastLogin' => null
-                ]
-            )));
+        if ($this->userActive) {
+            // API call for authentication
+            $this->apiFixtures->patch('/v1/auth')
+                ->respondWith(new Response(StatusCodeInterface::STATUS_OK, [], json_encode(
+                    [
+                        'Id'        => '123',
+                        'Email'     => $this->userEmail,
+                        'LastLogin' => null
+                    ]
+                )));
 
-        // Dashboard page checks for all LPA's for a user
-        $this->apiFixtures->get('/v1/lpas')
-            ->respondWith(new Response(StatusCodeInterface::STATUS_OK, [], json_encode([])));
+            // Dashboard page checks for all LPA's for a user
+            $this->apiFixtures->get('/v1/lpas')
+                ->respondWith(new Response(StatusCodeInterface::STATUS_OK, [], json_encode([])));
+        } else {
+            // API call for authentication
+            $this->apiFixtures->patch('/v1/auth')
+                ->respondWith(new Response(StatusCodeInterface::STATUS_UNAUTHORIZED, [], json_encode([])));
+        }
+
+        $this->ui->pressButton('Continue');
+    }
+
+    /**
+     * @When I enter incorrect login password
+     */
+    public function iEnterIncorrectLoginPassword()
+    {
+        $this->ui->fillField('email', $this->userEmail);
+        $this->ui->fillField('password', "inoc0rrectPassword");
+
+        // API call for authentication
+        $this->apiFixtures->patch('/v1/auth')
+            ->respondWith(new Response(StatusCodeInterface::STATUS_FORBIDDEN, [], json_encode([])));
+
+        $this->ui->pressButton('Continue');
+    }
+
+    /**
+     * @When I enter incorrect login email
+     */
+    public function iEnterIncorrectLoginEmail()
+    {
+        $this->ui->fillField('email', $this->userEmail);
+        $this->ui->fillField('password', "inoc0rrectPassword");
+
+        // API call for authentication
+        $this->apiFixtures->patch('/v1/auth')
+            ->respondWith(new Response(StatusCodeInterface::STATUS_NOT_FOUND, [], json_encode([])));
 
         $this->ui->pressButton('Continue');
     }
@@ -137,6 +196,14 @@ class AccountContext extends BaseUIContext
         $this->ui->fillField('email', 'test@example.com');
         $this->ui->fillField('email_confirm', 'test@example.com');
         $this->ui->pressButton('Email me the link');
+    }
+
+    /**
+     * @Given /^I have not activated my account$/
+     */
+    public function iHaveNotActivatedMyAccount()
+    {
+        $this->userActive = false;
     }
 
     /**
