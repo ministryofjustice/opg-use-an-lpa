cd3c0d57f5a45e4a87720e726719279647459f80
SeemaMenon <seema.workspace@gmail.com>
2020-01-27 22:39:27 +0000

    Added a form for cancel button to post code

---
+++ service-front/app/src/Actor/src/Handler/CancelCodeOptionHandler.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Actor\Handler;
+
+use Common\Exception\InvalidRequestException;
+use Common\Handler\AbstractHandler;
+use Common\Handler\Traits\User;
+use Common\Handler\UserAware;
+use Common\Service\Lpa\ViewerCodeService;
+use Common\Service\Lpa\LpaService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Expressive\Authentication\AuthenticationInterface;
+use Zend\Expressive\Helper\UrlHelper;
+use Zend\Expressive\Template\TemplateRendererInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use DateTime;
+
+class CancelCodeOptionHandler extends AbstractHandler implements UserAware
+{
+    use User;
+
+    /**
+     * @var ViewerCodeService
+     */
+    private $viewerCodeService;
+
+    /**
+     * @var LpaService
+     */
+    private $lpaService;
+
+    public function __construct(
+        TemplateRendererInterface $renderer,
+        UrlHelper $urlHelper,
+        AuthenticationInterface $authenticator,
+        LpaService $lpaService,
+        ViewerCodeService $viewerCodeService)
+    {
+        parent::__construct($renderer, $urlHelper);
+
+        $this->setAuthenticator($authenticator);
+        $this->lpaService = $lpaService;
+        $this->viewerCodeService = $viewerCodeService;
+    }
+
+    /**
+     * Handles a request and produces a response
+     *
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     * @throws InvalidRequestException
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+
+        if ($request->getMethod() === 'POST') {
+
+            var_dump("hello");
+            die;
+
+                return;
+            }
+        }
+}
