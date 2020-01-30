ac1a12ca374bd78c7bb8d8fe45c1606f32d230a4
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-01-30 14:39:23 +0000

    Setup tracing in the api layer

---
+++ service-api/app/src/App/src/Middleware/Logging/RequestTracingMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware\Logging;
+
+use Psr\Http\Server\RequestHandlerInterface as DelegateInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Class RequestTracingMiddleware
+ *
+ * Ensures that the Nginx attached tracing header is correctly made available
+ * to the $request as an attribute
+ *
+ * @package App\Middleware\Logging
+ */
+class RequestTracingMiddleware implements MiddlewareInterface
+{
+    const TRACE_HEADER_NAME = 'x-amzn-trace-id';
+
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate): ResponseInterface
+    {
+        $traceId = $request->getHeader(self::TRACE_HEADER_NAME);
+        $traceId = (count($traceId) > 0) ? $traceId[0] : '';
+
+        return $delegate->handle($request->withAttribute('amzn-trace-id', $traceId));
+    }
+}
