7a701358999900b5e069997921655ed7822653b2
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-02-04 17:23:20 +0000

    Changed error messages, updated behave tests, and added start & end tokens for the regex pattern

--- service-front/app/src/Viewer/src/Form/ShareCode.php
+++ service-front/app/src/Viewer/src/Form/ShareCode.php
@@ -8,6 +8,7 @@ use Common\Form\AbstractForm;
 use Zend\Expressive\Csrf\CsrfGuardInterface;
 use Zend\Filter\StringTrim;
 use Zend\InputFilter\InputFilterProviderInterface;
+use Zend\Validator\NotEmpty;
 use Zend\Validator\Regex;
 
 class ShareCode extends AbstractForm implements InputFilterProviderInterface
@@ -39,17 +40,33 @@ class ShareCode extends AbstractForm implements InputFilterProviderInterface
                 ],
                 'validators' => [
                     new Regex([
-                        'pattern' => '/[[:alnum:]]{4,4}(?\'dash\' |-)?[[:alnum:]]{4,4}(\g{dash})?[[:alnum:]]{4,4}/',
+                        'pattern' => '/^[[:alnum:]]{4,4}(?\'dash\' |-)?[[:alnum:]]{4,4}(\g{dash})?[[:alnum:]]{4,4}$/',
                         'message' => [
-                            Regex::NOT_MATCH => 'Enter an LPA share code in the correct format.'
+                            Regex::NOT_MATCH => 'LPA access codes are 13 numbers and letters long and start with a V'
                         ]
-                    ])
+                    ]),
+                    [
+                        'name'                   => NotEmpty::class,
+                        'break_chain_on_failure' => true,
+                        'options'                => [
+                            'message'  => 'Enter the LPA access code',
+                        ],
+                    ]
                 ]
             ],
             'donor_surname' => [
                 'required' => true,
                 'filters'  => [
                     ['name' => StringTrim::class],
+                ],
+                'validators' => [
+                    [
+                        'name'                   => NotEmpty::class,
+                        'break_chain_on_failure' => true,
+                        'options'                => [
+                            'message'  => 'Enter the donor\'s surname',
+                        ],
+                    ]
                 ]
             ]
         ];
