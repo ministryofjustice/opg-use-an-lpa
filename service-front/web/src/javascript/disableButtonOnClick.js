f43f77a18f1a1309861274ad27c651e65a3b2ca8
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-02-14 16:43:32 +0000

    code review tweaks to approach

--- service-front/web/src/javascript/disableButtonOnClick.js
+++ service-front/web/src/javascript/disableButtonOnClick.js
@@ -1,11 +1,13 @@
-const disableButtonOnClick = () => {
-  var btn = document.getElementsByClassName('govuk-button');
-  for (var i = 0; i < btn.length; i++) {
+const disableButtonOnClick = (form) => {
 
-      btn[i].addEventListener('click', function disableButton() {
-          this.disabled = true;
-          document.forms[this.form.name].submit()
+  for (let i = 0; i < form.length; i++) {
 
+      form[i].addEventListener('click', function disableButton(e) {
+
+          if (e.target.nodeName == 'BUTTON') {
+              e.target.disabled = true;
+              document.forms[form[i].name].submit()
+          }
       })
   }
 };
