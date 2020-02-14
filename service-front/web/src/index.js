7d1839c1399c1faffc8621c38da823790467cbdf
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-02-14 11:51:17 +0000

    disabled button after click

--- service-front/web/src/index.js
+++ service-front/web/src/index.js
@@ -7,3 +7,19 @@ import jsEnabled from './javascript/jsEnabled';
 initAll();
 
 jsEnabled(document.body);
+
+var btn = document.getElementsByClassName('govuk-button');
+debugger;
+for (var i = 0; i < btn.length; i++) {
+
+    btn[i].addEventListener('click', function disableButton() {
+        debugger;
+        this.disabled = true;
+
+    })
+}
+
+
+
+
+
