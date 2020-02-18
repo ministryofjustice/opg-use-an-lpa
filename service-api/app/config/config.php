4bc45d84e4e3457cbf67576eb4c37098e7920ad4
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-02-18 14:46:08 +0000

    Odd indentation snuck through

--- service-api/app/config/config.php
+++ service-api/app/config/config.php
@@ -12,36 +12,41 @@ $cacheConfig = [
     'config_cache_path' => '/tmp/config-cache.php',
 ];
 
-$aggregator = new ConfigAggregator([
-                                       \WShafer\PSR11MonoLog\ConfigProvider::class,
-                                       \Zend\HttpHandlerRunner\ConfigProvider::class,
-                                       \Zend\Expressive\Router\FastRouteRouter\ConfigProvider::class,
+$aggregator = new ConfigAggregator(
+    [
+        \WShafer\PSR11MonoLog\ConfigProvider::class,
+        \Zend\HttpHandlerRunner\ConfigProvider::class,
+        \Zend\Expressive\Router\FastRouteRouter\ConfigProvider::class,
 
-                                       // Include cache configuration
-                                       new ArrayProvider($cacheConfig),
+        // Include cache configuration
+        new ArrayProvider($cacheConfig),
 
-                                       \Zend\Expressive\Helper\ConfigProvider::class,
-                                       \Zend\Expressive\ConfigProvider::class,
-                                       \Zend\Expressive\Router\ConfigProvider::class,
+        \Zend\Expressive\Helper\ConfigProvider::class,
+        \Zend\Expressive\ConfigProvider::class,
+        \Zend\Expressive\Router\ConfigProvider::class,
 
-    // Swoole config to overwrite some services (if installed)
-    class_exists(\Zend\Expressive\Swoole\ConfigProvider::class)
-        ? \Zend\Expressive\Swoole\ConfigProvider::class
-        : function(){ return[]; },
+        // Swoole config to overwrite some services (if installed)
+        class_exists(\Zend\Expressive\Swoole\ConfigProvider::class)
+            ? \Zend\Expressive\Swoole\ConfigProvider::class
+            : function () {
+            return [];
+        },
 
-    // Default App module config
-    App\ConfigProvider::class,
+        // Default App module config
+        App\ConfigProvider::class,
 
-    // Load application config in a pre-defined order in such a way that local settings
-    // overwrite global settings. (Loaded as first to last):
-    //   - `global.php`
-    //   - `*.global.php`
-    //   - `local.php`
-    //   - `*.local.php`
-    new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
+        // Load application config in a pre-defined order in such a way that local settings
+        // overwrite global settings. (Loaded as first to last):
+        //   - `global.php`
+        //   - `*.global.php`
+        //   - `local.php`
+        //   - `*.local.php`
+        new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
 
-    // Load development config if it exists
-    new PhpFileProvider(realpath(__DIR__) . '/development.config.php'),
-], $cacheConfig['config_cache_path']);
+        // Load development config if it exists
+        new PhpFileProvider(realpath(__DIR__) . '/development.config.php'),
+    ],
+    $cacheConfig['config_cache_path']
+);
 
 return $aggregator->getMergedConfig();
