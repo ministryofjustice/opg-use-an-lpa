090114a16487847b51b0fa4b23e84b1eefc383ba
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-02-19 14:59:20 +0000

    re-add account ids for preprod and prod

--- pipeline_scripts/identify_account_id.sh
+++ pipeline_scripts/identify_account_id.sh
@@ -5,9 +5,9 @@ case "$1" in
 -h) echo "Usage: `basename $0` [environment name]"
     exit 0
     ;;
-preproduction)  echo "1"
+preproduction)  echo "888228022356"
     ;;
-production)  echo  "2"
+production)  echo  "690083044361"
     ;;
 *) echo "367815980639"
    ;;
