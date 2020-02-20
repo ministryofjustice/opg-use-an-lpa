d17320cd197409bb5bdadbb59623438e36f8d6cc
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-02-20 16:11:30 +0000

    add bash script to iterate through workspaces, runa destroy and remove the workspace

---
+++ scripts/workspace_cleanup/workspace_cleanup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+if [ $# -eq 0 ]
+  then
+    echo "Please provide workspaces to be removed."
+fi
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` [workspaces separated by a space]"
+  exit 0
+fi
+
+declare -a workspaces_to_delete=("$@")
+
+declare -a protected_workspaces=("production"
+                      "preproduction"
+                      "development"
+                      "demo"
+                      "ithc"
+                      )
+
+for workspace in "${workspaces_to_delete[@]}"
+do
+
+  if [[ ! " ${protected_workspaces[@]} " =~ " $workspace " ]]; then
+    echo "$workspace"
+      terraform workspace select "$workspace"
+      terraform destroy -auto-approve
+      terraform workspace select default
+      terraform workspace delete "$workspace"
+  fi
+done
