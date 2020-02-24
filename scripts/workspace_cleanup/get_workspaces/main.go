9ee6d8187f0894038563d4feddf1cbac4bacdecf
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-02-24 19:24:50 +0000

    make a go script for getting workspaces from DynamoDB

---
+++ scripts/workspace_cleanup/get_workspaces/main.go
@@ -0,0 +1,64 @@
+// +build example
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"log"
+)
+
+func exitWithError(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+}
+
+func main() {
+	sess, err := session.NewSession()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	role_arn := ""
+	if len(os.Getenv("CI")) > 0 {
+		role_arn = "arn:aws:iam::367815980639:role/opg-use-an-lpa-ci"
+	} else {
+		role_arn = "arn:aws:iam::367815980639:role/operator"
+	}
+
+	creds := stscreds.NewCredentials(sess, role_arn)
+	awsConfig := aws.Config{Credentials: creds, Region: aws.String("eu-west-1")}
+
+	svc := dynamodb.New(sess, &awsConfig)
+
+	params := &dynamodb.ScanInput{
+		TableName: aws.String("WorkspaceCleanup"),
+	}
+
+	result, err := svc.Scan(params)
+	if err != nil {
+		exitWithError(fmt.Errorf("failed to make Query API call, %v", err))
+	}
+
+	items := []Item{}
+
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &items)
+	if err != nil {
+		exitWithError(fmt.Errorf("failed to unmarshal Query result items, %v", err))
+	}
+
+	for i, item := range items {
+		fmt.Print(item.WorkspaceName, " ")
+		i++
+	}
+
+}
+
+type Item struct {
+	WorkspaceName string
+}
