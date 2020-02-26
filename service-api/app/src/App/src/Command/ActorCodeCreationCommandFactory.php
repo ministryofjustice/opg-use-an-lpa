cff2241fa0e6e54719a08f66bb0bf4ce3f92aa95
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-02-26 13:33:51 +0000

    Set a CLI specific trace-id into the container so calls to $container->get('trace-id') do not fail

--- service-api/app/src/App/src/Command/ActorCodeCreationCommandFactory.php
+++ service-api/app/src/App/src/Command/ActorCodeCreationCommandFactory.php
@@ -6,6 +6,7 @@ namespace App\Command;
 
 use App\DataAccess\ApiGateway\Lpas;
 use Aws\DynamoDb\DynamoDbClient;
+use DI\Container;
 use Psr\Container\ContainerInterface;
 
 class ActorCodeCreationCommandFactory
@@ -16,6 +17,14 @@ class ActorCodeCreationCommandFactory
 
         if (!isset($config['repositories']['dynamodb']['actor-codes-table'])) {
             throw new \Exception('Actor Codes table configuration not present');
+        }
+
+        // This factory will only be hit when building the ActorCodeCreationCommand so
+        // the trace-id will not be available to services requesting it. Lpas::class is one
+        // of those so here we add a dummy value. Runtime modification of the container outside
+        // of testing contexts should noe generally be done.
+        if ($container instanceof Container) {
+            $container->set('trace-id', 'CLI_TRACE_ID');
         }
 
         return new ActorCodeCreationCommand(
