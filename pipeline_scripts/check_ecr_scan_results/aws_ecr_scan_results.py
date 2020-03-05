ac1957a282b0b2533b1f508ded1b7cb11c0c5ccc
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-03-05 12:57:37 +0000

    script now directly handles slack messaging

--- pipeline_scripts/check_ecr_scan_results/aws_ecr_scan_results.py
+++ pipeline_scripts/check_ecr_scan_results/aws_ecr_scan_results.py
@@ -1,12 +1,9 @@
-import urllib.request
 import boto3
 import argparse
 import json
 import os
 import sys
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
+import requests
 
 
 class ECRScanChecker:
@@ -16,6 +13,8 @@ class ECRScanChecker:
     aws_ecr_repository_path = ''
     failed_checks = 0
     images_to_check = []
+    ecr_scan_results = []
+    report = ''
 
     def __init__(self, config_file):
         self.images_to_check = [
@@ -84,20 +83,25 @@ class ECRScanChecker:
             print("Unable to return ECR image scan results for", tag)
             exit(1)
 
-    def recursive_check(self, tag):
+    def recursive_check_make_report(self, tag):
         print("Checking ECR scan results...")
         for image in self.images_to_check:
             if self.get_ecr_scan_findings(image, tag)[
                     "imageScanFindings"]["findings"] != []:
-                print("found results for", self.aws_ecr_repository_path+image, ":")
-                pp.pprint(self.get_ecr_scan_findings(image, tag)[
-                    "imageScanFindings"]["findings"])
-                self.failed_checks += 1
-                # Use file to refer to the file object
-                with open('/tmp/findings.txt', 'a') as file:
-
-                    file.write(str(self.get_ecr_scan_findings(image, tag)[
-                        "imageScanFindings"]["findings"]))
+                cve = self.get_ecr_scan_findings(image, tag)[
+                    "imageScanFindings"]["findings"][0]["name"]
+                description = self.get_ecr_scan_findings(image, tag)[
+                    "imageScanFindings"]["findings"][0]["description"]
+                severity = self.get_ecr_scan_findings(image, tag)[
+                    "imageScanFindings"]["findings"][0]["severity"]
+                link = self.get_ecr_scan_findings(image, tag)[
+                    "imageScanFindings"]["findings"][0]["uri"]
+                title = "\n\n:warning: *AWS ECR Scan found results for {}:* \n\n".format(
+                    self.aws_ecr_repository_path+image)
+                result = "*Image:* {0} \n*Severity:* {1} \n*CVE:* {2} \n*Description:* {3} \n*Link:* {4}\n\n".format(
+                    image, severity, cve, description, link)
+                self.report += title + result
+                print(self.report)
 
     def get_ecr_scan_findings(self, image, tag):
         repository_name = self.aws_ecr_repository_path+image
@@ -108,19 +112,26 @@ class ECRScanChecker:
             },
             maxResults=1
         )
-
         return response
 
-    def pipeline_status(self):
-        if self.failed_checks > 0:
-            return "ECR scan results failure"
+    def post_to_slack(self, slack_webhook):
+        post_data = json.dumps({"text": self.report})
+        response = requests.post(
+            slack_webhook, data=post_data,
+            headers={'Content-Type': 'application/json'}
+        )
+        if response.status_code != 200:
+            raise ValueError(
+                'Request to slack returned an error %s, the response is:\n%s'
+                % (response.status_code, response.text)
+            )
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Check ECR Scan results for all service container images.")
 
-    parser.add_argument("config_file_path",
+    parser.add_argument("--config_file_path",
                         nargs='?',
                         default="/tmp/cluster_config.json",
                         type=str,
@@ -128,16 +139,21 @@ def main():
     parser.add_argument("--tag",
                         default="latest",
                         help="Image tag to check scan results for.")
+    parser.add_argument("--slack_webhook",
+                        default=os.getenv('SLACK_WEBHOOK'),
+                        help="Webhook to use, determines what channel to post to")
+    parser.add_argument("--post_to_slack",
+                        default=True,
+                        help="Optionally turn off posting messages to slack")
 
     args = parser.parse_args()
-
     work = ECRScanChecker(args.config_file_path)
-
     work.recursive_wait(args.tag)
-    work.recursive_check(args.tag)
-    status = work.pipeline_status()
-    os.environ['SCAN_FAILURE'] = status
-    print(status)
+    work.recursive_check_make_report(args.tag)
+    if args.slack_webhook is None:
+        print("No slack webhook provided, skipping post of results to slack")
+    if args.post_to_slack == True and args.slack_webhook is not None:
+        work.post_to_slack(args.slack_webhook)
 
 
 if __name__ == "__main__":
