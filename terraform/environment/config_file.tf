0b6826d5b625f8b39624d34fb6a84b1bc45bff80
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-03-24 12:57:45 +0000

    pass security group names in config file

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -5,14 +5,14 @@ resource "local_file" "cluster_config" {
 
 locals {
   cluster_config = {
-    cluster_name = aws_ecs_cluster.use-an-lpa.name
-    account_id   = local.account.account_id
-
-    actor_lpa_codes_table = aws_dynamodb_table.actor_codes_table.name
-    viewer_codes_table    = aws_dynamodb_table.viewer_codes_table.name
     actor_users_table     = aws_dynamodb_table.actor_users_table.name
-
-    actor_fqdn  = aws_route53_record.actor-use-my-lpa.fqdn
-    viewer_fqdn = aws_route53_record.viewer-use-my-lpa.fqdn
+    cluster_name                           = aws_ecs_cluster.use-an-lpa.name
+    account_id                             = local.account.account_id
+    actor_lpa_codes_table                  = aws_dynamodb_table.actor_codes_table.name
+    viewer_codes_table                     = aws_dynamodb_table.viewer_codes_table.name
+    actor_fqdn                             = aws_route53_record.actor-use-my-lpa.fqdn
+    viewer_fqdn                            = aws_route53_record.viewer-use-my-lpa.fqdn
+    viewer_load_balancer_security_group_id = aws_security_group.viewer_loadbalancer.name
+    actor_load_balancer_security_group_id  = aws_security_group.actor_loadbalancer.name
   }
 }
