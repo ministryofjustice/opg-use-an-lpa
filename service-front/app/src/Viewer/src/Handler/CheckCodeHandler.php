198b024b4d1d77a91b277dcf2a8c665cbe15ee50
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-04-06 22:13:53 +0100

    Configure the viewer code failure rate limiter

--- service-front/app/src/Viewer/src/Handler/CheckCodeHandler.php
+++ service-front/app/src/Viewer/src/Handler/CheckCodeHandler.php
@@ -8,8 +8,10 @@ use ArrayObject;
 use Common\Exception\ApiException;
 use Common\Handler\AbstractHandler;
 use Common\Handler\Traits\Session as SessionTrait;
+use Common\Middleware\Security\UserIdentificationMiddleware;
 use Common\Middleware\Session\SessionTimeoutException;
 use Common\Service\Lpa\LpaService;
+use Common\Service\Security\RateLimitService;
 use DateTime;
 use Fig\Http\Message\StatusCodeInterface;
 use Laminas\Diactoros\Response\HtmlResponse;
@@ -30,6 +32,11 @@ class CheckCodeHandler extends AbstractHandler
     private $lpaService;
 
     /**
+     * @var RateLimitService
+     */
+    private $failureRateLimiter;
+
+    /**
      * EnterCodeHandler constructor.
      * @param TemplateRendererInterface $renderer
      * @param UrlHelper $urlHelper
@@ -38,11 +45,13 @@ class CheckCodeHandler extends AbstractHandler
     public function __construct(
         TemplateRendererInterface $renderer,
         UrlHelper $urlHelper,
-        LpaService $lpaService
+        LpaService $lpaService,
+        RateLimitService $failureRateLimiter
     ) {
         parent::__construct($renderer, $urlHelper);
 
         $this->lpaService = $lpaService;
+        $this->failureRateLimiter = $failureRateLimiter;
     }
 
     /**
@@ -81,6 +90,7 @@ class CheckCodeHandler extends AbstractHandler
                 }
             }
 
+            $this->failureRateLimiter->limit($request->getAttribute(UserIdentificationMiddleware::IDENTIFY_ATTRIBUTE));
             return new HtmlResponse($this->renderer->render('viewer::check-code-not-found'));
         }
 
