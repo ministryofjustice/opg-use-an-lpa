346e1b3c914ffb526066ff51329c2df4a830507b
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-04-08 11:50:24 +0100

    Add new parts to uid service factory test for configurable hash

--- service-front/app/test/CommonTest/Service/Security/UserIdentificationServiceFactoryTest.php
+++ service-front/app/test/CommonTest/Service/Security/UserIdentificationServiceFactoryTest.php
@@ -8,6 +8,7 @@ use Common\Service\Security\UserIdentificationService;
 use Common\Service\Security\UserIdentificationServiceFactory;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
+use RuntimeException;
 
 class UserIdentificationServiceFactoryTest extends TestCase
 {
@@ -15,11 +16,55 @@ class UserIdentificationServiceFactoryTest extends TestCase
     public function it_creates_a_configured_uid_service()
     {
         $containerProphecy = $this->prophesize(ContainerInterface::class);
+        $containerProphecy
+            ->get('config')
+            ->shouldBeCalled()
+            ->willReturn(
+                [
+                    'security' => [
+                        'uid_hash_salt' => 'a_secure_hash_from_the_environment',
+                    ],
+                ]
+            );
 
         $factory = new UserIdentificationServiceFactory();
 
         $instance = $factory($containerProphecy->reveal());
 
         $this->assertInstanceOf(UserIdentificationService::class, $instance);
+    }
+
+    /** @test */
+    public function it_throws_an_exception_if_security_config_not_found()
+    {
+        $containerProphecy = $this->prophesize(ContainerInterface::class);
+        $containerProphecy
+            ->get('config')
+            ->shouldBeCalled()
+            ->willReturn([]);
+
+        $factory = new UserIdentificationServiceFactory();
+
+        $this->expectException(RuntimeException::class);
+        $instance = $factory($containerProphecy->reveal());
+    }
+
+    /** @test */
+    public function it_throws_an_exception_if_salt_config_not_found()
+    {
+        $containerProphecy = $this->prophesize(ContainerInterface::class);
+        $containerProphecy
+            ->get('config')
+            ->shouldBeCalled()
+            ->willReturn(
+                [
+                    'security' => [],
+                ]
+            );
+
+        $factory = new UserIdentificationServiceFactory();
+
+        $this->expectException(RuntimeException::class);
+        $instance = $factory($containerProphecy->reveal());
     }
 }
