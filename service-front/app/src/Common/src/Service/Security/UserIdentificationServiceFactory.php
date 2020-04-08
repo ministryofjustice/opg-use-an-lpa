1c433b3740b16f27d6fb477a00ab50c1a49e3030
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-04-08 13:22:51 +0100

    Fix up service to load from config as dictated by the tests

--- service-front/app/src/Common/src/Service/Security/UserIdentificationServiceFactory.php
+++ service-front/app/src/Common/src/Service/Security/UserIdentificationServiceFactory.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace Common\Service\Security;
 
+use Psr\Container\ContainerInterface;
+use RuntimeException;
+
 class UserIdentificationServiceFactory
 {
-    public function __invoke(): UserIdentificationService
+    public function __invoke(ContainerInterface $container): UserIdentificationService
     {
-        return new UserIdentificationService('salt_to_come_from_config');
+        $config = $container->get('config');
+
+        if (!isset($config['security'])) {
+            throw new RuntimeException('Missing security configuration');
+        }
+
+        if (!isset($config['security']['uid_hash_salt'])) {
+            throw new RuntimeException('Missing hashing salt for user identification');
+        }
+
+        return new UserIdentificationService($config['security']['uid_hash_salt']);
     }
 }
