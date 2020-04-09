9d6783d220ccfd762e2204d0783369edc018bf39
SeemaMenon <seema.workspace@gmail.com>
2020-04-09 15:49:57 +0100

    generate a container name from two parameters

---
+++ pipeline_scripts/set_environment_variables/set_container_name.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+function name_image() {
+  branch=$1
+  branch=${branch//-}
+  branch=${branch//_}
+  branch=${branch//\/}
+  branch=${branch:0:13}
+  branch=$(echo $branch | tr '[:upper:]' '[:lower:]')
+  short_hash=${2:0:7}
+  echo "$branch-$short_hash"
+}
+
+echo $(name_image $1 $2)
