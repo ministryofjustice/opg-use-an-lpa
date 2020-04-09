a0cc95cea98bc7dc59ebd9d219101b6ba774e065
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-04-09 17:19:54 +0100

    create a cloudwatch alarm for viewer 5xx errors

---
+++ terraform/environment/cloudwatch_alarms.tf
@@ -0,0 +1,21 @@
+resource "aws_cloudwatch_metric_alarm" "viewer_5xx_errors" {
+  actions_enabled = true
+  # alarm_actions       = [aws_sns_topic.cloudwatch_topic.arn]
+  alarm_description   = "Number of 5XX Errors returned to viewer users for ${local.environment}"
+  alarm_name          = "${local.environment}-viewer-5XX-errors"
+  comparison_operator = "GreaterThanThreshold"
+  datapoints_to_alarm = 3
+  dimensions = {
+    "LoadBalancer" = trimprefix(split(":", aws_lb.viewer.arn)[5], "loadbalancer/")
+  }
+  evaluation_periods        = 3
+  insufficient_data_actions = []
+  metric_name               = "HTTPCode_Target_5XX_Count"
+  namespace                 = "AWS/ApplicationELB"
+  # ok_actions                = [aws_sns_topic.cloudwatch_topic.arn]
+  period             = 60
+  statistic          = "Sum"
+  tags               = {}
+  threshold          = 5
+  treat_missing_data = "notBreaching"
+}
