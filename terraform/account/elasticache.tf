292798adc278adf2b12b9596cf6cea911026c9f1
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-04-15 11:56:13 +0100

    Add elasticache terraform configuration

---
+++ terraform/account/elasticache.tf
@@ -0,0 +1,24 @@
+resource "aws_elasticache_subnet_group" "brute_force_cache_subnet" {
+  name       = "brute-force-cache-subnet"
+  subnet_ids = [aws_subnet.private.id]
+
+  tags = merge(
+    local.default_tags,
+    {
+      "Name" = "private"
+    },
+  )
+}
+
+resource "aws_elasticache_cluster" "brute_force_cache" {
+  cluster_id           = "brute_force_cache"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis5.0"
+  engine_version       = "5.0.6"
+
+  subnet_group_name = aws_elasticache_subnet_group.brute_force_cache_subnet.name
+
+  tags = local.default_tags
+}
