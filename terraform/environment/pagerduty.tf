441d2b9683ede6c79345f6c16aa15b0c2574c9c2
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-04-20 13:25:50 +0100

    Include environment name pagerduty cloudwatch integration name

--- terraform/environment/pagerduty.tf
+++ terraform/environment/pagerduty.tf
@@ -7,7 +7,7 @@ data "pagerduty_vendor" "cloudwatch" {
 }
 
 resource "pagerduty_service_integration" "cloudwatch_integration" {
-  name    = data.pagerduty_vendor.cloudwatch.name
+  name    = "${data.pagerduty_vendor.cloudwatch.name} ${local.environment}"
   service = data.pagerduty_service.pagerduty.id
   vendor  = data.pagerduty_vendor.cloudwatch.id
 }
