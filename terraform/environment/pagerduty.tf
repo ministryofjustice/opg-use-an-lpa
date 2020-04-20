2c39b086f775cd8e17275fddb550289e4dfc754d
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-04-20 14:49:48 +0100

    set up sns topic and subscription for pagerduty

--- terraform/environment/pagerduty.tf
+++ terraform/environment/pagerduty.tf
@@ -11,3 +11,14 @@ resource "pagerduty_service_integration" "cloudwatch_integration" {
   service = data.pagerduty_service.pagerduty.id
   vendor  = data.pagerduty_vendor.cloudwatch.id
 }
+
+resource "aws_sns_topic" "cloudwatch_to_pagerduty" {
+  name = "CloudWatch-to-PagerDuty-${local.environment}"
+}
+
+resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription" {
+  topic_arn              = aws_sns_topic.cloudwatch_topic.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.cloudwatch_integration.integration_key}/enqueue"
+}
