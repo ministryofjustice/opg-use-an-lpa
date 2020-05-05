7b2b28c7b75d4627637cc5bf935608e13a46b393
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-05-05 11:33:48 +0100

    add GOARCH=amd64 to go build script

--- scripts/pipeline/workspace_cleanup/go_build_workspace_tools.sh
+++ scripts/pipeline/workspace_cleanup/go_build_workspace_tools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-env GOOS=linux go build -o get_workspaces_linux -a ./get_workspaces/
-env GOOS=linux go build -o put_workspace_linux -a ./put_workspace/
+env GOOS=linux GOARCH=amd64 go build -o get_workspaces_linux -a ./get_workspaces/
+env GOOS=linux GOARCH=amd64 go build -o put_workspace_linux -a ./put_workspace/
 
 env GOOS=darwin go build -o get_workspaces_mac -a ./get_workspaces/
 env GOOS=darwin go build -o put_workspace_mac -a ./put_workspace/
