e0dca8efefd092dc98b8e76cd28e9de119cb5f07
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-05-06 11:24:16 +0100

    created api route for change email and api handler

---
+++ service-api/app/src/App/src/Handler/ChangeEmailHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use App\Exception\BadRequestException;
+use App\Service\User\UserService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ChangeEmailHandler implements RequestHandlerInterface
+{
+    /**
+     * @var UserService
+     */
+    private $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     * @throws \Exception
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $requestData = $request->getParsedBody();
+
+        if (!isset($requestData['user-id'])) {
+            throw new BadRequestException('User Id must be provided');
+        }
+
+        if (!isset($requestData['new-email'])) {
+            throw new BadRequestException('New email address must be provided');
+        }
+
+        if (!isset($requestData['password'])) {
+            throw new BadRequestException('Current password must be provided');
+        }
+
+
+    }
+}
