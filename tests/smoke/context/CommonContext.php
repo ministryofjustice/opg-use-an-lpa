6be6e9470caf59ae781bd414060a79e3a606a543
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-05-06 13:51:47 +0100

    Refactor applicable test steps to CommonContext.php

--- tests/smoke/context/CommonContext.php
+++ tests/smoke/context/CommonContext.php
@@ -6,6 +6,8 @@ namespace Test\Context;
 
 use Behat\Behat\Context\Context;
 use Behat\Mink\Exception\ExpectationException;
+use DMore\ChromeDriver\ChromeDriver;
+use Fig\Http\Message\StatusCodeInterface;
 
 /**
  * Class CommonContext
@@ -19,11 +21,49 @@ class CommonContext implements Context
     use BaseContextTrait;
 
     /**
+     * @Given I access the service homepage
+     * @Given I access the viewer service
+     * @Given I access the actor service
+     */
+    public function iAccessTheServiceHomepage(): void
+    {
+        $this->ui->iAmOnHomepage();
+    }
+
+    /**
+     * @Given I access the service homepage insecurely
+     * @Given I access the viewer service insecurely
+     * @Given I access the actor service insecurely
+     */
+    public function iAccessTheViewerServiceInsecurely(): void
+    {
+        $baseUrlHost = parse_url($this->ui->getMinkParameter('base_url'), PHP_URL_HOST);
+        $insecureUrl = sprintf('http://%s/', $baseUrlHost);
+
+        $this->ui->visit($insecureUrl);
+    }
+
+    /**
      * @Given I fetch the healthcheck endpoint
      */
     public function iFetchTheHealthcheckEndpoint(): void
     {
         $this->ui->visit('/healthcheck');
+    }
+
+    /**
+     * @Then the service homepage should be shown securely
+     * @Then the viewer service homepage should be shown securely
+     * @Then the actor service homepage should be shown securely
+     */
+    public function theViewerServiceHomepageShouldBeShownSecurely(): void
+    {
+        $this->ui->assertResponseStatus(StatusCodeInterface::STATUS_OK);
+
+        $baseUrlHost = parse_url($this->ui->getMinkParameter('base_url'), PHP_URL_HOST);
+        $expectedUrl = sprintf('https://%s/', $baseUrlHost);
+
+        $this->assertExactUrl($expectedUrl);
     }
 
     /**
