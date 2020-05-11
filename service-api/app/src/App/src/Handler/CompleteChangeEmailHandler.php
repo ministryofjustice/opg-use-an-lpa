92bc232a5ff3b7b4d172817919864fda6d74781f
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-05-11 12:22:08 +0100

    Created complete email change api handler

---
+++ service-api/app/src/App/src/Handler/CompleteChangeEmailHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use App\Exception\BadRequestException;
+use App\Service\User\UserService;
+use Laminas\Diactoros\Response\JsonResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class CompleteChangeEmailHandler implements RequestHandlerInterface
+{
+    /**
+     * @var UserService
+     */
+    private $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     * @throws \Exception
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $requestData = $request->getParsedBody();
+
+        if (!isset($requestData['verification_token'])) {
+            throw new BadRequestException('Email verification token must be provided');
+        }
+
+
+        return new JsonResponse([]);
+    }
+}
