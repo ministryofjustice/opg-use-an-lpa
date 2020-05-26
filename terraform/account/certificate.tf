a2808a3c938186d70b964bb02feb3310f98296b1
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-05-26 10:14:08 +0100

    remove actor certificate

--- terraform/account/certificate.tf
+++ terraform/account/certificate.tf
@@ -3,30 +3,6 @@ data "aws_route53_zone" "opg_service_justice_gov_uk" {
   name     = "opg.service.justice.gov.uk"
 }
 
-
-//------------------------
-// Actors Certificates
-
-resource "aws_route53_record" "certificate_validation_actor" {
-  provider = aws.management
-  name     = aws_acm_certificate.certificate_actor.domain_validation_options[0].resource_record_name
-  type     = aws_acm_certificate.certificate_actor.domain_validation_options[0].resource_record_type
-  zone_id  = data.aws_route53_zone.opg_service_justice_gov_uk.zone_id
-  records  = [aws_acm_certificate.certificate_actor.domain_validation_options[0].resource_record_value]
-  ttl      = 60
-}
-
-resource "aws_acm_certificate_validation" "certificate_actor" {
-  certificate_arn         = aws_acm_certificate.certificate_actor.arn
-  validation_record_fqdns = [aws_route53_record.certificate_validation_actor.fqdn]
-}
-
-resource "aws_acm_certificate" "certificate_actor" {
-  domain_name       = "${local.dev_wildcard}actor.${local.dns_namespace_acc}use-an-lpa.opg.service.justice.gov.uk"
-  validation_method = "DNS"
-}
-
-
 //------------------------
 // View Certificates
 
