d232b4ea892b0190bcff83f3b5b9be203a7681e8
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-06-01 18:18:27 +0100

    Test the session cleaning middleware.

---
+++ service-front/app/test/CommonTest/Middleware/Session/SessionExpiredAttributeWhitelistMiddlewareTest.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommonTest\Middleware\Session;
+
+use Common\Middleware\Session\SessionExpiredAttributeWhitelistMiddleware;
+use Common\Service\Session\EncryptedCookiePersistence;
+use DateTime;
+use Mezzio\Session\Session;
+use Mezzio\Session\SessionMiddleware;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class SessionExpiredAttributeWhitelistMiddlewareTest extends TestCase
+{
+    /** @test */
+    public function it_correctly_handles_request_with_no_session(): void
+    {
+        $requestProphecy = $this->prophesize(ServerRequestInterface::class);
+        $requestProphecy
+            ->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE)
+            ->shouldBeCalled()
+            ->willReturn(null);
+
+        $delegateProphecy = $this->prophesize(RequestHandlerInterface::class);
+        $delegateProphecy
+            ->handle($requestProphecy->reveal())
+            ->shouldBeCalled()
+            ->willReturn($this->prophesize(ResponseInterface::class)->reveal());
+
+        $sem = new SessionExpiredAttributeWhitelistMiddleware();
+
+        $sem->process($requestProphecy->reveal(), $delegateProphecy->reveal());
+    }
+
+    /** @test */
+    public function it_correctly_handles_an_non_expired_session(): void
+    {
+        $sessionProphecy = $this->prophesize(Session::class);
+        $sessionProphecy
+            ->get(EncryptedCookiePersistence::SESSION_EXPIRED_KEY)
+            ->shouldBeCalled()
+            ->willReturn(null);
+
+        $requestProphecy = $this->prophesize(ServerRequestInterface::class);
+        $requestProphecy
+            ->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE)
+            ->shouldBeCalled()
+            ->willReturn($sessionProphecy->reveal());
+
+        $delegateProphecy = $this->prophesize(RequestHandlerInterface::class);
+        $delegateProphecy
+            ->handle($requestProphecy->reveal())
+            ->shouldBeCalled()
+            ->willReturn($this->prophesize(ResponseInterface::class)->reveal());
+
+        $sem = new SessionExpiredAttributeWhitelistMiddleware();
+
+        $sem->process($requestProphecy->reveal(), $delegateProphecy->reveal());
+    }
+
+    /** @test */
+    public function it_strips_session_values_with_correct_whitelisting(): void
+    {
+        $sessionData = [
+            'string' => 'one',
+            'bool' => true,
+            'DateTime' => new DateTime(),
+            EncryptedCookiePersistence::SESSION_TIME_KEY => time(),
+            EncryptedCookiePersistence::SESSION_EXPIRED_KEY => true
+        ];
+
+        $sessionProphecy = $this->prophesize(Session::class);
+        $sessionProphecy
+            ->get(EncryptedCookiePersistence::SESSION_EXPIRED_KEY)
+            ->shouldBeCalled()
+            ->willReturn(true);
+        $sessionProphecy
+            ->toArray()
+            ->shouldBeCalled()
+            ->willReturn($sessionData);
+        $sessionProphecy
+            ->unset(Argument::type('string'))
+            ->shouldBeCalledTimes(4); // SESSION_TIME_KEY is whitelisted
+
+        $requestProphecy = $this->prophesize(ServerRequestInterface::class);
+        $requestProphecy
+            ->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE)
+            ->shouldBeCalled()
+            ->willReturn($sessionProphecy->reveal());
+
+        $delegateProphecy = $this->prophesize(RequestHandlerInterface::class);
+        $delegateProphecy
+            ->handle($requestProphecy->reveal())
+            ->shouldBeCalled()
+            ->willReturn($this->prophesize(ResponseInterface::class)->reveal());
+
+        $sem = new SessionExpiredAttributeWhitelistMiddleware();
+
+        $sem->process($requestProphecy->reveal(), $delegateProphecy->reveal());
+    }
+}
