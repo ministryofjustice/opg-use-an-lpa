8b647e4e4e7bceb2bb449f3c4506344dd7b79f51
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-06-08 17:31:57 +0100

    Add some comments to the new exceptions

--- service-api/app/src/App/src/Exception/ActorCodeMarkAsUsedException.php
+++ service-api/app/src/App/src/Exception/ActorCodeMarkAsUsedException.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace App\Exception;
 
+/**
+ * Class ActorCodeMarkAsUsedException
+ *
+ * Thrown when an upstream service fails to mark an actor code as used.
+ *
+ * @package App\Exception
+ */
 class ActorCodeMarkAsUsedException extends \Exception
 {
-
 }
--- service-api/app/src/App/src/Exception/ActorCodeValidationException.php
+++ service-api/app/src/App/src/Exception/ActorCodeValidationException.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace App\Exception;
 
+/**
+ * Class ActorCodeValidationException
+ *
+ * Thrown when a set of actor code credentials (incl. DoB and LPA uId) fail to validate.
+ *
+ * @package App\Exception
+ */
 class ActorCodeValidationException extends \Exception
 {
-
 }
