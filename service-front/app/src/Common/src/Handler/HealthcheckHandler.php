b6caa481c1e524b8658fde1d79b9a98828091c43
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-06-15 16:07:32 +0100

    created healthcheck for lpa codes api and altered dynamo check to users table

--- service-front/app/src/Common/src/Handler/HealthcheckHandler.php
+++ service-front/app/src/Common/src/Handler/HealthcheckHandler.php
@@ -43,21 +43,21 @@ class HealthcheckHandler implements RequestHandlerInterface
             "healthy" => $this->isHealthy(),
             "version" => $this->version,
             "dependencies" => [
-                "api" => $this->checkApiEndpoint()
+                "api" => $this->checkDependencyEndpoints()
             ]
         ]);
     }
 
     protected function isHealthy(): bool
     {
-        if ($this->checkApiEndpoint()['healthy']) {
+        if ($this->checkDependencyEndpoints()['healthy']) {
             return true;
         } else {
             return false;
         }
     }
 
-    protected function checkApiEndpoint(): array
+    protected function checkDependencyEndpoints(): array
     {
         $data = [];
 
