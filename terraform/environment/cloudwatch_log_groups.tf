c81394401bd2010693dfb6a13e2648d78b51dd3b
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-06-25 13:55:49 +0100

    fix reference to log retention

--- terraform/environment/cloudwatch_log_groups.tf
+++ terraform/environment/cloudwatch_log_groups.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "application_logs" {
   name              = "${local.environment}_ecs_logs"
-  retention_in_days = local.account.retention_in_days
+  retention_in_days = local.account.log_retention_in_days
 
   tags = merge(
     local.default_tags,
