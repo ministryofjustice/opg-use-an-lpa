8f63b0dab0e56709852fca14761d7099168026aa
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-06-25 13:36:48 +0100

    pull metrics from environment logstream

--- terraform/environment/cloudwatch_metrics.tf
+++ terraform/environment/cloudwatch_metrics.tf
@@ -1,11 +1,11 @@
 resource "aws_cloudwatch_log_metric_filter" "account_creation" {
-  name           = "account_creation_messages"
+  name           = "${local.environment}_account_creation_messages"
   pattern        = "{ $.message = \"Account with Id*created using email*\" }"
-  log_group_name = data.aws_cloudwatch_log_group.use-an-lpa.name
+  log_group_name = aws_cloudwatch_log_group.application_logs.name
 
   metric_transformation {
-    name          = "account_creation_count"
-    namespace     = "account_creation"
+    name          = "AccountCreateEventCount"
+    namespace     = "${local.environment}_account_creation"
     value         = "1"
     default_value = "0"
   }
