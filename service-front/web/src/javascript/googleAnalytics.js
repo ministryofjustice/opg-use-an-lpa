6b5a7ecb1005b86cc797be4c1588c76be4453750
John Nolan <john.nolan@digital.justice.gov.uk>
2020-06-26 16:13:21 +0100

    Moved gtag to window for global access

--- service-front/web/src/javascript/googleAnalytics.js
+++ service-front/web/src/javascript/googleAnalytics.js
@@ -18,24 +18,23 @@ export default class GoogleAnalytics {
         //write a method that listens to google events
         // check data presence
         //sanitise it and send it
-
         let s = document.createElement('script');
         s.type = 'text/javascript';
         s.src = `https://www.googletagmanager.com/gtag/js?id=${this.analyticsId}`;
         document.getElementsByTagName('head')[0].appendChild(s);
         window.dataLayer = window.dataLayer || [];
-        this.gtag('js', new Date());
-        this.gtag('config', this.analyticsId);
+        window.gtag = function()
+        {
+            window.dataLayer.push(arguments);
+        }
+        window.gtag('js', new Date());
+        window.gtag('config', this.analyticsId);
     }
 
-    gtag(arguments){
-        window.dataLayer.push(arguments);
-    }
-
-    trackEvents(action, category, label, value)
+    trackEvent(action, category, label, value)
     {
         console.log({action, category, label, value});
-        this.gtag('event', action, {
+        window.gtag('event', action, {
                     'event_category': category,
                     'event_label': label,
                     'value': value
