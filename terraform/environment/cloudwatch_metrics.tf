6a0f600d6be4b8326c70164b68dc1fb13e017e2b
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-06-26 12:33:55 +0100

    create a metric for all event_codes

--- terraform/environment/cloudwatch_metrics.tf
+++ terraform/environment/cloudwatch_metrics.tf
@@ -1,11 +1,22 @@
-resource "aws_cloudwatch_log_metric_filter" "account_creation" {
-  name           = "${local.environment}_account_creation_messages"
-  pattern        = "{ $.context.event_code = \"ACCOUNT_ACTIVATED\" }"
+locals {
+  event_codes = [
+    "ACCOUNT_ACTIVATED",
+    "ACCOUNT_CREATED",
+    "ACCOUNT_DELETED",
+    "IDENTITY_HASH_CHANGE",
+    "SHARE_CODE_NOT_FOUND",
+  ]
+}
+
+resource "aws_cloudwatch_log_metric_filter" "log_metric" {
+  for_each       = toset(local.event_codes)
+  name           = "${local.environment}_${lower(each.value)}"
+  pattern        = "{ $.context.event_code = \"${each.value}\" }"
   log_group_name = aws_cloudwatch_log_group.application_logs.name
 
   metric_transformation {
-    name          = "AccountCreateEventCount"
-    namespace     = "${local.environment}_account_creation"
+    name          = "${lower(each.value)}_event"
+    namespace     = "${local.environment}_events"
     value         = "1"
     default_value = "0"
   }
