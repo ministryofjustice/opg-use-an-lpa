a3fce46005a86030166c26a9f36d9a6e8dfaa4b0
John Nolan <john.nolan@digital.justice.gov.uk>
2020-06-29 09:36:16 +0100

    Add sanitise data for analytics code

--- service-front/web/src/javascript/googleAnalytics.js
+++ service-front/web/src/javascript/googleAnalytics.js
@@ -17,7 +17,7 @@ export default class GoogleAnalytics {
 
         //write a method that listens to google events
         // check data presence
-        //sanitise it and send it
+        //sanitise it and send it - Done
         let s = document.createElement('script');
         s.type = 'text/javascript';
         s.src = `https://www.googletagmanager.com/gtag/js?id=${this.analyticsId}`;
@@ -34,14 +34,28 @@ export default class GoogleAnalytics {
     trackEvent(action, category, label, value)
     {
         console.log({action, category, label, value});
-        window.gtag('event', action, {
-                    'event_category': category,
-                    'event_label': label,
-                    'value': value
-        }
-    );
-
-
+        window.gtag('event', this._sanitiseData(action), {
+                    'event_category': this._sanitiseData(category),
+                    'event_label': this._sanitiseData(label),
+                    'value': this._sanitiseData(value)
+            }
+        );
     }
 
+    _sanitiseData(data) {
+        const sanitisedDataRegex = [
+            /[^\\s=/?&]+(?:@|%40)[^\\s=/?&]+/g, // Email
+            /[A-PR-UWYZ][A-HJ-Z]?[0-9][0-9A-HJKMNPR-Y]?(?:[\\s+]|%20)*[0-9][ABD-HJLNPQ-Z]{2}/gi, // Postcode
+            /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/g, // Date
+            /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/g, // Telephone
+        ];
+
+        let dataCleansed = data;
+
+        for (let i = 0; i < sanitisedDataRegex.length; i++) {
+            dataCleansed = dataCleansed.replace(sanitisedDataRegex[i], '[sanitised]')
+        }
+
+        return dataCleansed;
+    }
 }
