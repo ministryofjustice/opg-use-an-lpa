48f8784da7a4f8c4b3c1c651a2187443079ce1ef
William Falconer <williamfalconeruk@users.noreply.github.com>
2020-06-30 09:11:12 +0100

    UML-875 Add a trim filter and php unit tests - also minor tidy up (#411)
    * Add a trim filter and php unit tests - also minor tidy up
    
    * oops rename test!
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>

--- service-front/app/test/CommonTest/Form/Fieldset/DobTrimFilterTest.php
+++ service-front/app/test/CommonTest/Form/Fieldset/DobTrimFilterTest.php
@@ -4,72 +4,74 @@ declare(strict_types=1);
 
 namespace CommonTest\Form\Fieldset;
 
-use Common\Form\Fieldset\DatePrefixFilter;
+use Common\Form\Fieldset\DateTrimFilter;
 use PHPUnit\Framework\TestCase;
 
-class DobPrefixFilterTest extends TestCase
+class DobTrimFilterTest extends TestCase
 {
     /**
-     * @var DatePrefixFilter
+     * @var DateTrimFilter
      */
-    private DatePrefixFilter $filter;
+    private DateTrimFilter $filter;
 
     public function setUp()
     {
-        $this->filter = new DatePrefixFilter();
+        $this->filter = new DateTrimFilter();
     }
 
     /**
      * @dataProvider validFormatProvider
+     * @param array $expected
+     * @param array $dob
      */
-    public function testIsDobFormattedWithLeadingZeroes(array $expected, array $dob)
+    public function testIsDobTrimmed(array $expected, array $dob)
     {
         $formattedDate = $this->filter->filter($expected);
         $this->assertEquals($formattedDate, $dob);
     }
 
-    public function validFormatProvider() : array
+    public function validFormatProvider(): array
     {
         return [
             [
                 [
-                    'day' => '1',
-                    'month' => '10',
-                    'year' => '1980',
+                    'day' => ' 1',
+                    'month' => ' 10',
+                    'year' => ' 1980',
                 ],
                 [
-                    'day' => '01',
+                    'day' => '1',
                     'month' => '10',
                     'year' => '1980',
                 ]
             ],
             [
+                [
+                    'day' => '11 ',
+                    'month' => '8 ',
+                    'year' => '1980 ',
+                ],
                 [
                     'day' => '11',
                     'month' => '8',
                     'year' => '1980',
-                ],
-                [
-                    'day' => '11',
-                    'month' => '08',
-                    'year' => '1980',
                 ]
             ],
             [
+                [
+                    'day' => ' 1 ',
+                    'month' => ' 1 ',
+                    'year' => ' 1980 ',
+                ],
                 [
                     'day' => '1',
                     'month' => '1',
                     'year' => '1980',
-                ],
-                [
-                    'day' => '01',
-                    'month' => '01',
-                    'year' => '1980',
                 ]
             ],
             [
                 [
-                    'day' => '11',
+                    'day' => ' 11',
                     'month' => '10',
                     'year' => '1980',
                 ],
