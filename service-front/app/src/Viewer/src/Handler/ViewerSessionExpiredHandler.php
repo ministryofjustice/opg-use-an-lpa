47f992b6006330139c9bf49c0c3e6f3f55488fcc
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-07-02 14:19:35 +0100

    created handler,route and twig for viewer session expiry

---
+++ service-front/app/src/Viewer/src/Handler/ViewerSessionExpiredHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Viewer\Handler;
+
+use Common\Handler\AbstractHandler;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class ViewerSessionExpiredHandler extends AbstractHandler
+{
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return new HtmlResponse($this->renderer->render('viewer::viewer-session-expired'));
+    }
+}
