ac52f664e89636d1360064221662da7da38b3c51
william Falconer <william.falconer@digital.justice.gov,uk>
2020-07-03 09:43:17 +0100

    phase 1 using basic tf replication group setup

--- terraform/account/elasticache.tf
+++ terraform/account/elasticache.tf
@@ -9,6 +9,7 @@ resource "aws_elasticache_subnet_group" "private_subnets" {
   subnet_ids = aws_subnet.private[*].id
 }
 
+
 resource "aws_elasticache_cluster" "brute_force_cache" {
   cluster_id           = "brute-force-cache"
   engine               = "redis"
@@ -21,4 +22,24 @@ resource "aws_elasticache_cluster" "brute_force_cache" {
   security_group_ids = [aws_security_group.brute_force_cache_service.id]
 
   tags = local.default_tags
+}
+
+
+resource "aws_elasticache_replication_group" "brute_force_cache_rg" {
+  automatic_failover_enabled    = true
+  replication_group_id          = "brute-force-cache-rg"
+  replication_group_description = "brute force redis cache replication group"
+  parameter_group_name          = "default.redis5.0"
+  engine_version                = "5.0.6"
+  node_type                     = "cache.t2.micro"
+  engine                        = "redis"
+  number_cache_clusters         = 1
+  transit_encryption_enabled    = true
+  at_rest_encryption_enabled    = true
+
+  subnet_group_name  = aws_elasticache_subnet_group.private_subnets.name
+  security_group_ids = [aws_security_group.brute_force_cache_service.id]
+
+  tags = local.default_tags
+
 }
