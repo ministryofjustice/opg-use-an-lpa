a04e32b2a87d570cb86eab2f471b0b04ffef756e
SeemaMenon <seema.menon@digital.justice.gov.uk>
2020-07-06 16:58:33 +0100

    UML-644 GA setup  (#407)
    * updated to add validation of LPA prior to codegen.
    
    * Add TF and Docker compose entries - no values as yet
    
    * Added extension to get get the config value in twig file heaedr
    
    * Moved gtag to window for global access
    
    * Add sanitise data for analytics code
    
    * Added opt-out GA config
    
    * Only run GA when cookies allow
    
    * Add tests to Google Analytics js
    
    * Added cross domain linker + tests
    
    * Add TF and Docker compose entries - no values as yet
    
    * Added extension to get get the config value in twig file heaedr
    
    * creating new google analytics js
    
    * Moved gtag to window for global access
    
    * Added opt-out GA config
    
    * Only run GA when cookies allow
    
    * Add tests to Google Analytics js
    
    * Added cross domain linker + tests
    
    * added unit test for factory added
    
    * Added set up for external link tracking and added js tests
    
    * Review changes added
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>
    Co-authored-by: William Falconer <williamfalconeruk@users.noreply.github.com>
    Co-authored-by: John Nolan <john.nolan@digital.justice.gov.uk>

---
+++ service-front/web/src/javascript/googleAnalytics.test.js
@@ -0,0 +1,142 @@
+import googleAnalytics from './googleAnalytics';
+
+    const linkList = `
+    <div>
+        <a href="/">Relative URL</a>
+        <a href="http://localhost">HTTP URL</a>
+        <a href="https://localhost">HTTPS URL</a>
+    </div>
+    `;
+
+    describe('given Google Analytics is enabled', () => {
+        let useAnalytics;
+        beforeEach(() => {
+            document.body.innerHTML = linkList;
+            global.dataLayer = [];
+            useAnalytics = new googleAnalytics('UA-12345');
+        });
+
+        /**
+         * Description of data layer 2 and 3 in gtag
+         * [Arguments] {
+          '0': 'event',
+          '1': 'event',
+          '2': {
+                event_category: 'event_category',
+                event_label: 'event_label',
+                value: 'value'
+             }
+            }
+         */
+        test('it fires click events on the 2 external links', () => {
+            const linkSelector = document.querySelectorAll('a');
+            for (let i = 0; i < linkSelector.length; i++) {
+                linkSelector[i].click();
+            }
+
+            expect(global.dataLayer[2][0]).toBe('event');
+            expect(global.dataLayer[2][1]).toBe('click');
+            expect(global.dataLayer[2][2].event_category).toBe('outbound');
+            expect(global.dataLayer[2][2].event_label).toBe('http://localhost/');
+            expect(global.dataLayer[3][0]).toBe('event');
+            expect(global.dataLayer[3][1]).toBe('click');
+            expect(global.dataLayer[3][2].event_category).toBe('outbound');
+            expect(global.dataLayer[3][2].event_label).toBe('https://localhost/');
+            expect(global.dataLayer.length).toEqual(4);
+        });
+
+        /**
+         * Description of data layer 1 in gtag
+         *  'config',
+         *  this.analyticsId, {
+                'linker': {
+                    'domains': ['www.gov.uk']
+            },
+            'transport_type': 'beacon',
+            'anonymize_ip': true,
+            'allow_google_signals': false, //display-features
+            'allow_ad_personalization_signals': false //display-features
+         *
+         */
+    test('it should have the correct config setup', () => {
+        expect(global.dataLayer.length).toEqual(2);
+        expect(global.dataLayer[1][0]).toBe('config');
+        expect(global.dataLayer[1][1]).toBe('UA-12345');
+
+        expect(global.dataLayer[1][2].linker).not.toBeUndefined();
+        expect(global.dataLayer[1][2].linker.domains.length).toBe(1);
+        expect(global.dataLayer[1][2].linker.domains[0]).toBe('www.gov.uk');
+
+        expect(global.dataLayer[1][2].anonymize_ip).not.toBeUndefined();
+        expect(global.dataLayer[1][2].anonymize_ip).toBe(true);
+
+        expect(global.dataLayer[1][2].transport_type).not.toBeUndefined();
+        expect(global.dataLayer[1][2].transport_type).toBe('beacon');
+
+        expect(global.dataLayer[1][2].allow_google_signals).not.toBeUndefined();
+        expect(global.dataLayer[1][2].allow_google_signals).toBe(false);
+
+        expect(global.dataLayer[1][2].allow_ad_personalization_signals).not.toBeUndefined();
+        expect(global.dataLayer[1][2].allow_ad_personalization_signals).toBe(false);
+    });
+
+        /**
+         * Description of data layer 2 in gtag
+         * [Arguments] {
+          '0': 'event',
+          '1': 'event',
+          '2': {
+                event_category: 'event_category',
+                event_label: 'event_label',
+                value: 'value'
+             }
+            }
+         */
+    test('it should fire events correctly', () => {
+        useAnalytics.trackEvent('event', 'event_category', 'event_label', 'value')
+
+        console.log(global.dataLayer)
+        expect(global.dataLayer.length).toEqual(3);
+        expect(global.dataLayer[2]).not.toBeUndefined();
+        expect(global.dataLayer[2][0]).toBe('event');
+        expect(global.dataLayer[2][1]).toBe('event');
+
+        expect(global.dataLayer[2][2].event_category).not.toBeUndefined();
+        expect(global.dataLayer[2][2].event_category).toBe('event_category');
+
+        expect(global.dataLayer[2][2].event_label).not.toBeUndefined();
+        expect(global.dataLayer[2][2].event_label).toBe('event_label');
+
+        expect(global.dataLayer[2][2].value).not.toBeUndefined();
+        expect(global.dataLayer[2][2].value).toBe('value');
+    });
+
+        /**
+         * Description of data layer 2 in gtag
+         * [Arguments] {
+          '0': 'event',
+          '1': 'event',
+          '2': {
+                event_category: 'event_category',
+                event_label: 'event_label',
+                value: 'value'
+             }
+            }
+         */
+    test('it should sanitize the data being sent', () => {
+        useAnalytics.trackEvent('test@test.com', '01234567890', 'NG156WL', '28/06/1984')
+        expect(global.dataLayer.length).toEqual(3);
+        expect(global.dataLayer[2]).not.toBeUndefined();
+        expect(global.dataLayer[2][0]).toBe('event');
+        expect(global.dataLayer[2][1]).toBe('[sanitised]');
+
+        expect(global.dataLayer[2][2].event_category).not.toBeUndefined();
+        expect(global.dataLayer[2][2].event_category).toBe('[sanitised]');
+
+        expect(global.dataLayer[2][2].event_label).not.toBeUndefined();
+        expect(global.dataLayer[2][2].event_label).toBe('[sanitised]');
+
+        expect(global.dataLayer[2][2].value).not.toBeUndefined();
+        expect(global.dataLayer[2][2].value).toBe('[sanitised]');
+    });
+});
