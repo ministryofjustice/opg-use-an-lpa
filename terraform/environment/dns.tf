acb42c87f13aa7f3eb937cc87e5795207641fa9f
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-07-13 11:12:55 +0100

    use team facing and public facing resource names

--- terraform/environment/dns.tf
+++ terraform/environment/dns.tf
@@ -21,7 +21,29 @@ resource "aws_service_discovery_private_dns_namespace" "internal" {
 //-------------------------------------------------------------
 // View
 
-resource "aws_route53_record" "viewer-use-my-lpa" {
+resource "aws_route53_record" "public_facing_view_lasting_power_of_attorney" {
+  # view-lasting-power-of-attorney.service.gov.uk
+  provider = aws.management
+  zone_id  = data.aws_route53_zone.live_service_view_lasting_power_of_attorney.zone_id
+  name     = "${local.dns_namespace_env}"
+  type     = "A"
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_lb.viewer.dns_name
+    zone_id                = aws_lb.viewer.zone_id
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "viewer-use-an-lpa-domain" {
+  value = "https://${aws_route53_record.public_facing_view_lasting_power_of_attorney.fqdn}"
+}
+
+resource "aws_route53_record" "team_facing_viewer_use_my_lpa" {
   # view.lastingpowerofattorney.opg.service.justice.gov.uk
   provider = aws.management
   zone_id  = data.aws_route53_zone.opg_service_justice_gov_uk.zone_id
@@ -43,27 +65,7 @@ output "viewer-use-an-lpa-domain" {
   value = "https://${aws_route53_record.viewer-use-my-lpa.fqdn}"
 }
 
-resource "aws_route53_record" "live_service_view_lasting_power_of_attorney" {
-  # view-lasting-power-of-attorney.service.gov.uk
-  provider = aws.management
-  zone_id  = data.aws_route53_zone.live_service_view_lasting_power_of_attorney.zone_id
-  name     = "${local.dns_namespace_env}"
-  type     = "A"
 
-  alias {
-    evaluate_target_health = false
-    name                   = aws_lb.viewer.dns_name
-    zone_id                = aws_lb.viewer.zone_id
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-output "viewer-use-an-lpa-domain" {
-  value = "https://${aws_route53_record.live_service_view_lasting_power_of_attorney.fqdn}"
-}
 
 //-------------------------------------------------------------
 // Use
