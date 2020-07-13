769647ab312956c72bdea01fb14533d1a6390bf3
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-07-13 11:18:31 +0100

    create dns record for use live service URL

--- terraform/environment/dns.tf
+++ terraform/environment/dns.tf
@@ -39,11 +39,11 @@ resource "aws_route53_record" "public_facing_view_lasting_power_of_attorney" {
   }
 }
 
-output "viewer-use-an-lpa-domain" {
+output "public_facing_view_domain" {
   value = "https://${aws_route53_record.public_facing_view_lasting_power_of_attorney.fqdn}"
 }
 
-resource "aws_route53_record" "team_facing_viewer_use_my_lpa" {
+resource "aws_route53_record" "viewer-use-my-lpa" {
   # view.lastingpowerofattorney.opg.service.justice.gov.uk
   provider = aws.management
   zone_id  = data.aws_route53_zone.opg_service_justice_gov_uk.zone_id
@@ -69,6 +69,27 @@ output "viewer-use-an-lpa-domain" {
 
 //-------------------------------------------------------------
 // Use
+
+resource "aws_route53_record" "public_facing_use_lasting_power_of_attorney" {
+  # use-lasting-power-of-attorney.service.gov.uk
+  provider = aws.management
+  zone_id  = data.aws_route53_zone.live_service_use_lasting_power_of_attorney.zone_id
+  name     = "${local.dns_namespace_env}"
+  type     = "A"
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_lb.actor.dns_name
+    zone_id                = aws_lb.actor.zone_id
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "public_facing_use_domain" {
+  value = "https://${aws_route53_record.public_facing_use_lasting_power_of_attorney.fqdn}"
+}
 
 resource "aws_route53_record" "actor-use-my-lpa" {
   # use.lastingpowerofattorney.opg.service.justice.gov.uk
