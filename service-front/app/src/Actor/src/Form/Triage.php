1cb12f07efb9fa0f9bed65569e93fb7a13194152
SeemaMenon <seema.workspace@gmail.com>
2020-07-13 11:50:41 +0100

    Added a triage page on actor side

---
+++ service-front/app/src/Actor/src/Form/Triage.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Actor\Form;
+
+use Application\Form\AbstractCsrfForm;
+use Common\Form\AbstractForm;
+use Mezzio\Csrf\CsrfGuardInterface;
+use Laminas\InputFilter\InputFilterProviderInterface;
+use Laminas\Validator\NotEmpty;
+
+class Triage extends AbstractForm implements InputFilterProviderInterface
+{
+    const FORM_NAME = 'triage';
+
+    /**
+     * Error codes
+     * @const string
+     */
+    const INVALID_LOGIN = 'invalidLogin';
+
+    /**
+     * LpaAdd constructor.
+     * @param CsrfGuardInterface $csrfGuard
+     */
+    public function __construct(CsrfGuardInterface $csrfGuard)
+    {
+        parent::__construct(self::FORM_NAME, $csrfGuard);
+
+        $this->add([
+            'name'       => 'triageEntry',
+            'type'       => 'Radio',
+            'options'    => [
+                'value_options' => [
+                    'yes' => 'yes',
+                    'no' => 'no',
+                ]
+            ]
+        ]);
+    }
+
+    public function getInputFilterSpecification(): array
+    {
+        return [
+            'triageEntry' => [
+                'required'   => true,
+                'validators' => [
+                    [
+                        'name'                   => NotEmpty::class,
+                        'break_chain_on_failure' => true,
+                        'options'                => [
+                            'messages'           => [
+                                NotEmpty::IS_EMPTY => 'Select yes if you have a Use a lasting power of attorney account',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
