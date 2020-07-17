c7b47bb4ea587f0cd069b8abb2192d1b818b5f0c
william Falconer <william.falconer@digital.justice.gov,uk>
2020-07-17 11:19:28 +0100

    initial fix - check state of user in login on actor.

--- service-front/app/src/Actor/src/Handler/ActorTriagePageHandler.php
+++ service-front/app/src/Actor/src/Handler/ActorTriagePageHandler.php
@@ -8,6 +8,10 @@ use Actor\Form\Triage;
 use Common\Handler\AbstractHandler;
 use Common\Handler\Traits\CsrfGuard;
 use Common\Handler\CsrfGuardAware;
+use Common\Handler\Traits\User;
+use Common\Handler\UserAware;
+use Mezzio\Authentication\AuthenticationInterface;
+use Mezzio\Authentication\UserInterface;
 use Mezzio\Helper\ServerUrlHelper;
 use Psr\Http\Message\{ResponseInterface, ServerRequestInterface};
 use Laminas\Diactoros\Response\HtmlResponse;
@@ -15,14 +19,14 @@ use Laminas\Diactoros\Response\RedirectResponse;
 use Mezzio\Template\TemplateRendererInterface;
 use Mezzio\Helper\UrlHelper;
 
-
 /**
  * Class ActorTriagePageHandler
  * @package Actor\Handler
  * @codeCoverageIgnore
  */
-class ActorTriagePageHandler extends AbstractHandler implements CsrfGuardAware
+class ActorTriagePageHandler extends AbstractHandler implements CsrfGuardAware, UserAware
 {
+    use User;
     use CsrfGuard;
 
     /** @var ServerUrlHelper */
@@ -32,13 +36,15 @@ class ActorTriagePageHandler extends AbstractHandler implements CsrfGuardAware
      * CreateAccountHandler constructor.
      * @param TemplateRendererInterface $renderer
      * @param UrlHelper $urlHelper
+     * @param AuthenticationInterface $authenticator
      */
     public function __construct(
         TemplateRendererInterface $renderer,
-        UrlHelper $urlHelper
-    )
-    {
+        UrlHelper $urlHelper,
+        AuthenticationInterface $authenticator
+    ) {
         parent::__construct($renderer, $urlHelper);
+        $this->setAuthenticator($authenticator);
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
@@ -49,6 +55,15 @@ class ActorTriagePageHandler extends AbstractHandler implements CsrfGuardAware
             return $this->handlePost($request);
         }
 
+        $user = $this->getUser($request);
+
+        if (! is_null($user)) {
+            if (empty($user->getDetail('LastLogin'))) {
+                return $this->redirectToRoute('lpa.add');
+            } else {
+                return $this->redirectToRoute('lpa.dashboard');
+            }
+        }
         return new HtmlResponse($this->renderer->render('actor::home-page', [
             'form' => $form->prepare()
         ]));
