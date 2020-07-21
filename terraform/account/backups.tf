58c4b0d5e2957ed0793151b13ee39a84ab53bc9a
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-07-21 14:13:28 +0100

    only create account level resources for backups

--- terraform/account/backups.tf
+++ terraform/account/backups.tf
@@ -54,15 +54,3 @@ resource "aws_iam_role_policy_attachment" "aws_backup_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
   role       = aws_iam_role.aws_backup_role.name
 }
-
-# resource "aws_backup_selection" "example" {
-#   iam_role_arn = "${aws_iam_role.example.arn}"
-#   name         = "tf_example_backup_selection"
-#   plan_id      = "${aws_backup_plan.example.id}"
-
-#   resources = [
-#     "${aws_db_instance.example.arn}",
-#     "${aws_ebs_volume.example.arn}",
-#     "${aws_efs_file_system.example.arn}",
-#   ]
-# }
