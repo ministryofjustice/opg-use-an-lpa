6f77f9de62ee52973a376a149db4b504cc76ebc9
Raffers <chris.rafferty@digital.justice.gov.uk>
2020-07-31 15:49:48 +0100

    Removed alarms from env level & move to account level. Configured pagerDuty on the account level so alerts are pushed correctly

--- terraform/environment/cloudwatch_alarms.tf
+++ terraform/environment/cloudwatch_alarms.tf
@@ -41,24 +41,3 @@ resource "aws_cloudwatch_metric_alarm" "actor_5xx_errors" {
   threshold                 = 2
   treat_missing_data        = "notBreaching"
 }
-
-resource "aws_cloudwatch_metric_alarm" "elasticache_001_high_cpu_utilization" {
-  actions_enabled                       = true
-  alarm_actions                         = [aws_sns_topic.cloudwatch_to_pagerduty.arn]
-  alarm_description                     = "High CPU usage on Elasticache 001"
-  alarm_name                            = "High CPU Utilization on ElastiCache 001"
-  comparison_operator                   = "GreaterThanThreshold"
-  datapoints_to_alarm                   = 2
-  dimensions = {
-    CacheClusterId = data.aws_elasticache_replication_group.brute_force_cache_replication_group.member_clusters[0]
-  }
-  evaluation_periods        = 2
-  insufficient_data_actions = []
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/ElastiCache"
-  ok_actions                = [aws_sns_topic.cloudwatch_to_pagerduty.arn]
-  period                    = "60"
-  statistic                 = "Average"
-  threshold                 = 0.01
-  treat_missing_data        = "notBreaching"
-}
