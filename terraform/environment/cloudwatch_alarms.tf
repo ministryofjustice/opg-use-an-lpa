dc74526ab827545d98b62295ae5dda2850a1261b
Raffers <chris.rafferty@digital.justice.gov.uk>
2020-07-31 14:53:47 +0100

    Removed env versions file

--- terraform/environment/cloudwatch_alarms.tf
+++ terraform/environment/cloudwatch_alarms.tf
@@ -42,22 +42,23 @@ resource "aws_cloudwatch_metric_alarm" "actor_5xx_errors" {
   treat_missing_data        = "notBreaching"
 }
 
-resource "aws_cloudwatch_metric_alarm" "elasticache_high_cpu_utilization" {
-  actions_enabled = true
-  alarm_actions = [aws_sns_topic.cloudwatch_to_pagerduty.arn]
-  alarm_description = "High CPU usage on Elasticache"
-  alarm_name = "High CPU Utilization on ElastiCache"
-  comparison_operator = "GreaterThanThreshold"
-  evaluate_low_sample_count_percentiles = "ignore"
-  datapoints_to_alarm = 2
-  evaluation_periods = 2
+resource "aws_cloudwatch_metric_alarm" "elasticache_001_high_cpu_utilization" {
+  actions_enabled                       = true
+  alarm_actions                         = [aws_sns_topic.cloudwatch_to_pagerduty.arn]
+  alarm_description                     = "High CPU usage on Elasticache 001"
+  alarm_name                            = "High CPU Utilization on ElastiCache 001"
+  comparison_operator                   = "GreaterThanThreshold"
+  datapoints_to_alarm                   = 2
+  dimensions = {
+    CacheClusterId = data.aws_elasticache_replication_group.brute_force_cache_replication_group.member_clusters[0]
+  }
+  evaluation_periods        = 2
   insufficient_data_actions = []
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/ElastiCache"
   ok_actions                = [aws_sns_topic.cloudwatch_to_pagerduty.arn]
-  period                    = 60
-  statistic                 = "Sum"
-  tags                      = {}
-  threshold                 = 1
+  period                    = "60"
+  statistic                 = "Average"
+  threshold                 = 0.01
   treat_missing_data        = "notBreaching"
 }
