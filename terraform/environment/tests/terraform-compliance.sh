a163d0184256da4728dd13fa0ed64feb5bcaa334
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-08-10 21:05:07 +0100

    update script

--- terraform/environment/tests/terraform-compliance.sh
+++ terraform/environment/tests/terraform-compliance.sh
@@ -6,7 +6,6 @@ terraform state pull > tests/state.out
 terraform show -json tests/state.out > tests/state.out.json
 terraform-compliance -p tests/state.out.json -f tests/
 
-
 # check diff
 # aws-vault exec identity -- terraform plan -out=plan.out
 # aws-vault exec identity -- terraform show -json tests/plan.out > tests/plan.out.json
