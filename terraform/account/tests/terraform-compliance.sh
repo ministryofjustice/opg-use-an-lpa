16666ffdcbc10c9763692f368c131493b16a2812
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-08-10 21:05:50 +0100

    add account leveliac tests?

---
+++ terraform/account/tests/terraform-compliance.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function terraform-compliance { docker run --rm -v $(pwd):/target -i -t eerkunt/terraform-compliance "$@"; }
+
+terraform state pull > tests/state.out
+terraform show -json tests/state.out > tests/state.out.json
+terraform-compliance -p tests/state.out.json -f tests/
+
+# check diff
+# aws-vault exec identity -- terraform plan -out=plan.out
+# aws-vault exec identity -- terraform show -json tests/plan.out > tests/plan.out.json
+# terraform-compliance -p tests/plan.out.json -f tests/
