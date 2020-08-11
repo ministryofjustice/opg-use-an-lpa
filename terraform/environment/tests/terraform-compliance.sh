fe18e6be32a683a6bd6dfdfbe479045afb4d18b5
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-08-11 20:19:43 +0100

    improve script

--- terraform/environment/tests/terraform-compliance.sh
+++ terraform/environment/tests/terraform-compliance.sh
@@ -2,11 +2,13 @@
 
 function terraform-compliance { docker run --rm -v $(pwd):/target -i -t eerkunt/terraform-compliance "$@"; }
 
+rm tests/state.out*
+
+terraform init
+
+terraform refresh
 terraform state pull > tests/state.out
 terraform show -json tests/state.out > tests/state.out.json
 terraform-compliance -p tests/state.out.json -f tests/
 
-# check diff
-# aws-vault exec identity -- terraform plan -out=plan.out
-# aws-vault exec identity -- terraform show -json tests/plan.out > tests/plan.out.json
-# terraform-compliance -p tests/plan.out.json -f tests/
+rm tests/state.out*
