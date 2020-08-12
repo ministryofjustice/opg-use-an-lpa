e864c4b44405d9f529949653f1313dfd19f1aa0a
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-08-12 12:08:16 +0100

    add version to required provider

--- terraform/environment/versions.tf
+++ terraform/environment/versions.tf
@@ -1,13 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 2.70.0"
     }
     local = {
       source = "hashicorp/local"
     }
     pagerduty = {
-      source = "terraform-providers/pagerduty"
+      source  = "terraform-providers/pagerduty"
+      version = "~> 1.7.4"
     }
   }
   required_version = ">= 0.13"
