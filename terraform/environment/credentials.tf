daa8d7a0a3c21f20032be61bccc7ef213e5b590a
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-08-12 10:39:21 +0100

    remove unused source argument from providers

--- terraform/environment/credentials.tf
+++ terraform/environment/credentials.tf
@@ -18,7 +18,6 @@ variable "management_role" {
 }
 
 provider "aws" {
-  # source  = "hashicorp/aws"
   version = "~> 2.70.0"
   region  = "eu-west-1"
 
@@ -29,7 +28,6 @@ provider "aws" {
 }
 
 provider "aws" {
-  # source  = "hashicorp/aws"
   version = "~> 2.70.0"
   region  = "us-east-1"
   alias   = "us-east-1"
@@ -41,7 +39,6 @@ provider "aws" {
 }
 
 provider "aws" {
-  # source  = "hashicorp/aws"
   version = "~> 2.70.0"
   region  = "eu-west-1"
   alias   = "management"
@@ -53,7 +50,6 @@ provider "aws" {
 }
 
 provider "pagerduty" {
-  # source  = "terraform-providers/pagerduty"
   version = "~> 1.7.4"
   token   = var.pagerduty_token
 }
