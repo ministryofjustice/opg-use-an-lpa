929d18147c9e277f329172e49bd8338697c2158e
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-08-14 14:20:30 +0100

    update certificate validation for view to use for_each as they are no longer indexable lists

--- terraform/account/certificate.tf
+++ terraform/account/certificate.tf
@@ -16,18 +16,49 @@ data "aws_route53_zone" "live_service_view_lasting_power_of_attorney" {
 //------------------------
 // View Certificates
 
+# resource "aws_route53_record" "certificate_validation_view" {
+#   provider = aws.management
+#   name     = aws_acm_certificate.certificate_view.domain_validation_options[0].resource_record_name
+#   type     = aws_acm_certificate.certificate_view.domain_validation_options[0].resource_record_type
+#   zone_id  = data.aws_route53_zone.opg_service_justice_gov_uk.zone_id
+#   records  = [aws_acm_certificate.certificate_view.domain_validation_options[0].resource_record_value]
+#   ttl      = 60
+# }
+
+# resource "aws_route53_record" "certificate_validation_view" {
+#   provider = aws.management
+#   name     = tolist(aws_acm_certificate.certificate_view.domain_validation_options)[0].resource_record_name
+#   type     = tolist(aws_acm_certificate.certificate_view.domain_validation_options)[0].resource_record_type
+#   zone_id  = data.aws_route53_zone.opg_service_justice_gov_uk.zone_id
+#   records  = [tolist(aws_acm_certificate.certificate_view.domain_validation_options)[0].resource_record_value]
+#   ttl      = 60
+# }
+
+# resource "aws_acm_certificate_validation" "certificate_view" {
+#   certificate_arn         = aws_acm_certificate.certificate_view.arn
+#   validation_record_fqdns = [aws_route53_record.certificate_validation_view.fqdn]
+# }
+
 resource "aws_route53_record" "certificate_validation_view" {
-  provider = aws.management
-  name     = aws_acm_certificate.certificate_view.domain_validation_options[0].resource_record_name
-  type     = aws_acm_certificate.certificate_view.domain_validation_options[0].resource_record_type
-  zone_id  = data.aws_route53_zone.opg_service_justice_gov_uk.zone_id
-  records  = [aws_acm_certificate.certificate_view.domain_validation_options[0].resource_record_value]
-  ttl      = 60
+  for_each = {
+    for dvo in aws_acm_certificate.certificate_view.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.opg_service_justice_gov_uk.zone_id
 }
 
 resource "aws_acm_certificate_validation" "certificate_view" {
   certificate_arn         = aws_acm_certificate.certificate_view.arn
-  validation_record_fqdns = [aws_route53_record.certificate_validation_view.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.certificate_validation_view : record.fqdn]
 }
 
 resource "aws_acm_certificate" "certificate_view" {
