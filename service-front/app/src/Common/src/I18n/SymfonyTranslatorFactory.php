7685800cd3ae3a6aadab84fe2171419276970a47
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-08-14 17:16:47 +0100

    Improve loading of the language configuration

--- service-front/app/src/Common/src/I18n/SymfonyTranslatorFactory.php
+++ service-front/app/src/Common/src/I18n/SymfonyTranslatorFactory.php
@@ -15,7 +15,7 @@ class SymfonyTranslatorFactory
     {
         $config = $container->get('config');
 
-        if (! isset($config['i18n'])) {
+        if (! isset($config['i18n']['default_locale'])) {
             throw new \RuntimeException('Language file configuration must be specified');
         }
 
@@ -23,12 +23,14 @@ class SymfonyTranslatorFactory
         $translator->setFallbackLocales([$config['i18n']['default_locale']]);
         $translator->addLoader('mo', new MoFileLoader());
 
-        // Welsh
-        $translator->addResource(
-            $config['i18n']['languages']['welsh']['format'],
-            $config['i18n']['languages']['welsh']['resource'],
-            $config['i18n']['languages']['welsh']['locale'],
-        );
+        // loop our configured languages and push them into the translator
+        foreach ($config['i18n']['languages'] as $language) {
+            $translator->addResource(
+                $language['format'],
+                $language['resource'],
+                $language['locale'],
+            );
+        }
 
         return $translator;
     }
