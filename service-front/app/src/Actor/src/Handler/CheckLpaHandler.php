2e6fa004a14bb20b01e89dd87887c23ff5bef197
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-08-20 16:16:05 +0100

    started to refactor the checkLpa handler, split up get and post code

--- service-front/app/src/Actor/src/Handler/CheckLpaHandler.php
+++ service-front/app/src/Actor/src/Handler/CheckLpaHandler.php
@@ -5,12 +5,7 @@ declare(strict_types=1);
 namespace Actor\Handler;
 
 use Actor\Form\LpaConfirm;
-use App\Service\User\UserService;
-use ArrayObject;
-use Common\Entity\CaseActor;
-use Common\Entity\Lpa;
 use Common\Exception\ApiException;
-use Common\Exception\RateLimitExceededException;
 use Common\Handler\AbstractHandler;
 use Common\Handler\CsrfGuardAware;
 use Common\Handler\LoggerAware;
@@ -88,6 +83,18 @@ class CheckLpaHandler extends AbstractHandler implements CsrfGuardAware, UserAwa
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        switch ($request->getMethod()) {
+            case 'POST':
+                return $this->handlePost($request);
+            case 'GET':
+                return $this->handleGet($request);
+            default:
+                return $this->handleGet($request);
+        }
+    }
+
+    public function handleGet(ServerRequestInterface $request): ResponseInterface
+    {
         $session = $this->getSession($request, 'session');
 
         $form = new LpaConfirm($this->getCsrfGuard($request));
@@ -99,128 +106,147 @@ class CheckLpaHandler extends AbstractHandler implements CsrfGuardAware, UserAwa
         $referenceNumber = $session->get('reference_number');
         $dob = $session->get('dob');
 
-        if (isset($identity) && isset($passcode) && isset($referenceNumber) && isset($dob)) {
-            try {
-                if ($request->getMethod() === 'POST') {
-                    $form->setData($request->getParsedBody());
+        if (!isset($identity) && !isset($passcode) && !isset($referenceNumber) && !isset($dob)) {
+            // We don't have a code so the session has timed out
+            // TODO this can be reached if the session is still perfectly valid but the lpa search/response
+            //      failed in some way. Make this better.
+            throw new SessionTimeoutException();
+        }
 
-                    if ($form->isValid()) {
-                        $actorCode = $this->lpaService->confirmLpaAddition(
-                            $identity,
-                            $passcode,
-                            $referenceNumber,
-                            $dob
-                        );
+        try {
+            $lpaData = $this->lpaService->getLpaByPasscode(
+                $identity,
+                $passcode,
+                $referenceNumber,
+                $dob
+            );
 
-                        $this->getLogger()->info(
-                            'Account with Id {id} has added LPA with Id {uId} to their account',
-                            [
-                                'id' => $identity,
-                                'uId' => $referenceNumber
-                            ]
-                        );
+            $lpa = $lpaData['lpa'];
+            $actor = $lpaData['actor']['details'];
 
-                        if (!is_null($actorCode)) {
+            $this->getLogger()->debug(
+                'Account with Id {id} has found an LPA with Id {uId} using their passcode',
+                [
+                    'id' => $identity,
+                    'uId' => $referenceNumber
+                ]
+            );
 
-                            /** @var FlashMessagesInterface $flash */
-                            $flash = $request->getAttribute(FlashMessageMiddleware::FLASH_ATTRIBUTE);
-                            $donor = $session->get('donor_name');
-                            $lpaType = $session->get('lpa_type');
-
-                            $flash->flash(self::ADD_LPA_FLASH_MSG, "You've added $donor's $lpaType LPA");
-
-                            return new RedirectResponse($this->urlHelper->generate('lpa.dashboard'));
-                        }
-                    }
-                }
-
-                // is a GET or failed POST
-                $lpaData = $this->lpaService->getLpaByPasscode(
-                    $identity,
-                    $passcode,
-                    $referenceNumber,
-                    $dob
-                );
-
-                $lpa = $lpaData['lpa'];
-                $actor = $lpaData['actor']['details'];
+            if (!is_null($lpa) && (strtolower($lpa->getStatus()) === 'registered')) {
+                // Are we displaying Donor or Attorney user role
+                $actorRole = ($lpa->getDonor()->getId() === $actor->getId()) ?
+                    'Donor' :
+                    'Attorney';
 
                 $this->getLogger()->debug(
-                    'Account with Id {id} has found an LPA with Id {uId} using their passcode',
+                    'Account with Id {id} identified as Role {role} on LPA with Id {uId}',
                     [
                         'id' => $identity,
+                        'role' => $actorRole,
                         'uId' => $referenceNumber
                     ]
                 );
 
-                if (!is_null($lpa) && (strtolower($lpa->getStatus()) === 'registered')) {
-                    // Are we displaying Donor or Attorney user role
-                    $actorRole = ($lpa->getDonor()->getId() === $actor->getId()) ?
-                        'Donor' :
-                        'Attorney';
+                // data to be used in flash message
+                $session->set('donor_name', $lpa->getDonor()->getFirstname() . ' ' . $lpa->getDonor()->getSurname());
+                $session->set('lpa_type', $lpa->getCaseSubtype() === 'hw' ? 'health and welfare' : 'property and finance');
 
-                    $this->getLogger()->debug(
-                        'Account with Id {id} identified as Role {role} on LPA with Id {uId}',
-                        [
-                            'id' => $identity,
-                            'role' => $actorRole,
-                            'uId' => $referenceNumber
-                        ]
-                    );
+                return new HtmlResponse($this->renderer->render('actor::check-lpa', [
+                    'form' => $form,
+                    'lpa' => $lpa,
+                    'user' => $actor,
+                    'userRole' => $actorRole,
+                ]));
 
-                    // data to be used in flash message
-                    $session->set('donor_name', $lpa->getDonor()->getFirstname() . ' ' . $lpa->getDonor()->getSurname());
-                    $session->set('lpa_type', $lpa->getCaseSubtype() === 'hw' ? 'health and welfare' : 'property and finance');
-
-                    return new HtmlResponse($this->renderer->render('actor::check-lpa', [
-                        'form' => $form,
-                        'lpa' => $lpa,
-                        'user' => $actor,
-                        'userRole' => $actorRole,
-                    ]));
-                } else {
-                    $this->getLogger()->debug(
-                        'LPA with Id {uId} has {status} status and hence cannot be added',
-                        [
-                            'uId' => $referenceNumber,
-                            'status' => $lpaData['lpa']->getStatus()
-                        ]
-                    );
-                    //  Show LPA not found page
-                    return new HtmlResponse($this->renderer->render('actor::lpa-not-found', [
-                        'user'              => $user,
-                        'dob'               => $dob,
-                        'referenceNumber'   => $referenceNumber,
-                        'passcode'          => $passcode
-                    ]));
-                }
-            } catch (ApiException $aex) {
-                if ($aex->getCode() === StatusCodeInterface::STATUS_NOT_FOUND) {
-                    $this->getLogger()->info(
-                        'Account with Id {id} has failed to add an LPA to their account',
-                        [
-                            'id' => $identity
-                        ]
-                    );
-
-                    $this->rateLimitService->
-                        limit($request->getAttribute(UserIdentificationMiddleware::IDENTIFY_ATTRIBUTE));
-                    //  Show LPA not found page
-                    return new HtmlResponse($this->renderer->render('actor::lpa-not-found', [
-                        'user' => $user,
-                        'dob'               => $dob,
-                        'referenceNumber'   => $referenceNumber,
-                        'passcode'          => $passcode
-                    ]));
-                }
-
-                throw $aex;
+            } else {
+                $this->getLogger()->debug(
+                    'LPA with Id {uId} has {status} status and hence cannot be added',
+                    [
+                        'uId' => $referenceNumber,
+                        'status' => $lpaData['lpa']->getStatus()
+                    ]
+                );
+                //  Show LPA not found page
+                return new HtmlResponse($this->renderer->render('actor::lpa-not-found', [
+                    'user'              => $user,
+                    'dob'               => $dob,
+                    'referenceNumber'   => $referenceNumber,
+                    'passcode'          => $passcode
+                ]));
             }
+        } catch (ApiException $aex) {
+            if ($aex->getCode() === StatusCodeInterface::STATUS_NOT_FOUND) {
+                $this->getLogger()->info(
+                    'Account with Id {id} has failed to add an LPA to their account',
+                    [
+                        'id' => $identity
+                    ]
+                );
+
+                $this->rateLimitService->
+                limit($request->getAttribute(UserIdentificationMiddleware::IDENTIFY_ATTRIBUTE));
+                //  Show LPA not found page
+                return new HtmlResponse($this->renderer->render('actor::lpa-not-found', [
+                    'user' => $user,
+                    'dob'               => $dob,
+                    'referenceNumber'   => $referenceNumber,
+                    'passcode'          => $passcode
+                ]));
+            }
+
+            throw $aex;
+        }
+    }
+
+    public function handlePost(ServerRequestInterface $request): ResponseInterface
+    {
+        $session = $this->getSession($request, 'session');
+
+        $form = new LpaConfirm($this->getCsrfGuard($request));
+
+        $user = $this->getUser($request);
+        $identity = (!is_null($user)) ? $user->getIdentity() : null;
+
+        $passcode = $session->get('passcode');
+        $referenceNumber = $session->get('reference_number');
+        $dob = $session->get('dob');
+
+        if (!isset($identity) && !isset($passcode) && !isset($referenceNumber) && !isset($dob)) {
+            // We don't have a code so the session has timed out
+            // TODO this can be reached if the session is still perfectly valid but the lpa search/response
+            //      failed in some way. Make this better.
+            throw new SessionTimeoutException();
         }
 
-        // We don't have a code so the session has timed out
-        // TODO this can be reached if the session is still perfectly valid but the lpa search/response
-        //      failed in some way. Make this better.
-        throw new SessionTimeoutException();
+        $form->setData($request->getParsedBody());
+
+        if ($form->isValid()) {
+            $actorCode = $this->lpaService->confirmLpaAddition(
+                $identity,
+                $passcode,
+                $referenceNumber,
+                $dob
+            );
+
+            $this->getLogger()->info(
+                'Account with Id {id} has added LPA with Id {uId} to their account',
+                [
+                    'id' => $identity,
+                    'uId' => $referenceNumber
+                ]
+            );
+
+            if (!is_null($actorCode)) {
+
+                /** @var FlashMessagesInterface $flash */
+                $flash = $request->getAttribute(FlashMessageMiddleware::FLASH_ATTRIBUTE);
+                $donor = $session->get('donor_name');
+                $lpaType = $session->get('lpa_type');
+
+                $flash->flash(self::ADD_LPA_FLASH_MSG, "You've added $donor's $lpaType LPA");
+
+                return new RedirectResponse($this->urlHelper->generate('lpa.dashboard'));
+            }
+        }
     }
 }
