d9884df93535e4e0520c9b52be67226a3e252146
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2020-08-21 16:15:12 +0100

    Moved SessionCheckHandler to common namespace. Removed both Actor and Viewer files. Created unit tests for handler.

---
+++ service-front/app/src/Common/src/Handler/SessionCheckHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Common\Handler;
+
+use Common\Handler\Traits\Session as SessionTrait;
+use Laminas\Diactoros\Response\JsonResponse;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
+
+class SessionCheckHandler implements RequestHandlerInterface
+{
+    use SessionTrait;
+
+    /**
+     * Key used within the session for the initiated time
+     */
+    public const SESSION_TIME_KEY = '__TIME__';
+
+    private ContainerInterface $container;
+
+    /**
+     * SessionCheckHandler constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $session = $this->getSession($request, 'session');
+        $config = $this->container->get('config');
+
+        $expiresAt = $session->get(self::SESSION_TIME_KEY) + $config['session']['expires'];
+        // Do we have 5min remaining
+        $sessionWarning = ($expiresAt - time()) <= 300;
+
+        return new JsonResponse(['session_warning' => $sessionWarning], 201);
+    }
+}
