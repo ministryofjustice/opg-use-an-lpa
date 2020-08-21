2ae3b87e05ced9f67f3124de2263e3192cff48d1
william Falconer <william.falconer@digital.justice.gov,uk>
2020-08-21 13:39:49 +0100

    added replacement code for error messages and validation.

--- service-front/app/src/Common/src/View/Twig/GovUKLaminasFormErrorsExtension.php
+++ service-front/app/src/Common/src/View/Twig/GovUKLaminasFormErrorsExtension.php
@@ -12,16 +12,29 @@ use Laminas\Form\FormInterface;
 
 class GovUKLaminasFormErrorsExtension extends AbstractExtension
 {
-    const THEME_FILE = '@partials/govuk_error.html.twig';
+    public const THEME_FILE = '@partials/govuk_error.html.twig';
 
     /**
      * @return array
+     * @inheritDoc
      */
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('govuk_error', [$this, 'errorMessage'], ['needs_environment' => true, 'is_safe' => ['html']]),
-            new TwigFunction('govuk_error_summary', [$this, 'errorSummary'], ['needs_environment' => true, 'is_safe' => ['html']])
+            new TwigFunction(
+                'govuk_error',
+                [$this, 'errorMessage'],
+                ['needs_environment' => true, 'is_safe' => ['html']]
+            ),
+            new TwigFunction(
+                'govuk_error_summary',
+                [$this, 'errorSummary'],
+                ['needs_environment' => true, 'is_safe' => ['html']]
+            ),
+            new TwigFunction(
+                "translate_validation_messages",
+                [$this,'applyTranslatedMessages']
+            ),
         ];
     }
 
@@ -33,6 +46,7 @@ class GovUKLaminasFormErrorsExtension extends AbstractExtension
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
+     * Generate an error message
      */
     public function errorMessage(Environment $twigEnv, ElementInterface $element): string
     {
@@ -52,18 +66,31 @@ class GovUKLaminasFormErrorsExtension extends AbstractExtension
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
+     * Generate an error summary
      */
     public function errorSummary(Environment $twigEnv, FormInterface $form): string
     {
         $template = $twigEnv->load(self::THEME_FILE);
 
         // if the form has no overall errors it'll be an empty array
+
+        $returnedErrors = $form->getMessages();
         $errors = $form->getMessages();
         $invalidInput = $form->getInputFilter()->getInvalidInput();
 
         //  Flatten each set of messages for each input
         foreach ($invalidInput as $name => $input) {
-            $errors[$name] = $this->flattenMessages($input->getMessages());
+            $inputMessages = $input->getMessages();
+
+            // check and replace where you can the error message.
+            foreach ($inputMessages as $key => $value) {
+                if (key_exists($name, $returnedErrors) && key_exists($key, $returnedErrors[$name])) {
+                    $inputMessages[$key] = $returnedErrors[$name][$key];
+                }
+            }
+
+            // this sets the error array format needed for the sumary render.
+            $errors[$name] = $this ->flattenMessages($inputMessages);
         }
 
         return $template->renderBlock('error_summary', [
@@ -74,6 +101,7 @@ class GovUKLaminasFormErrorsExtension extends AbstractExtension
     /**
      * @param array $messages
      * @return array
+     * Flattens the input validation messages into a form acceptable to the error summary.
      */
     private function flattenMessages(array $messages): array
     {
@@ -84,5 +112,48 @@ class GovUKLaminasFormErrorsExtension extends AbstractExtension
         });
 
         return $messagesToPrint;
+    }
+
+    /**
+     * @param FormInterface $form
+     * @param $messages
+     * Apply  any translated messages, if found in the twig template.
+     */
+    public function applyTranslatedMessages(FormInterface $form, $messages)
+    {
+        //1. retrieve messages that are already set.
+        $formMessages = $form->getMessages();
+
+        //2. replace messages.
+        $replacedErrors = $this->replaceMessages($messages, $formMessages);
+
+        //3. reapply to the form.
+        $form->setMessages($replacedErrors);
+    }
+
+
+    /**
+     * @param $translatedMessages
+     * @param $formMessages
+     * @return array
+     * replaces messages with the translated messages.
+     * this is assumed to be both in the same structure all the way through.
+     */
+    public function replaceMessages($translatedMessages, $formMessages)
+    {
+        $replacedArray = [];
+        foreach ($formMessages as $elementName => $messages) {
+            if (key_exists($elementName, $translatedMessages)) {
+                $result = $translatedMessages[$elementName];
+                $replacedArray[$elementName] = $messages;
+
+                foreach ($replacedArray[$elementName] as $message => $value) {
+                    if (key_exists($message, $result)) {
+                        $replacedArray[$elementName][$message] = $result[$message];
+                    }
+                }
+            }
+        }
+        return $replacedArray;
     }
 }
