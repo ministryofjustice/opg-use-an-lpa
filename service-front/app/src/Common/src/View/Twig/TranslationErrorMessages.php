a07a4c6a00cdc33bcc0bae875ccfef7d339b72a4
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-08-27 12:58:31 +0100

    created list of error messages for translation

---
+++ service-front/app/src/Common/src/View/Twig/TranslationErrorMessages.php
@@ -0,0 +1,63 @@
+{# List of the error messages the actor forms might put out. #}
+
+{# actor __csrf token #}
+{% trans %}As you have not used this service for over 20 minutes, the page has timed out. We\'ve now refreshed the page - please try to sign in again{% endtrans %}
+
+{# Change email #}
+{% trans %}Enter your new email address{% endtrans %}
+{% trans %}Enter an email address in the correct format, like name@example.com{% endtrans %}
+{% trans %}The new email address you entered is the same as your current email address. They must be different.{% endtrans %}
+{% trans %}Enter your password{% endtrans %}
+{% trans %}The password you entered is incorrect{% endtrans %}
+
+{# Check lpa #}
+{% trans %}Enter an organisation name{% endtrans %}
+
+{# Create account #}
+{% trans %}Password must be 8 characters or more{% endtrans %}
+{% trans %}Passwords do not match{% endtrans %}
+{% trans %}Password must include a number{% endtrans %}
+{% trans %}Password must include a lower case letter{% endtrans %}
+{% trans %}Password must include a capital letter{% endtrans %}
+{% trans %}Confirm your password{% endtrans %}
+{% trans %}You must accept the terms of use to create an account{% endtrans %}
+
+{# Triage page #}
+{% trans %}Select yes if you have a Use a lasting power of attorney account{% endtrans %}
+
+{# Login #}
+{% trans %}Security validation failed. Please try again.{% endtrans %}
+{% trans %}We cannot find an account with that email address and password{% endtrans %}
+
+{# Add lpa #}
+{% trans %}Enter your activation key{% endtrans %}
+{% trans %}Enter an activation key in the correct format{% endtrans %}
+{% trans %}The LPA reference number you entered is too short{% endtrans %}
+{% trans %}The LPA reference number you entered is too long{% endtrans %}
+{% trans %}Enter the 12 numbers of the LPA reference number. Do not include letters or other characters{% endtrans %}
+{% trans %}Date of birth value must be provided in an array{% endtrans %}
+{% trans %}Enter your date of birth{% endtrans %}
+{% trans %}Date of birth must be a real date{% endtrans %}
+{% trans %}Date of birth must include a day{% endtrans %}
+{% trans %}Date of birth must include a month{% endtrans %}
+{% trans %}Date of birth must include a year{% endtrans %}
+{% trans %}Date of birth must be in the past{% endtrans %}
+{% trans %}Check your date of birth is correct - you cannot be an attorney or donor if you’re under 18{% endtrans %}
+
+{# Create viewer code #}
+{% trans %}Enter an organisation name{% endtrans %}
+
+{# Change password #}
+{% trans %}Enter your current password{% endtrans %}
+{% trans %}Current password is incorrect{% endtrans %}
+{% trans %}Enter your new password{% endtrans %}
+{% trans %}New password and confirm new password do not match{% endtrans %}
+{% trans %}Enter your password again to confirm it{% endtrans %}
+
+{# Password reset request #}
+{% trans %}Confirm your email address{% endtrans %}
+{% trans %}The emails did not match{% endtrans %}
+
+{# Password reset (messages already listed in file) #}
+
+{# Cookies (messages already listed in file) #}
