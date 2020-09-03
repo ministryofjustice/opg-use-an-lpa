b87f3f59bdc9f68e052c1ea191c680954fc009f8
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-09-03 15:31:19 +0100

    Set config to null to disable as per docblock comments

--- service-front/app/features/bootstrap/config.php
+++ service-front/app/features/bootstrap/config.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Laminas\ConfigAggregator\ArrayProvider;
 use Laminas\ConfigAggregator\ConfigAggregator;
 use Laminas\ConfigAggregator\PhpFileProvider;
 
@@ -11,14 +10,6 @@ $aggregator = new ConfigAggregator([
 
     // Load development config if it exists
     new PhpFileProvider(realpath(__DIR__) . '/behat.config.php'),
-
-    // Include cache configuration
-    new ArrayProvider(
-        [
-            'debug' => true,
-            ConfigAggregator::ENABLE_CACHE => false,
-        ]
-    ),
-]);
+], null);
 
 return $aggregator->getMergedConfig();
