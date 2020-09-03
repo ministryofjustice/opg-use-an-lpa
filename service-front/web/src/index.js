6aa5ef4d4f8d97f0d764c4adab4b8331b34f5b60
SeemaMenon <seema.workspace@gmail.com>
2020-09-03 14:50:56 +0100

    review suggestions added

--- service-front/web/src/index.js
+++ service-front/web/src/index.js
@@ -1,4 +1,5 @@
 /* istanbul ignore file */
+
 import './scss.js';
 import { initAll } from 'govuk-frontend';
 import jsEnabled from './javascript/jsEnabled';
@@ -6,9 +7,11 @@ import disableButtonOnClick from './javascript/disableButtonOnClick';
 import copyAccessCode from './javascript/copyAccessCode';
 import cookieConsent from './javascript/cookieConsent';
 import sessionDialog from './javascript/sessionDialog';
+
 initAll();
+
 jsEnabled(document.body);
 disableButtonOnClick(document.getElementsByTagName('form'));
 new cookieConsent(document.getElementsByClassName('cookie-banner')[0], window.location.pathname === '/cookies');
-copyAccessCode();
+copyAccessCode(document.getElementById("copybutton"));
 // new sessionDialog(document.getElementById("dialog"), 20); // TODO: Disabled for now until we know how we are going to trigger it
--- service-front/web/src/javascript/copyAccessCode.test.js
+++ service-front/web/src/javascript/copyAccessCode.test.js
@@ -1,33 +1,3 @@
-// import copyAccessCode from './copyAccessCode';
-// describe('given a copy access code is found', () => {
-//     beforeEach(() => {
-//         document.execCommand = jest.fn();
-//     });
-//     jest.useFakeTimers();
-//     test('it should copy the code', () => {
-//         document.body.innerHTML = `
-//       <input readonly type="text" value="this-is-a-code-test" id="accesscodecopy" class="offscreen" aria-hidden="true">
-//       <button id="copybutton" class="app-copy-button">Copy code</button>
-//     `;
-//         const button = document.getElementById("copybutton");
-//         copyAccessCode(button);
-//         expect(button.innerHTML).toBe("Copy code");
-//         button.click();
-//         expect(document.execCommand).toHaveBeenCalledWith('copy');
-//         expect(button.innerHTML).toBe("Code copied");
-//         jest.advanceTimersByTime(4000);
-//         expect(button.innerHTML).toBe("Copy code");
-//     });
-//     test('it should not copy the code if the id is not found', () => {
-//         document.body.innerHTML = `
-//       <input readonly type="text" value="this-is-a-code-test" id="accesscodecopy" class="offscreen" aria-hidden="true">
-//       <button class="app-copy-button">Copy code</button>
-//     `;
-//         const button = document.getElementById("copybutton");
-//         copyAccessCode(button);
-//         expect(button).toBeNull();
-//     });
-// });
 import copyAccessCode from './copyAccessCode';
 describe('given a copy access code is found', () => {
     beforeEach(() => {
@@ -36,45 +6,26 @@ describe('given a copy access code is found', () => {
     jest.useFakeTimers();
     test('it should copy the code', () => {
         document.body.innerHTML = `
-        <div class="js-accesscodecopy">
-        <input readonly type="text" value="12345" class="js-accesscodecopy-button offscreen" aria-hidden="true">
-        <button class="js-accesscodecopy-button govuk-button govuk-button__copy">
-            <span class="js-accesscodecopy-default">{% trans %}Copy code{% endtrans %}</span>
-            <span class="js-accesscodecopy-success hidden">{% trans %}Code copied{% endtrans %}</span>
-        </button>
-      </div>
+      <input readonly type="text" value="this-is-a-code-test" id="accesscodecopy" class="offscreen" aria-hidden="true">
+      <button id="copybutton" class="app-copy-button">Copy code</button>
     `;
-        const button = document.querySelector(".js-accesscodecopy-button");
-        const deafultText = document.querySelector(".js-accesscodecopy-default");
-        const successText = document.querySelector(".js-accesscodecopy-success");
-        copyAccessCode();
-        expect(deafultText.classList).toHaveLength(1);
-        expect(deafultText.classList).not.toContain('hidden');
-        expect(successText.classList).toHaveLength(2);
-        expect(successText.classList).toContain('hidden');
+        const button = document.getElementById("copybutton");
+        copyAccessCode(button);
+        expect(button.innerHTML).toBe("Copy code");
         button.click();
         expect(document.execCommand).toHaveBeenCalledWith('copy');
-        expect(deafultText.classList).toHaveLength(2);
-        expect(deafultText.classList).toContain('hidden');
-        expect(successText.classList).toHaveLength(1);
-        expect(successText.classList.not).toContain('hidden');
+        expect(button.innerHTML).toBe("Code copied");
         jest.advanceTimersByTime(4000);
-        expect(deafultText.classList).toHaveLength(1);
-        expect(deafultText.classList).not.toContain('hidden');
-        expect(successText.classList).toHaveLength(2);
-        expect(successText.classList).toContain('hidden');
+        expect(button.innerHTML).toBe("Copy code");
     });
     test('it should not copy the code if the id is not found', () => {
         document.body.innerHTML = `
-        <div class="">
-        <input readonly type="text" value="{{ add_hyphen_to_viewer_code(code) }}" class="js-accesscodecopy-button offscreen" aria-hidden="true">
-        <button class="js-accesscodecopy-button govuk-button govuk-button__copy">
-            <span class="js-accesscodecopy-default">{% trans %}Copy code{% endtrans %}</span>
-            <span class="js-accesscodecopy-success hidden">{% trans %}Code copied{% endtrans %}</span>
-        </button>
-      </div>
+      <input readonly type="text" value="this-is-a-code-test" id="accesscodecopy" class="offscreen" aria-hidden="true">
+      <button class="app-copy-button">Copy code</button>
     `;
-        copyAccessCode();
+        const button = document.getElementById("copybutton");
+        copyAccessCode(button);
         expect(button).toBeNull();
     });
 });
+
