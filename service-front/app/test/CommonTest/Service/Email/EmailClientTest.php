7ec8dd4b213facebdccac303380935fd763ed00c
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-09-03 08:02:51 +0100

    added unit tests for every welsh email case for code cov

--- service-front/app/test/CommonTest/Service/Email/EmailClientTest.php
+++ service-front/app/test/CommonTest/Service/Email/EmailClientTest.php
@@ -150,7 +150,27 @@ class EmailClientTest extends TestCase
     }
 
     /** @test */
-    public function its_sends_a_welsh_email_if_locale_is_cy()
+    public function can_send_account_activation_email_if_locale_is_cy()
+    {
+        $this->locale = "cy";
+        $recipient = 'a@b.com';
+
+        $this->notifyClientProphecy->sendEmail(
+            $recipient,
+            EmailClient::WELSH_TEMPLATE_ID_ACCOUNT_ACTIVATION,
+            [
+                'activate-account-url' => 'http://localhost:9002/cy/activate-account/activateAccountAABBCCDDEE'
+            ]
+        )
+            ->shouldBeCalledOnce();
+
+        $emailClient = new EmailClient($this->notifyClientProphecy->reveal(), $this->locale);
+
+        $emailClient->sendAccountActivationEmail($recipient, 'http://localhost:9002/cy/activate-account/activateAccountAABBCCDDEE');
+    }
+
+    /** @test */
+    public function can_send_already_registered_email_if_locale_is_cy()
     {
         $this->locale = "cy";
         $recipient = 'a@b.com';
@@ -161,5 +181,102 @@ class EmailClientTest extends TestCase
         $emailClient = new EmailClient($this->notifyClientProphecy->reveal(), $this->locale);
 
         $emailClient->sendAlreadyRegisteredEmail($recipient);
+    }
+
+    /** @test */
+    public function can_send_password_reset_email_if_locale_is_cy()
+    {
+        $this->locale = "cy";
+        $recipient = 'a@b.com';
+
+        $this->notifyClientProphecy->sendEmail(
+            $recipient,
+            EmailClient::WELSH_TEMPLATE_ID_PASSWORD_RESET,
+            [
+                'password-reset-url' => 'http://localhost:9002/cy/password-reset/passwordResetAABBCCDDEE'
+            ]
+        )
+            ->shouldBeCalledOnce();
+
+        $emailClient = new EmailClient($this->notifyClientProphecy->reveal(), $this->locale);
+
+        $emailClient->sendPasswordResetEmail($recipient, 'http://localhost:9002/cy/password-reset/passwordResetAABBCCDDEE');
+    }
+
+    /** @test */
+    public function can_send_password_change_email_if_locale_is_cy()
+    {
+        $this->locale = "cy";
+        $recipient = 'a@b.com';
+
+        $this->notifyClientProphecy->sendEmail(
+            $recipient,
+            EmailClient::WELSH_TEMPLATE_ID_PASSWORD_CHANGE
+        )
+            ->shouldBeCalledOnce();
+
+        $emailClient = new EmailClient($this->notifyClientProphecy->reveal(), $this->locale);
+
+        $emailClient->sendPasswordChangedEmail($recipient);
+    }
+
+    /** @test */
+    public function can_send_change_email_to_current_email_if_locale_is_cy()
+    {
+        $this->locale = "cy";
+        $recipient = 'current@email.com';
+        $newEmail = 'new@email.com';
+
+        $this->notifyClientProphecy->sendEmail(
+            $recipient,
+            EmailClient::WELSH_TEMPLATE_ID_EMAIL_CHANGE_SENT_TO_CURRENT_EMAIL,
+            [
+                'new-email-address' => $newEmail
+            ]
+        )
+            ->shouldBeCalledOnce();
+
+        $emailClient = new EmailClient($this->notifyClientProphecy->reveal(), $this->locale);
+
+        $emailClient->sendRequestChangeEmailToCurrentEmail($recipient, $newEmail);
+    }
+
+    /** @test */
+    public function can_send_change_email_verify_to_new_email_if_locale_is_cy()
+    {
+        $this->locale = "cy";
+        $recipient = 'new@email.com';
+
+        $this->notifyClientProphecy->sendEmail(
+            $recipient,
+            EmailClient::WELSH_TEMPLATE_ID_EMAIL_CHANGE_SENT_TO_NEW_EMAIL,
+            [
+                'verify-new-email-url' => 'http://localhost:9002/cy/verify-new-email/verifyNewEmailAABBCCDDEE'
+            ]
+        )
+            ->shouldBeCalledOnce();
+
+        $emailClient = new EmailClient($this->notifyClientProphecy->reveal(), $this->locale);
+
+        $emailClient->sendRequestChangeEmailToNewEmail(
+            $recipient,
+            'http://localhost:9002/cy/verify-new-email/verifyNewEmailAABBCCDDEE'
+        );
+    }
+
+    /** @test */
+    public function can_send_someone_tried_to_use_your_email_for_email_change_if_locale_is_cy()
+    {
+        $this->locale = "cy";
+        $recipient = 'new@email.com';
+
+        $this->notifyClientProphecy->sendEmail(
+            $recipient,
+            EmailClient::WELSH_TEMPLATE_ID_RESET_CONFLICT_EMAIL_CHANGE_INCOMPLETE
+        )->shouldBeCalledOnce();
+
+        $emailClient = new EmailClient($this->notifyClientProphecy->reveal(), $this->locale);
+
+        $emailClient->sendSomeoneTriedToUseYourEmailInEmailResetRequest($recipient);
     }
 }
