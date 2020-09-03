6ebce61099913aabac96604c7fc28a4d0bf5801f
SeemaMenon <seema.workspace@gmail.com>
2020-09-03 13:56:54 +0100

    JS changes to accomodate welsh translation for copy code text in banner and test changes

--- service-front/web/src/index.js
+++ service-front/web/src/index.js
@@ -1,5 +1,4 @@
 /* istanbul ignore file */
-
 import './scss.js';
 import { initAll } from 'govuk-frontend';
 import jsEnabled from './javascript/jsEnabled';
@@ -7,11 +6,9 @@ import disableButtonOnClick from './javascript/disableButtonOnClick';
 import copyAccessCode from './javascript/copyAccessCode';
 import cookieConsent from './javascript/cookieConsent';
 import sessionDialog from './javascript/sessionDialog';
-
 initAll();
-
 jsEnabled(document.body);
 disableButtonOnClick(document.getElementsByTagName('form'));
 new cookieConsent(document.getElementsByClassName('cookie-banner')[0], window.location.pathname === '/cookies');
-copyAccessCode(document.getElementById("copybutton"));
+copyAccessCode();
 // new sessionDialog(document.getElementById("dialog"), 20); // TODO: Disabled for now until we know how we are going to trigger it
