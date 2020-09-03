3d39bb94fb50ee6eee0003b3d6cfb9a59a16f611
SeemaMenon <seema.workspace@gmail.com>
2020-09-03 13:31:02 +0100

    copy code changes to accomodate welsh translation

--- service-front/web/src/javascript/copyAccessCode.js
+++ service-front/web/src/javascript/copyAccessCode.js
@@ -1,14 +1,38 @@
-const copyAccessCode = (element) => {
-  if (element) {
-    element.onclick = function() {
-      let copyText = document.getElementById("accesscodecopy");
-      copyText.select();
-      copyText.setSelectionRange(0, 99999);
+// const copyAccessCode = (element) => {
+//   if (element) {
+//     element.onclick = function() {
+//       let copyText = document.getElementById("accesscodecopy");
+//       copyText.select();
+//       copyText.setSelectionRange(0, 99999);
+//       document.execCommand("copy");
+//       element.innerHTML = "Code copied";
+//       setTimeout(function(){ element.innerHTML = "Copy code"; }, 4000);
+//     }
+//   }
+// }
+//
+// export default copyAccessCode;
+const copyAccessCode = () => {
+  let elements = document.querySelectorAll(".js-accesscodecopy");
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i];
+    let copyButton = element.querySelector('.js-accesscodecopy-button');
+    let copyButtonValue = element.querySelector('.js-accesscodecopy-value');
+    let copyDefaultMessage = element.querySelector('.js-accesscodecopy-default');
+    let copySuccessMessage = element.querySelector('.js-accesscodecopy-success');
+    copyButton.onclick = function() {
+      copyButtonValue.select();
+      copyButtonValue.setSelectionRange(0, 99999);
       document.execCommand("copy");
-      element.innerHTML = "Code copied";
-      setTimeout(function(){ element.innerHTML = "Copy code"; }, 4000);
-    }
+      copyDefaultMessage.classList.toggle('hide', true)
+      copySuccessMessage.classList.toggle('hide', false)
+      setTimeout(function()
+          {
+            copyDefaultMessage.classList.toggle('hide', false)
+            copySuccessMessage.classList.toggle('hide', true)
+          }
+          , 4000);
+    };
   }
 }
-
 export default copyAccessCode;
--- service-front/web/src/javascript/copyAccessCode.test.js
+++ service-front/web/src/javascript/copyAccessCode.test.js
@@ -1,3 +1,33 @@
+// import copyAccessCode from './copyAccessCode';
+// describe('given a copy access code is found', () => {
+//     beforeEach(() => {
+//         document.execCommand = jest.fn();
+//     });
+//     jest.useFakeTimers();
+//     test('it should copy the code', () => {
+//         document.body.innerHTML = `
+//       <input readonly type="text" value="this-is-a-code-test" id="accesscodecopy" class="offscreen" aria-hidden="true">
+//       <button id="copybutton" class="app-copy-button">Copy code</button>
+//     `;
+//         const button = document.getElementById("copybutton");
+//         copyAccessCode(button);
+//         expect(button.innerHTML).toBe("Copy code");
+//         button.click();
+//         expect(document.execCommand).toHaveBeenCalledWith('copy');
+//         expect(button.innerHTML).toBe("Code copied");
+//         jest.advanceTimersByTime(4000);
+//         expect(button.innerHTML).toBe("Copy code");
+//     });
+//     test('it should not copy the code if the id is not found', () => {
+//         document.body.innerHTML = `
+//       <input readonly type="text" value="this-is-a-code-test" id="accesscodecopy" class="offscreen" aria-hidden="true">
+//       <button class="app-copy-button">Copy code</button>
+//     `;
+//         const button = document.getElementById("copybutton");
+//         copyAccessCode(button);
+//         expect(button).toBeNull();
+//     });
+// });
 import copyAccessCode from './copyAccessCode';
 describe('given a copy access code is found', () => {
     beforeEach(() => {
@@ -6,25 +36,45 @@ describe('given a copy access code is found', () => {
     jest.useFakeTimers();
     test('it should copy the code', () => {
         document.body.innerHTML = `
-      <input readonly type="text" value="this-is-a-code-test" id="accesscodecopy" class="offscreen" aria-hidden="true">
-      <button id="copybutton" class="app-copy-button">Copy code</button>
+        <div class="js-accesscodecopy">
+        <input readonly type="text" value="12345" class="js-accesscodecopy-button offscreen" aria-hidden="true">
+        <button class="js-accesscodecopy-button govuk-button govuk-button__copy">
+            <span class="js-accesscodecopy-default">{% trans %}Copy code{% endtrans %}</span>
+            <span class="js-accesscodecopy-success hidden">{% trans %}Code copied{% endtrans %}</span>
+        </button>
+      </div>
     `;
-        const button = document.getElementById("copybutton");
-        copyAccessCode(button);
-        expect(button.innerHTML).toBe("Copy code");
+        const button = document.querySelector(".js-accesscodecopy-button");
+        const deafultText = document.querySelector(".js-accesscodecopy-default");
+        const successText = document.querySelector(".js-accesscodecopy-success");
+        copyAccessCode();
+        expect(deafultText.classList).toHaveLength(1);
+        expect(deafultText.classList).not.toContain('hidden');
+        expect(successText.classList).toHaveLength(2);
+        expect(successText.classList).toContain('hidden');
         button.click();
         expect(document.execCommand).toHaveBeenCalledWith('copy');
-        expect(button.innerHTML).toBe("Code copied");
+        expect(deafultText.classList).toHaveLength(2);
+        expect(deafultText.classList).toContain('hidden');
+        expect(successText.classList).toHaveLength(1);
+        expect(successText.classList.not).toContain('hidden');
         jest.advanceTimersByTime(4000);
-        expect(button.innerHTML).toBe("Copy code");
+        expect(deafultText.classList).toHaveLength(1);
+        expect(deafultText.classList).not.toContain('hidden');
+        expect(successText.classList).toHaveLength(2);
+        expect(successText.classList).toContain('hidden');
     });
     test('it should not copy the code if the id is not found', () => {
         document.body.innerHTML = `
-      <input readonly type="text" value="this-is-a-code-test" id="accesscodecopy" class="offscreen" aria-hidden="true">
-      <button class="app-copy-button">Copy code</button>
+        <div class="">
+        <input readonly type="text" value="{{ add_hyphen_to_viewer_code(code) }}" class="js-accesscodecopy-button offscreen" aria-hidden="true">
+        <button class="js-accesscodecopy-button govuk-button govuk-button__copy">
+            <span class="js-accesscodecopy-default">{% trans %}Copy code{% endtrans %}</span>
+            <span class="js-accesscodecopy-success hidden">{% trans %}Code copied{% endtrans %}</span>
+        </button>
+      </div>
     `;
-        const button = document.getElementById("copybutton");
-        copyAccessCode(button);
+        copyAccessCode();
         expect(button).toBeNull();
     });
 });
