d40c24b479815f8b800f9d180c37a6669435d35c
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-09-07 15:38:42 +0100

    refactored JS and created test with john's help

--- service-front/web/src/javascript/showHidePassword.js
+++ service-front/web/src/javascript/showHidePassword.js
@@ -1,40 +1,40 @@
 const showHidePassword = () => {
-    if (document.getElementById('showHidePassword')) {
+    let elements = document.querySelectorAll(".js-showhidepassword");
 
-        var link = document.getElementById('showHidePassword');
+    for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
+        let showHidePwdButton = element.querySelector('.js-showhidepassword-button');
+        let showHidePwdInput = element.querySelector('.js-showhidepassword-input');
+        let showHidePwdInputConfirm = element.querySelector('.js-showhidepassword-confirm');
+        let showPasswordText = showHidePwdButton.dataset.showpassword;
+        let hidePasswordText = showHidePwdButton.dataset.hidepassword;
 
-        link.addEventListener('click', function showHideToggle(e) {
+        showHidePwdButton.innerText = showPasswordText;
 
-            e.preventDefault();
-            var pwdConfirmParent = document.getElementById("password_confirm").parentElement;
-            var passwordElement = document.getElementById("password");
-
-            var pwd = this.getAttribute('data-for');
-            var hideConfirm = this.getAttribute('data-hideConfirmPassword');
+        showHidePwdButton.onclick = function () {
 
             //  Determine if we are showing or hiding the password confirm input
-            var isShowing = (pwd === "password");
+            let isShowing = (showHidePwdInput.getAttribute('type') === "password");
+
+            let showHidePwdInputHasConfirm = showHidePwdInputConfirm !== null;
 
             if (isShowing) {
-                if (hideConfirm) {
-                    pwdConfirmParent.hidden = true;
-                    passwordElement.setAttribute('type', 'text');
-                    this.setAttribute('data-for', 'password_confirm');
+                showHidePwdInput.setAttribute('type', 'text');
+                if (showHidePwdInputHasConfirm) {
+                    showHidePwdInputConfirm.parentElement.hidden = true;
                 }
             } else {
-                if (hideConfirm) {
-                    pwdConfirmParent.hidden = false;
-                    passwordElement.setAttribute('type', 'password');
-                    this.setAttribute('data-for', 'password');
+                showHidePwdInput.setAttribute('type', 'password');
+                if (showHidePwdInputHasConfirm) {
+                    showHidePwdInputConfirm.parentElement.hidden = false;
                 }
             }
 
             //  Change the link text
-            link.textContent = (isShowing ? 'Hide password' : 'Show password');
-
-            return false;
-        });
+            showHidePwdButton.innerText = (isShowing ? hidePasswordText : showPasswordText);
+        }
     }
+
 }
 
 export default showHidePassword;
--- service-front/web/src/javascript/showHidePassword.test.js
+++ service-front/web/src/javascript/showHidePassword.test.js
@@ -1,4 +1,71 @@
 import showHidePassword from './showHidePassword';
-describe('hides the confirm password box and displays password', () => {
-    // help
+describe('given a password and confirm password input is defined', () => {
+    test('it hides the confirm password box and changes the password type to text', () => {
+        document.body.innerHTML = `
+            <div class="js-showhidepassword">
+                <div class="govuk-form-group">
+                    <label class="govuk-label" for="password">
+                        Create a password
+                    </label>
+                    <input class="govuk-input js-showhidepassword-input" id="password" name="password" type="password" value="">
+            </div>
+
+            <button class="govuk-button govuk-button--secondary js-showhidepassword-button" data-module="govuk-button" type="button" data-showpassword="Show password" data-hidepassword="Hide password">Show password</button>
+
+            <div class="govuk-form-group" id="confirm-password">
+                <label class="govuk-label" for="password_confirm">
+                    Confirm your password
+                </label>
+                <input class="govuk-input js-showhidepassword-confirm" id="password_confirm" name="password_confirm" type="password" value="">
+            </div>
+    `;
+        const button = document.querySelector(".js-showhidepassword-button");
+        const confirmPassword = document.querySelector("#confirm-password");
+        const passwordInput = document.querySelector(".js-showhidepassword-input");
+
+        showHidePassword();
+        expect(confirmPassword.hidden).toBeFalsy();
+        expect(passwordInput.getAttribute('type')).toBe('password');
+        expect(button.innerText).toBe('Show password');
+        button.click();
+        expect(confirmPassword.hidden).toBeTruthy();
+        expect(passwordInput.getAttribute('type')).toBe('text');
+        expect(button.innerText).toBe('Hide password');
+        button.click();
+        expect(confirmPassword.hidden).toBeFalsy();
+        expect(passwordInput.getAttribute('type')).toBe('password');
+        expect(button.innerText).toBe('Show password');
+    });
+});
+
+describe('given only a password input is defined', () => {
+    test('it shows and hides the password as text', () => {
+        document.body.innerHTML = `
+            <div class="js-showhidepassword">
+                <div class="govuk-form-group">
+
+                    <label class="govuk-label" for="password">
+                        Create a password
+                    </label>
+
+                    <input class="govuk-input js-showhidepassword-input" id="password" name="password" type="password" value="">
+            </div>
+
+            <button class="govuk-button govuk-button--secondary js-showhidepassword-button" data-module="govuk-button" type="button" data-showpassword="Show password" data-hidepassword="Hide password">Show password</button>
+
+        </div>
+    `;
+        const button = document.querySelector(".js-showhidepassword-button");
+        const passwordInput = document.querySelector(".js-showhidepassword-input");
+
+        showHidePassword();
+        expect(passwordInput.getAttribute('type')).toBe('password');
+        expect(button.innerText).toBe('Show password');
+        button.click();
+        expect(passwordInput.getAttribute('type')).toBe('text');
+        expect(button.innerText).toBe('Hide password');
+        button.click();
+        expect(passwordInput.getAttribute('type')).toBe('password');
+        expect(button.innerText).toBe('Show password');
+    });
 });
