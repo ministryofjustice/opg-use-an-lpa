d92ac32344aeb7fa05354736786fb00afd3dd810
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-09-08 17:06:16 +0100

    added unit test for url validity class and updated po files

--- service-front/app/test/CommonTest/Service/Url/UrlValidityCheckServiceTest.php
+++ service-front/app/test/CommonTest/Service/Url/UrlValidityCheckServiceTest.php
@@ -36,12 +36,15 @@ class UrlValidityCheckServiceTest extends TestCase
      */
     private $urlHelperProphecy;
 
+    private $locale;
+
     public function setUp()
     {
         $this->serverRequestFactoryProphecy = $this->prophesize(ServerRequestFactory::class);
         $this->routerProphecy = $this->prophesize(RouterInterface::class);
         $this->serverRequestInterfaceProphecy = $this->prophesize(ServerRequestInterface::class);
         $this->urlHelperProphecy = $this->prophesize(UrlHelper::class);
+        $this->locale = "en_GB";
     }
 
     /** @test */
@@ -194,5 +197,73 @@ class UrlValidityCheckServiceTest extends TestCase
         $resultReferer = $service->setValidReferer(null);
 
         $this->assertEquals($homeUrl, $resultReferer);
+    }
+
+    /** @test */
+    public function it_returns_a_welsh_url_for_home_if_referer_is_invalid_and_locale_is_cy()
+    {
+        $this->locale = "cy";
+        $englishRefererUrl = 'https://use.lastingpowerofattorney.opg.service.justice.gov.uk/login';
+        $homeUrl = 'https://localhost:9002/home';
+        $welshHomeUrl = 'https://localhost:9002/cy/home';
+
+        $routeResult = $this->prophesize(RouteResult::class);
+        $requestReturn =  new ServerRequest(
+            [],
+            [],
+            $englishRefererUrl,
+            "method",
+            'php://temp'
+        );
+
+        $this->serverRequestFactoryProphecy->createServerRequest('GET', $englishRefererUrl)->willReturn($requestReturn);
+
+        $this->routerProphecy->match($requestReturn)->willReturn($routeResult->reveal());
+
+        $routeResult->isSuccess()->willReturn(false);
+
+        $service = new UrlValidityCheckService(
+            $this->serverRequestFactoryProphecy->reveal(),
+            $this->routerProphecy->reveal(),
+            $this->urlHelperProphecy->reveal()
+        );
+
+        $this->urlHelperProphecy->generate('home')->willReturn($homeUrl);
+
+        $resultReferer = $service->setValidReferer($refererUrl);
+
+        $this->assertEquals($welshHomeUrl, $resultReferer);
+    }
+
+    /** @test */
+    public function it_returns_a_valid_welsh_referer_if_locale_is_cy()
+    {
+        $this->locale = "cy";
+        $refererUrl = 'https://use.lastingpowerofattorney.opg.service.justice.gov.uk/cy/login';
+        $englishRefererUrl = 'https://use.lastingpowerofattorney.opg.service.justice.gov.uk/login';
+
+        $routeResult = $this->prophesize(RouteResult::class);
+        $requestReturn =  new ServerRequest(
+            [],
+            [],
+            $englishRefererUrl,
+            "method",
+            'php://temp'
+        );
+
+        $this->serverRequestFactoryProphecy->createServerRequest('GET', $englishRefererUrl)->willReturn($requestReturn);
+
+        $this->routerProphecy->match($requestReturn)->willReturn($routeResult->reveal());
+
+        $routeResult->isSuccess()->willReturn(true);
+
+        $service = new UrlValidityCheckService(
+            $this->serverRequestFactoryProphecy->reveal(),
+            $this->routerProphecy->reveal(),
+            $this->urlHelperProphecy->reveal()
+        );
+
+        $resultReferer = $service->setValidReferer($refererUrl);
+        $this->assertEquals($refererUrl, $resultReferer);
     }
 }
