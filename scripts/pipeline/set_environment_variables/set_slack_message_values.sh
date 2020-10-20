e7956cf69afc98d554c99e2873b0d060095ee80c
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-10-20 15:51:00 +0100

    print out "commit message" to see if quotes are escaped

--- scripts/pipeline/set_environment_variables/set_slack_message_values.sh
+++ scripts/pipeline/set_environment_variables/set_slack_message_values.sh
@@ -5,3 +5,4 @@ echo 'export USE_DOMAIN="$(jq -r .actor_fqdn /tmp/cluster_config.json)"'
 echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn /tmp/cluster_config.json)"'
 echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn /tmp/cluster_config.json)"'
 echo "export COMMIT_MESSAGE=$(git log -1 --pretty=%B | sed 's/\"/\\"/g')"
+echo $COMMIT_MESSAGE
