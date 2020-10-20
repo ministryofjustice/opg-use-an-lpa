f8ef105693e8ebf1c0bf2f1ba33c6039cda6ce77
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-10-20 16:14:42 +0100

    remove "all" double quotes

--- scripts/pipeline/set_environment_variables/set_slack_message_values.sh
+++ scripts/pipeline/set_environment_variables/set_slack_message_values.sh
@@ -4,5 +4,5 @@ export VIEW_DOMAIN="$(jq -r .viewer_fqdn /tmp/cluster_config.json)"
 export USE_DOMAIN="$(jq -r .actor_fqdn /tmp/cluster_config.json)"
 export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn /tmp/cluster_config.json)"
 export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn /tmp/cluster_config.json)"
-export COMMIT_MESSAGE="$(git log -1 --pretty=%B | sed 's/\"/\\"/g')"
+export COMMIT_MESSAGE="$(git log -1 --pretty=%B | sed 's/"//g')"
 echo $COMMIT_MESSAGE
