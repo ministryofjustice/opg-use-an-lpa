564284a2cfc632dee82c1a907cf927b0ca9ccf55
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-10-20 16:12:13 +0100

    putting "quotes in" to see if script works

--- scripts/pipeline/set_environment_variables/set_slack_message_values.sh
+++ scripts/pipeline/set_environment_variables/set_slack_message_values.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo 'export VIEW_DOMAIN="$(jq -r .viewer_fqdn /tmp/cluster_config.json)"'
-echo 'export USE_DOMAIN="$(jq -r .actor_fqdn /tmp/cluster_config.json)"'
-echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn /tmp/cluster_config.json)"'
-echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn /tmp/cluster_config.json)"'
-echo "export COMMIT_MESSAGE=$(git log -1 --pretty=%B | sed 's/\"/\\"/g')"
+export VIEW_DOMAIN="$(jq -r .viewer_fqdn /tmp/cluster_config.json)"
+export USE_DOMAIN="$(jq -r .actor_fqdn /tmp/cluster_config.json)"
+export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn /tmp/cluster_config.json)"
+export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn /tmp/cluster_config.json)"
+export COMMIT_MESSAGE="$(git log -1 --pretty=%B | sed 's/\"/\\"/g')"
 echo $COMMIT_MESSAGE
