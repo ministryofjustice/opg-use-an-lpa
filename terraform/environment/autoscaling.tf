9e66553b2204de0fa291e9de51eee4337bb0ec36
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-10-21 14:05:41 +0100

    add autoscaling for all ecs services

--- terraform/environment/autoscaling.tf
+++ terraform/environment/autoscaling.tf
@@ -7,3 +7,30 @@ module "view_ecs_autoscaling" {
   ecs_task_autoscaling_minimum     = local.account.autoscaling.view.minimum
   ecs_task_autoscaling_maximum     = local.account.autoscaling.view.maximum
 }
+module "use_ecs_autoscaling" {
+  source                           = "./modules/ecs_autoscaling"
+  environment                      = local.environment
+  aws_ecs_cluster_name             = aws_ecs_cluster.use-an-lpa.name
+  aws_ecs_service_name             = aws_ecs_service.actor.name
+  ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
+  ecs_task_autoscaling_minimum     = local.account.autoscaling.use.minimum
+  ecs_task_autoscaling_maximum     = local.account.autoscaling.use.maximum
+}
+module "api_ecs_autoscaling" {
+  source                           = "./modules/ecs_autoscaling"
+  environment                      = local.environment
+  aws_ecs_cluster_name             = aws_ecs_cluster.use-an-lpa.name
+  aws_ecs_service_name             = aws_ecs_service.api.name
+  ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
+  ecs_task_autoscaling_minimum     = local.account.autoscaling.api.minimum
+  ecs_task_autoscaling_maximum     = local.account.autoscaling.api.maximum
+}
+module "pdf_ecs_autoscaling" {
+  source                           = "./modules/ecs_autoscaling"
+  environment                      = local.environment
+  aws_ecs_cluster_name             = aws_ecs_cluster.use-an-lpa.name
+  aws_ecs_service_name             = aws_ecs_service.pdf.name
+  ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
+  ecs_task_autoscaling_minimum     = local.account.autoscaling.pdf.minimum
+  ecs_task_autoscaling_maximum     = local.account.autoscaling.pdf.maximum
+}
