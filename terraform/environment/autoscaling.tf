e65f8116f3212595a032285c26f985ca10e164ea
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-10-21 13:27:28 +0100

    add autoscaling for the view service

--- terraform/environment/autoscaling.tf
+++ terraform/environment/autoscaling.tf
@@ -1,9 +1,9 @@
-module "front_ecs_autoscaling" {
+module "view_ecs_autoscaling" {
   source                           = "./modules/ecs_autoscaling"
   environment                      = local.environment
   aws_ecs_cluster_name             = aws_ecs_cluster.use-an-lpa.name
-  aws_ecs_service_name             = aws_ecs_service.front.name
+  aws_ecs_service_name             = aws_ecs_service.viewer.name
   ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
-  ecs_task_autoscaling_minimum     = local.account.autoscaling.front.minimum
-  ecs_task_autoscaling_maximum     = local.account.autoscaling.front.maximum
+  ecs_task_autoscaling_minimum     = local.account.autoscaling.view.minimum
+  ecs_task_autoscaling_maximum     = local.account.autoscaling.view.maximum
 }
