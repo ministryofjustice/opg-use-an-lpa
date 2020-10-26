49ee35af71035b9a913105f4304897a40581b6f7
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-10-26 12:08:01 +0000

    split "commit" message into own function and "escape" all quotes

--- scripts/pipeline/set_environment_variables/set_slack_message_values.sh
+++ scripts/pipeline/set_environment_variables/set_slack_message_values.sh
@@ -4,5 +4,9 @@ export VIEW_DOMAIN="$(jq -r .viewer_fqdn /tmp/cluster_config.json)"
 export USE_DOMAIN="$(jq -r .actor_fqdn /tmp/cluster_config.json)"
 export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn /tmp/cluster_config.json)"
 export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn /tmp/cluster_config.json)"
-export COMMIT_MESSAGE="$(git log -1 --pretty=%B | sed 's/"//g')"
-echo $COMMIT_MESSAGE
+
+function get_commit_message() {
+  message=$(git log -1 --pretty=%B | sed 's/\"/\\"/g')
+  echo $message
+}
+export COMMIT_MESSAGE="$(get_commit_message)"
