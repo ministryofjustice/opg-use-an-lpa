4cf7085b46ef30f98f687921c925aa7f7291b2e6
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-11-24 12:00:38 +0000

    added start and end date parameters for scan

--- scripts/analysis_scripts/get_organisation_names.py
+++ scripts/analysis_scripts/get_organisation_names.py
@@ -4,14 +4,17 @@ import csv
 from boto3.dynamodb.conditions import Key
 from datetime import datetime
 from dateutil import parser
+from datetime import date
 
 class OrganisationsViewedChecker:
     aws_account_id = ''
     aws_iam_session = ''
     aws_dynamodb_client = ''
     environment = ''
+    start_date = ''
+    end_date = ''
 
-    def __init__(self, environment):
+    def __init__(self, environment, start_date, end_date):
         self.aws_account_ids = {
             'production': "690083044361",
             'preproduction': "888228022356",
@@ -20,6 +23,10 @@ class OrganisationsViewedChecker:
         self.environment = environment
         self.aws_account_id = self.aws_account_ids.get(
             self.environment, "367815980639")
+
+        self.start_date = date.fromisoformat(start_date)
+        self.end_date = date.fromisoformat(end_date)
+        assert self.start_date < self.end_date
 
         self.set_iam_role_session()
 
@@ -48,11 +55,15 @@ class OrganisationsViewedChecker:
     def get_viewer_codes(self):
         paginator = self.aws_dynamodb_client.get_paginator("scan")
         TableName = '{}-ViewerActivity'.format(self.environment)
-        FilterExpression = "attribute_exists(ViewedBy)"
+        FilterExpression = "attribute_exists(ViewedBy) AND Viewed BETWEEN :fromdate AND :todate"
 
         for page in paginator.paginate(
                 TableName=TableName,
-                FilterExpression=FilterExpression):
+                FilterExpression=FilterExpression,
+                ExpressionAttributeValues={
+                    ':fromdate': {'S': str(self.start_date)},
+                    ':todate': {'S': str(self.end_date)}
+                }):
             yield from page["Items"]
 
     def get_created_by_date(self, viewer_code):
@@ -66,6 +77,7 @@ class OrganisationsViewedChecker:
         return date
 
     def write_csv(self):
+        print('Collecting data and writing CSV...')
         n = 1
         with open('some.csv', 'w', newline='') as f:
             writer = csv.writer(
@@ -74,14 +86,12 @@ class OrganisationsViewedChecker:
             viewer_codes = self.get_viewer_codes()
 
             for Item in viewer_codes:
-                print(n)
                 org = Item['ViewedBy']['S']
                 viewed_date = parser.isoparse(Item['Viewed']['S']).date()
                 created_date = self.get_created_by_date(Item['ViewerCode']['S'])
-                #print(str(org), viewed_date, created_date)
                 writer.writerow([str(org), viewed_date, created_date])
                 n += 1
-            print("Done!")
+            print("Done! Collected {} records".format(n))
 
 
 def main():
@@ -91,9 +101,19 @@ def main():
                         default="production",
                         help="The environment to get organistion names for")
 
+    parser.add_argument("--start_date",
+                        default="2020-07-17",
+                        help="Where to start collecting data from. Defaults to the launch of the service")
+
+    parser.add_argument("--end_date",
+                        default=str(date.today()),
+                        help="Where to end collecting data. Defaults to today")
+
     args = parser.parse_args()
     work = OrganisationsViewedChecker(
-        args.environment)
+        args.environment,
+        args.start_date,
+        args.end_date)
     work.write_csv()
 
 
