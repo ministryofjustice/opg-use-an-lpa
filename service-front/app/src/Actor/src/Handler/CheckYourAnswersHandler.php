78295d45a8214e5d55324e6729e0ecce97f97930
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-12-08 12:28:09 +0000

    setup request key form, modified govuk_form to allow styling on fieldset, setup check your answers handler, route and twig file

---
+++ service-front/app/src/Actor/src/Handler/CheckYourAnswersHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Actor\Handler;
+
+use Common\Handler\AbstractHandler;
+use Common\Handler\Traits\User;
+use Common\Handler\UserAware;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Mezzio\Authentication\AuthenticationInterface;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CheckYourAnswersHandler extends AbstractHandler implements UserAware
+{
+    use User;
+
+    public function __construct(
+        TemplateRendererInterface $renderer,
+        AuthenticationInterface $authenticator,
+        UrlHelper $urlHelper
+    ) {
+        parent::__construct($renderer, $urlHelper);
+
+        $this->setAuthenticator($authenticator);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return new HtmlResponse($this->renderer->render('actor::check-your-answers', [
+            'user' => $this->getUser($request)
+        ]));
+    }
+}
--- service-front/app/src/Actor/src/Handler/RequestActivationKeyHandler.php
+++ service-front/app/src/Actor/src/Handler/RequestActivationKeyHandler.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Actor\Handler;
 
-use Common\Handler\{AbstractHandler, UserAware};
+use Common\Handler\{AbstractHandler, CsrfGuardAware, Traits\CsrfGuard, Traits\Session as SessionTrait, UserAware};
+use Actor\Form\RequestActivationKey;
 use Common\Handler\Traits\User;
 use Psr\Http\Message\{ResponseInterface, ServerRequestInterface};
 use Mezzio\Authentication\AuthenticationInterface;
@@ -17,9 +18,11 @@ use Laminas\Diactoros\Response\HtmlResponse;
  * @package Actor\Handler
  * @codeCoverageIgnore
  */
-class RequestActivationKeyHandler extends AbstractHandler implements UserAware
+class RequestActivationKeyHandler extends AbstractHandler implements UserAware, CsrfGuardAware
 {
     use User;
+    use CsrfGuard;
+    use SessionTrait;
 
     public function __construct(
         TemplateRendererInterface $renderer,
@@ -37,10 +40,49 @@ class RequestActivationKeyHandler extends AbstractHandler implements UserAware
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $user = $this->getUser($request);
+        $form = new RequestActivationKey($this->getCsrfGuard($request));
+
+        if ($request->getMethod() == 'POST') {
+            return $this->handlePost($request);
+        }
 
         return new HtmlResponse($this->renderer->render('actor::request-activation-key', [
-            'user' => $user
+            'user' => $this->getUser($request),
+            'form' => $form->prepare()
+        ]));
+    }
+
+    public function handlePost(ServerRequestInterface $request): ResponseInterface
+    {
+        $form = new RequestActivationKey($this->getCsrfGuard($request));
+        $form->setData($request->getParsedBody());
+
+        $session = $this->getSession($request, 'session');
+
+        if ($form->isValid()) {
+            $postData = $form->getData();
+
+            //  Convert the date of birth
+            $dobString = sprintf(
+                '%s-%s-%s',
+                $postData['dob']['year'],
+                $postData['dob']['month'],
+                $postData['dob']['day']
+            );
+
+            //  Set the data in the session and pass to the check your answers handler
+            $session->set('opg_reference_number', $postData['opg_reference_number']);
+            $session->set('first_names', $postData['first_names']);
+            $session->set('last_name', $postData['last_name']);
+            $session->set('dob', $dobString);
+            $session->set('postcode', $postData['postcode']);
+
+            return $this->redirectToRoute('lpa.check-answers');
+        }
+
+        return new HtmlResponse($this->renderer->render('actor::request-activation-key', [
+            'user' => $this->getUser($request),
+            'form' => $form->prepare()
         ]));
     }
 }
