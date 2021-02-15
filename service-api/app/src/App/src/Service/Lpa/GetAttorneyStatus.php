7eb0c08b0a657aac4b3b0a7dc7e5aef353c5473c
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2021-02-15 17:56:08 +0000

    Integrate new Validation class into current LpaService

--- service-api/app/src/App/src/Service/Lpa/GetAttorneyStatus.php
+++ service-api/app/src/App/src/Service/Lpa/GetAttorneyStatus.php
@@ -8,9 +8,9 @@ use Psr\Log\LoggerInterface;
 
 class GetAttorneyStatus
 {
-    private const ACTIVE_ATTORNEY = 0;
-    private const GHOST_ATTORNEY = 1;
-    private const INACTIVE_ATTORNEY = 2;
+    public const ACTIVE_ATTORNEY = 0;
+    public const GHOST_ATTORNEY = 1;
+    public const INACTIVE_ATTORNEY = 2;
 
     private LoggerInterface $logger;
 
@@ -22,12 +22,12 @@ class GetAttorneyStatus
     public function __invoke(array $attorney): int
     {
         if (empty($attorney['firstname']) && empty($attorney['surname'])) {
-            $this->logger->info('Looked up attorney {id} but is a ghost', ['id' => $attorney['uId']]);
+            $this->logger->debug('Looked up attorney {id} but is a ghost', ['id' => $attorney['uId']]);
             return self::GHOST_ATTORNEY;
         }
 
         if (!$attorney['systemStatus']) {
-            $this->logger->info('Looked up attorney {id} but is inactive', ['id' => $attorney['uId']]);
+            $this->logger->debug('Looked up attorney {id} but is inactive', ['id' => $attorney['uId']]);
             return self::INACTIVE_ATTORNEY;
         }
 
