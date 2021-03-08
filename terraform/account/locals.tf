05509761e176a4d704f1e0a88819ba1ce1adeded
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-03-08 10:42:31 +0000

    rename feature flag variable to meet coding standard

--- terraform/account/locals.tf
+++ terraform/account/locals.tf
@@ -8,11 +8,11 @@ variable "account_mapping" {
 variable "accounts" {
   type = map(
     object({
-      account_id             = string
-      is_production          = bool
-      retention_in_days      = number
-      pagerduty_service_name = string
-      has_metrics_queue      = bool
+      account_id                 = string
+      is_production              = bool
+      retention_in_days          = number
+      pagerduty_service_name     = string
+      ship_metrics_queue_enabled = bool
     })
   )
 }
