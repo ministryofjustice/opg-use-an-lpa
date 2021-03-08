3c0b587cc0d183f677ded00ccff1bc180afd1090
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-03-08 12:08:58 +0000

    conditionally create the queue

--- terraform/account/ship_metrics_queue.tf
+++ terraform/account/ship_metrics_queue.tf
@@ -1,5 +1,6 @@
 
 resource "aws_sqs_queue" "ship_to_opg_metrics" {
+  count                     = local.account.ship_metrics_queue_enabled == true ? 1 : 0
   name                      = "${local.environment}-ship-to-opg-metrics"
   delay_seconds             = 90
   max_message_size          = 2048
@@ -9,14 +10,16 @@ resource "aws_sqs_queue" "ship_to_opg_metrics" {
 }
 
 resource "aws_sqs_queue_policy" "ship_to_opg_metrics" {
-  queue_url = aws_sqs_queue.ship_to_opg_metrics.id
-  policy    = data.aws_iam_policy_document.ship_to_opg_metrics.json
+  count     = local.account.ship_metrics_queue_enabled == true ? 1 : 0
+  queue_url = aws_sqs_queue.ship_to_opg_metrics[0].id
+  policy    = data.aws_iam_policy_document.ship_to_opg_metrics[0].json
 }
 
 data "aws_iam_policy_document" "ship_to_opg_metrics" {
+  count = local.account.ship_metrics_queue_enabled == true ? 1 : 0
   statement {
     effect    = "Allow"
-    resources = [aws_sqs_queue.ship_to_opg_metrics.arn]
+    resources = [aws_sqs_queue.ship_to_opg_metrics[0].arn]
     actions = [
       "sqs:ChangeMessageVisibility",
       "sqs:DeleteMessage",
