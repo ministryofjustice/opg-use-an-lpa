25c31d2b76c3b52c7a5c6cf1937e5a58e83f1cd5
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-03-11 17:48:47 +0000

    create an ecr repository for each lambda function

---
+++ terraform/account/lambda_ecr.tf
@@ -0,0 +1,97 @@
+locals {
+  images = [
+    "${local.environment}/cslf_to_sqs",
+    "${local.environment}/ship_to_opg_metrics",
+  ]
+}
+
+resource "aws_ecr_repository" "lambda" {
+  for_each = toset(local.images)
+  name     = each.value
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository_policy" "lambda" {
+  for_each   = toset(values(aws_ecr_repository.lambda)[*].name)
+  policy     = data.aws_iam_policy_document.lambda.json
+  repository = each.value
+}
+
+resource "aws_ecr_lifecycle_policy" "lambda" {
+  for_each   = toset(values(aws_ecr_repository.lambda)[*].name)
+  repository = each.value
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images more than 10 production deployments ago",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["master", "main"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Expire untagged images quickly",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 3,
+            "description": "Expire all non-production tagged images older than 14 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    sid    = "UseAnLPALambdaAccountAccess"
+    effect = "Allow"
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+    ]
+
+    principals {
+      identifiers = [
+        "arn:aws:iam::${local.account.account_id}:root",
+      ]
+
+      type = "AWS"
+    }
+  }
+
+}
