b27143de68d01040eb284da14b87b0f4959d67f7
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-03-15 11:31:50 +0000

    use hyphens not underscores

--- terraform/environment/ship_events_to_opg_metrics.tf
+++ terraform/environment/ship_events_to_opg_metrics.tf
@@ -6,7 +6,7 @@ locals {
 }
 resource "aws_cloudwatch_log_subscription_filter" "events" {
   for_each        = local.account.ship_metrics_queue_enabled == true ? toset(local.clsf_event_codes) : []
-  name            = "${local.environment}_clsf_to_sqs_${lower(each.value)}"
+  name            = "${local.environment}-clsf-to-sqs-${lower(each.value)}"
   log_group_name  = aws_cloudwatch_log_group.application_logs.name
   filter_pattern  = "{$.context.event_code = ${each.value}}"
   destination_arn = data.aws_lambda_function.clsf_to_sqs[0].arn
