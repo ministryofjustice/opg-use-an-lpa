7020e8a76ba205895b8949b74f89600045a5030c
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-03-19 15:04:32 +0000

    use the actor lpa map table

--- scripts/analysis_scripts/count_lpas_per_account.py
+++ scripts/analysis_scripts/count_lpas_per_account.py
@@ -1,7 +1,6 @@
 import argparse
 import csv
 import boto3
-import numpy as np
 import pandas as pd
 
 class AccountLookup:
@@ -45,64 +44,46 @@ class AccountLookup:
         session = sts.assume_role(
             RoleArn=role_arn,
             RoleSessionName='getting_service_statistics',
-            DurationSeconds=900
+            DurationSeconds=3600
         )
         self.aws_iam_session = session
 
-    def get_actor_users(self):
+    def get_lpa_actor_maps(self):
         paginator = self.aws_dynamodb_client.get_paginator("scan")
         for page in paginator.paginate(
-                TableName='{}-ActorUsers'.format(self.environment),
-                FilterExpression="attribute_exists(Email)",
+                TableName='{}-UserLpaActorMap'.format(self.environment),
                 ):
             yield from page["Items"]
 
-    def get_lpas_by_user_id(self, user_id):
-        response = self.aws_dynamodb_client.query(
-            IndexName='UserIndex',
-            TableName='{}-UserLpaActorMap'.format(self.environment),
-            KeyConditionExpression='UserId = :user_id',
-            ExpressionAttributeValues={
-                ':user_id': {'S': user_id}
-            }
-        )
-        lpas = {}
-        for lpa in response['Items']:
-            lpas.update({lpa['SiriusUid']['S']:lpa['Added']['S']})
-        return lpas
+    def count_by_maps(self):
+        print("counting lpas per account...")
+        list_of_userids = []
+        indicator = 0
+        lpa_actor_maps = self.get_lpa_actor_maps()
 
-    def count_by_user(self):
-        list_of_counts = []
-        with open('lpas_per_account.csv', 'w', newline='') as file:
-            writer = csv.writer(
-                file, quoting=csv.QUOTE_NONNUMERIC)
-            writer.writerow(["lpas_in_account_{}".format(self.environment)])
+        try:
+          for lpa_map in lpa_actor_maps:
+            list_of_userids.append(lpa_map['UserId']['S'])
+            indicator += 1
+            if indicator % 1000 == 0:
+                print("Maps processed: ", indicator)
+        except:
+          print("An exception occurred")
 
-            actor_users = self.get_actor_users()
-
-            for user in actor_users:
-                lpas = self.get_lpas_by_user_id(user['Id']['S'])
-                count = len(lpas)
-                print(count)
-                list_of_counts.append(count)
-                writer.writerow([str(count)])
-
-        lpa_counts_series = pd.Series(list_of_counts)
-        print(lpa_counts_series.value_counts())
-
+        lpa_counts_series = pd.Series(list_of_userids)
+        s = lpa_counts_series.value_counts()
+        print(s.value_counts())
 
 def main():
     arguments = argparse.ArgumentParser(
         description="Look up an account by email address.")
     arguments.add_argument("--environment",
                         default="demo",
-                        help="The environment to target. Defaults to production")
+                        help="The environment to target. Defaults to demo")
 
     args = arguments.parse_args()
     work = AccountLookup(args.environment)
-    work.count_by_user()
-
-
+    work.count_by_maps()
 
 
 if __name__ == "__main__":
