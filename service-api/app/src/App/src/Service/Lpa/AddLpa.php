0d3a9c87c4f4972e34faaeb84cc5f24353b36525
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-04-01 11:23:42 +0100

    added logging for all possible outcomes

--- service-api/app/src/App/src/Service/Lpa/AddLpa.php
+++ service-api/app/src/App/src/Service/Lpa/AddLpa.php
@@ -41,19 +41,46 @@ class AddLpa
     public function validateAddLpaData(array $data, string $userId): array
     {
         if (null !== $lpaAddedData = ($this->lpaAlreadyAdded)($userId, $data['uid'])) {
+            $this->logger->notice(
+                'User {id} attempted to add an LPA {uId} which already exists in their account',
+                [
+                    'id' => $userId,
+                    'uId' => $data['uid']
+                ]
+            );
             throw new BadRequestException('LPA already added', $lpaAddedData);
         }
 
         $lpaData = $this->actorCodeService->validateDetails($data['actor-code'], $data['uid'], $data['dob']);
 
         if (!is_array($lpaData)) {
+            $this->logger->notice(
+                'Code validation failed for user {id}',
+                [
+                    'id' => $userId,
+                ]
+            );
             throw new NotFoundException('Code validation failed');
         }
 
         if (strtolower($lpaData['lpa']['status']) === 'registered') {
+            $this->logger->notice(
+                'User {id} has found an LPA with Id {uId} using their activation key',
+                [
+                    'id' => $userId,
+                    'uId' => $data['uid']
+                ]
+            );
             return $lpaData;
         }
 
+        $this->logger->notice(
+            'Failed to add an LPA for user {id} as the LPA {uId} status is not registered',
+            [
+                'id' => $userId,
+                'uId' => $data['uid']
+            ]
+        );
         throw new BadRequestException('LPA status is not registered');
     }
 }
