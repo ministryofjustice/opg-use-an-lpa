3ebac46037a8cd2ae86c53cf140f6c8ab2548e7a
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-04-09 10:18:41 +0100

    removed lpaServiceTests that we're no longer neeed

--- service-front/app/src/Common/src/Service/Lpa/LpaService.php
+++ service-front/app/src/Common/src/Service/Lpa/LpaService.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Common\Service\Lpa;
 
 use ArrayObject;
-use Common\Entity\Lpa;
 use Common\Exception\ApiException;
 use Common\Service\ApiClient\Client as ApiClient;
 use Common\Service\Log\EventCodes;
