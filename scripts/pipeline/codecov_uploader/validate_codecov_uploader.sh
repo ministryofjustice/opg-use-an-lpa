b9b9952629156adbffd5b6af10a1343052ce490b
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-04-20 15:25:31 +0100

    make executable

--- scripts/pipeline/codecov_uploader/validate_codecov_uploader.sh
+++ scripts/pipeline/codecov_uploader/validate_codecov_uploader.sh
@@ -6,6 +6,6 @@ echo $VERSION
 
 for i in 1 256 512
 do
-  shasum -a $i -c <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM") ||
+  shasum -a $i -c --ignore-missing <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM") ||
   shasum -a $i -c <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM")
 done
