b1e11d1d900a9e2cb89ffb772256351ace3d7e44
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-04-20 16:30:26 +0100

    pull codecov and verify

--- scripts/pipeline/codecov_uploader/validate_codecov_uploader.sh
+++ scripts/pipeline/codecov_uploader/validate_codecov_uploader.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+
+curl -s https://codecov.io/env > env;
 curl -s https://codecov.io/bash > codecov;
 VERSION=$(grep 'VERSION=\"[0-9\.]*\"' codecov | cut -d'"' -f2);
 echo $VERSION
 
 for i in 1 256 512
 do
-  shasum -a $i -c --ignore-missing <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM") ||
-  shasum -a $i -c <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM")
+  curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM" > codecov-hashes
+  shasum -a $i -c --ignore-missing codecov-hashes ||
+  shasum -a $i -c codecov-hashes
 done
