8dd22028686841e75e8fbfec4944c2021bd86b5c
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-04-20 14:49:16 +0100

    add script to validate codecov before running upload

---
+++ scripts/pipeline/codecov_uploader/validate_codecov_uploader.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+curl -s https://codecov.io/bash > codecov;
+VERSION=$(grep 'VERSION=\"[0-9\.]*\"' codecov | cut -d'"' -f2);
+echo $VERSION
+
+for i in 1 256 512
+do
+  shasum -a $i -c <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM") ||
+  shasum -a $i -c <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM")
+done
