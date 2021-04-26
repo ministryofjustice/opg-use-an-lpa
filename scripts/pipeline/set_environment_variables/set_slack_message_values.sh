aaae6799244b1d5c315cbdb3db88bacc0cc262c9
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-04-26 11:58:05 +0100

    clear, export and echo variables directly

--- scripts/pipeline/set_environment_variables/set_slack_message_values.sh
+++ scripts/pipeline/set_environment_variables/set_slack_message_values.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
-echo 'export VIEW_DOMAIN="$(jq -r .viewer_fqdn /tmp/cluster_config.json)"'
-echo 'export USE_DOMAIN="$(jq -r .actor_fqdn /tmp/cluster_config.json)"'
-echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn /tmp/cluster_config.json)"'
-echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn /tmp/cluster_config.json)"'
-echo 'export COMMIT_MESSAGE="$(git log -1 --pretty=%B)"'
+config_path=$1
+
+# echo 'export VIEW_DOMAIN="$(jq -r .viewer_fqdn $config_path)"'
+# echo 'export USE_DOMAIN="$(jq -r .actor_fqdn $config_path)"'
+# echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn $config_path)"'
+# echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn $config_path)"'
+# echo 'export COMMIT_MESSAGE="$(git log -1 --pretty=%B)"'
+
+unset $SET_VIEW_DOMAIN
+unset $SET_USE_DOMAIN
+unset $SET_PUBLIC_FACING_VIEW_DOMAIN
+unset $SET_PUBLIC_FACING_USE_DOMAIN
+unset $SET_COMMIT_MESSAGE
+
+export SET_VIEW_DOMAIN="$(jq -r .viewer_fqdn $config_path)"
+export SET_USE_DOMAIN="$(jq -r .actor_fqdn $config_path)"
+export SET_PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn $config_path)"
+export SET_PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn $config_path)"
+export SET_COMMIT_MESSAGE="$(git log -1 --pretty=%B)"
+
+# echo $SET_VIEW_DOMAIN
+# echo $SET_USE_DOMAIN
+# echo $SET_PUBLIC_FACING_VIEW_DOMAIN
+# echo $SET_PUBLIC_FACING_USE_DOMAIN
+echo $SET_COMMIT_MESSAGE
