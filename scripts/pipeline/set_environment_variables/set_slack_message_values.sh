64e548bfd08e09dd0d135e89fb205dda3e755acc
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-04-26 12:17:53 +0100

    echo exports to environment

--- scripts/pipeline/set_environment_variables/set_slack_message_values.sh
+++ scripts/pipeline/set_environment_variables/set_slack_message_values.sh
@@ -2,11 +2,6 @@
 
 config_path=$1
 
-# echo 'export VIEW_DOMAIN="$(jq -r .viewer_fqdn $config_path)"'
-# echo 'export USE_DOMAIN="$(jq -r .actor_fqdn $config_path)"'
-# echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn $config_path)"'
-# echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn $config_path)"'
-# echo 'export COMMIT_MESSAGE="$(git log -1 --pretty=%B)"'
 
 unset $SET_VIEW_DOMAIN
 unset $SET_USE_DOMAIN
@@ -14,11 +9,17 @@ unset $SET_PUBLIC_FACING_VIEW_DOMAIN
 unset $SET_PUBLIC_FACING_USE_DOMAIN
 unset $SET_COMMIT_MESSAGE
 
-export SET_VIEW_DOMAIN="$(jq -r .viewer_fqdn $config_path)"
-export SET_USE_DOMAIN="$(jq -r .actor_fqdn $config_path)"
-export SET_PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn $config_path)"
-export SET_PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn $config_path)"
-export SET_COMMIT_MESSAGE="$(git log -1 --pretty=%B)"
+echo 'export VIEW_DOMAIN="$(jq -r .viewer_fqdn $config_path)"'
+echo 'export USE_DOMAIN="$(jq -r .actor_fqdn $config_path)"'
+echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn $config_path)"'
+echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn $config_path)"'
+echo 'export COMMIT_MESSAGE="$(git log -1 --pretty=%B)"'
+
+# export SET_VIEW_DOMAIN="$(jq -r .viewer_fqdn $config_path)"
+# export SET_USE_DOMAIN="$(jq -r .actor_fqdn $config_path)"
+# export SET_PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn $config_path)"
+# export SET_PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn $config_path)"
+# export SET_COMMIT_MESSAGE="$(git log -1 --pretty=%B)"
 
 echo $SET_VIEW_DOMAIN
 echo $SET_USE_DOMAIN
