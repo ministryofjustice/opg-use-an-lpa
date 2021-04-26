f51dd1046356761d589a7346c1c82c3c10fd0e48
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-04-26 12:25:09 +0100

    echo to bash env inside script

--- scripts/pipeline/set_environment_variables/set_slack_message_values.sh
+++ scripts/pipeline/set_environment_variables/set_slack_message_values.sh
@@ -9,20 +9,14 @@ unset $SET_PUBLIC_FACING_VIEW_DOMAIN
 unset $SET_PUBLIC_FACING_USE_DOMAIN
 unset $SET_COMMIT_MESSAGE
 
-echo 'export VIEW_DOMAIN="$(jq -r .viewer_fqdn $config_path)"'
-echo 'export USE_DOMAIN="$(jq -r .actor_fqdn $config_path)"'
-echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn $config_path)"'
-echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn $config_path)"'
-echo 'export COMMIT_MESSAGE="$(git log -1 --pretty=%B)"'
+echo 'export VIEW_DOMAIN="$(jq -r .viewer_fqdn $config_path)"' >> $BASH_ENV
+echo 'export USE_DOMAIN="$(jq -r .actor_fqdn $config_path)"' >> $BASH_ENV
+echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn $config_path)"' >> $BASH_ENV
+echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn $config_path)"' >> $BASH_ENV
+echo 'export COMMIT_MESSAGE="$(git log -1 --pretty=%B)"' >> $BASH_ENV
 
 # export SET_VIEW_DOMAIN="$(jq -r .viewer_fqdn $config_path)"
 # export SET_USE_DOMAIN="$(jq -r .actor_fqdn $config_path)"
 # export SET_PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn $config_path)"
 # export SET_PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn $config_path)"
 # export SET_COMMIT_MESSAGE="$(git log -1 --pretty=%B)"
-
-echo $SET_VIEW_DOMAIN
-echo $SET_USE_DOMAIN
-echo $SET_PUBLIC_FACING_VIEW_DOMAIN
-echo $SET_PUBLIC_FACING_USE_DOMAIN
-echo $SET_COMMIT_MESSAGE
