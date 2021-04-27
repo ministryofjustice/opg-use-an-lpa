ce997175b961761ec8fdfe345910139ebc186558
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-04-27 14:10:23 +0100

    use input parameter so that path to config file can be consistently updated for both steps

--- scripts/pipeline/set_environment_variables/set_slack_message_values.sh
+++ scripts/pipeline/set_environment_variables/set_slack_message_values.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-echo 'export VIEW_DOMAIN="$(jq -r .viewer_fqdn /tmp/cluster_config.json)"'
-echo 'export USE_DOMAIN="$(jq -r .actor_fqdn /tmp/cluster_config.json)"'
-echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn /tmp/cluster_config.json)"'
-echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn /tmp/cluster_config.json)"'
+config_path=$1
+
+echo 'export VIEW_DOMAIN="$(jq -r .viewer_fqdn ${1})"'
+echo 'export USE_DOMAIN="$(jq -r .actor_fqdn ${1})"'
+echo 'export PUBLIC_FACING_VIEW_DOMAIN="$(jq -r .public_facing_view_fqdn ${1})"'
+echo 'export PUBLIC_FACING_USE_DOMAIN="$(jq -r .public_facing_use_fqdn ${1})"'
 echo 'export COMMIT_MESSAGE="$(git log -1 --pretty=%B)"'
