596201589db6b7ca698476eaeb307ccc8594950a
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-05-06 10:44:54 +0100

    reformat task definitions

--- terraform/environment/actor_ecs.tf
+++ terraform/environment/actor_ecs.tf
@@ -112,131 +112,127 @@ data "aws_iam_policy_document" "actor_permissions_role" {
 // Actor ECS Service Task Container level config
 
 locals {
-  actor_web = jsonencode(
-    {
-      "cpu" : 1,
-      "essential" : true,
-      "image" : "${data.aws_ecr_repository.use_an_lpa_front_web.repository_url}:${var.container_version}",
-      "mountPoints" : [],
-      "name" : "web",
-      "portMappings" : [
-        {
-          "containerPort" : 80,
-          "hostPort" : 80,
-          "protocol" : "tcp"
-        }
-      ],
-      "volumesFrom" : [],
-      "logConfiguration" : {
-        "logDriver" : "awslogs",
-        "options" : {
-          "awslogs-group" : aws_cloudwatch_log_group.application_logs.name,
-          "awslogs-region" : "eu-west-1",
-          "awslogs-stream-prefix" : "${local.environment}.actor-web.use-an-lpa"
-        }
+  actor_web = jsonencode({
+    cpu         = 1,
+    essential   = true,
+    image       = "${data.aws_ecr_repository.use_an_lpa_front_web.repository_url}=${var.container_version}",
+    mountPoints = [],
+    name        = "web",
+    portMappings = [{
+      hostPort      = 80,
+      containerPort = 80,
+      protocol      = "tcp"
+    }],
+    # volumesFrom = [],
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.application_logs.name,
+        awslogs-region        = "eu-west-1",
+        awslogs-stream-prefix = "${local.environment}.actor-web.use-an-lpa"
+      }
+    },
+    environment = [
+      {
+        name  = "APP_HOST",
+        value = "127.0.0.1"
       },
-      "environment" : [
-        {
-          "name" : "APP_HOST",
-          "value" : "127.0.0.1"
-        },
-        {
-          "name" : "APP_PORT",
-          "value" : "9000"
-        },
-        {
-          "name" : "TIMEOUT",
-          "value" : "60"
-        },
-        {
-          "name" : "CONTAINER_VERSION",
-          "value" : var.container_version
-      }]
+      {
+        name  = "APP_PORT",
+        value = "9000"
+      },
+      {
+        name  = "TIMEOUT",
+        value = "60"
+      },
+      {
+        name  = "CONTAINER_VERSION",
+        value = var.container_version
+    }]
   })
 
 
-  actor_app = jsonencode(
-    {
-      "cpu" : 1,
-      "essential" : true,
-      "image" : "${data.aws_ecr_repository.use_an_lpa_front_app.repository_url}:${var.container_version}",
-      "mountPoints" : [],
-      "name" : "app",
-      "portMappings" : [
-        {
-          "containerPort" : 9000,
-          "hostPort" : 9000,
-          "protocol" : "tcp"
-        }
-      ],
-      "volumesFrom" : [],
-      "logConfiguration" : {
-        "logDriver" : "awslogs",
-        "options" : {
-          "awslogs-group" : aws_cloudwatch_log_group.application_logs.name,
-          "awslogs-region" : "eu-west-1",
-          "awslogs-stream-prefix" : "${local.environment}.actor-app.use-an-lpa"
-        }
+  actor_app = jsonencode({
+    cpu         = 1,
+    essential   = true,
+    image       = "${data.aws_ecr_repository.use_an_lpa_front_app.repository_url}:${var.container_version}",
+    mountPoints = [],
+    name        = "app",
+    portMappings = [
+      {
+        containerPort = 9000,
+        hostPort      = 9000,
+        protocol      = "tcp"
+      }
+    ],
+    volumesFrom = [],
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.application_logs.name,
+        awslogs-region        = "eu-west-1",
+        awslogs-stream-prefix = "${local.environment}.actor-app.use-an-lpa"
+      }
+    },
+    secrets = [
+      {
+        name      = "NOTIFY_API_KEY",
+        valueFrom = data.aws_secretsmanager_secret.notify_api_key.arn
+    }],
+    environment = [
+      {
+        name  = "CONTEXT",
+        value = "actor"
       },
-      "secrets" : [
-        {
-          "name" : "NOTIFY_API_KEY",
-          "valueFrom" : data.aws_secretsmanager_secret.notify_api_key.arn
-      }],
-      "environment" : [
-        {
-          "name" : "CONTEXT",
-          "value" : "actor"
-        },
-        {
-          "name" : "KMS_SESSION_CMK_ALIAS",
-          "value" : data.aws_kms_alias.sessions_actor.name
-        },
-        {
-          "name" : "CONTAINER_VERSION",
-          "value" : var.container_version
-        },
-        {
-          "name" : "API_SERVICE_URL",
-          "value" : "http://${local.api_service_fqdn}"
-        },
-        {
-          "name" : "SESSION_EXPIRES",
-          "value" : local.account.session_expires_use
-        },
-        {
-          "name" : "SESSION_EXPIRY_WARNING",
-          "value" : local.account.session_expiry_warning
-        },
-        {
-          "name" : "COOKIE_EXPIRES",
-          "value" : local.account.cookie_expires_use
-        },
-        {
-          "name" : "GOOGLE_ANALYTICS_ID",
-          "value" : local.account.google_analytics_id_use
-        },
-        {
-          "name" : "LOGGING_LEVEL",
-          "value" : local.account.logging_level
-        },
-        {
-          "name" : "BRUTE_FORCE_CACHE_URL",
-          "value" : "tls://${data.aws_elasticache_replication_group.brute_force_cache_replication_group.primary_endpoint_address}"
-        },
-        {
-          "name" : "BRUTE_FORCE_CACHE_PORT",
-          "value" : data.aws_elasticache_replication_group.brute_force_cache_replication_group.port
-        },
-        {
-          "name" : "BRUTE_FORCE_CACHE_TIMEOUT",
-          "value" : "60"
-        },
-        {
-          "name" : "USE_OLDER_LPA_JOURNEY",
-          "value" : local.account.use_older_lpa_journey
-        }
-      ]
+      {
+        name  = "KMS_SESSION_CMK_ALIAS",
+        value = data.aws_kms_alias.sessions_actor.name
+      },
+      {
+        name  = "CONTAINER_VERSION",
+        value = var.container_version
+      },
+      {
+        name  = "API_SERVICE_URL",
+        value = "http=//${local.api_service_fqdn}"
+      },
+      {
+        name  = "SESSION_EXPIRES",
+        value = local.account.session_expires_use
+      },
+      {
+        name  = "SESSION_EXPIRY_WARNING",
+        value = local.account.session_expiry_warning
+      },
+      {
+        name  = "COOKIE_EXPIRES",
+        value = local.account.cookie_expires_use
+      },
+      {
+        name  = "GOOGLE_ANALYTICS_ID",
+        value = local.account.google_analytics_id_use
+      },
+      {
+        name  = "LOGGING_LEVEL",
+        value = local.account.logging_level
+      },
+      {
+        name  = "BRUTE_FORCE_CACHE_URL",
+        value = "tls=//${data.aws_elasticache_replication_group.brute_force_cache_replication_group.primary_endpoint_address}"
+      },
+      {
+        name  = "BRUTE_FORCE_CACHE_PORT",
+        value = data.aws_elasticache_replication_group.brute_force_cache_replication_group.port
+      },
+      {
+        name  = "BRUTE_FORCE_CACHE_TIMEOUT",
+        value = "60"
+      },
+      {
+        name  = "USE_OLDER_LPA_JOURNEY",
+        value = local.account.use_older_lpa_journey
+      }
+    ]
   })
 }
 
