f6c4da098d674a0865d3a26f8d7a56f17c0c5a83
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-05-06 16:04:53 +0100

    remove interpolation only expression

--- terraform/environment/actor_ecs.tf
+++ terraform/environment/actor_ecs.tf
@@ -130,7 +130,7 @@ locals {
       logConfiguration = {
         logDriver = "awslogs",
         options = {
-          awslogs-group         = "${aws_cloudwatch_log_group.application_logs.name}",
+          awslogs-group         = aws_cloudwatch_log_group.application_logs.name,
           awslogs-region        = "eu-west-1",
           awslogs-stream-prefix = "${local.environment}.actor-web.use-an-lpa"
         }
