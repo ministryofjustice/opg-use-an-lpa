bc8ebb88dd8f8d0f8bd0a9f2f29a492bd073a634
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-05-19 12:11:18 +0100

    create a cognito client for use with the admin app lb

--- terraform/environment/cognito_client.tf
+++ terraform/environment/cognito_client.tf
@@ -1,23 +1,29 @@
-# data "aws_cognito_user_pools" "use_a_lasting_power_of_attorney_admin" {
-#   name = "use-a-lasting-power-of-attorney-admin"
-# }
+data "aws_cognito_user_pools" "use_a_lasting_power_of_attorney_admin" {
+  name = "use-a-lasting-power-of-attorney-admin"
+}
 
 # # I think I should create a client per environment in order to create the correct URLs
-# resource "aws_cognito_user_pool_client" "use_a_lasting_power_of_attorney_admin" {
-#   name                                 = "${local.environment}-admin-alb-auth"
-#   user_pool_id                         = data.aws_cognito_user_pool.use_a_lasting_power_of_attorney_admin.id
-#   allowed_oauth_flows                  = ["code"]
-#   allowed_oauth_scopes                 = ["openid"]
-#   supported_identity_providers         = ["COGNITO"]
-#   allowed_oauth_flows_user_pool_client = true
+resource "aws_cognito_user_pool_client" "use_a_lasting_power_of_attorney_admin" {
+  name                                 = "${local.environment}-admin-alb-auth"
+  user_pool_id                         = tolist(data.aws_cognito_user_pools.use_a_lasting_power_of_attorney_admin.ids)[0]
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_scopes                 = ["openid"]
+  supported_identity_providers         = ["COGNITO"]
+  allowed_oauth_flows_user_pool_client = true
+  explicit_auth_flows                  = []
+  generate_secret                      = true
+  access_token_validity                = 0
+  id_token_validity                    = 0
+  read_attributes                      = []
+  write_attributes                     = []
 
-#   callback_urls = [
-#     "https://${aws_route53_record.admin-use-my-lpa.fqdn}/oauth2/idpresponse",
-#     "https://${aws_route53_record.admin-use-my-lpa.fqdn}"
-#   ]
-#   default_redirect_uri = "https://${aws_route53_record.admin-use-my-lpa.fqdn}/oauth2/idpresponse"
-#   logout_urls = [
-#     "https://${aws_route53_record.admin-use-my-lpa.fqdn}/logout",
-#     "https://${aws_route53_record.admin-use-my-lpa.fqdn}/"
-#   ]
-# }
+  callback_urls = [
+    "https://${aws_route53_record.admin_use_my_lpa[0].fqdn}/oauth2/idpresponse",
+    "https://${aws_route53_record.admin_use_my_lpa[0].fqdn}"
+  ]
+  default_redirect_uri = "https://${aws_route53_record.admin_use_my_lpa[0].fqdn}/oauth2/idpresponse"
+  logout_urls = [
+    "https://${aws_route53_record.admin_use_my_lpa[0].fqdn}/logout",
+    "https://${aws_route53_record.admin_use_my_lpa[0].fqdn}/"
+  ]
+}
