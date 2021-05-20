44bb608e4fefcc6ebb5fdc9d0d93ba7361f690f1
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-05-20 09:19:51 +0100

    remove certificates for custom cognito domains

--- terraform/environment/cognito_user_pool.tf
+++ terraform/environment/cognito_user_pool.tf
@@ -45,34 +45,3 @@ resource "aws_cognito_user_pool_domain" "use_a_lasting_power_of_attorney_admin" 
   domain       = "login-admin-lastingpowerofattorney"
   user_pool_id = aws_cognito_user_pool.use_a_lasting_power_of_attorney_admin.id
 }
-
-resource "aws_route53_record" "certificate_validation_login_admin" {
-  provider = aws.management
-  for_each = {
-    for dvo in aws_acm_certificate.certificate_login_admin.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.opg_service_justice_gov_uk.zone_id
-  depends_on      = [aws_acm_certificate.certificate_login_admin]
-}
-
-resource "aws_acm_certificate_validation" "certificate_validation_login_admin" {
-  # provider                = aws.identity
-  certificate_arn         = aws_acm_certificate.certificate_login_admin.arn
-  validation_record_fqdns = [for record in aws_route53_record.certificate_validation_login_admin : record.fqdn]
-}
-
-resource "aws_acm_certificate" "certificate_login_admin" {
-  # provider          = aws.identity
-  domain_name       = "login.admin.lastingpowerofattorney.opg.service.justice.gov.uk"
-  validation_method = "DNS"
-}
