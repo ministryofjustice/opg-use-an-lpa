beff641af059d365fb0aa51a2f22cfd57b3c001a
SeemaMenon <seema.workspace@gmail.com>
2021-06-03 10:31:39 +0100

    added form test

---
+++ service-front/app/test/ActorTest/Form/CreateNewActivationKeyTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ActorTest\Form;
+
+use Common\Form\CreateNewActivationKey;
+use Common\Form\AbstractForm;
+use Common\Form\Element\Csrf;
+use CommonTest\Form\{TestsLaminasForm, LaminasFormTests};
+use PHPUnit\Framework\TestCase;
+use Mezzio\Csrf\CsrfGuardInterface;
+use Laminas\Form\Element\{Radio, Hidden};
+
+class CreateNewActivationKeyTest extends TestCase implements TestsLaminasForm
+{
+    use LaminasFormTests;
+
+    /** @var CreateNewActivationKey */
+    protected $form;
+
+    public function getForm(): AbstractForm
+    {
+        return $this->form;
+    }
+
+    public function getFormName(): string
+    {
+        return 'create_new_activation_key';
+    }
+
+    public function getFormElements(): array
+    {
+        return [
+            '__csrf'                => Csrf::class,
+            'reference_number'      => Hidden::class,
+            'first_names'           => Hidden::class,
+            'last_name'             => Hidden::class,
+            'dob'                   => Hidden::class,
+            'postcode'              => Hidden::class,
+            'force_activation_key'  => Hidden::class,
+        ];
+    }
+
+    public function setUp()
+    {
+        $guardProphecy = $this->prophesize(CsrfGuardInterface::class);
+        $this->form = new CreateNewActivationKey($guardProphecy->reveal());
+    }
+}
