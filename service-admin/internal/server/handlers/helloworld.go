568240284c8a511f3caf96f7128835f656f5e8d8
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2021-06-03 14:49:34 +0100

    ensure we log out template rendering issues.

--- service-admin/internal/server/handlers/helloworld.go
+++ service-admin/internal/server/handlers/helloworld.go
@@ -11,7 +11,7 @@ func HelloHandler() http.HandlerFunc {
 		log.Ctx(r.Context()).Info().Msg("viewed the hello world page")
 
 		if err := GetTemplate(r.Context(), "helloworld.gohtml").ExecuteTemplate(w, "default", nil); err != nil {
-			log.Err(err)
+			log.Err(err).Msg("error whilst rendering template")
 		}
 	}
 }
