a2cb769ee2c25d5f29dc4d7d7b4050111e16cc10
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2021-06-11 17:13:21 +0100

    Test helloworld handler

---
+++ service-admin/internal/server/handlers/helloworld_test.go
@@ -0,0 +1,27 @@
+package handlers_test
+
+import (
+	"context"
+	"net/http/httptest"
+	"testing"
+
+	. "github.com/ministryofjustice/opg-use-an-lpa/service-admin/internal/server/handlers"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func TestHelloHandler(t *testing.T) {
+	t.Parallel()
+
+	log.Logger = zerolog.Nop()
+
+	rw := httptest.NewRecorder()
+
+	rq := httptest.NewRequest("", "/", nil)
+	rq = rq.WithContext(context.WithValue(context.Background(), TemplateContextKey, &mockTemplates{}))
+
+	handler := HelloHandler()
+
+	// will fail to find template in mockTemplates so tests failure case at this time
+	handler(rw, rq)
+}
