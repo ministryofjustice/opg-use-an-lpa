0155e70978bcb43a8ffe277327e9a040f72d9619
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2021-06-11 16:56:35 +0100

    Finish off testing of the handlers file

--- service-admin/internal/server/handlers/handler_test.go
+++ service-admin/internal/server/handlers/handler_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"html/template"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	. "github.com/ministryofjustice/opg-use-an-lpa/service-admin/internal/server/handlers"
@@ -16,10 +18,21 @@ type mockTemplates struct{}
 
 func (m *mockTemplates) Get(name string) (*template.Template, error) {
 	if name == "test" {
-		return &template.Template{}, nil
+		return mockTemplate(), nil
+	} else if name == "badlayout" {
+		return template.New("").Parse("Template Content")
 	} else {
 		return nil, errors.New("template not found")
 	}
+}
+
+func mockTemplate() *template.Template {
+	t, err := template.New("default").Parse("Template Content")
+	if err != nil {
+		panic("creation of mock template failed")
+	}
+
+	return t
 }
 
 func TestGetTemplate(t *testing.T) {
@@ -44,7 +57,7 @@ func TestGetTemplate(t *testing.T) {
 				ctx:  context.WithValue(context.Background(), TemplateContextKey, &mockTemplates{}),
 				name: "test",
 			},
-			want:    &template.Template{},
+			want:    mockTemplate(),
 			wantErr: false,
 		},
 		{
@@ -79,6 +92,68 @@ func TestGetTemplate(t *testing.T) {
 			}
 
 			assert.Equal(t, got, tt.want, "GetTemplate() = %v, want %v", got, tt.want)
+		})
+	}
+}
+
+func TestRenderTemplate(t *testing.T) {
+	t.Parallel()
+
+	log.Logger = zerolog.Nop()
+
+	type args struct {
+		w    http.ResponseWriter
+		ctx  context.Context
+		name string
+		data interface{}
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "it successfully renders a given template",
+			args: args{
+				w:    httptest.NewRecorder(),
+				ctx:  context.WithValue(context.Background(), TemplateContextKey, &mockTemplates{}),
+				name: "test",
+				data: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "it errors when template not in context templates",
+			args: args{
+				w:    httptest.NewRecorder(),
+				ctx:  context.WithValue(context.Background(), TemplateContextKey, &mockTemplates{}),
+				name: "notfound",
+				data: nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "it errors when layout not in template",
+			args: args{
+				w:    httptest.NewRecorder(),
+				ctx:  context.WithValue(context.Background(), TemplateContextKey, &mockTemplates{}),
+				name: "badlayout",
+				data: nil,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if err := RenderTemplate(tt.args.w, tt.args.ctx, tt.args.name, tt.args.data); (err != nil) != tt.wantErr {
+				t.Errorf("RenderTemplate() error = %v, wantErr %v", err, tt.wantErr)
+			}
 		})
 	}
 }
