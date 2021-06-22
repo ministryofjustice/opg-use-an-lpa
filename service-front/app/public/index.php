31c0e62150926199e4a0db0fb6a678f2214524f0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2021-06-22 09:49:23 +0100

    Update service-front/app/public/index.php

--- service-front/app/public/index.php
+++ service-front/app/public/index.php
@@ -7,7 +7,7 @@ if (PHP_SAPI==='cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
 
-chdir(dirname(__DIR__));require 'vendor/autoload.php';
+chdir(dirname(__DIR__));require 'vendor/autoload.php'; 
 
 /**
  * Self-called anonymous function that creates its own scope and keep the global namespace clean.
