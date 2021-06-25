2daaec0440c35b683d3ae4933dd19ce940dde359
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2021-06-25 11:57:54 +0100

    add some mocked out search results

--- service-admin/internal/server/handlers/search.go
+++ service-admin/internal/server/handlers/search.go
@@ -11,22 +11,32 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+const (
+	EmailQuery queryType = iota
+	ActivationCodeQuery
+)
+
+var (
+	//nolint:stylecheck // actual message shown to users
+	ErrNotEmailOrCode error = errors.New("Enter an email address or activation code")
+
+	activationCodeRegexp *regexp.Regexp = regexp.MustCompile(`(?i)^c(-|)[a-z0-9]{4}(-|)[a-z0-9]{4}(-|)[a-z0-9]{4}$`)
+)
+
+type queryType int
+
 type search struct {
 	Query   string
+	Type    queryType
 	Results map[string]interface{}
 	Errors  validation.Errors
 }
-
-var (
-	ErrIsNotEmailOrCode  error          = errors.New("Enter an email address or activation code") //nolint:stylecheck // actual message shown to users
-	activationCodeRegexp *regexp.Regexp = regexp.MustCompile(`(?i)^C?(-|)[a-z0-9]{4}(-|)[a-z0-9]{4}(-|)[a-z0-9]{4}$`)
-)
 
 func (s *search) Validate() error {
 	e := validation.ValidateStruct(s,
 		validation.Field(&s.Query,
 			validation.Required.Error("Enter a search query"),
-			validation.By(checkEmailOrCode)),
+			validation.By(s.checkEmailOrCode)),
 	)
 
 	if errs, ok := e.(validation.Errors); ok {
@@ -36,23 +46,25 @@ func (s *search) Validate() error {
 	return e
 }
 
-func checkEmailOrCode(value interface{}) error {
+func (s *search) checkEmailOrCode(value interface{}) error {
 	isEmail := is.Email.Validate(value)
-
-	isCode := validation.Match(activationCodeRegexp).Validate(value)
-
-	if isEmail != nil && isCode != nil {
-		return ErrIsNotEmailOrCode
+	if isEmail == nil {
+		s.Type = EmailQuery
+		return nil
 	}
 
-	return nil
+	isCode := validation.Match(activationCodeRegexp).Validate(value)
+	if isCode == nil {
+		s.Type = ActivationCodeQuery
+		return nil
+	}
+
+	return ErrNotEmailOrCode
 }
 
 func SearchHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		s := &search{
-			Query: "",
-		}
+		s := &search{}
 
 		if r.Method == "POST" {
 			err := r.ParseForm()
@@ -64,12 +76,30 @@ func SearchHandler() http.HandlerFunc {
 
 			err = s.Validate()
 			if err != nil {
-				log.Error().Err(err).Msg("")
+				log.Debug().AnErr("form-error", err).Msg("")
 			}
+
+			s.Results = doSearch(s.Type, s.Query)
 		}
 
 		if err := RenderTemplate(w, r.Context(), "search", s); err != nil {
 			log.Panic().Err(err).Msg(err.Error())
 		}
 	}
+}
+
+func doSearch(t queryType, q string) map[string]interface{} {
+	switch t {
+	case EmailQuery:
+		return nil
+	case ActivationCodeQuery:
+		return map[string]interface{}{
+			"Activation key": q,
+			"Used":           "Yes",
+			"DateTime":       "2021-03-01T12:37:12Z+0000",
+			"Email":          "name@email.com",
+		}
+	}
+
+	return nil
 }
