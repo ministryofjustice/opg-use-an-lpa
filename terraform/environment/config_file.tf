c8d037a38bd1c0c55d17cb950a9bfa09515ea8f4
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-07-14 10:08:06 +0100

    add admin fqdn to config file

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -13,7 +13,7 @@ locals {
     user_lpa_actor_map                       = aws_dynamodb_table.user_lpa_actor_map.name
     actor_fqdn                               = aws_route53_record.actor-use-my-lpa.fqdn
     viewer_fqdn                              = aws_route53_record.viewer-use-my-lpa.fqdn
-    admin_fqdn                               = aws_route53_record.admin_use_my_lpa[0].fqdn
+    admin_fqdn                               = local.account.build_admin == true ? aws_route53_record.admin_use_my_lpa[0].fqdn : ""
     public_facing_use_fqdn                   = aws_route53_record.public_facing_use_lasting_power_of_attorney.fqdn
     public_facing_view_fqdn                  = aws_route53_record.public_facing_view_lasting_power_of_attorney.fqdn
     viewer_load_balancer_security_group_name = aws_security_group.viewer_loadbalancer.name
