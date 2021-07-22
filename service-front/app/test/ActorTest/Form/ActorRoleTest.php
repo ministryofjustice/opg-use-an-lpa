6aa31a1e3856f935dc83e43993bde8607e31bd57
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-07-22 15:16:07 +0100

    created form unit test for actor role form

---
+++ service-front/app/test/ActorTest/Form/ActorRoleTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ActorTest\Form;
+
+use Actor\Form\ActorRole;
+use Common\Form\AbstractForm;
+use Common\Form\Element\Csrf;
+use CommonTest\Form\LaminasFormTests;
+use CommonTest\Form\TestsLaminasForm;
+use Laminas\Form\Element\Radio;
+use Mezzio\Csrf\CsrfGuardInterface;
+use PHPUnit\Framework\TestCase;
+
+class ActorRoleTest extends TestCase implements TestsLaminasForm
+{
+    use LaminasFormTests;
+
+    protected ActorRole $form;
+
+    public function getForm(): AbstractForm
+    {
+        return $this->form;
+    }
+
+    public function getFormName(): string
+    {
+        return 'actor_role';
+    }
+
+    public function getFormElements(): array
+    {
+        return [
+            '__csrf'           => Csrf::class,
+            'actor_role_radio' => Radio::class
+        ];
+    }
+
+    public function setUp()
+    {
+        $guardProphecy = $this->prophesize(CsrfGuardInterface::class);
+        $this->form = new ActorRole($guardProphecy->reveal());
+    }
+
+}
