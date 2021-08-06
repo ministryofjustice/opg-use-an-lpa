c32a46b557c59b72c2887495626e5f97b7e339f7
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-08-06 16:04:35 +0100

    unset donor details in actor role handler if user selects donor option

--- service-front/app/src/Actor/src/Handler/RequestActivationKey/ActorRoleHandler.php
+++ service-front/app/src/Actor/src/Handler/RequestActivationKey/ActorRoleHandler.php
@@ -7,13 +7,16 @@ namespace Actor\Handler\RequestActivationKey;
 use Actor\Form\RequestActivationKey\ActorRole;
 use Common\Handler\AbstractHandler;
 use Common\Handler\CsrfGuardAware;
+use Common\Handler\SessionAware;
 use Common\Handler\Traits\CsrfGuard;
+use Common\Handler\Traits\Session as SessionTrait;
 use Common\Handler\Traits\User;
 use Common\Handler\UserAware;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Mezzio\Authentication\AuthenticationInterface;
 use Mezzio\Authentication\UserInterface;
 use Mezzio\Helper\UrlHelper;
+use Mezzio\Session\SessionInterface;
 use Mezzio\Template\TemplateRendererInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -23,12 +26,14 @@ use Psr\Http\Message\ServerRequestInterface;
  * @package Actor\RequestActivationKey\Handler
  * @codeCoverageIgnore
  */
-class ActorRoleHandler extends AbstractHandler implements UserAware, CsrfGuardAware
+class ActorRoleHandler extends AbstractHandler implements UserAware, CsrfGuardAware, SessionAware
 {
     use User;
     use CsrfGuard;
+    use SessionTrait;
 
     private ActorRole $form;
+    private ?SessionInterface $session;
     private ?UserInterface $user;
 
     public function __construct(
@@ -48,6 +53,7 @@ class ActorRoleHandler extends AbstractHandler implements UserAware, CsrfGuardAw
     {
         $this->form = new ActorRole($this->getCsrfGuard($request));
         $this->user = $this->getUser($request);
+        $this->session = $this->getSession($request, 'session');
 
         if ($request->getMethod() == 'POST') {
             return $this->handlePost($request);
@@ -74,6 +80,10 @@ class ActorRoleHandler extends AbstractHandler implements UserAware, CsrfGuardAw
             $selected = $this->form->getData()['actor_role_radio'];
 
             if ($selected === 'Donor') {
+                // these will have been set if the actor was the attorney for a previous request
+                $this->session->unset('donor_firstnames');
+                $this->session->unset('donor_lastname');
+                $this->session->unset('donor_dob');
                 return $this->redirectToRoute('lpa.add.contact-details');
             } else {
                 return $this->redirectToRoute('lpa.add.donor-details');
