e76e1ee3514ca21696d7ddf640ec7f1c28956cff
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-08-10 12:05:23 +0100

    remove unused comment

--- terraform/environment/actor_load_balancer.tf
+++ terraform/environment/actor_load_balancer.tf
@@ -49,7 +49,7 @@ resource "aws_lb_listener" "actor_loadbalancer" {
   load_balancer_arn = aws_lb.actor.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-FS-1-2-2019-08" #forward security
+  ssl_policy        = "ELBSecurityPolicy-FS-1-2-2019-08"
 
   certificate_arn = data.aws_acm_certificate.certificate_use.arn
 
