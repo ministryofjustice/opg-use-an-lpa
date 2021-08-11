4bf229a22e098059a7e122361dca0ba09e64ea66
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-08-11 10:49:37 +0100

    use new module to create combined step autoscling

--- terraform/environment/autoscaling.tf
+++ terraform/environment/autoscaling.tf
@@ -1,13 +1,14 @@
-# module "view_ecs_autoscaling" {
-#   source                           = "./modules/ecs_autoscaling"
-#   environment                      = local.environment
-#   aws_ecs_cluster_name             = aws_ecs_cluster.use-an-lpa.name
-#   aws_ecs_service_name             = aws_ecs_service.viewer.name
-#   ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
-#   ecs_task_autoscaling_minimum     = local.account.autoscaling.view.minimum
-#   ecs_task_autoscaling_maximum     = local.account.autoscaling.view.maximum
-#   max_scaling_alarm_actions        = [aws_sns_topic.cloudwatch_to_pagerduty.arn]
-# }
+module "view_ecs_autoscaling" {
+  source                           = "./modules/ecs_autoscaling_combined"
+  environment                      = local.environment
+  aws_ecs_cluster_name             = aws_ecs_cluster.use-an-lpa.name
+  aws_ecs_service_name             = aws_ecs_service.viewer.name
+  ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
+  ecs_task_autoscaling_minimum     = local.account.autoscaling.view.minimum
+  ecs_task_autoscaling_maximum     = local.account.autoscaling.view.maximum
+  max_scaling_alarm_actions        = [aws_sns_topic.cloudwatch_to_pagerduty.arn]
+}
+
 module "use_ecs_autoscaling" {
   source                           = "./modules/ecs_autoscaling"
   environment                      = local.environment
