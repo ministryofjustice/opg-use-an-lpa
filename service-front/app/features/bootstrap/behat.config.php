2ba40888ad7d3c34d85f56ca3a7a87251e3f2dea
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-09-08 09:17:35 +0100

    removed allow_older_lpas and save_older_lpa_requests feature flags from config in front as theyre no longer needed

--- service-front/app/features/bootstrap/behat.config.php
+++ service-front/app/features/bootstrap/behat.config.php
@@ -27,9 +27,7 @@ return [
 
     'feature_flags' => [
         'use_older_lpa_journey' => true,
-        'delete_lpa_feature'    => true,
-        'allow_older_lpas'      => true,
-        'save_older_lpa_requests' => true,
+        'delete_lpa_feature'    => true
     ],
 
     'notify' => [
