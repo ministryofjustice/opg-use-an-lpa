a0e4105ae13637a5ed343a0f916c9645776a62ff
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-09-09 12:02:49 +0100

    block all public and cross account access at account level

--- terraform/account/s3.tf
+++ terraform/account/s3.tf
@@ -2,6 +2,14 @@ data "aws_elb_service_account" "main" {
   region = "eu-west-1"
 }
 
+resource "aws_s3_account_public_access_block" "block_all" {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
 data "aws_iam_policy_document" "viewer_loadbalancer" {
   statement {
     sid = "accessLogBucketAccess"
