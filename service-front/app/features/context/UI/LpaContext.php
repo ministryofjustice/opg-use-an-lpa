ce16d663b23a8e79d3a6275cfec49041ffae3761
lukejolliffe <84718230+lukejolliffe@users.noreply.github.com>
2021-09-15 17:09:31 +0100

    Test to check content of banner changes on flag

--- service-front/app/features/context/UI/LpaContext.php
+++ service-front/app/features/context/UI/LpaContext.php
@@ -506,13 +506,25 @@ class LpaContext implements Context
     }
 
     /**
-     * @Then /^I can see banner about existing LPAs$/
+     * @Then /^I can see banner about existing LPAs pre 2019$/
      */
 
-    public function iCanSeeBannerAboutExistingLPAs()
+    public function iCanSeeBannerAboutExistingLPAsPre2019()
     {
         $this->ui->assertElementOnPage(".moj-banner__message");
+        $this->ui->assertElementContainsText(".moj-banner__message","2019");
     }
+
+    /**
+     * @Then /^I can see banner about existing LPAs after 2016$/
+     */
+
+    public function iCanSeeBannerAboutExistingLPAsAfter2016()
+    {
+        $this->ui->assertElementOnPage(".moj-banner__message");
+        $this->ui->assertElementContainsText(".moj-banner__message","2016");
+    }
+
 
     /**
      * @Then /^I can see (.*) link along with the instructions or preference message$/
