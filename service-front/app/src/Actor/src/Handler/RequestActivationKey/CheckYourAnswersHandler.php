cc062111caf81c6905bd8893da1af4b27bb767f2
lukejolliffe <84718230+lukejolliffe@users.noreply.github.com>
2021-09-24 11:36:16 +0100

    Add new lpa not found page

--- service-front/app/src/Actor/src/Handler/RequestActivationKey/CheckYourAnswersHandler.php
+++ service-front/app/src/Actor/src/Handler/RequestActivationKey/CheckYourAnswersHandler.php
@@ -195,6 +195,17 @@ class CheckYourAnswersHandler extends AbstractHandler implements UserAware, Csrf
                         ));
                     }
                 case OlderLpaApiResponse::NOT_FOUND:
+                    if (($this->featureEnabled)('allow_older_lpas')) {
+                        return new HtmlResponse(
+                            $this->renderer->render(
+                                'actor::lpa-not-found-with-reference-number',
+                                [
+                                    'user' => $this->user,
+                                    'lpa_reference_number' => $this->data['reference_number']
+                                ]
+                            )
+                        );
+                    }
                     return new HtmlResponse($this->renderer->render(
                         'actor::cannot-find-lpa',
                         ['user'  => $this->user]
