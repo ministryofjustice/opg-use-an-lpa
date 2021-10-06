7630a81bf97a86551b105cde5c8cf18ddb1432d2
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-10-06 17:37:37 +0100

    allow execution_role to decrypt secrets manager secrets

--- terraform/environment/ecs_cluster.tf
+++ terraform/environment/ecs_cluster.tf
@@ -66,4 +66,15 @@ data "aws_iam_policy_document" "execution_role" {
       "secretsmanager:GetSecretValue",
     ]
   }
+  statement {
+    effect = "Allow"
+
+    resources = [data.aws_kms_alias.secrets_manager.target_key_arn]
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+  }
 }
