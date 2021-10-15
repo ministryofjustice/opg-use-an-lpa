15618f3fc3500c207f76720af79c028bd88d354b
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-10-15 14:55:59 +0100

    remove default tags on individual resources

--- terraform/environment/cloudwatch_log_groups.tf
+++ terraform/environment/cloudwatch_log_groups.tf
@@ -2,10 +2,7 @@ resource "aws_cloudwatch_log_group" "application_logs" {
   name              = "${local.environment}_application_logs"
   retention_in_days = local.account.log_retention_in_days
 
-  tags = merge(
-    local.default_tags,
-    {
-      "Name" = "${local.environment}_application_logs"
-    },
-  )
+  tags = {
+    "Name" = "${local.environment}_application_logs"
+  }
 }
