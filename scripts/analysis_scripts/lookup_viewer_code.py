da6f3544c9d9aad23dcbf2f68f4dd8a4bed5bd02
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-10-15 13:45:33 +0100

    linting

--- scripts/analysis_scripts/lookup_viewer_code.py
+++ scripts/analysis_scripts/lookup_viewer_code.py
@@ -4,17 +4,17 @@ import boto3
 
 
 class AccountLookup:
-    aws_account_id = ''
-    aws_iam_session = ''
-    aws_dynamodb_client = ''
-    environment = ''
+    aws_account_id = ""
+    aws_iam_session = ""
+    aws_dynamodb_client = ""
+    environment = ""
     output_json = {}
 
     def __init__(self, environment):
         aws_account_ids = {
-            'production': "690083044361",
-            'preproduction': "888228022356",
-            'development': "367815980639",
+            "production": "690083044361",
+            "preproduction": "888228022356",
+            "development": "367815980639",
         }
         self.environment = environment
 
@@ -24,57 +24,55 @@ class AccountLookup:
         self.set_iam_role_session()
 
         self.aws_dynamodb_client = boto3.client(
-            'dynamodb',
-            region_name='eu-west-1',
+            "dynamodb",
+            region_name="eu-west-1",
             aws_access_key_id=self.aws_iam_session['Credentials']['AccessKeyId'],
             aws_secret_access_key=self.aws_iam_session['Credentials']['SecretAccessKey'],
             aws_session_token=self.aws_iam_session['Credentials']['SessionToken'])
 
     def set_iam_role_session(self):
         if self.environment == "production":
-            role_arn = 'arn:aws:iam::{}:role/read-only-db'.format(
-                self.aws_account_id)
+            role_arn = f"arn:aws:iam::{self.aws_account_id}:role/read-only-db"
         else:
-            role_arn = 'arn:aws:iam::{}:role/operator'.format(
-                self.aws_account_id)
+            role_arn = f"arn:aws:iam::{self.aws_account_id}:role/operator"
 
         sts = boto3.client(
-            'sts',
-            region_name='eu-west-1',
+            "sts",
+            region_name="eu-west-1",
         )
         session = sts.assume_role(
             RoleArn=role_arn,
-            RoleSessionName='getting_service_statistics',
+            RoleSessionName="getting_service_statistics",
             DurationSeconds=900
         )
         self.aws_iam_session = session
 
     def get_viewer_code(self, viewer_code):
         response = self.aws_dynamodb_client.query(
-            TableName='{}-ViewerCodes'.format(self.environment),
-            KeyConditionExpression='ViewerCode = :viewer_code',
+            TableName=f"{self.environment}-ViewerCodes",
+            KeyConditionExpression="ViewerCode = :viewer_code",
             ExpressionAttributeValues={
-                ':viewer_code': {'S': viewer_code}
+                ":viewer_code": {"S": viewer_code}
             }
         )
         return response['Items'][0]
 
     def get_user_lpa_actor_from_actor_map(self, user_lpa_actor):
         response = self.aws_dynamodb_client.query(
-            TableName='{}-UserLpaActorMap'.format(self.environment),
-            KeyConditionExpression='Id = :user_lpa_actor',
+            TableName=f"{self.environment}-UserLpaActorMap",
+            KeyConditionExpression="Id = :user_lpa_actor",
             ExpressionAttributeValues={
-                ':user_lpa_actor': {'S': user_lpa_actor}
+                ":user_lpa_actor": {"S": user_lpa_actor}
             }
         )
         return response['Items'][0]
 
     def get_user(self, user_id):
         response = self.aws_dynamodb_client.query(
-            TableName='{}-ActorUsers'.format(self.environment),
-            KeyConditionExpression='Id = :user_id',
+            TableName=f"{self.environment}-ActorUsers",
+            KeyConditionExpression="Id = :user_id",
             ExpressionAttributeValues={
-                ':user_id': {'S': user_id}
+                ":user_id": {"S": user_id}
             }
         )
 
@@ -86,20 +84,20 @@ class AccountLookup:
             viewer_code_data = self.get_viewer_code(viewer_code)
         except IndexError:
             viewer_code_data = {
-                'ViewerCode': {
-                    'S': "{} Not Found".format(viewer_code)
+                "ViewerCode": {
+                    "S": f"{viewer_code} Not Found"
                 },
-                'SiriusUid': {
-                    'S': ""
+                "SiriusUid": {
+                    "S": ""
                 },
-                'Added': {
-                    'S': ""
+                "Added": {
+                    "S": ""
                 },
-                'Expires': {
-                    'S': ""
+                "Expires": {
+                    "S": ""
                 },
-                'UserLpaActor': {
-                    'S': "Not Found"
+                "UserLpaActor": {
+                    "S": "Not Found"
                 }
             }
 
@@ -123,15 +121,11 @@ class AccountLookup:
 
     def print_plaintext(self):
         print(
-            "Viewer Code: {}".format(
-                self.output_json['viewer_code']['code']),
-            "\n\tEmail: {}".format(self.output_json['email']),
-            "\n\tLPA Id: {}".format(
-                self.output_json['viewer_code']['lpa_id']),
-            "\n\tAdded: {}".format(
-                self.output_json['viewer_code']['added']),
-            "\n\tExpires: {}".format(
-                self.output_json['viewer_code']['expires']),
+            f"Viewer Code: {self.output_json['viewer_code']['code']}",
+            f"\n\tEmail: {self.output_json['email']}",
+            f"\n\tLPA Id: {self.output_json['viewer_code']['lpa_id']}",
+            f"\n\tAdded: {self.output_json['viewer_code']['added']}",
+            f"\n\tExpires: {self.output_json['viewer_code']['expires']}",
             "\n"
         )
 
@@ -147,9 +141,9 @@ def main():
                            default="",
                            help="Viewer Code to look up")
 
-    arguments.add_argument('--json', dest='output_json', action='store_const',
+    arguments.add_argument("--json", dest="output_json", action="store_const",
                            const=True, default=False,
-                           help='Output json data instead of plaintext to terminal')
+                           help="Output json data instead of plaintext to terminal")
 
     args = arguments.parse_args()
     work = AccountLookup(args.environment)
