fc2b31811dce1b63c93a8821a244a9219292207e
John Nolan <john.nolan@digital.justice.gov.uk>
2021-10-26 18:23:12 +0100

    Dependencies (#1185)
    * service-front web dependencies
    
    * service-pdf app dependency updates
    
    * Move to jsdom for jest and into jest.config
    
    * Ignore test-results from git
    
    * Fix missing Date mock from cookieHelper test
    
    * Fix beforeEach tests and JSON null error

--- service-front/web/jest.config.js
+++ service-front/web/jest.config.js
@@ -1,3 +1,15 @@
-{
-  reporters: ["default", "jest-junit"];
-}
+const config = {
+  testEnvironment: "jsdom",
+  reporters: ["default", "jest-junit"],
+  resetMocks: true,
+  testMatch: [
+    "**/?(*.)+(test).js"
+  ],
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ]
+};
+
+module.exports = config;
