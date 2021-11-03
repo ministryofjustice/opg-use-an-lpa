121121942e19f3c2f7c90fb1aaab314b019d0177
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-11-03 13:24:09 +0000

    create kms key for pagerduty sns

--- terraform/account/pagerduty.tf
+++ terraform/account/pagerduty.tf
@@ -1,3 +1,67 @@
+resource "aws_kms_key" "pagerduty_sns" {
+  description             = "KMS Key for encryption of AWS Config SNS Messages"
+  deletion_window_in_days = 10
+  policy                  = data.aws_iam_policy_document.pagerduty_sns_kms.json
+  enable_key_rotation     = true
+}
+
+data "aws_iam_policy_document" "pagerduty_sns_kms" {
+  statement {
+    sid       = "Allow Key to be used for Encryption by AWS Cloudwatch"
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*",
+    ]
+    principals {
+      identifiers = ["cloudwatch.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+  statement {
+    sid       = "Enable Root account permissions on Key"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
+    }
+  }
+  statement {
+    sid       = "Key Administrator"
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"]
+    }
+  }
+}
+
+
+
 data "pagerduty_service" "pagerduty" {
   name = local.account.pagerduty_service_name
 }
