c7fafe4c926525c48caff296989421ef11cda192
John Nolan <john.nolan@digital.justice.gov.uk>
2021-11-04 14:18:53 +0000

    Correct filter_pattern for subscription filter

--- terraform/environment/ship_to_metrics.tf
+++ terraform/environment/ship_to_metrics.tf
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_log_subscription_filter" "events" {
   count           = local.account.ship_metrics_queue_enabled == true ? 1 : 0
   name            = "${local.environment}-clsf-to-sqs"
   log_group_name  = aws_cloudwatch_log_group.application_logs.name
-  filter_pattern  = "{$.context.event_code EXISTS}"
+  filter_pattern  = "{ $.context.event_code = * }"
   destination_arn = data.aws_lambda_function.clsf_to_sqs[0].arn
   depends_on      = [aws_lambda_permission.allow_cloudwatch]
 }
