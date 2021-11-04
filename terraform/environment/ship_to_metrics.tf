e1ae290fa51feb6d3be919ff5fd606bc14834a31
John Nolan <john.nolan@digital.justice.gov.uk>
2021-11-04 13:35:21 +0000

    Remove foreach from name

--- terraform/environment/ship_to_metrics.tf
+++ terraform/environment/ship_to_metrics.tf
@@ -14,7 +14,7 @@ data "aws_lambda_function" "ship_to_opg_metrics" {
 
 resource "aws_cloudwatch_log_subscription_filter" "events" {
   count           = local.account.ship_metrics_queue_enabled == true ? 1 : 0
-  name            = "${local.environment}-clsf-to-sqs-${lower(each.value)}"
+  name            = "${local.environment}-clsf-to-sqs}"
   log_group_name  = aws_cloudwatch_log_group.application_logs.name
   filter_pattern  = "{$.context.event_code EXISTS}"
   destination_arn = data.aws_lambda_function.clsf_to_sqs[0].arn
