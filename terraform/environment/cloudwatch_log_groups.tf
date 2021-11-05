a3483b61775b4dacf0b163a72305c2bc4107cb0f
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-11-05 09:09:47 +0000

    use cloudwatch_encryption CMK for app logs

--- terraform/environment/cloudwatch_log_groups.tf
+++ terraform/environment/cloudwatch_log_groups.tf
@@ -1,6 +1,7 @@
 resource "aws_cloudwatch_log_group" "application_logs" {
   name              = "${local.environment}_application_logs"
   retention_in_days = local.account.log_retention_in_days
+  kms_key_id        = data.aws_kms_alias.cloudwatch_encryption.target_key_arn
 
   tags = {
     "Name" = "${local.environment}_application_logs"
