c47fc720a624216c1873905336c3f57055c4d938
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2021-11-09 16:22:20 +0000

    Tidy up handler code.

--- service-front/app/src/Actor/src/Handler/RequestActivationKey/RequestActivationKeyInfoHandler.php
+++ service-front/app/src/Actor/src/Handler/RequestActivationKey/RequestActivationKeyInfoHandler.php
@@ -4,18 +4,13 @@ declare(strict_types=1);
 
 namespace Actor\Handler\RequestActivationKey;
 
-use Common\Handler\{AbstractHandler,
-    UserAware,
-    Traits\Session as SessionTrait,
-    Traits\User};
-use Common\Service\Session\RemoveAccessForAllSessionValues;
+use Common\Handler\{AbstractHandler, Traits\Session as SessionTrait, Traits\User, UserAware};
+use Common\Service\Features\FeatureEnabled;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Mezzio\Authentication\AuthenticationInterface;
 use Mezzio\Helper\UrlHelper;
-use Mezzio\Session\SessionInterface;
 use Mezzio\Template\TemplateRendererInterface;
 use Psr\Http\Message\{ResponseInterface, ServerRequestInterface};
-use Common\Service\Features\FeatureEnabled;
 
 /**
  * Class RequestActivationKeyInfoHandler
@@ -27,15 +22,13 @@ class RequestActivationKeyInfoHandler extends AbstractHandler implements UserAwa
     use User;
     use SessionTrait;
 
-    private RemoveAccessForAllSessionValues $removeAccessForAllSessionValues;
     private FeatureEnabled $featureEnabled;
 
     public function __construct(
         TemplateRendererInterface $renderer,
         AuthenticationInterface $authenticator,
         UrlHelper $urlHelper,
-        FeatureEnabled $featureEnabled,
-        RemoveAccessForAllSessionValues $removeAccessForAllSessionValues
+        FeatureEnabled $featureEnabled
     ) {
         parent::__construct($renderer, $urlHelper);
 
@@ -49,15 +42,22 @@ class RequestActivationKeyInfoHandler extends AbstractHandler implements UserAwa
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $session = $this->getSession($request, 'session');
         $user = $this->getUser($request);
+
         if (($this->featureEnabled)('allow_older_lpas')) {
-            return new HtmlResponse($this->renderer->render('actor::before-requesting-activation-key-info', [
-                'user' => $user,
-            ]));
+            return new HtmlResponse($this->renderer->render(
+                'actor::request-activation-key/before-requesting-activation-key-info',
+                [
+                    'user' => $user,
+                ]
+            ));
+        } else {
+            return new HtmlResponse($this->renderer->render(
+                'actor::request-activation-key/info',
+                [
+                    'user' => $user,
+                ]
+            ));
         }
-        return new HtmlResponse($this->renderer->render('actor::request-activation-key/info', [
-            'user' => $user,
-        ]));
     }
 }
