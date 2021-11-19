4aa6d9eb3f4f508eb2026d97e9db43ad6c846c68
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-11-19 12:34:21 +0000

    working directory is already set

--- terraform/environment/api_ecs.tf
+++ terraform/environment/api_ecs.tf
@@ -257,7 +257,7 @@ locals {
         }
       ],
       healthCheck = {
-        command     = ["CMD", "./app/docker-healthcheck"],
+        command     = ["CMD", "docker-healthcheck"],
         startPeriod = 30,
         interval    = 15,
         timeout     = 30,
