3413b8a1d1f3c5df981eff61ff6c4b223a660edc
lukejolliffe <84718230+lukejolliffe@users.noreply.github.com>
2021-11-24 17:23:06 +0000

    Add instructions and preferences before 2016 pages and handlers

---
+++ service-front/app/src/Actor/src/Handler/InstructionsPreferencesBefore2016Handler.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Actor\Handler;
+
+use Common\Handler\AbstractHandler;
+use Common\Handler\Traits\User;
+use Common\Handler\UserAware;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Mezzio\Authentication\AuthenticationInterface;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
+use Laminas\Diactoros\Response\HtmlResponse;
+
+/**
+ * Class InstructionsPreferencesHandler
+ *
+ * @package Actor\Handler
+ * @codeCoverageIgnore
+ */
+class InstructionsPreferencesBefore2016Handler extends AbstractHandler implements UserAware
+{
+    use User;
+
+    public function __construct(
+        TemplateRendererInterface $renderer,
+        AuthenticationInterface $authenticator,
+        UrlHelper $urlHelper
+    ) {
+        parent::__construct($renderer, $urlHelper);
+
+        $this->setAuthenticator($authenticator);
+    }
+
+    /**
+     * Handles a request and produces a response
+     *
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $user = $this->getUser($request);
+
+        return new HtmlResponse($this->renderer->render('actor::instructions-preferences-signed-before-2016', [
+            'user' => $user,
+        ]));
+    }
+}
