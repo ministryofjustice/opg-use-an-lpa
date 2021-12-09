e3e2de9cd5123c61049cb6dacca74120f4b34848
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-12-09 13:47:21 +0000

    correct pathing for healthcheck script

--- terraform/environment/api_ecs.tf
+++ terraform/environment/api_ecs.tf
@@ -257,7 +257,7 @@ locals {
         }
       ],
       healthCheck = {
-        command     = ["CMD", "docker-healthcheck"],
+        command     = ["CMD", "docker-healthcheck.sh"],
         startPeriod = 30,
         interval    = 15,
         timeout     = 30,
