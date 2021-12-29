3cc0863a7d046524d0cbf8f60f9e61fc96e4ede4
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-12-29 17:16:57 +0000

    multi-line strings and if statements

--- scripts/pipeline/check_ecr_scan_results/aws_ecr_scan_results.py
+++ scripts/pipeline/check_ecr_scan_results/aws_ecr_scan_results.py
@@ -1,12 +1,12 @@
-import botocore
-import boto3
 import argparse
-import requests
 import json
 import os
-import pprint
 from datetime import date
 from datetime import datetime
+import sys
+import botocore
+import boto3
+import requests
 
 
 class ECRScanChecker:
@@ -57,16 +57,16 @@ class ECRScanChecker:
     def get_repositories(self, search_term):
         images_to_check = []
         response = self.aws_ecr_client.describe_repositories()
-        for repository in response["repositories"]:
-            if search_term in repository["repositoryName"]:
-                images_to_check.append(repository["repositoryName"])
+        for repository in response['repositories']:
+            if search_term in repository['repositoryName']:
+                images_to_check.append(repository['repositoryName'])
         return images_to_check
 
     def recursive_wait(self, tag):
-        print("Waiting for ECR scans to complete...")
+        print('Waiting for ECR scans to complete...')
         for image in self.images_to_check:
             self.wait_for_scan_completion(image, tag)
-        print("ECR image scans complete")
+        print('ECR image scans complete')
 
     def wait_for_scan_completion(self, image, tag):
         try:
@@ -82,40 +82,57 @@ class ECRScanChecker:
                 }
             )
         except botocore.exceptions.WaiterError as error:
-            if 'Error' in error.last_response and 'ScanNotFoundException' in error.last_response['Error']['Code']:
-                print(
-                    f"No ECR image scan results for image {image}, tag {tag}")
-            if 'Error' in error.last_response and not 'ScanNotFoundException' in error.last_response['Error']['Code']:
+            if (
+                'Error' in error.last_response
+                and not 'ScanNotFoundException' in error.last_response['Error']['Code']
+            ):
                 print(error.last_response['Error']['Code'],
                       error.last_response['Error']['Message'])
-                exit(1)
+                sys.exit(1)
+            if (
+                'Error' in error.last_response
+                and 'ScanNotFoundException' in error.last_response['Error']['Code']
+            ):
+                print(
+                    f'No ECR image scan results for image {image}, tag {tag}')
 
     def recursive_check_make_report(self, tag, report_limit, print_to_terminal):
-        print("Checking ECR scan results...")
+        print('Checking ECR scan results...')
         for image in self.images_to_check:
             try:
                 findings = self.list_findings(image, tag, report_limit)
-                if findings["findings"] != []:
+                if findings['findings'] != []:
 
-                    self.report = f"\n\n:warning: *AWS ECR Scan found results for {image}:* \n" + \
-                        f"Vulnerability Reports Found.\nDisplaying the first {report_limit} in order of severity\n\n"
+                    self.report = (
+                        f'\n\n:warning: *AWS ECR Scan found results for {image}:* \n'
+                        f'Vulnerability Reports Found.\n'
+                        f'Displaying the first {report_limit} in order of severity\n\n'
+                    )
 
-                    for finding in findings["findings"]:
-                        cve = finding["title"]
-                        severity = finding["severity"]
-                        description = "None"
-                        if "description" in finding:
-                            description = finding["description"]
-                        link = finding["packageVulnerabilityDetails"]["sourceUrl"]
-                        vuln_type = finding["type"]
-                        result = f"*Image:* {image} \n*Tag:* {tag} \n*Severity:* {severity} \n*CVE:* {cve} \n*Description:* {description} \n*Link:* `{link}`\n*Type:* `{vuln_type}`\n\n"
+                    for finding in findings['findings']:
+                        cve = finding['title']
+                        severity = finding['severity']
+                        description = 'None'
+                        if 'description' in finding:
+                            description = finding['description']
+                        link = finding['packageVulnerabilityDetails']['sourceUrl']
+                        vuln_type = finding['type']
+                        result = (
+                            f'*Image:* {image} \n'
+                            f'*Tag: * {tag} \n'
+                            f'*Severity:* {severity} \n'
+                            f'*Type:* `{vuln_type}`\n'
+                            f'*CVE:* {cve} \n'
+                            f'*Description:* {description} \n'
+                            f'*Link:* `{link}`\n\n'
+                        )
                         self.report += result
                     if print_to_terminal:
                         print(self.report)
             except botocore.exceptions.ClientError as error:
                 print(error.response['Error']['Code'],
                       error.response['Error']['Message'])
-                exit(1)
+                sys.exit(1)
 
     def list_findings(self, image, tag, report_limit):
         response = self.aws_inspector2_client.list_findings(
@@ -154,47 +171,50 @@ class ECRScanChecker:
         return response
 
     def post_to_slack(self, slack_webhook):
-        if self.report != "":
-            build_url = os.getenv('CIRCLE_BUILD_URL', "")
-            circleci_branch = os.getenv('CIRCLE_BRANCH', "")
-            branch_info = f"*Github Branch:* {circleci_branch}\n*CircleCI Job Link:* {build_url}\n\n"
+        if self.report != '':
+            build_url = os.getenv('CIRCLE_BUILD_URL', '')
+            circleci_branch = os.getenv('CIRCLE_BRANCH', '')
+            branch_info = (
+                f'*Github Branch:* {circleci_branch}\n'
+                f'*CircleCI Job Link:* {build_url}\n\n'
+            )
             self.report += branch_info
 
-            post_data = json.dumps({"text": self.report})
+            post_data = json.dumps({'text': self.report})
             response = requests.post(
                 slack_webhook, data=post_data,
                 headers={'Content-Type': 'application/json'}
             )
             if response.status_code != 200:
                 raise ValueError(
-                    'Request to slack returned an error %s, the response is:\n%s'
-                    % (response.status_code, response.text)
+                    f'Request to slack returned an error {response.status_code}, the response is:\n'
+                    f'{response.text}'
                 )
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Check ECR Scan results for all service container images.")
-    parser.add_argument("--search",
-                        default="",
-                        help="The root part oof the ECR repositry path, for example online-lpa")
-    parser.add_argument("--tag",
-                        default="latest",
-                        help="Image tag to check scan results for.")
-    parser.add_argument("--ecr_pushed_date_inclusive",
+        description='Check ECR Scan results for all service container images.')
+    parser.add_argument('--search',
+                        default='',
+                        help='The root part oof the ECR repositry path, for example online-lpa')
+    parser.add_argument('--tag',
+                        default='latest',
+                        help='Image tag to check scan results for.')
+    parser.add_argument('--ecr_pushed_date_inclusive',
                         default=date.today(),
-                        help="ECR Image push datetime in format YYYY-MM-dd")
-    parser.add_argument("--result_limit",
+                        help='ECR Image push datetime in format YYYY-MM-dd')
+    parser.add_argument('--result_limit',
                         default=5,
-                        help="How many results for each image to return. Defaults to 5")
-    parser.add_argument("--slack_webhook",
+                        help='How many results for each image to return. Defaults to 5')
+    parser.add_argument('--slack_webhook',
                         default=os.getenv('SLACK_WEBHOOK'),
-                        help="Webhook to use, determines what channel to post to")
+                        help='Webhook to use, determines what channel to post to')
     parser.add_argument('--print_to_terminal', dest='print_to_terminal', action='store_const',
                         const=True, default=False,
                         help='print findings to terminal')
-    parser.add_argument('--post_to_slack', dest='post_to_slack', action='store_const',
-                        const=True, default=False,
+    parser.add_argument('--skip_post_to_slack', dest='skip_post_to_slack', action='store_const',
+                        const=False, default=True,
                         help='Optionally turn off posting messages to slack')
 
     args = parser.parse_args()
@@ -202,11 +222,11 @@ def main():
     # work.recursive_wait(args.tag)
     work.recursive_check_make_report(
         args.tag, args.result_limit, args.print_to_terminal)
-    if args.slack_webhook is None:
-        print("No slack webhook provided, skipping post of results to slack")
-    if args.post_to_slack and args.slack_webhook is not None:
+    if args.skip_post_to_slack and args.slack_webhook is not None:
         work.post_to_slack(args.slack_webhook)
+    else:
+        print('Skipping post of results to slack')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
