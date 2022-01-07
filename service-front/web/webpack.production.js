91245a38ab81604f9a54fe1512b9be002f74a94d
John Nolan <john.nolan@digital.justice.gov.uk>
2022-01-07 14:45:54 +0000

    Target ie11 and remove all es6 syntax

--- service-front/web/webpack.production.js
+++ service-front/web/webpack.production.js
@@ -15,13 +15,13 @@ module.exports = merge(common, {
       // The environment supports BigInt as literal (123n).
       bigIntLiteral: false,
       // The environment supports const and let for variable declarations.
-      const: true,
+      const: false,
       // The environment supports destructuring ('{ a, b } = obj').
-      destructuring: true,
+      destructuring: false,
       // The environment supports an async import() function to import EcmaScript modules.
       dynamicImport: false,
       // The environment supports 'for of' iteration ('for (const x of array) { ... }').
-      forOf: true,
+      forOf: false,
       // The environment supports ECMAScript Module syntax to import ECMAScript modules (import ... from '...').
       module: false,
     },
