640fde265c6e89b240a828e9ec25b341bd6e3ee3
SeemaMenon <seema.menon@digital.justice.gov.uk>
2022-01-15 01:06:15 +0000

    fix broken test

--- service-front/app/test/ActorTest/Form/RequestActivationKey/RequestReferenceNumberTest.php
+++ service-front/app/test/ActorTest/Form/RequestActivationKey/RequestReferenceNumberTest.php
@@ -10,12 +10,19 @@ use CommonTest\Form\{LaminasFormTests, TestsLaminasForm};
 use Mezzio\Csrf\CsrfGuardInterface;
 use PHPUnit\Framework\TestCase;
 use Common\Service\Features\FeatureEnabled;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class RequestReferenceNumberTest extends TestCase implements TestsLaminasForm
 {
     use LaminasFormTests;
 
     protected RequestReferenceNumber $form;
+
+    /** @var ObjectProphecy|FeatureEnabled */
+    private $featureEnabledProphecy;
+
+    /** @var ObjectProphecy|CsrfGuardInterface */
+    private $guardProphecy;
 
     public function getForm(): AbstractForm
     {
@@ -37,9 +44,30 @@ class RequestReferenceNumberTest extends TestCase implements TestsLaminasForm
 
     public function setUp()
     {
-        $guardProphecy = $this->prophesize(CsrfGuardInterface::class);
-        $featureEnabledProphecy = $this->prophesize(FeatureEnabled::class);
+        $this->guardProphecy = $this->prophesize(CsrfGuardInterface::class);
+        $this->featureEnabledProphecy = $this->prophesize(FeatureEnabled::class);
+        $this->featureEnabledProphecy->__invoke('allow_meris_lpas')->willReturn(true);
 
-        $this->form = new RequestReferenceNumber($guardProphecy->reveal(), $featureEnabledProphecy->reveal());
+        $this->form = new RequestReferenceNumber($this->guardProphecy->reveal(), $this->featureEnabledProphecy->reveal());
+    }
+
+    /** @test */
+    public function it_sets_correct_validator_when_flag_set_to_true()
+    {
+        $validators = $this->getForm()->getInputFilterSpecification()['opg_reference_number']['validators'];
+        $key = array_search('Common\Validator\ChecksumValidator', array_column($validators, 'name'));
+
+        $this->assertContains($validators[$key]['name'], 'Common\Validator\ChecksumValidator');
+    }
+
+    /** @test */
+    public function it_sets_correct_validator_when_flag_set_to_false()
+    {
+        $this->featureEnabledProphecy->__invoke('allow_meris_lpas')->willReturn(false);
+
+        $validators = $this->getForm()->getInputFilterSpecification()['opg_reference_number']['validators'];
+        $key = array_search('Laminas\Validator\StringLength', array_column($validators, 'name'));
+
+        $this->assertContains($validators[$key]['name'], 'Laminas\Validator\StringLength');
     }
 }
