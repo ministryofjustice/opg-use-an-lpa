de1f2f5b90400c5969e11fb313b4e94694bb301e
lukejolliffe <84718230+lukejolliffe@users.noreply.github.com>
2022-01-21 11:49:52 +0000

    Create conditional route middleware

---
+++ service-front/app/src/Common/src/Middleware/Routing/ConditionalRoutingMiddleware.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Common\Middleware\Routing;
+
+use Psr\Container\ContainerInterface;
+use UnexpectedValueException;
+use Mezzio\MiddlewareContainer;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ConditionalRoutingMiddleware implements MiddlewareInterface
+{
+    private ContainerInterface $middlewareContainer;
+    private string $featureFlagName;
+    private string $trueRoute;
+    private string $falseRoute;
+
+    public function __construct(
+        ContainerInterface $container,
+        string $featureFlagName,
+        string $trueRoute,
+        string $falseRoute) {
+        $this->middlewareContainer = $container;
+        $this->featureFlagName = $featureFlagName;
+        $this->trueRoute = $trueRoute;
+        $this->falseRoute = $falseRoute;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $config = $this->middlewareContainer->get('config');
+
+        if (!isset($config['feature_flags']) || !is_array($config['feature_flags'])) {
+            throw new UnexpectedValueException('Missing feature flags configuration');
+        }
+
+        $flagEnabled = $config['feature_flags'][$this->featureFlagName] ?? False;
+
+        $middleware = $this->middlewareContainer->get($flagEnabled ? $this->trueRoute : $this->falseRoute);
+
+        return $middleware->handle($request, $handler);
+
+    }
+}
