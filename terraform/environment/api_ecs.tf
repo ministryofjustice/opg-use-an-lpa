a439ac2ad633fb15e0c6b6171756689c524263e7
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-01-24 14:08:41 +0000

    deploy sidecar in api service

--- terraform/environment/api_ecs.tf
+++ terraform/environment/api_ecs.tf
@@ -122,7 +122,7 @@ resource "aws_ecs_task_definition" "api" {
   network_mode             = "awsvpc"
   cpu                      = 512
   memory                   = 1024
-  container_definitions    = "[${local.api_web}, ${local.api_app}]"
+  container_definitions    = "[${local.api_web}, ${local.api_app} ${local.environment.deploy_opentelemetry_sidecar ? ", ${local.api_aws_otel_collector}" : ""}]"
   task_role_arn            = aws_iam_role.api_task_role.arn
   execution_role_arn       = aws_iam_role.execution_role.arn
 }
@@ -145,6 +145,21 @@ resource "aws_iam_role_policy" "api_permissions_role" {
   Defines permissions that the application running within the task has.
 */
 data "aws_iam_policy_document" "api_permissions_role" {
+  statement {
+    sid    = "xrayaccess"
+    effect = "Allow"
+
+    actions = [
+      "xray:PutTraceSegments",
+      "xray:PutTelemetryRecords",
+      "xray:GetSamplingRules",
+      "xray:GetSamplingTargets",
+      "xray:GetSamplingStatisticSummaries",
+    ]
+
+    resources = ["*"]
+  }
+
   statement {
     effect = "Allow"
 
@@ -241,6 +256,28 @@ locals {
       }]
   })
 
+  api_aws_otel_collector = jsonencode(
+    {
+      cpu         = 0,
+      essential   = true,
+      image       = "public.ecr.aws/aws-observability/aws-otel-collector:v0.14.1",
+      mountPoints = [],
+      name        = "aws-otel-collector",
+      command = [
+        "--config=/etc/ecs/ecs-cloudwatch-xray.yaml"
+      ],
+      portMappings = [],
+      volumesFrom  = [],
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.application_logs.name,
+          awslogs-region        = "eu-west-1",
+          awslogs-stream-prefix = "${local.environment_name}.actor-otel.use-an-lpa"
+        }
+      },
+      environment = []
+  })
 
   api_app = jsonencode(
     {
