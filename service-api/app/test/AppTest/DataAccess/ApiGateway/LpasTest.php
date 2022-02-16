167ab16965a0228e59800a8aa4c8003ffd328aa0
lukejolliffe <luke.jolliffe@digital.justice.gov.uk>
2022-02-16 17:03:11 +0000

    Add missing tests to improve coverage before work begins

--- service-api/app/test/AppTest/DataAccess/ApiGateway/LpasTest.php
+++ service-api/app/test/AppTest/DataAccess/ApiGateway/LpasTest.php
@@ -6,12 +6,14 @@ namespace AppTest\DataAccess\ApiGateway;
 
 use App\DataAccess\ApiGateway\Lpas;
 use App\DataAccess\Repository\DataSanitiserStrategy;
+use App\DataAccess\Repository\Response\LpaInterface;
 use App\Exception\ApiException;
 use Aws\Credentials\Credentials;
 use Aws\Signature\SignatureV4;
 use Fig\Http\Message\StatusCodeInterface;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Promise\FulfilledPromise;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -53,6 +55,73 @@ class LpasTest extends TestCase
             $this->traceId,
             $this->dataSanitiserStrategy->reveal()
         );
+    }
+
+    /** @test */
+    public function can_get_an_lpa(): void
+    {
+        $assert = $this;
+
+        $responseProphecy = $this->prophesize(Response::class);
+        $responseProphecy->getStatusCode()->willReturn(200);
+        $responseProphecy->getBody()->willReturn('{ "uId": "7000-0005-5554" }');
+        $responseProphecy->getHeaderLine('Date')->willReturn('Wed, 16 Feb 2022 16:45:46 GMT');
+
+        $this->httpClientProphecy->sendAsync(Argument::type(Request::class), Argument::any())->willReturn( new FulfilledPromise($responseProphecy->reveal()));
+        $this->dataSanitiserStrategy->sanitise(Argument::any())->will(function ($args) {
+           return $args[0];
+        });
+        $this->signatureV4Prophecy
+            ->signRequest(
+                Argument::type(Request::class),
+                Argument::type(Credentials::class)
+            )->shouldBeCalled()
+            ->will(function ($args) use ($assert) {
+                // assert the request has trace-id
+                /** @var Request $request */
+                $request = $args[0];
+                $assert->assertEquals($assert->traceId, $request->getHeaderLine('x-amzn-trace-id'));
+
+                return $request;
+            });
+
+        $shouldBeAnLPA = $this->getLpas()->get('700000055554');
+
+        $this->assertInstanceOf(LpaInterface::class, $shouldBeAnLPA);
+        $this->assertEquals('7000-0005-5554', $shouldBeAnLPA->getData()['uId']);
+
+
+    }
+
+    /** @test  */
+    public function lpa_not_found_gives_null(): void
+    {
+        $assert = $this;
+
+        $responseProphecy = $this->prophesize(Response::class);
+        $responseProphecy->getStatusCode()->willReturn(404);
+
+        $this->httpClientProphecy->sendAsync(Argument::type(Request::class), Argument::any())->willReturn( new FulfilledPromise($responseProphecy->reveal()));
+        $this->dataSanitiserStrategy->sanitise(Argument::any())->will(function ($args) {
+            return $args[0];
+        });
+        $this->signatureV4Prophecy
+            ->signRequest(
+                Argument::type(Request::class),
+                Argument::type(Credentials::class)
+            )->shouldBeCalled()
+            ->will(function ($args) use ($assert) {
+                // assert the request has trace-id
+                /** @var Request $request */
+                $request = $args[0];
+                $assert->assertEquals($assert->traceId, $request->getHeaderLine('x-amzn-trace-id'));
+
+                return $request;
+            });
+
+        $shouldBeNull = $this->getLpas()->get('700000055554');
+
+        $this->assertNull($shouldBeNull);
     }
 
     /** @test */
