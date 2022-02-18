90390394ba67814400d87bfc9ad23c9752f7ff1b
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-02-18 11:13:33 +0000

    make api respond to meris ids

--- mock-integrations/merge.sh
+++ mock-integrations/merge.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 script_path=$(dirname $0)
-rm ${script_path}/swagger.yaml
+rm -f ${script_path}/swagger.yaml
 
 sed -i -e 's|${allowed_roles}||' ${script_path}/lpa-openapi.yml && \
   python3 ${script_path}/scripts/merge_yaml.py ${script_path}/lpa-openapi.yml ${script_path}/swagger-examples.yaml > ${script_path}/swagger.yaml && \
