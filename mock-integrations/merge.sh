b136739af63b94f1abe73bb2ddde083ac485f012
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-02-18 16:20:33 +0000

    wget the spec, but not right now

--- mock-integrations/merge.sh
+++ mock-integrations/merge.sh
@@ -3,6 +3,7 @@
 script_path=$(dirname $0)
 rm -f ${script_path}/swagger.yaml
 
+#wget -O ${script_path}/lpa-openapi.yml https://raw.githubusercontent.com/ministryofjustice/opg-data-lpa/main/lambda_functions/v1/openapi/lpa-openapi.yml
 sed -i -e 's|${allowed_roles}||' ${script_path}/lpa-openapi.yml
 yq ea '. as $item ireduce ({}; . * $item )' ${script_path}/swagger-examples.yaml ${script_path}/lpa-openapi.yml > ${script_path}/swagger.yaml
 
