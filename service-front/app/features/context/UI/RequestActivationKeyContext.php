984cbc358a02dc07900ebf2df70ca0311aab7a40
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2022-02-21 13:42:14 +0000

    Add test to cover the scenario where workflow data should be removed by logout.

--- service-front/app/features/context/UI/RequestActivationKeyContext.php
+++ service-front/app/features/context/UI/RequestActivationKeyContext.php
@@ -435,6 +435,29 @@ class RequestActivationKeyContext implements Context
     }
 
     /**
+     * @Then /^I do not see my information$/
+     */
+    public function iDoNotSeeMyInformation()
+    {
+        $this->ui->assertPageAddress('/lpa/request-code/your-name');
+
+        $this->ui->assertFieldNotContains('first_names', 'The Attorney');
+        $this->ui->assertFieldNotContains('last_name', 'Person');
+    }
+
+    /**
+     * @Given /^starts the Add an Older LPA journey$/
+     */
+    public function startsTheAddAnOlderLPAJourney()
+    {
+        $this->iHaveBeenGivenAccessToUseAnLPAViaCredentials();
+
+        $this->ui->visit('/lpa/request-code/lpa-reference-number');
+        $this->ui->fillField('opg_reference_number', $this->lpa->uId);
+        $this->ui->pressButton('Continue');
+    }
+
+    /**
      * @Given /^The activation key not been received or was lost$/
      * @Then /^I will receive an email confirming this information$/
      * @Given /^My LPA was registered 'on or after' 1st September 2019$/
@@ -537,7 +560,8 @@ class RequestActivationKeyContext implements Context
     }
 
     /**
-     * @Given /^I have requested an activation key with valid details$/
+     * @Given I have requested an activation key with valid details
+     * @Given I reach the Check answers part of the Add an Older LPA journey
      */
     public function iHaveRequestedAnActivationKeyWithValidDetails()
     {
@@ -916,7 +940,7 @@ class RequestActivationKeyContext implements Context
      */
     public function iVisitTheYourNamePageWithoutFillingOutTheForm()
     {
-        $this->ui->visit('lpa/request-code/your-name');
+        $this->ui->visit('/lpa/request-code/your-name');
     }
 
     /**
