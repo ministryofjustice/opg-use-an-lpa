0797fd44faec57eb1acbd255c57df4482377cfeb
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-02-23 16:38:50 +0000

    provide list to allow and block

--- terraform/account/dns_firewall.tf
+++ terraform/account/dns_firewall.tf
@@ -20,16 +20,23 @@ resource "aws_route53_resolver_query_log_config_association" "egress" {
   resource_id                  = aws_default_vpc.default.id
 }
 
+locals {
+  interpolated_dns = [
+    aws_elasticache_replication_group.brute_force_cache_replication_group.primary_endpoint_address,
+    "api.${local.environment}-internal.",
+    "pdf.${local.environment}-internal.",
+  ]
+}
 resource "aws_route53_resolver_firewall_domain_list" "egress_allow" {
   count   = local.account.dns_firewall.enabled ? 1 : 0
-  name    = "egress"
-  domains = local.account.dns_firewall.domains_allowed
+  name    = "egress_allowed"
+  domains = concat(local.interpolated_dns, local.account.dns_firewall.domains_allowed)
 }
 
 resource "aws_route53_resolver_firewall_domain_list" "egress_block" {
   count   = local.account.dns_firewall.enabled ? 1 : 0
-  name    = "egress"
-  domains = ["*"]
+  name    = "egress_blocked"
+  domains = local.account.dns_firewall.domains_blocked
 }
 
 resource "aws_route53_resolver_firewall_rule_group" "egress" {
@@ -39,21 +46,22 @@ resource "aws_route53_resolver_firewall_rule_group" "egress" {
 
 resource "aws_route53_resolver_firewall_rule" "egress_allow" {
   count                   = local.account.dns_firewall.enabled ? 1 : 0
-  name                    = "egress"
+  name                    = "egress_allowed"
   action                  = "ALLOW"
   firewall_domain_list_id = aws_route53_resolver_firewall_domain_list.egress_allow[0].id
   firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.egress[0].id
-  priority                = 100
+  priority                = 200
 }
 
 resource "aws_route53_resolver_firewall_rule" "egress_block" {
-  count                   = local.account.dns_firewall.enabled ? 1 : 0
-  name                    = "egress"
-  action                  = "BLOCK"
-  block_response          = "NODATA"
+  count  = local.account.dns_firewall.enabled ? 1 : 0
+  name   = "egress_blocked"
+  action = "ALERT"
+  # action                  = "BLOCK"
+  # block_response          = "NODATA"
   firewall_domain_list_id = aws_route53_resolver_firewall_domain_list.egress_block[0].id
   firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.egress[0].id
-  priority                = 200
+  priority                = 300
 }
 
 resource "aws_route53_resolver_firewall_rule_group_association" "egress" {
