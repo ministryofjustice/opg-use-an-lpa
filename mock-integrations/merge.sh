77d7b97dbe7727857d61e37bda06772a1e7d8c5a
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-02-28 14:46:17 +0000

    remove x-amazon-apigateway-policy variable when creating swagger.yaml

--- mock-integrations/merge.sh
+++ mock-integrations/merge.sh
@@ -4,9 +4,9 @@ script_path=$(dirname $0)
 rm -f ${script_path}/swagger.yaml
 rm -f ${script_path}/lpa-openapi.yml
 
-wget -O ${script_path}/lpa-openapi.yml https://raw.githubusercontent.com/ministryofjustice/opg-data-lpa/4fd4e8a3de180715e7d195dfd9a419c80261f263/lambda_functions/v1/openapi/lpa-openapi.yml
+wget -O ${script_path}/lpa-openapi.yml https://raw.githubusercontent.com/ministryofjustice/opg-data-lpa/9ae43ed252d4d88a4bb968fa3b9a8c2489bdef73/lambda_functions/v1/openapi/lpa-openapi.yml
 #wget -O ${script_path}/lpa-openapi.yml https://raw.githubusercontent.com/ministryofjustice/opg-data-lpa/main/lambda_functions/v1/openapi/lpa-openapi.yml
-# sed -i -e 's|${allowed_roles}||' ${script_path}/lpa-openapi.yml
+sed -i -e 's|${allowed_roles}||' ${script_path}/lpa-openapi.yml # removes x-amazon-apigateway-policy principles variable
 yq ea '. as $item ireduce ({}; . * $item )' ${script_path}/swagger-examples.yaml ${script_path}/lpa-openapi.yml > ${script_path}/swagger.yaml
 
 rm -f ${script_path}/lpa-openapi.yml-e
