3c5130c0a78f26285f67837dfe9c9b07e6fa1601
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-03-01 11:37:33 +0000

    restructure generate examples script

--- mock-integrations/generate_examples/make_yaml.sh
+++ mock-integrations/generate_examples/make_yaml.sh
@@ -3,16 +3,18 @@
 rm -f swagger.yaml
 rm -f temp.yaml
 touch swagger.yaml
+touch temp.yaml
+cat examples_sample.yaml >> temp.yaml
 for n in $(cat list.txt )
 do
     echo "Working on $n..."
     example_name="lpa${n: -5:4}"
-    lpa_yaml_data=$(python ../../scripts/call-api-gateway/call_api_gateway.py $n | yq -P >> ./test-data/cases.yaml)
+    lpa_yaml_data=$(python ../../scripts/call-api-gateway/call_api_gateway.py $n | yq -P)
     lpa_example_name=$example_name lpa_data=$lpa_yaml_data \
     yq 'with(.paths[].get.responses.[].content.[].examples; . | .[env(lpa_example_name)].value=env(lpa_data))' \
-    examples_sample.yaml >> temp.yaml
+    temp.yaml >> temp.yaml
 done
 
-yq ea '. as $item ireduce ({}; . * $item )' temp.yaml > swagger.yaml
+yq ea '. as $item ireduce ({}; . * $item )' temp.yaml > openapi-examples.yaml
 
 rm -f temp.yaml
