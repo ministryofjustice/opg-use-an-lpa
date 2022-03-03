c95052dae23cf33e17e959c112ffc204efd8c6fb
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-03-03 15:11:44 +0000

    generate mappings for 200 responses in the nginx file

--- mock-integrations/generate_examples/make_yaml.sh
+++ mock-integrations/generate_examples/make_yaml.sh
@@ -3,14 +3,16 @@
 rm -f temp.yaml
 touch temp.yaml
 cat examples-template.yaml >> temp.yaml
+cp nginx-template.conf nginx-generated.conf
 for n in $(cat list.txt )
 do
     echo "Working on $n..."
-    example_name="lpa${n: -5:4}"
+    example_name="lpa${n: -4:4}"
     lpa_yaml_data=$(python ../../scripts/call-api-gateway/call_api_gateway.py $n | yq -P)
     lpa_example_name=$example_name lpa_data=$lpa_yaml_data \
     yq 'with(.paths[].get.responses.[].content.[].examples; . | .[env(lpa_example_name)].value=env(lpa_data))' \
     temp.yaml >> temp.yaml
+    sed -i '' -e "5s/^//p; 6s/^.*/    \"~$n\" \"$example_name\";/" nginx-generated.conf
 done
 
 yq ea '. as $item ireduce ({}; . * $item )' temp.yaml > mock-openapi-examples.yaml
