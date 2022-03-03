d0f2d0170e61b8ee764e039f6bde561c2d53d4ae
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-03-03 17:01:44 +0000

    UML-2104 - New mock for data-lpa (#1379)
    * Temporarily run a prism for data-lpa mock
    
    * leave this functional
    
    * make standing up a mock quicker
    
    * use new api-gateway path
    
    * make api respond to meris ids
    
    * run new mocks on when bring service up
    
    * a working setup for the mock
    
    * update readme and examples
    
    * use images instead of dockerfiles
    
    * restart gateway and mocksirius as part of mock updates
    
    * rename mocksirius to data-lpa
    
    * add several cases
    
    * generate examples from current api
    
    * add instructions to generate yaml from the current api
    
    * update readme
    
    * correct name of api-gateway service
    
    * remove unused items
    
    * use yq to make swagger doc
    
    * wget the spec, but not right now
    
    * update readme to show you need yq and wget
    
    * remove python scripts
    
    * better feedback
    
    * rewrite version out of path
    
    * make mock represent the single item response
    
    * update swagger.yaml
    
    * pull data-lpa spec from branch
    
    * remove x-amazon-apigateway-policy variable when creating swagger.yaml
    
    * fix port
    
    * make conf.d writeable
    
    * pass an env in for data-lpa testing
    
    * add logging to get feedback outside of output
    
    * log the response
    
    * rename to openapi
    
    * update instructions in the readme
    
    * add error handling and logging
    
    * log message for any error
    
    * generate examples with a script
    
    * restructure generate examples script
    
    * delete unused files
    
    * update documentation
    
    * test the new endpoint
    
    * reset default env lpas collection endpoint address
    
    * pull spec from main branch of opg-data-lpa
    
    * switch to dev from integration as production like endpoint
    
    * make clear what's for mock and what's from data-lpa
    
    * rename template and output of example generation
    
    * don't need to create a swagger.yaml anymore
    
    * changes to the data-lpa spec must be done in that repo
    
    * update name of files in readme
    
    * update name of files, and description of process in readme
    
    * put depends on in
    
    * gateway depends on data-lpa
    
    * generate mappings for 200 responses in the nginx file
    
    * rename to make_examples
    
    * don't commit these
    
    * replace with generated nginx config
    
    * delete old generated file
    
    * exit on error

---
+++ mock-integrations/README.md
@@ -0,0 +1,72 @@
+# Gateway mock
+
+Mock servers which together mimic the data-lpa API.
+
+Runs using [Prism](https://stoplight.io/open-source/prism), delivering the API specified in the latest version of the
+[OPG Data LPA openapi doc](https://github.com/ministryofjustice/opg-data-lpa/blob/main/lambda_functions/v1/openapi/lpa-openapi.yml).
+
+A Prism instance (data-lpa) and an nginx instance (api-gateway) are spun up as part of the docker-compose service to provide lightweight testing of the Use a Lasting Power of Attorney service without a full Sirius stack.
+
+In `nginx.conf`, the IDs of LPAs are mapped to named examples added in `mock-openapi-examples.yaml`.
+The example name is then used in the gateway nginx proxy to add a `Prefer: example=<name>` header to requests, which are forwarded to Prism. This `Prefer` header [instructs Prism to a return a particular Swagger example as a response](https://github.com/stoplightio/prism/blob/master/docs/guides/01-mocking.md#Response-Generation), allowing us to predictably return the correct LPA for a given request.
+
+Any changes that are required to the opg-data-lpa openapi spec must be done in [opg-data-lpa](https://github.com/ministryofjustice/opg-data-lpa).
+
+## Prerequisites
+
+You will need yq and wget to work with this mock!
+
+```shell
+brew install yq wget
+```
+
+## Using the mock
+
+To start the mock alone, run
+
+```shell
+make up_mock
+```
+
+Or bring up the whole service
+
+```shell
+make up_all
+```
+
+Once started, when there are updates to the openapi spec for opg-data-lpa, or the examples, run an update
+
+```shell
+make update_mock
+```
+
+This will generate a new mock-openapi.yaml file and restart the mock docker compose services.
+
+## Viewing the examples
+
+It's possible to invoke the mock endpoints using curl or similar as follows:
+
+```shell
+# Valid LPA Uid
+curl -i -H "Authorization: sigv4 x" -k http://localhost:7010/use-an-lpa/lpas/700000000047
+
+# LPA not found
+curl -i -H "Authorization: sigv4 x" -k http://localhost:7010/use-an-lpa/lpas/700000000000
+```
+
+The URLs above cause Prism to return examples with specific statuses, as shown.
+Any other LPA ID (the last part of the path) will return a "Received" status.
+
+## Working on the examples
+
+Yaml examples can be generated from the data-lpa api using a script in ./mock-integrations/generate_examples
+
+Add LPAs from the Sirius Integration environment to list.txt (each Uid as a new line), then run the script.
+
+```shell
+cd ./mock-integrations/generate_examples
+
+aws-vaul exec identity -- ./make_examples.sh
+```
+
+Review the output in ./mock-integrations/generate_examples/mock-openapi-examples.yaml  and ./mock-integrations/generate_examples/nginx-generated and if happy copy to ./mock-integrations/mock-openapi-examples.yaml, replacing the old file.
--- scripts/call-api-gateway/call_api_gateway.py
+++ scripts/call-api-gateway/call_api_gateway.py
@@ -1,9 +1,12 @@
-import boto3
 import argparse
+import os
+import sys
+import logging
 import json
+import boto3
 from requests_aws4auth import AWS4Auth
 import requests
-import os
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
 
 class APIGatewayCaller:
@@ -13,8 +16,8 @@ class APIGatewayCaller:
     aws_iam_session = ''
     aws_auth = ''
 
-    def __init__(self, target_production):
-        self.choose_target_gateway(target_production)
+    def __init__(self, target_production, dev_endpoint_url):
+        self.choose_target_gateway(target_production, dev_endpoint_url)
         self.aws_iam_role = os.getenv('AWS_IAM_ROLE')
         self.set_iam_role_session()
         self.aws_auth = AWS4Auth(
@@ -24,20 +27,20 @@ class APIGatewayCaller:
             'execute-api',
             session_token=self.aws_iam_session['Credentials']['SessionToken'])
 
-    def choose_target_gateway(self, target_production):
+    def choose_target_gateway(self, target_production, dev_endpoint_url):
         if target_production:
             self.aws_account_id = '690083044361'
             self.api_gateway_url = 'https://lpa.api.opg.service.justice.gov.uk/v1/use-an-lpa/lpas/'
         else:
             self.aws_account_id = '367815980639'
-            self.api_gateway_url = 'https://integration.dev.lpa.api.opg.service.justice.gov.uk/v1/use-an-lpa/lpas/'
+            self.api_gateway_url = f'https://{dev_endpoint_url}/v1/use-an-lpa/lpas/'
+        logging.info('targetting endpoint at: %s', self.api_gateway_url)
 
     def set_iam_role_session(self):
         if os.getenv('CI'):
-            role_arn = 'arn:aws:iam::{}:role/ci'.format(self.aws_account_id)
+            role_arn = f'arn:aws:iam::{self.aws_account_id}:role/ci'
         else:
-            role_arn = 'arn:aws:iam::{0}:role/operator'.format(
-                self.aws_account_id)
+            role_arn = f'arn:aws:iam::{self.aws_account_id}:role/operator'
 
         sts = boto3.client(
             'sts',
@@ -57,13 +60,23 @@ class APIGatewayCaller:
         url = str(self.api_gateway_url+lpa_id)
         response = requests.request(
             method, url, auth=self.aws_auth, data=body, headers=headers)
-        print(response.text)
+        logging.info('status code: %s', response.status_code)
+        if response.status_code != 200:
+            error_message = json.loads(response.text)[
+                "body"]["error"]["message"] or "unknown error"
+            logging.warning(
+                'response: %s', error_message)
+            sys.exit(1)
+        return response.text
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Look up LPA IDs on the Sirius API Gateway.")
 
+    parser.add_argument("--dev_endpoint_url", type=str,
+                        default='dev.lpa.api.opg.service.justice.gov.uk',
+                        help="endpoint other than integration to use for testing")
     parser.add_argument("lpa_id", type=str,
                         help="LPA ID to look up in API Gateway")
     parser.add_argument('--production', dest='target_production', action='store_const',
@@ -71,8 +84,9 @@ def main():
                         help='target the production sirius api gateway')
 
     args = parser.parse_args()
-    work = APIGatewayCaller(args.target_production)
-    work.call_api_gateway(args.lpa_id)
+    work = APIGatewayCaller(args.target_production, args.dev_endpoint_url)
+    lpa_data = work.call_api_gateway(args.lpa_id)
+    print(lpa_data)
 
 
 if __name__ == "__main__":
