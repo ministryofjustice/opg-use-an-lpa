9844b4f4569ec6fba1fc8ec4b6bdc59a0dfbb6d1
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-03-03 10:13:36 +0000

    pull spec from main branch of opg-data-lpa

--- mock-integrations/merge.sh
+++ mock-integrations/merge.sh
@@ -4,8 +4,7 @@ script_path=$(dirname $0)
 rm -f ${script_path}/openapi.yaml
 rm -f ${script_path}/lpa-openapi.yml
 
-wget -O ${script_path}/lpa-openapi.yml https://raw.githubusercontent.com/ministryofjustice/opg-data-lpa/9ae43ed252d4d88a4bb968fa3b9a8c2489bdef73/lambda_functions/v1/openapi/lpa-openapi.yml
-#wget -O ${script_path}/lpa-openapi.yml https://raw.githubusercontent.com/ministryofjustice/opg-data-lpa/main/lambda_functions/v1/openapi/lpa-openapi.yml
+wget -O ${script_path}/lpa-openapi.yml https://raw.githubusercontent.com/ministryofjustice/opg-data-lpa/main/lambda_functions/v1/openapi/lpa-openapi.yml
 sed -i -e 's|${allowed_roles}||' ${script_path}/lpa-openapi.yml # removes x-amazon-apigateway-policy principles variable
 yq ea '. as $item ireduce ({}; . * $item )' ${script_path}/openapi-examples.yaml ${script_path}/lpa-openapi.yml > ${script_path}/openapi.yaml
 
