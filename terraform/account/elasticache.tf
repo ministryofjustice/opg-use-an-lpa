0b77e04fa8b64d48e591136ac44611e2ef7469c4
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-03-18 09:43:19 +0000

    replace deprecated arguments

--- terraform/account/elasticache.tf
+++ terraform/account/elasticache.tf
@@ -13,16 +13,16 @@ resource "aws_elasticache_subnet_group" "private_subnets" {
 }
 
 resource "aws_elasticache_replication_group" "brute_force_cache_replication_group" {
-  automatic_failover_enabled    = true
-  replication_group_id          = "brute-force-cache-replication-group"
-  replication_group_description = "brute force redis cache replication group"
-  parameter_group_name          = "default.redis5.0"
-  engine_version                = "5.0.6"
-  node_type                     = "cache.t2.micro"
-  engine                        = "redis"
-  number_cache_clusters         = 2
-  transit_encryption_enabled    = true
-  at_rest_encryption_enabled    = true
-  subnet_group_name             = aws_elasticache_subnet_group.private_subnets.name
-  security_group_ids            = [aws_security_group.brute_force_cache_service.id]
+  automatic_failover_enabled = true
+  replication_group_id       = "brute-force-cache-replication-group"
+  description                = "brute force redis cache replication group"
+  parameter_group_name       = "default.redis5.0"
+  engine_version             = "5.0.6"
+  node_type                  = "cache.t2.micro"
+  engine                     = "redis"
+  num_cache_clusters         = 2
+  transit_encryption_enabled = true
+  at_rest_encryption_enabled = true
+  subnet_group_name          = aws_elasticache_subnet_group.private_subnets.name
+  security_group_ids         = [aws_security_group.brute_force_cache_service.id]
 }
