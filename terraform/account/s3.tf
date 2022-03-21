b359be88ad3adab337eaae07603a00ef93e66a00
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-03-21 15:44:14 +0000

    UML-2129 - Upgrade account terraform aws provider to version 4 (#1426)
    * refactor bucket acl into new resource
    
    * refactor bucket sse configuration
    
    * refactor bucket object versioning to separate resource
    
    * replace empty strings with null
    
    * replace deprecated arguments
    
    * use a customer managed key for encryption
    
    * add s3 access logging
    
    * pin to AWS provider v4.5.0

--- terraform/account/s3.tf
+++ terraform/account/s3.tf
@@ -1,5 +1,5 @@
 data "aws_elb_service_account" "main" {
-  region = "eu-west-1"
+  region = data.aws_region.current.name
 }
 
 data "aws_iam_policy_document" "access_log" {
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "access_log" {
     effect  = "Allow"
     actions = ["s3:PutObject"]
     principals {
-      identifiers = [data.aws_elb_service_account.main.id]
+      identifiers = [data.aws_elb_service_account.main.arn]
       type        = "AWS"
     }
   }
@@ -37,19 +37,138 @@ data "aws_iam_policy_document" "access_log" {
   }
 }
 
-resource "aws_s3_bucket" "access_log" {
-  bucket = "opg-ual-${local.environment}-lb-access-logs"
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
+resource "aws_kms_key" "access_log" {
+  description             = "S3 bucket encryption key for access_log"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.access_log_kms.json
+}
+
+resource "aws_kms_alias" "access_log" {
+  name          = "alias/s3-access-log-${local.environment}"
+  target_key_id = aws_kms_key.access_log.key_id
+}
+
+data "aws_iam_policy_document" "access_log_kms" {
+  statement {
+    sid       = "Enable Root account permissions on Key"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
     }
   }
+
+  statement {
+    sid       = "Allow Key to be used for Encryption"
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "logs.${data.aws_region.current.name}.amazonaws.com",
+        "s3.amazonaws.com",
+      ]
+    }
+  }
+
+  statement {
+    sid       = "Allow Key to be used by LB for Encryption"
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        data.aws_elb_service_account.main.arn
+      ]
+    }
+  }
+
+  statement {
+    sid       = "Key Administrator"
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"]
+    }
+  }
+}
+
+resource "aws_s3_bucket" "access_log" {
+  bucket = "opg-ual-${local.environment}-lb-access-logs"
+}
+
+resource "aws_s3_bucket_acl" "access_log" {
+  bucket = aws_s3_bucket.access_log.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "access_log" {
+  bucket = aws_s3_bucket.access_log.id
+  rule {
+    bucket_key_enabled = true
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.access_log.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "access_log" {
+  bucket = aws_s3_bucket.access_log.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+data "aws_s3_bucket" "s3_access_logging" {
+  bucket = "${local.account.s3_access_log_bucket_name}-${data.aws_region.current.name}"
+}
+
+resource "aws_s3_bucket_logging" "access_log" {
+  bucket = aws_s3_bucket.access_log.id
+
+  target_bucket = data.aws_s3_bucket.s3_access_logging.id
+  target_prefix = "lb-access-log/"
 }
 
 resource "aws_s3_bucket_policy" "access_log" {
