78995a49e8d6eb46655effaeb61b01e3ffc0cb10
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-03-22 09:45:08 +0000

    UML-2187 - remove CMK for lb access logging (#1430)
    * Fix up policy for s3 bucket
    * remove cmk for now

--- terraform/account/s3.tf
+++ terraform/account/s3.tf
@@ -1,138 +1,3 @@
-data "aws_elb_service_account" "main" {
-  region = data.aws_region.current.name
-}
-
-data "aws_iam_policy_document" "access_log" {
-  statement {
-    sid = "accessLogBucketAccess"
-    resources = [
-      aws_s3_bucket.access_log.arn,
-      "${aws_s3_bucket.access_log.arn}/*",
-    ]
-    effect  = "Allow"
-    actions = ["s3:PutObject"]
-    principals {
-      identifiers = [data.aws_elb_service_account.main.arn]
-      type        = "AWS"
-    }
-  }
-
-  statement {
-    sid     = "AllowSSLRequestsOnly"
-    effect  = "Deny"
-    actions = ["s3:*"]
-    resources = [
-      aws_s3_bucket.access_log.arn,
-      "${aws_s3_bucket.access_log.arn}/*",
-    ]
-    condition {
-      test     = "Bool"
-      values   = ["false"]
-      variable = "aws:SecureTransport"
-    }
-    principals {
-      identifiers = ["*"]
-      type        = "AWS"
-    }
-  }
-}
-
-resource "aws_kms_key" "access_log" {
-  description             = "S3 bucket encryption key for access_log"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true
-  policy                  = data.aws_iam_policy_document.access_log_kms.json
-}
-
-resource "aws_kms_alias" "access_log" {
-  name          = "alias/s3-access-log-${local.environment}"
-  target_key_id = aws_kms_key.access_log.key_id
-}
-
-data "aws_iam_policy_document" "access_log_kms" {
-  statement {
-    sid       = "Enable Root account permissions on Key"
-    effect    = "Allow"
-    actions   = ["kms:*"]
-    resources = ["*"]
-
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
-      ]
-    }
-  }
-
-  statement {
-    sid       = "Allow Key to be used for Encryption"
-    effect    = "Allow"
-    resources = ["*"]
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey",
-    ]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "logs.${data.aws_region.current.name}.amazonaws.com",
-        "s3.amazonaws.com",
-      ]
-    }
-  }
-
-  statement {
-    sid       = "Allow Key to be used by LB for Encryption"
-    effect    = "Allow"
-    resources = ["*"]
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey",
-    ]
-
-    principals {
-      type = "AWS"
-      identifiers = [
-        data.aws_elb_service_account.main.arn
-      ]
-    }
-  }
-
-  statement {
-    sid       = "Key Administrator"
-    effect    = "Allow"
-    resources = ["*"]
-    actions = [
-      "kms:Create*",
-      "kms:Describe*",
-      "kms:Enable*",
-      "kms:List*",
-      "kms:Put*",
-      "kms:Update*",
-      "kms:Revoke*",
-      "kms:Disable*",
-      "kms:Get*",
-      "kms:Delete*",
-      "kms:TagResource",
-      "kms:UntagResource",
-      "kms:ScheduleKeyDeletion",
-      "kms:CancelKeyDeletion"
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"]
-    }
-  }
-}
-
 resource "aws_s3_bucket" "access_log" {
   bucket = "opg-ual-${local.environment}-lb-access-logs"
 }
@@ -145,10 +10,8 @@ resource "aws_s3_bucket_acl" "access_log" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "access_log" {
   bucket = aws_s3_bucket.access_log.id
   rule {
-    bucket_key_enabled = true
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.access_log.arn
-      sse_algorithm     = "aws:kms"
+      sse_algorithm = "aws:kms"
     }
   }
 }
@@ -174,6 +37,77 @@ resource "aws_s3_bucket_logging" "access_log" {
 resource "aws_s3_bucket_policy" "access_log" {
   bucket = aws_s3_bucket.access_log.id
   policy = data.aws_iam_policy_document.access_log.json
+}
+
+data "aws_elb_service_account" "main" {
+  region = data.aws_region.current.name
+}
+
+data "aws_iam_policy_document" "access_log" {
+  statement {
+    sid = "accessLogBucketAccess"
+    resources = [
+      aws_s3_bucket.access_log.arn,
+      "${aws_s3_bucket.access_log.arn}/*",
+    ]
+    effect  = "Allow"
+    actions = ["s3:PutObject"]
+    principals {
+      identifiers = [data.aws_elb_service_account.main.id]
+      type        = "AWS"
+    }
+  }
+
+  statement {
+    sid = "accessLogDelivery"
+    resources = [
+      aws_s3_bucket.access_log.arn,
+      "${aws_s3_bucket.access_log.arn}/*",
+    ]
+    effect  = "Allow"
+    actions = ["s3:PutObject"]
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+    condition {
+      test     = "StringEquals"
+      values   = ["bucket-owner-full-control"]
+      variable = "s3:x-amz-acl"
+    }
+  }
+
+  statement {
+    sid = "accessGetAcl"
+    resources = [
+      aws_s3_bucket.access_log.arn
+    ]
+    effect  = "Allow"
+    actions = ["s3:GetBucketAcl"]
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+
+  statement {
+    sid     = "AllowSSLRequestsOnly"
+    effect  = "Deny"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.access_log.arn,
+      "${aws_s3_bucket.access_log.arn}/*",
+    ]
+    condition {
+      test     = "Bool"
+      values   = ["false"]
+      variable = "aws:SecureTransport"
+    }
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "access_log" {
