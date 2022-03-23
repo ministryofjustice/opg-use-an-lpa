1586762e5c35c6a28b22c267b7580f6eac015f90
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-03-23 14:11:52 +0000

    fix name and priority

--- terraform/environment/dns_firewall.tf
+++ terraform/environment/dns_firewall.tf
@@ -1,6 +1,6 @@
 
 resource "aws_route53_resolver_firewall_domain_list" "environment_egress_allow" {
-  name = "${local.environment}_environment_egress_allow"
+  name = "${local.environment_name}_environment_egress_allow"
   domains = [
     local.api_service_fqdn,
     local.pdf_service_fqdn
@@ -8,11 +8,11 @@ resource "aws_route53_resolver_firewall_domain_list" "environment_egress_allow" 
 }
 
 resource "aws_route53_resolver_firewall_rule_group" "environment_egress_allow" {
-  name = "${local.environment}_environment_egress_allow"
+  name = "${local.environment_name}_environment_egress_allow"
 }
 
 resource "aws_route53_resolver_firewall_rule" "environment_egress_allow" {
-  name                    = "${local.environment}_environment_egress_allow"
+  name                    = "${local.environment_name}_environment_egress_allow"
   action                  = "ALLOW"
   firewall_domain_list_id = aws_route53_resolver_firewall_domain_list.environment_egress_allow.id
   firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.environment_egress_allow.id
@@ -21,8 +21,8 @@ resource "aws_route53_resolver_firewall_rule" "environment_egress_allow" {
 
 # how do I resolve the need to prioritise rules group association
 resource "aws_route53_resolver_firewall_rule_group_association" "environment_egress_allow" {
-  name                   = "${local.environment}_environment_egress_allow"
+  name                   = "${local.environment_name}_environment_egress_allow"
   firewall_rule_group_id = aws_route53_resolver_firewall_rule_group.environment_egress_allow.id
-  priority               = 300
+  priority               = 200
   vpc_id                 = data.aws_vpc.default.id
 }
