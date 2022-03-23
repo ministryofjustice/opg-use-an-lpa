c48407cfe69f3469d8fd0cb66e49a71ab185b34b
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-03-23 15:38:12 +0000

    use random int between 1 and 499 for priority

--- terraform/environment/dns_firewall.tf
+++ terraform/environment/dns_firewall.tf
@@ -1,9 +1,13 @@
+resource "random_integer" "environment_egress_rule_group_association_priority" {
+  min = 100
+  max = 499
+}
 
 resource "aws_route53_resolver_firewall_domain_list" "environment_egress_allow" {
   name = "${local.environment_name}_environment_egress_allow"
   domains = [
-    local.api_service_fqdn,
-    local.pdf_service_fqdn
+    "${local.api_service_fqdn}.",
+    "${local.pdf_service_fqdn}."
   ]
 }
 
@@ -19,10 +23,9 @@ resource "aws_route53_resolver_firewall_rule" "environment_egress_allow" {
   priority                = 300
 }
 
-# how do I resolve the need to prioritise rules group association
 resource "aws_route53_resolver_firewall_rule_group_association" "environment_egress_allow" {
   name                   = "${local.environment_name}_environment_egress_allow"
   firewall_rule_group_id = aws_route53_resolver_firewall_rule_group.environment_egress_allow.id
-  priority               = 200
+  priority               = random_integer.environment_egress_rule_group_association_priority.result
   vpc_id                 = data.aws_vpc.default.id
 }
