47f7d4bc89d068d0abe3936fd032c572b2fad6cb
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-03-23 13:57:24 +0000

    add environment config for firewall

---
+++ terraform/environment/dns_firewall.tf
@@ -0,0 +1,28 @@
+
+resource "aws_route53_resolver_firewall_domain_list" "environment_egress_allow" {
+  name = "${local.environment}_environment_egress_allow"
+  domains = [
+    local.api_service_fqdn,
+    local.pdf_service_fqdn
+  ]
+}
+
+resource "aws_route53_resolver_firewall_rule_group" "environment_egress_allow" {
+  name = "${local.environment}_environment_egress_allow"
+}
+
+resource "aws_route53_resolver_firewall_rule" "environment_egress_allow" {
+  name                    = "${local.environment}_environment_egress_allow"
+  action                  = "ALLOW"
+  firewall_domain_list_id = aws_route53_resolver_firewall_domain_list.environment_egress_allow.id
+  firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.environment_egress_allow.id
+  priority                = 300
+}
+
+# how do I resolve the need to prioritise rules group association
+resource "aws_route53_resolver_firewall_rule_group_association" "environment_egress_allow" {
+  name                   = "${local.environment}_environment_egress_allow"
+  firewall_rule_group_id = aws_route53_resolver_firewall_rule_group.environment_egress_allow.id
+  priority               = 300
+  vpc_id                 = data.aws_vpc.default.id
+}
