cc04b8ceb6ab4fba1d000514b63ba68fa5871ba6
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-03-31 15:50:41 +0100

    remove create log group permissions

--- terraform/account/modules/dynamodb_cloudtrail/cloudtrail.tf
+++ terraform/account/modules/dynamodb_cloudtrail/cloudtrail.tf
@@ -49,7 +49,6 @@ resource "aws_iam_role_policy" "cloudtrail" {
 data "aws_iam_policy_document" "cloudtrail_role_policy" {
   statement {
     actions = [
-      "logs:CreateLogGroup",
       "logs:CreateLogStream",
       "logs:PutLogEvents",
       "logs:DescribeLogGroups",
