ce3ae58c3a0a203db6fbed6473b380c54df01e24
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-03-31 15:12:18 +0100

    don't deploy in dev

--- terraform/account/dynamodb_data_plane_cloudtrail.tf
+++ terraform/account/dynamodb_data_plane_cloudtrail.tf
@@ -1,5 +1,6 @@
 module "production_dynamodb_cloudtrail" {
   source                        = "./modules/dynamodb_cloudtrail"
+  count                         = local.account.dynamodb_cloudtrail.enabled == true ? 1 : 0
   trail_name_suffix             = local.account.dynamodb_cloudtrail.trail_name_suffix
   bucket_name_suffix            = local.account.dynamodb_cloudtrail.bucket_name_suffix
   s3_access_logging_bucket_name = "${local.account.s3_access_log_bucket_name}-${data.aws_region.current.name}"
