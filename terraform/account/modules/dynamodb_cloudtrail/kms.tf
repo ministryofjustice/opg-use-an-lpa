9d80af67393d158c1c6527cdc7dc6a6bd80d920a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-04-05 10:50:07 +0100

    UML-2206 - Log DynamoDB Data Plane Events (#1447)
    * craete module to provision cloudtrail for dynamodb data plane events
    
    * disable ddb cloudtrail in dev and preprod

---
+++ terraform/account/modules/dynamodb_cloudtrail/kms.tf
@@ -0,0 +1,152 @@
+resource "aws_kms_key" "cloudtrail_s3" {
+  description             = "KMS Key for Encrypting DynamoDB CloudTrail Logs in S3"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.cloudtrail_s3_key.json
+}
+
+resource "aws_kms_alias" "cloudtrail_s3" {
+  name          = "alias/dynamodb_cloudtrail_s3_${replace(var.trail_name_suffix, "-", "_")}"
+  target_key_id = aws_kms_key.cloudtrail_s3.key_id
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "cloudtrail_s3_key" {
+  statement {
+    sid       = "Enable IAM User Permissions"
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["kms:*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
+    sid       = "Allow access for Key Administrators"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ci"]
+    }
+  }
+
+  statement {
+    sid       = "Allow use of the key by Cloudtrail"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_kms_key" "cloudtrail_log_group_key" {
+  description             = "Cloudtrail log group encryption"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.cloudtrail_log_group_key.json
+}
+
+resource "aws_kms_alias" "cloudtrail_log_group_key" {
+  name          = "alias/ddb_cloudtrail_log_group_key_encryption"
+  target_key_id = aws_kms_key.cloudtrail_log_group_key.key_id
+}
+
+# See the following link for further information
+# https://docs.aws.amazon.com/kms/latest/developerguide/key-policies.html
+data "aws_iam_policy_document" "cloudtrail_log_group_key" {
+  statement {
+    sid       = "Enable Root account permissions on Key"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
+    }
+  }
+
+  statement {
+    sid       = "Allow Key to be used for Encryption"
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "logs.${data.aws_region.current.name}.amazonaws.com",
+        "events.amazonaws.com"
+      ]
+    }
+  }
+
+  statement {
+    sid       = "Key Administrator"
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"]
+    }
+  }
+}
