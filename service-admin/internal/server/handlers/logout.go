ae861bd7bcc4268297030a6c80a9b0a1499bd94b
SeemaMenon <seema.menon@digital.justice.gov.uk>
2022-04-21 16:57:17 +0100

    unused package removed

---
+++ service-admin/internal/server/handlers/logout.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/rs/zerolog/log"
+)
+
+func LogoutHandler(cognitoLogoutURL *url.URL) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {   
+		log.Ctx(r.Context()).Debug().Msg("Redirecting to cognito logout URL")
+
+		l := &url.URL{
+			Scheme: "https",
+			Host: r.Host,
+			Path: r.RequestURI,
+		}
+		
+		v := cognitoLogoutURL.Query()
+		v.Set("logout_uri", l.String())
+		cognitoLogoutURL.RawQuery = v.Encode()
+	
+		http.Redirect(w, r, cognitoLogoutURL.String(), 301)
+	}
+}
