c27c8c90c0fdc1809f765bab03ed40fa06fa0ec7
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-05-09 10:18:47 +0100

    UML-2238 / UML-2292 - format service discovery dns to support wildcarding (#1524)
    * create new sd dns alongside existing one
    
    * format new service discovery dns to support wildcarding

--- terraform/environment/api_ecs.tf
+++ terraform/environment/api_ecs.tf
@@ -49,6 +49,25 @@ resource "aws_service_discovery_service" "api" {
   }
 }
 
+resource "aws_service_discovery_service" "api_ecs" {
+  name = "api"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.internal_ecs.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
 //
 locals {
   api_service_fqdn = "${aws_service_discovery_service.api.name}.${aws_service_discovery_private_dns_namespace.internal.name}"
