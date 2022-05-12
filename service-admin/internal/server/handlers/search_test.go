8972f0308e92538724542f4146ebd5d4e4bdb611
lukejolliffe <luke.jolliffe@digital.justice.gov.uk>
2022-05-12 10:09:01 +0100

    fix lint errors

--- service-admin/internal/server/handlers/search_test.go
+++ service-admin/internal/server/handlers/search_test.go
@@ -5,71 +5,8 @@ import (
 	"reflect"
 	"testing"
 
-	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/ministryofjustice/opg-use-an-lpa/service-admin/internal/server/data"
 )
-
-func Test_search_Validate(t *testing.T) {
-	type fields struct {
-		Query  string
-		Type   queryType
-		Result interface{}
-		Errors validation.Errors
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &search{
-				Query:  tt.fields.Query,
-				Type:   tt.fields.Type,
-				Result: tt.fields.Result,
-				Errors: tt.fields.Errors,
-			}
-			if err := s.Validate(); (err != nil) != tt.wantErr {
-				t.Errorf("search.Validate() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func Test_search_checkEmailOrCode(t *testing.T) {
-	type fields struct {
-		Query  string
-		Type   queryType
-		Result interface{}
-		Errors validation.Errors
-	}
-	type args struct {
-		value interface{}
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &search{
-				Query:  tt.fields.Query,
-				Type:   tt.fields.Type,
-				Result: tt.fields.Result,
-				Errors: tt.fields.Errors,
-			}
-			if err := s.checkEmailOrCode(tt.args.value); (err != nil) != tt.wantErr {
-				t.Errorf("search.checkEmailOrCode() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
 
 type mockAccountService struct {
 	ActorByUserEmail func(context.Context, string) (*data.ActorUser, error)
@@ -80,6 +17,7 @@ func (m *mockAccountService) GetActorUserByEmail(ctx context.Context, email stri
 	if m.ActorByUserEmail != nil {
 		return m.ActorByUserEmail(ctx, email)
 	}
+
 	return &data.ActorUser{ID: ""}, nil
 }
 
@@ -87,6 +25,7 @@ func (m *mockAccountService) GetEmailByUserID(ctx context.Context, userID string
 	if m.EmailByUserID != nil {
 		return m.EmailByUserID(ctx, userID)
 	}
+
 	return "", nil
 }
 
@@ -99,6 +38,7 @@ func (m *mockLPAService) GetLpasByUserID(ctx context.Context, userID string) ([]
 	if m.LPAsByUserID != nil {
 		return m.LPAsByUserID(ctx, userID)
 	}
+
 	return []*data.LPA{}, nil
 }
 
@@ -106,10 +46,13 @@ func (m *mockLPAService) GetLPAByActivationCode(ctx context.Context, code string
 	if m.LPAByActivationCode != nil {
 		return m.LPAByActivationCode(ctx, code)
 	}
+
 	return nil, nil
 }
 
 func Test_doSearch(t *testing.T) {
+
+	t.Parallel()
 
 	testLPA := &data.LPA{
 		SiriusUID: "700000000123",
@@ -124,6 +67,7 @@ func Test_doSearch(t *testing.T) {
 		queryType      queryType
 		q              string
 	}
+
 	tests := []struct {
 		name string
 		args args
@@ -157,8 +101,10 @@ func Test_doSearch(t *testing.T) {
 			want: &data.ActorUser{ID: "TestID", Email: "test@email.com", LastLogin: "TestTime", LPAs: []*data.LPA{testLPA}},
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			if got := doSearch(tt.args.ctx, tt.args.accountService, tt.args.lpaService, tt.args.queryType, tt.args.q); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("doSearch() = %v, want %v", got, tt.want)
 			}
