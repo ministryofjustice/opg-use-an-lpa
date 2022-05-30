65219c332661e1ae157b405a67fce72229c837e5
John Nolan <john.nolan@digital.justice.gov.uk>
2022-05-30 12:02:48 +0100

    Made jest tests less brittle
    * Removed commented examples
    * Change from absolute array index searching to looking for expected object
    * When additional properties now get added it won't break the tests

--- service-front/web/src/javascript/googleAnalytics.test.js
+++ service-front/web/src/javascript/googleAnalytics.test.js
@@ -19,52 +19,27 @@ describe('given Google Analytics is enabled', () => {
         useAnalytics = new googleAnalytics('UA-12345');
     });
 
-    /**
-     * Description of data layer 1 in gtag
-     *  'config',
-     *  this.analyticsId, {
-            'linker': {
-                'domains': ['www.gov.uk']
-        },
-        'transport_type': 'beacon',
-        'anonymize_ip': true,
-        'allow_google_signals': false, //display-features
-        'allow_ad_personalization_signals': false //display-features
-     *
-     */
     test('it should have the correct config setup', () => {
-        expect(global.dataLayer.length).toEqual(2);
-        expect(global.dataLayer[1][0]).toBe('config');
-        expect(global.dataLayer[1][1]).toBe('UA-12345');
-
-        expect(global.dataLayer[1][2].linker).not.toBeUndefined();
-        expect(global.dataLayer[1][2].linker.domains.length).toBe(1);
-        expect(global.dataLayer[1][2].linker.domains[0]).toBe('www.gov.uk');
-
-        expect(global.dataLayer[1][2].anonymize_ip).not.toBeUndefined();
-        expect(global.dataLayer[1][2].anonymize_ip).toBe(true);
-
-        expect(global.dataLayer[1][2].transport_type).not.toBeUndefined();
-        expect(global.dataLayer[1][2].transport_type).toBe('beacon');
-
-        expect(global.dataLayer[1][2].allow_google_signals).not.toBeUndefined();
-        expect(global.dataLayer[1][2].allow_google_signals).toBe(false);
-
-        expect(global.dataLayer[1][2].allow_ad_personalization_signals).not.toBeUndefined();
-        expect(global.dataLayer[1][2].allow_ad_personalization_signals).toBe(false);
+        expect(global.dataLayer).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    '0': 'set',
+                    '1': { cookie_flags: 'SameSite=None;Secure' }
+                }),
+                expect.objectContaining({
+                    '0': 'config',
+                    '1': 'UA-12345',
+                    '2': {
+                        'linker': { 'domains': ['www.gov.uk'] },
+                        'transport_type': 'beacon',
+                        'anonymize_ip': true,
+                        'allow_google_signals': false,
+                        'allow_ad_personalization_signals': false,
+                        'page_title': 'Test Page Title',
+                        'page_path': '/use-lpa'
+                    }
+                })
+            ])
+        )
     });
-
-    /**
-     * Description of data layer 2 in gtag
-     * [Arguments] {
-      '0': 'event',
-      '1': 'event',
-      '2': {
-            event_category: 'event_category',
-            event_label: 'event_label',
-            value: 'value'
-         }
-        }
-     */
-
 });
