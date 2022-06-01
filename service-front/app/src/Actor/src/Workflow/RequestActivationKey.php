028853d65adc7a7e3dcbe98bd4f336d52f068791
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2022-06-01 11:01:54 +0100

    Uml 2213 Add collection of address to start of journey (#1586)
    * Cleanup existing handler
    
    * Add behat scenario and fixup additional tests to function with new page in place
    
    * Add new fields to workflow store
    
    * Add form for address capture
    
    * New handler and page template
    
    * Route to page handler
    
    * Insert new page in workflow and amend existing steps navigational and validation functions to be correct
    
    * Update translation template file with new content and edit form to be more accurate
    
    * Missing test file for the form
    
    * Indentation fixes
    
    * Turn on allow_older_lpas on PR env
    
    * Fix page title
    
    * New language extract

--- service-front/app/src/Actor/src/Workflow/RequestActivationKey.php
+++ service-front/app/src/Actor/src/Workflow/RequestActivationKey.php
@@ -20,6 +20,7 @@ class RequestActivationKey implements WorkflowState
     private ?string $actorType = null;
     public ?DateTimeImmutable $dob;
     public ?DateTimeImmutable $donorDob;
+
     /**
      * Lovely constructor promotion
      */
@@ -35,6 +36,9 @@ class RequestActivationKey implements WorkflowState
         public ?string $donorFirstNames = null,
         public ?string $donorLastName = null,
         ?string $donorDob = null,
+        public ?string $actorAddress1 = null,
+        public ?string $actorAddress2 = null,
+        public ?string $actorAddressTown = null,
         public ?string $telephone = null,
         public ?bool $noTelephone = null,
         // not used for entered data but to track workflow path
@@ -63,6 +67,9 @@ class RequestActivationKey implements WorkflowState
         $this->donorFirstNames = null;
         $this->donorLastName = null;
         $this->donorDob = null;
+        $this->actorAddress1 = null;
+        $this->actorAddress2 = null;
+        $this->actorAddressTown = null;
         $this->telephone = null;
         $this->noTelephone = null;
 
