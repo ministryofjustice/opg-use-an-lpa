f36fd5208c88fe2ecbd7c5d88bc24a8850317121
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2022-06-24 17:47:34 +0100

    Introduce a shared state singlton that can be used for the sharing of data between contexts.

---
+++ service-front/app/features/context/UI/SharedState.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BehatTest\Context\UI;
+
+final class SharedState
+{
+    private static ?SharedState $instance = null;
+
+    /** @var string The base part of the URL, typically '/' but could be a language prefix i.e. '/cy' */
+    public string $basePath = '';
+
+    /**
+     * @return SharedState
+     */
+    public static function getInstance(): SharedState
+    {
+        if (self::$instance === null) {
+            self::$instance = new SharedState();
+        }
+
+        return self::$instance;
+    }
+}
