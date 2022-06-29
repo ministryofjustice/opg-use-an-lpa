8c7e058f5d893d21fead2fdaa509e2a39929ff35
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-29 09:54:13 +0100

    redirect viewer service too

--- terraform/environment/viewer_load_balancer.tf
+++ terraform/environment/viewer_load_balancer.tf
@@ -130,12 +130,14 @@ resource "aws_lb_listener_rule" "viewer_maintenance" {
   listener_arn = aws_lb_listener.viewer_loadbalancer.arn
   priority     = 101 # Specifically set so that maintenance mode scripts can locate the correct rule to modify
   action {
-    type = "fixed-response"
+    type = "redirect"
 
-    fixed_response {
-      content_type = "text/html"
-      message_body = file("${path.module}/maintenance/viewer_maintenance.html")
-      status_code  = "503"
+    redirect {
+      host        = "maintenance.opg.service.justice.gov.uk"
+      path        = "/maintenance"
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_302"
     }
   }
   condition {
@@ -157,12 +159,14 @@ resource "aws_lb_listener_rule" "viewer_maintenance_welsh" {
   listener_arn = aws_lb_listener.viewer_loadbalancer.arn
   priority     = 100 # Specifically set so that maintenance mode scripts can locate the correct rule to modify
   action {
-    type = "fixed-response"
+    type = "redirect"
 
-    fixed_response {
-      content_type = "text/html"
-      message_body = file("${path.module}/maintenance/viewer_maintenance_welsh.html")
-      status_code  = "503"
+    redirect {
+      host        = "maintenance.opg.service.justice.gov.uk"
+      path        = "/maintenance"
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_302"
     }
   }
   condition {
