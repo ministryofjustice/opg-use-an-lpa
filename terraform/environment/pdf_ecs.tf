25559617c52a2c06e4233020e854ca2a98f4343a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-07-28 15:57:08 +0100

    UML-2367 - Define some default values for ECS services (#1702)
    * create an env
    
    * define deployment attributes

--- terraform/environment/pdf_ecs.tf
+++ terraform/environment/pdf_ecs.tf
@@ -23,6 +23,15 @@ resource "aws_ecs_service" "pdf" {
     weight            = 100
   }
 
+  deployment_circuit_breaker {
+    enable   = false
+    rollback = false
+  }
+
+  deployment_controller {
+    type = "ECS"
+  }
+
   wait_for_steady_state = true
 
   lifecycle {
