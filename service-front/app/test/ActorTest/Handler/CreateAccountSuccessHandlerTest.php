223fcd4ee9a6521fa794ee25f4c11514620533cf
SeemaMenon <seema.menon@digital.justice.gov.uk>
2022-08-11 01:37:41 +0100

    fix broken unit test

--- service-front/app/test/ActorTest/Handler/CreateAccountSuccessHandlerTest.php
+++ service-front/app/test/ActorTest/Handler/CreateAccountSuccessHandlerTest.php
@@ -6,7 +6,7 @@ namespace ActorTest\Handler;
 
 use Actor\Handler\CreateAccountSuccessHandler;
 use Common\Exception\ApiException;
-use Common\Service\Email\EmailClient;
+use Common\Service\Notify\NotifyService;
 use Common\Service\User\UserService;
 use Fig\Http\Message\StatusCodeInterface;
 use Laminas\Diactoros\Response\HtmlResponse;
@@ -36,9 +36,9 @@ class CreateAccountSuccessHandlerTest extends TestCase
     private $userServiceProphecy;
 
     /**
-     * @var EmailClient
+     * @var NotifyService
      */
-    private $emailClientProphecy;
+    private $notifyServiceProphecy;
 
     /**
      * @var ServerUrlHelper
@@ -56,8 +56,8 @@ class CreateAccountSuccessHandlerTest extends TestCase
         $this->templateRendererProphecy = $this->prophesize(TemplateRendererInterface::class);
         $this->urlHelperProphecy = $this->prophesize(UrlHelper::class);
         $this->userServiceProphecy = $this->prophesize(UserService::class);
-        $this->emailClientProphecy = $this->prophesize(EmailClient::class);
         $this->serverUrlHelperProphecy = $this->prophesize(ServerUrlHelper::class);
+        $this->notifyServiceProphecy = $this->prophesize(NotifyService::class);
 
         // The request
         $this->requestProphecy = $this->prophesize(ServerRequestInterface::class);
@@ -73,7 +73,7 @@ class CreateAccountSuccessHandlerTest extends TestCase
         }))->willReturn('');
 
         //  Set up the handler
-        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->emailClientProphecy->reveal(), $this->serverUrlHelperProphecy->reveal());
+        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->serverUrlHelperProphecy->reveal(), $this->notifyServiceProphecy->reveal());
 
         $this->requestProphecy->getQueryParams()
             ->willReturn([
@@ -91,7 +91,7 @@ class CreateAccountSuccessHandlerTest extends TestCase
             ->willReturn('/create-account');
 
         //  Set up the handler
-        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->emailClientProphecy->reveal(), $this->serverUrlHelperProphecy->reveal());
+        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->serverUrlHelperProphecy->reveal(), $this->notifyServiceProphecy->reveal());
 
         $this->requestProphecy->getQueryParams()
             ->willReturn([]);
@@ -121,7 +121,7 @@ class CreateAccountSuccessHandlerTest extends TestCase
         $this->serverUrlHelperProphecy->generate('/activate-account/activate1234567890')
             ->willReturn('http://localhost/activate-account/activate1234567890');
 
-        $this->emailClientProphecy->sendAccountActivationEmail('a@b.com', 'http://localhost/activate-account/activate1234567890')
+        $this->notifyServiceProphecy->sendEmailToUser('a@b.com', 'http://localhost/activate-account/activate1234567890','AccountActivationEmail',null,null,null)
             ->shouldBeCalled();
 
         $this->templateRendererProphecy->render('actor::create-account-success', new CallbackToken(function($options) {
@@ -134,7 +134,7 @@ class CreateAccountSuccessHandlerTest extends TestCase
         }))->willReturn('');
 
         //  Set up the handler
-        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->emailClientProphecy->reveal(), $this->serverUrlHelperProphecy->reveal());
+        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->serverUrlHelperProphecy->reveal(),$this->notifyServiceProphecy->reveal());
 
         $this->requestProphecy->getQueryParams()
             ->willReturn([
@@ -164,11 +164,11 @@ class CreateAccountSuccessHandlerTest extends TestCase
             ])
             ->willReturn('/create-account');
 
-        $this->emailClientProphecy->sendAccountActivationEmail('a@b.com', 'http://localhost/activate-account/activate1234567890')
+        $this->notifyServiceProphecy->sendEmailToUser('a@b.com', 'http://localhost/activate-account/activate1234567890','AccountActivationEmail',null,null,null)
             ->shouldNotBeCalled();
 
         //  Set up the handler
-        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->emailClientProphecy->reveal(), $this->serverUrlHelperProphecy->reveal());
+        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->serverUrlHelperProphecy->reveal(),$this->notifyServiceProphecy->reveal());
 
         $this->requestProphecy->getQueryParams()
             ->willReturn([
@@ -201,11 +201,11 @@ class CreateAccountSuccessHandlerTest extends TestCase
             ])
             ->willReturn('/create-account');
 
-        $this->emailClientProphecy->sendAccountActivationEmail('a@b.com', 'http://localhost/activate-account/activate1234567890')
+        $this->notifyServiceProphecy->sendEmailToUser('a@b.com', 'http://localhost/activate-account/activate1234567890','AccountActivationEmail',null,null,null)
             ->shouldNotBeCalled();
 
         //  Set up the handler
-        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->emailClientProphecy->reveal(), $this->serverUrlHelperProphecy->reveal());
+        $handler = new CreateAccountSuccessHandler($this->templateRendererProphecy->reveal(), $this->urlHelperProphecy->reveal(), $this->userServiceProphecy->reveal(), $this->serverUrlHelperProphecy->reveal(),$this->notifyServiceProphecy->reveal());
 
         $this->requestProphecy->getQueryParams()
             ->willReturn([
