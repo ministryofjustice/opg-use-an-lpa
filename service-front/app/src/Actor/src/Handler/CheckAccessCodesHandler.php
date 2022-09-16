6da5744248d0e8610ce7a2991a3818b277febc52
lukejolliffe <luke.jolliffe@digital.justice.gov.uk>
2022-09-16 08:07:22 +0100

    continue loop rather than check for existence of ActorId everywhere

--- service-front/app/src/Actor/src/Handler/CheckAccessCodesHandler.php
+++ service-front/app/src/Actor/src/Handler/CheckAccessCodesHandler.php
@@ -103,10 +103,14 @@ class CheckAccessCodesHandler extends AbstractHandler implements UserAware, Csrf
                 $shareCodes[$key]['form'] = $form;
             }
 
+            if (!isset($code['ActorId'])) {
+                continue;
+            }
+
             $this->logger->debug(
                 'Resolved actor id to {type}:{actor_id}',
                 [
-                    'actor_id' => $code['ActorId'] ?? 'ActorId not present',
+                    'actor_id' => $code['ActorId'],
                     'type' => gettype($code['ActorId']),
                 ]
             );
