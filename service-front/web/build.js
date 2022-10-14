2bd47c3b74da80c2874bb4a9e12f6072e48d01a5
lukejolliffe <luke.jolliffe@digital.justice.gov.uk>
2022-10-14 13:49:47 +0100

    Make things work and streamline things, remove node-sass in favour of sass
    Co-authored-by: John Nolan <johnnolan@users.noreply.github.com>

--- service-front/web/build.js
+++ service-front/web/build.js
@@ -1,49 +1,50 @@
-import {sassPlugin} from 'esbuild-sass-plugin'
-import esbuild from 'esbuild'
-import { cp } from 'fs/promises';
+import esbuild from 'esbuild';
+import { default as fsWithCallbacks } from 'fs';
+const fs = fsWithCallbacks.promises;
 
 (async () => {
-
-  const width= process.stdout.columns || 40;
-  const hr = "\r\n".padStart(width/1.5, '-');
+  const width = process.stdout.columns || 40;
+  const hr = '\r\n'.padStart(width / 1.5, '-');
 
   let config = {
-    entrypoints : ['./src/index.js'],
-    copy_files : {
-      './src/robots.txt':'robots.txt',
-      './node_modules/govuk-frontend/govuk/assets':'assets',
-      './src/images':'assets/images',
-      './node_modules/@ministryofjustice/frontend/moj/assets':'assets'
+    entrypoints: ['./src/index.js'],
+    copy_files: {
+      './src/robots.txt': 'robots.txt',
+      './node_modules/govuk-frontend/govuk/assets': 'assets',
+      './src/images': 'assets/images',
+      './node_modules/@ministryofjustice/frontend/moj/assets': 'assets',
     },
-    out_dir:'./dist'
-  }
+    out_dir: './dist',
+  };
 
-  console.log(`${hr}- Building with:\r\n\r\n${config.entrypoints.join("\r\n")}\r\n${hr}`);
-
+  console.log(
+    `${hr}- Building with:\r\n\r\n${config.entrypoints.join('\r\n')}\r\n${hr}`,
+  );
 
   //files to copy (uses experimental node cp)
-  console.log(`- Copying files:\r\n`)
-  for (const [file, file_dest] of Object.entries(config.copy_files)) { 
+  console.log(`- Copying files:\r\n`);
+  for (const [file, file_dest] of Object.entries(config.copy_files)) {
     const destination = config.out_dir + '/' + file_dest;
-    await cp(file, destination, {recursive:true});
+    await fs.cp(file, destination, { recursive: true });
     console.log(`Copied file from ${file} file to ${destination}`);
   }
 
-  let result = await esbuild.build({
+  let result = await esbuild
+    .build({
       entryPoints: config.entrypoints,
       bundle: true,
       outdir: config.out_dir,
       minify: false,
       target: ['es2016'],
       plugins: [],
-      metafile : true,
+      metafile: true,
       treeShaking: false,
-      }).catch(() => process.exit(1))
+    })
+    .catch(() => process.exit(1));
   console.log(hr);
 
-  let text = await esbuild.analyzeMetafile(result.metafile)
-  console.log(`- Analysis:\r\n${text}${hr}`)
+  let text = await esbuild.analyzeMetafile(result.metafile);
+  console.log(`- Analysis:\r\n${text}${hr}`);
 
   console.log(hr);
-
-})()
+})();
