579d46e1a02094d4ddb235746b748163f9e581fd
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2022-10-24 16:31:18 +0100

    Move parts of the pipeline around
    Start handling logged in session issues as a part of the authentication middleware pipe.

---
+++ service-front/app/src/Common/src/Middleware/Authentication/CredentialAuthenticationMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Common\Middleware\Authentication;
+
+use Common\Middleware\Session\SessionExpiryMiddleware;
+use Laminas\Diactoros\Response\RedirectResponse;
+use Mezzio\Authentication\AuthenticationInterface;
+use Mezzio\Authentication\Session\Exception\MissingSessionContainerException;
+use Mezzio\Authentication\UserInterface;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Session\SessionMiddleware;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class CredentialAuthenticationMiddleware implements MiddlewareInterface
+{
+    public function __construct(protected AuthenticationInterface $auth, protected UrlHelper $helper)
+    {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $session = $request->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE);
+        if (! $session) {
+            throw MissingSessionContainerException::create();
+        }
+
+        // if an expiry key has been set then it is the case we should direct user to the session-expired page
+        if ($session->has(SessionExpiryMiddleware::SESSION_EXPIRED_KEY)) {
+            return new RedirectResponse($this->helper->generate('session-expired'));
+        }
+
+        $user = $this->auth->authenticate($request);
+
+        if (null !== $user) {
+            return $handler->handle($request->withAttribute(UserInterface::class, $user));
+        }
+
+        return $this->auth->unauthorizedResponse($request);
+    }
+}
