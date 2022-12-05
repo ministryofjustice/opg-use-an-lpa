9c1fa3fa33ce7a7dfb8a1774f5e271bf9270359a
SeemaMenon <seema.menon@digital.justice.gov.uk>
2022-12-05 18:22:26 +0000

    UML-2671: Show metrics to accounts in stats page (#2082)
    * changes to now retrieve metrics from stats table and show on page including tests
    
    * Seeding few more metric values to local stats table
    
    * test amendments and design change
    
    * unwanted file removed
    
    * Linting and code style changes
    
    * Abstract time to and interface so we can test things better.
    
    Co-authored-by: Adam Cooper <adam.cooper@digital.justice.gov.uk>

--- service-admin/internal/server/handlers/logout.go
+++ service-admin/internal/server/handlers/logout.go
@@ -8,30 +8,30 @@ import (
 )
 
 func LogoutHandler(cognitoLogoutURL *url.URL) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {   
+	return func(w http.ResponseWriter, r *http.Request) {
 		log.Ctx(r.Context()).Debug().Msg("Redirecting to cognito logout URL")
 
 		cookie := &http.Cookie{
-			Name: "AWSELBAuthSessionCookie-0",
+			Name:   "AWSELBAuthSessionCookie-0",
 			MaxAge: -1,
 		}
-		
-		http.SetCookie(w,cookie)
+
+		http.SetCookie(w, cookie)
 
 		w.Header().Add("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0") // HTTP 1.1.
-		w.Header().Add("Pragma", "no-cache"); // HTTP 1.0.
-		w.Header().Add("Expires", "0"); // Proxies.
+		w.Header().Add("Pragma", "no-cache")                                              // HTTP 1.0.
+		w.Header().Add("Expires", "0")                                                    // Proxies.
 
 		l := &url.URL{
 			Scheme: "https",
-			Host: r.Host,
-			Path: "/",
+			Host:   r.Host,
+			Path:   "/",
 		}
-		
+
 		v := cognitoLogoutURL.Query()
 		v.Set("logout_uri", l.String())
 		cognitoLogoutURL.RawQuery = v.Encode()
-	
+
 		http.Redirect(w, r, cognitoLogoutURL.String(), http.StatusMovedPermanently)
 	}
 }
