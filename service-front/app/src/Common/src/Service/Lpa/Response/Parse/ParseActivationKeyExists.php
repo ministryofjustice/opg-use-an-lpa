c77eac3eeb041f4f1782d71c5248497c779947e3
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2023-03-16 11:07:43 +0000

    Attempt at complexity reducing

--- service-front/app/src/Common/src/Service/Lpa/Response/Parse/ParseActivationKeyExists.php
+++ service-front/app/src/Common/src/Service/Lpa/Response/Parse/ParseActivationKeyExists.php
@@ -6,6 +6,7 @@ namespace Common\Service\Lpa\Response\Parse;
 
 use Common\Service\Lpa\LpaFactory;
 use Common\Service\Lpa\Response\ActivationKeyExists;
+use Exception;
 use InvalidArgumentException;
 
 class ParseActivationKeyExists
@@ -18,15 +19,14 @@ class ParseActivationKeyExists
     {
     }
 
+    /**
+     * @param array{donor: array, caseSubtype: string} $data
+     * @return ActivationKeyExists
+     * @throws Exception
+     */
     public function __invoke(array $data): ActivationKeyExists
     {
-        if (
-            !isset($data['donor']['uId']) ||
-            !array_key_exists('firstname', $data['donor']) ||
-            !array_key_exists('middlenames', $data['donor']) ||
-            !array_key_exists('surname', $data['donor']) ||
-            !isset($data['caseSubtype'])
-        ) {
+        if (!$this->isValidData($data)) {
             throw new InvalidArgumentException(
                 'The data array passed to ' . __METHOD__ . ' does not contain the required fields'
             );
@@ -41,5 +41,24 @@ class ParseActivationKeyExists
         }
 
         return $response;
+    }
+
+    /**
+     * @param array{donor: array, caseSubtype: string} $data
+     * @return bool
+     */
+    private function isValidData(array $data): bool
+    {
+        if (
+            !isset($data['donor']['uId']) ||
+            !array_key_exists('firstname', $data['donor']) ||
+            !array_key_exists('middlenames', $data['donor']) ||
+            !array_key_exists('surname', $data['donor']) ||
+            !isset($data['caseSubtype'])
+        ) {
+            return false;
+        }
+
+        return true;
     }
 }
