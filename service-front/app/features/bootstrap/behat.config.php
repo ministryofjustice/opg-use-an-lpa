8905c384a17527c422fa3a5a7fb165e1794fc9a9
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2023-06-20 08:37:55 +0100

    turn instructions and preferences back off for now in dev as smoke test will fail until next part of work on front is done

--- service-front/app/features/bootstrap/behat.config.php
+++ service-front/app/features/bootstrap/behat.config.php
@@ -38,7 +38,7 @@ return [
         'allow_older_lpas'                                           => true,
         'save_older_lpa_requests'                                    => true,
         'dont_send_lpas_registered_after_sep_2019_to_cleansing_team' => true,
-        'instructions_and_preferences'                               => true,
+        'instructions_and_preferences'                               => false,
     ],
     'notify'                       => [
         'api' => [
