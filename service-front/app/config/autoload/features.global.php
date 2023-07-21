1eaa09d91a1dfba0ee54c50e77c40a6481026f53
Lbagg1 <lowenna.baggaley@digital.justice.gov.uk>
2023-07-21 17:05:03 +0100

    uml-2905 (#2291)
    * deleted flag from terraform
    
    * removed flag from docker-compose
    
    * removed flag from actor.ecs.tf
    
    * delete flag from features.global.php and refactored
    
    * deleted flag conditional from routes.php
    
    * removed flags from activation-key twig
    
    * removed feature flag from check-lpa twig
    
    * tidy up
    
    * deleted feature flag test
    
    * delete flag from behat.config
    
    * delete flag
    
    * update trans tag

--- service-front/app/config/autoload/features.global.php
+++ service-front/app/config/autoload/features.global.php
@@ -4,14 +4,29 @@ declare(strict_types=1);
 
 return [
     'feature_flags' => [
-        'use_older_lpa_journey'                                      => filter_var(getenv('USE_OLDER_LPA_JOURNEY'), FILTER_VALIDATE_BOOLEAN) ?: false,
-        'delete_lpa_feature'                                         => filter_var(getenv('DELETE_LPA_FEATURE'), FILTER_VALIDATE_BOOLEAN) ?: false,
-        'allow_older_lpas'                                           => filter_var(getenv('ALLOW_OLDER_LPAS'), FILTER_VALIDATE_BOOLEAN) ?: false,
-        'allow_meris_lpas'                                           => filter_var(getenv('ALLOW_MERIS_LPAS'), FILTER_VALIDATE_BOOLEAN) ?: false,
+        'delete_lpa_feature' => filter_var(
+            getenv('DELETE_LPA_FEATURE'),
+            FILTER_VALIDATE_BOOLEAN
+        ) ?: false,
+
+        'allow_older_lpas' => filter_var(
+            getenv('ALLOW_OLDER_LPAS'),
+            FILTER_VALIDATE_BOOLEAN
+        ) ?: false,
+
+        'allow_meris_lpas' => filter_var(
+            getenv('ALLOW_MERIS_LPAS'),
+            FILTER_VALIDATE_BOOLEAN
+        ) ?: false,
+
         'dont_send_lpas_registered_after_sep_2019_to_cleansing_team' => filter_var(
             getenv('DONT_SEND_LPAS_REGISTERED_AFTER_SEP_2019_TO_CLEANSING_TEAM'),
             FILTER_VALIDATE_BOOLEAN
         ) ?: false,
-        'instructions_and_preferences' => filter_var(getenv('INSTRUCTIONS_AND_PREFERENCES'), FILTER_VALIDATE_BOOLEAN) ?: false,
+
+        'instructions_and_preferences' => filter_var(
+            getenv('INSTRUCTIONS_AND_PREFERENCES'),
+            FILTER_VALIDATE_BOOLEAN
+        ) ?: false,
     ],
 ];
