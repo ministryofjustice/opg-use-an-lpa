be32d9f1273b051cb7970fbef4d50ba308950bc4
Lowenna Baggaley <lowenna.baggaley@digital.justice.gov.uk>
2023-09-20 17:35:23 +0100

    config and cache factory

--- service-api/app/src/App/src/Service/Cache/CacheFactory.php
+++ service-api/app/src/App/src/Service/Cache/CacheFactory.php
@@ -2,21 +2,33 @@
 
 declare(strict_types=1);
 
-namespace Api\Service\Cache;
+namespace App\Service\Cache;
 
+use Laminas\Cache\Psr\SimpleCache\SimpleCacheDecorator;
+use Laminas\Cache\Service\StorageAdapterFactoryInterface;
 use Psr\Container\ContainerInterface;
 use Laminas\Cache\Storage\Adapter\Apcu;
+use Psr\SimpleCache\CacheInterface;
 
 class CacheFactory
 {
-    public function __invoke(ContainerInterface $container)
+    public function __construct(private ContainerInterface $container)
     {
-        $cache = new Apcu();
+    }
 
-        $cache->setOptions([
-          'ttl' => 3600,
-        ]);
+    public function __invoke(string $cacheName): CacheInterface
+    {
+        $config = $this->container->get('config');
 
-        return $cache;
+        if (!isset($config['cache'])) {
+            throw new \RuntimeException('Missing cache configuration');
+        }
+        if (!isset($config['cache'][$cacheName])) {
+            throw new \RuntimeException('Missing cache configuration for ' . $cacheName);
+        }
+        $factory = $this->container->get(StorageAdapterFactoryInterface::class);
+        /** @var Apcu $cacheAdaptor */
+        $cacheAdaptor = $factory->createFromArrayConfiguration($config['cache'][$cacheName]);
+        return new SimpleCacheDecorator($cacheAdaptor);
     }
 }
