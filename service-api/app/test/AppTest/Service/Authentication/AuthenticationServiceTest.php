79359326c1823d03056efa670efb1730763bb057
Mishkat Najam <mishkat.najam@digital.justice.gov.uk>
2023-10-05 15:12:57 +0100

    revert to unit test

--- service-api/app/test/AppTest/Service/Authentication/AuthenticationServiceTest.php
+++ service-api/app/test/AppTest/Service/Authentication/AuthenticationServiceTest.php
@@ -28,7 +28,7 @@ class AuthenticationServiceTest extends TestCase
     }
 
     /**
-     * @integration
+     * @test
      */
     public function get_redirect_uri_en(): void
     {
@@ -37,6 +37,6 @@ class AuthenticationServiceTest extends TestCase
         $this->assertStringContainsString('client_id=client-id', $redirectUri);
         $this->assertStringContainsString('scope=openid+email', $redirectUri);
         $this->assertStringContainsString('vtr=%5B%22Cl.Cm.P2%22%5D', $redirectUri);
-        $this->assertStringContainsString('ui_locales=cy', $redirectUri);
+        $this->assertStringContainsString('ui_locales=en', $redirectUri);
     }
 }
