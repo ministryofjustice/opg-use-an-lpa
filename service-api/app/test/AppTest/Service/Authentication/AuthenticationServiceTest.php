5cec7df69adee4630295e3a816ca3c8c53140d27
Mishkat Najam <mishkat.najam@digital.justice.gov.uk>
2023-10-05 14:46:16 +0100

    Test setting test as acceptance test

--- service-api/app/test/AppTest/Service/Authentication/AuthenticationServiceTest.php
+++ service-api/app/test/AppTest/Service/Authentication/AuthenticationServiceTest.php
@@ -6,8 +6,6 @@ namespace AppTest\Service\Authentication;
 
 use App\Service\Authentication\AuthenticationService;
 use App\Service\Authentication\JWKFactory;
-use Facile\OpenIDClient\Issuer\IssuerBuilder;
-use Facile\OpenIDClient\Issuer\IssuerInterface;
 use Jose\Component\Core\JWK;
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
@@ -27,22 +25,18 @@ class AuthenticationServiceTest extends TestCase
         $this->JWKFactory = $this->prophesize(JWKFactory::class);
         $this->JWKFactory->__invoke()->willReturn($jwk);
         $this->logger = $this->prophesize(LoggerInterface::class);
-
-//        $issuer = $this->prophesize(IssuerBuilder::class);
-//        $issuer->reveal();
-//        $issuer->build('http://mock-one-login:8080/.well-kown/openid-configuration');
     }
 
     /**
-     * @test
+     * @acceptance
      */
-    public function getRedirectUri(): void
+    public function get_redirect_uri_en(): void
     {
         $authenticationService = new AuthenticationService($this->JWKFactory->reveal(), $this->logger->reveal());
-        // TODO this needs to become an integration test
         $redirectUri = $authenticationService->redirect('en');
         $this->assertStringContainsString('client_id=client-id', $redirectUri);
         $this->assertStringContainsString('scope=openid+email', $redirectUri);
         $this->assertStringContainsString('vtr=%5B%22Cl.Cm.P2%22%5D', $redirectUri);
+        $this->assertStringContainsString('ui_locales=en', $redirectUri);
     }
 }
