b87582a95dd1529e5a3f292a3522136a4a8517a7
MishNajam <61416092+MishNajam@users.noreply.github.com>
2023-10-11 14:46:07 +0100

    UML-3056 initial code and working test (#2360)
    * UML-3056 initial code and working test
    
    * UML-3056 fix php linting
    
    * get mock onelogin from ecr and spin that up locally and in CI
    
    * tell docker where image is
    
    * move ecr login earlier as its needed to pull down mock login container
    
    * use variable for registry
    
    * add docker logs
    
    * force a rebuild
    
    * docker inspect to get ip addr of mock
    
    * ecr login
    
    * add jwks to authService and test, and check ui_locale set to en or cy
    
    * Turn AuthenticationServiceTest into unit test
    
    * Remove try catch in createAuthorisationRequest
    
    ---------
    
    Co-authored-by: Nick Davis <nicholas.davis@digital.justice.gov.uk>
    Co-authored-by: Lbagg1 <lowenna.baggaley@digital.justice.gov.uk>
    Co-authored-by: Nick Davis <62664046+nickdavis2001@users.noreply.github.com>

---
+++ service-api/app/src/App/src/Service/Authentication/OneLoginAuthorisationRequestService.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Authentication;
+
+use Facile\OpenIDClient\Client\ClientBuilder;
+use Facile\OpenIDClient\Client\Metadata\ClientMetadata;
+use Facile\OpenIDClient\Issuer\IssuerBuilderInterface;
+use Facile\OpenIDClient\Service\Builder\AuthorizationServiceBuilder;
+
+use function Facile\OpenIDClient\base64url_encode;
+
+class OneLoginAuthorisationRequestService
+{
+    public function __construct(
+        private JWKFactory $JWKFactory,
+        private IssuerBuilderInterface $issuerBuilder,
+    ) {
+    }
+
+    public function createAuthorisationRequest(string $uiLocale): string
+    {
+        //TODO UML-3080 Configure cache
+
+        $issuer = $this->issuerBuilder
+            ->build('http://mock-one-login:8080/.well-known/openid-configuration');
+
+
+        $clientMetadata = ClientMetadata::fromArray([
+            'client_id'                  => 'client-id',
+            'client_secret'              => 'my-client-secret',
+            'token_endpoint_auth_method' => 'private_key_jwt',
+            'jwks'                       => [
+                'keys' => [
+                    ($this->JWKFactory)(),
+                ],
+            ],
+                                                    ]);
+
+        $client = (new ClientBuilder())
+            ->setIssuer($issuer)
+            ->setClientMetadata($clientMetadata)
+            ->build();
+
+        $authorisationService = (new AuthorizationServiceBuilder())->build();
+
+        return $authorisationService->getAuthorizationUri(
+            $client,
+            [
+                'scope'        => 'openid email',
+                'state'        => base64url_encode(random_bytes(12)),
+                'redirect_uri' => '/lpa/dashboard',
+                'nonce'        => openssl_digest(base64url_encode(random_bytes(12)), 'sha256'),
+                'vtr'          => '["Cl.Cm.P2"]',
+                'ui_locales'   => $uiLocale,
+                'claims'       => '{"userinfo":{"https://vocab.account.gov.uk/v1/coreIdentityJWT": null}}',
+            ]
+        );
+    }
+}
---
+++ service-api/app/test/AppTest/Service/Authentication/OneLoginAuthorisationRequestServiceTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Service\Authentication;
+
+use App\Service\Authentication\OneLoginAuthorisationRequestService;
+use App\Service\Authentication\JWKFactory;
+use Facile\OpenIDClient\Issuer\IssuerBuilder;
+use Facile\OpenIDClient\Issuer\IssuerBuilderInterface;
+use Facile\OpenIDClient\Issuer\IssuerInterface;
+use Facile\OpenIDClient\Issuer\Metadata\IssuerMetadataInterface;
+use Jose\Component\Core\JWK;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
+
+class OneLoginAuthorisationRequestServiceTest extends TestCase
+{
+    use ProphecyTrait;
+
+    private ObjectProphecy|JWKFactory $JWKFactory;
+    private ObjectProphecy|IssuerBuilder $issuerBuilder;
+
+    public function setup(): void
+    {
+        $jwk                 = $this->prophesize(JWK::class);
+        $this->JWKFactory    = $this->prophesize(JWKFactory::class);
+        $this->issuerBuilder = $this->prophesize(IssuerBuilderInterface::class);
+        $issuer              = $this->prophesize(IssuerInterface::class);
+        $issuerMetaData      = $this->prophesize(IssuerMetadataInterface::class);
+
+        $this->JWKFactory->__invoke()->willReturn($jwk);
+        $issuer->getMetadata()->willReturn($issuerMetaData);
+        $issuerMetaData->getAuthorizationEndpoint()->willReturn('fake endpoint');
+        $this->issuerBuilder->build('http://mock-one-login:8080/.well-known/openid-configuration')->willReturn($issuer);
+    }
+
+    /**
+     * @test
+     */
+    public function create_authorisation_request(): void
+    {
+        $authorisationRequestService = new OneLoginAuthorisationRequestService(
+            $this->JWKFactory->reveal(),
+            $this->issuerBuilder->reveal()
+        );
+        $authorisationRequest        = $authorisationRequestService->createAuthorisationRequest('en');
+        $this->assertStringContainsString('client_id=client-id', $authorisationRequest);
+        $this->assertStringContainsString('scope=openid+email', $authorisationRequest);
+        $this->assertStringContainsString('vtr=%5B%22Cl.Cm.P2%22%5D', $authorisationRequest);
+        $this->assertStringContainsString('ui_locales=en', $authorisationRequest);
+        $this->assertStringContainsString('redirect_uri=%2Flpa%2Fdashboard', $authorisationRequest);
+    }
+}
