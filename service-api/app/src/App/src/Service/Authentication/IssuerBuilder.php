aa92096d95ad998dd67847f51d6ca6a52716eefe
Lowenna Baggaley <lowenna.baggaley@digital.justice.gov.uk>
2023-10-12 09:21:27 +0100

    fix linting error

--- service-api/app/src/App/src/Service/Authentication/IssuerBuilder.php
+++ service-api/app/src/App/src/Service/Authentication/IssuerBuilder.php
@@ -7,7 +7,7 @@ namespace App\Service\Authentication;
 use Facile\OpenIDClient\Issuer\IssuerBuilderInterface;
 use Facile\OpenIDClient\Issuer\IssuerInterface;
 use Facile\OpenIDClient\Issuer\Metadata\Provider\MetadataProviderBuilder;
-use \Facile\OpenIDClient\Issuer\IssuerBuilder as FacileIssuerBuilder;
+use Facile\OpenIDClient\Issuer\IssuerBuilder as FacileIssuerBuilder;
 
 class IssuerBuilder implements IssuerBuilderInterface
 {
