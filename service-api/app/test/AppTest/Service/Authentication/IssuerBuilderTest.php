ca62110e24b731579d84d43ed86b3acaeaaf11c3
Mishkat Najam <mishkat.najam@digital.justice.gov.uk>
2023-10-12 14:24:32 +0100

    Add unit test coverage for IssuerBuilder setMetaDataProviderBuilder()

---
+++ service-api/app/test/AppTest/Service/Authentication/IssuerBuilderTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Service\Authentication;
+
+use App\Service\Authentication\IssuerBuilder;
+use Facile\OpenIDClient\Issuer\Metadata\Provider\MetadataProviderBuilder;
+use PHPUnit\Framework\TestCase;
+
+class IssuerBuilderTest extends TestCase
+{
+    private IssuerBuilder $issuerBuilder;
+    public function setUp(): void
+    {
+        $this->issuerBuilder = new IssuerBuilder();
+    }
+
+    /** @test */
+    public function can_set_metadata_provider_builder(): void
+    {
+        $issuerBuilder = $this->issuerBuilder->setMetadataProviderBuilder(new MetadataProviderBuilder());
+        self::assertInstanceOf(IssuerBuilder::class, $issuerBuilder);
+    }
+}
