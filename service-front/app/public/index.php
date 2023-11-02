2cf22f830e280c6242d25cbc63e37a5c6f592a1c
Lowenna Baggaley <lowenna.baggaley@digital.justice.gov.uk>
2023-11-02 11:00:40 +0000

    phpcbf app/public

--- service-front/app/public/index.php
+++ service-front/app/public/index.php
@@ -14,17 +14,21 @@ require 'vendor/autoload.php';
  * Self-called anonymous function that creates its own scope and keep the global namespace clean.
  */
 (function () {
-    /** @var \Psr\Container\ContainerInterface $container */
-    $container = require 'config/container.php';
+    /**
+ * @var \Psr\Container\ContainerInterface $container 
+*/
+    $container = include 'config/container.php';
 
-    /** @var \Mezzio\Application $app */
+    /**
+ * @var \Mezzio\Application $app 
+*/
     $app = $container->get(\Mezzio\Application::class);
     $factory = $container->get(\Mezzio\MiddlewareFactory::class);
 
     // Execute programmatic/declarative middleware pipeline and routing
     // configuration statements
-    (require 'config/pipeline.php')($app, $factory, $container);
-    (require 'config/routes.php')($app, $factory, $container);
+    (include 'config/pipeline.php')($app, $factory, $container);
+    (include 'config/routes.php')($app, $factory, $container);
 
     $app->run();
 })();
