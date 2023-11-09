2df333bc19b892faeb588c449cc0504ded37a278
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-11-09 13:55:01 +0000

    UML-3159 Move VPC flow log IAM (#2420)
    * UML-3174 Allow Renovate to manage TF version
    
    * UML-3159 Move IAM for VPC flow logs

--- terraform/account/region.tf
+++ terraform/account/region.tf
@@ -5,6 +5,7 @@ module "eu_west_1" {
   account_name             = local.account_name
   environment_name         = local.environment
   lambda_container_version = var.lambda_container_version
+  vpc_flow_logs_iam_role   = aws_iam_role.vpc_flow_logs
 
   depends_on = [
     module.cloudwatch_mrk,
