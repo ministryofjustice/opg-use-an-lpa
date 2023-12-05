09e25fa3c1c61a9c2cc8d17e61df41491ca230c8
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2023-12-05 15:12:07 +0000

    test for server.go is passing now

--- service-admin/internal/server/handlers/systemmessage.go
+++ service-admin/internal/server/handlers/systemmessage.go
@@ -1,17 +1,18 @@
 package handlers
 
 import (
-	"github.com/ministryofjustice/opg-use-an-lpa/service-admin/internal/server/data"
 	"github.com/rs/zerolog/log"
 	"net/http"
 )
 
+type SystemMessageService interface{}
+
 type SystemMessageServer struct {
-	systemMessageService data.SystemMessageService
+	systemMessageService SystemMessageService
 	templateService      TemplateWriterService
 }
 
-func NewSystemMessageServer(systemMessageService data.SystemMessageService, templateWriterService TemplateWriterService) *SystemMessageServer {
+func NewSystemMessageServer(systemMessageService SystemMessageService, templateWriterService TemplateWriterService) *SystemMessageServer {
 	return &SystemMessageServer{
 		systemMessageService: systemMessageService,
 		templateService:      templateWriterService,
@@ -19,9 +20,6 @@ func NewSystemMessageServer(systemMessageService data.SystemMessageService, temp
 }
 
 func (s *SystemMessageServer) SystemMessageHandler(w http.ResponseWriter, r *http.Request) {
-	search := &Search{
-		Path: r.URL.Path,
-	}
 
 	if r.Method == "POST" {
 		err := r.ParseForm()
@@ -32,7 +30,7 @@ func (s *SystemMessageServer) SystemMessageHandler(w http.ResponseWriter, r *htt
 
 	// TODO pass textarea values in to template
 
-	if err := s.templateService.RenderTemplate(w, r.Context(), "systemmessage.page.gohtml", search); err != nil {
+	if err := s.templateService.RenderTemplate(w, r.Context(), "systemmessage.page.gohtml", nil); err != nil {
 		log.Panic().Err(err).Msg(err.Error())
 	}
 }
