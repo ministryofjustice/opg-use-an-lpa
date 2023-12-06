ff0cec8cd866a409c974ffe390c205e39d55ffa8
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2023-12-06 18:32:47 +0000

    Allow configuration of insecure https

--- tests/smoke/src/Drivers/ChromeDriver.php
+++ tests/smoke/src/Drivers/ChromeDriver.php
@@ -11,6 +11,7 @@ class ChromeDriver implements Driver
     private ?Process $process;
 
     public function __construct(
+        bool $allowInsecureHttps = false,
         private string $binPath = '/usr/bin/google-chrome-stable',
         private array $binArguments = [
             '--disable-gpu',
@@ -25,10 +26,13 @@ class ChromeDriver implements Driver
             '--no-startup-window',
             '--no-first-run',
             '--no-pings',
-            '--ignore-certificate-errors',
         ],
     ) {
         $this->process = null;
+
+        if ($allowInsecureHttps) {
+            $this->binArguments[] = '--ignore-certificate-errors';
+        }
     }
 
     public function start(): void
