0d5c88654fdad73979ebc92831afddb5a384c501
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2023-12-18 16:02:27 +0000

    mocked out service in tests (WIP)

--- service-admin/internal/server/handlers/systemmessage_test.go
+++ service-admin/internal/server/handlers/systemmessage_test.go
@@ -1,10 +1,19 @@
 package handlers_test
 
 import (
+	"context"
 	"testing"
 )
 
-type MockSSMConnection struct{}
+type mockSystemMessageService struct{}
+
+func (m mockSystemMessageService) GetSystemMessages(ctx context.Context) (systemMessages map[string]string, err error) {
+	return nil, nil
+}
+
+func (m mockSystemMessageService) PutSystemMessages(ctx context.Context, messages map[string]string) (err error) {
+	return nil
+}
 
 // 1. rendering template loads data from parameter store
 // 2. save button calls appropriate functions (4 combinations) to save to parameter store
@@ -15,4 +24,10 @@ func Test_RenderTemplateLoadsFromParameterStore(t *testing.T) {
 	// mock class that loads
 	t.Parallel()
 	t.Errorf("Oh no !!!")
+}
+
+func Test_SaveButtonSavesToParameterStore(t *testing.T) {
+	// mock class that loads
+	t.Parallel()
+	t.Errorf("Oh no 2!!!")
 }
