7d3d427be7e1f0a41774c36c8bb52b4ff380e68d
Matt Machell <matt.machell@digital.justice.gov.uk>
2023-12-19 13:47:19 +0000

    pin docker bare. Remove older zapcli.

--- zap/scripts/viewer.sh
+++ zap/scripts/viewer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zap-cli --verbose quick-scan --self-contained http://host.docker.internal:9001 --spider -o '-config api.disablekey=true' -l Medium
+zap.sh -t https://host.docker.internal:9041/home  -J 
