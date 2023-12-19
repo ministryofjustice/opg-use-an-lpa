1532317b84c54576bf9349afa4b7bb91b126ee37
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-12-19 15:01:44 +0000

    Pull corret image

--- terraform/environment/region/api_ecs.tf
+++ terraform/environment/region/api_ecs.tf
@@ -364,7 +364,7 @@ locals {
     {
       cpu       = 0,
       essential = false,
-      image     = "ntse/export-php-metrics:v0.5.0",
+      image     = "ntse/export-php-metrics:v0.6.0",
       name      = "fpm-stats-export",
       logConfiguration = {
         logDriver = "awslogs",
