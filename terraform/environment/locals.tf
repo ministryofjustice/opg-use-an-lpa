9d5a2fe651764dd97a8e4d875e776c9323d2e82a
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-12-21 16:31:44 +0000

    UML-3225 Remove unnecessary data lookups (#2472)
    * UML-3225 Remove unnecessary data lookups

--- terraform/environment/locals.tf
+++ terraform/environment/locals.tf
@@ -117,13 +117,11 @@ variable "environments" {
 }
 
 locals {
-  environment_name     = lower(replace(terraform.workspace, "_", "-"))
-  environment          = contains(keys(var.environments), local.environment_name) ? var.environments[local.environment_name] : var.environments["default"]
-  dns_namespace_acc    = local.environment_name == "production" ? "" : "${local.environment.account_name}."
-  dns_namespace_env    = local.environment.account_name == "production" ? "" : "${local.environment_name}."
-  dev_wildcard         = local.environment.account_name == "production" ? "" : "*."
-  capacity_provider    = local.environment.fargate_spot ? "FARGATE_SPOT" : "FARGATE"
-  policy_region_prefix = lower(replace(data.aws_region.current.name, "-", ""))
+  environment_name  = lower(replace(terraform.workspace, "_", "-"))
+  environment       = contains(keys(var.environments), local.environment_name) ? var.environments[local.environment_name] : var.environments["default"]
+  dns_namespace_env = local.environment.account_name == "production" ? "" : "${local.environment_name}."
+  dev_wildcard      = local.environment.account_name == "production" ? "" : "*."
+  capacity_provider = local.environment.fargate_spot ? "FARGATE_SPOT" : "FARGATE"
 
   mandatory_moj_tags = {
     business-unit    = "OPG"
