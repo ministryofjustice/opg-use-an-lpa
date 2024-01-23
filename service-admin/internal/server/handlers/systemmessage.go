8475d929164dd969847129e726197ce909a5bcd4
Lowenna Baggaley <lowenna.baggaley@digital.justice.gov.uk>
2024-01-23 14:44:03 +0000

    refactor of handler with bilingual message validation check

--- service-admin/internal/server/handlers/systemmessage.go
+++ service-admin/internal/server/handlers/systemmessage.go
@@ -24,25 +24,50 @@ func NewSystemMessageServer(systemMessageService SystemMessageService, templateW
 }
 
 func (s *SystemMessageServer) SystemMessageHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
 	if r.Method == "POST" {
-
 		err := r.ParseForm()
-		if err == nil {
-			messages := map[string]string{
-				"system-message-use-en":  r.PostFormValue("use-eng"),
-				"system-message-use-cy":  r.PostFormValue("use-cy"),
-				"system-message-view-en": r.PostFormValue("view-eng"),
-				"system-message-view-cy": r.PostFormValue("view-cy"),
-			}
-
-			s.systemMessageService.PutSystemMessages(r.Context(), messages)
-		} else {
+		if err != nil {
 			log.Error().Err(err).Msg("failed to parse form input")
+
+			http.Error(w, "Error parsing form input", http.StatusBadRequest)
+			return
+		}
+
+		messages := map[string]string{
+			"system-message-use-en":  r.PostFormValue("use-eng"),
+			"system-message-use-cy":  r.PostFormValue("use-cy"),
+			"system-message-view-en": r.PostFormValue("view-eng"),
+			"system-message-view-cy": r.PostFormValue("view-cy"),
+		}
+
+		// Checks English and Welsh are present
+		if (messages["system-message-use-en"] == "" && messages["system-message-use-cy"] != "") ||
+			(messages["system-message-use-en"] != "" && messages["system-message-use-cy"] == "") ||
+			(messages["system-message-view-en"] == "" && messages["system-message-view-cy"] != "") ||
+			(messages["system-message-view-en"] != "" && messages["system-message-view-cy"] == "") {
+			http.Error(w, "Both English and Welsh versions are required for each message", http.StatusBadRequest)
+			return
+		}
+
+		err = s.systemMessageService.PutSystemMessages(ctx, messages)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to store system messages")
+			http.Error(w, "Error storing system messages", http.StatusInternalServerError)
+			return
 		}
 	}
 
-	messages, _ := s.systemMessageService.GetSystemMessages(r.Context())
-	if err := s.templateService.RenderTemplate(w, r.Context(), "systemmessage.page.gohtml", messages); err != nil {
+	messages, err := s.systemMessageService.GetSystemMessages(ctx)
+	if err != nil {
 		log.Panic().Err(err).Msg(err.Error())
+		http.Error(w, "Error retrieving system messages", http.StatusInternalServerError)
+		return
+	}
+
+	if err := s.templateService.RenderTemplate(w, ctx, "systemmessage.page.gohtml", messages); err != nil {
+		log.Panic().Err(err).Msg(err.Error())
+		http.Error(w, "error rendering template", http.StatusInternalServerError)
 	}
 }
