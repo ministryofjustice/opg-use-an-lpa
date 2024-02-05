5f982caa91084c8c40428f48607fb5bca4d46520
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-02-05 14:16:02 +0000

    Wrap random_bytes in function to handle exceptions better

---
+++ service-api/app/test/AppTest/Service/RandomByteGeneratorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Service;
+
+use App\Service\RandomByteGenerator;
+use PHPUnit\Framework\TestCase;
+
+class RandomByteGeneratorTest extends TestCase
+{
+    /** @test */
+    public function it_returns_random_bytes(): void
+    {
+        $sut = new RandomByteGenerator();
+
+        $this->assertEquals(15, strlen(($sut)(15)));
+    }
+}
