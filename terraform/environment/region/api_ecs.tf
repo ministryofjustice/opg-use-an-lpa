201ceade86725b4a6892513fdb43b7c75add8da9
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2024-02-08 14:19:01 +0000

    bumpers

--- terraform/environment/region/api_ecs.tf
+++ terraform/environment/region/api_ecs.tf
@@ -364,7 +364,7 @@ locals {
     {
       cpu       = 0,
       essential = false,
-      image     = "ntse/export-php-metrics:v0.15.0",
+      image     = "ntse/export-php-metrics:v0.16.0",
       name      = "fpm-stats-export",
       logConfiguration = {
         logDriver = "awslogs",
