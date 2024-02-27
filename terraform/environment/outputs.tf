ac81a01e3e6a4b39f71d4ab755a31ca5b78581f2
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2024-02-27 15:49:30 +0000

    UML-3312 Fixing Terraform linting issues (#2536)

--- terraform/environment/outputs.tf
+++ terraform/environment/outputs.tf
@@ -1,11 +1,29 @@
 output "admin_domain" {
-  value = "https://${local.cluster_config.admin_fqdn}"
+  value       = "https://${local.cluster_config.admin_fqdn}"
+  description = "The URL of the admin interface"
 }
 
 output "public_facing_use_domain" {
-  value = "https://${local.cluster_config.public_facing_use_fqdn}"
+  value       = "https://${local.cluster_config.public_facing_use_fqdn}"
+  description = "The URL of the public facing use interface"
 }
 
 output "public_facing_view_domain" {
-  value = "https://${local.cluster_config.public_facing_view_fqdn}"
+  value       = "https://${local.cluster_config.public_facing_view_fqdn}"
+  description = "The URL of the public facing view interface"
+}
+
+output "container_version" {
+  value       = var.container_version
+  description = "The tag of the container image that has been deployed"
+}
+
+output "admin_container_version" {
+  value       = var.admin_container_version
+  description = "The tag of the admin container image that has been deployed"
+}
+
+output "workspace_name" {
+  value       = terraform.workspace
+  description = "The name of the Terraform workspace"
 }
