5027d80a4f6a1de210bc82ce2c4b66f1ed539388
Lowenna Baggaley <lowenna.baggaley@digital.justice.gov.uk>
2024-03-14 17:06:00 +0000

    test for SystemMessage

---
+++ service-api/app/test/AppTest/Service/ViewerCodes/SystemMessageTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Service;
+
+use App\Service\SystemMessage;
+use Aws\Ssm\SsmClient;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\MockObject\IncompatibleReturnValueException;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class SystemMessageTest extends TestCase
+{
+    /**
+ * @var SsmClient&MockObject
+ */
+    private SsmClient $ssmClient;
+    private SystemMessage $systemMessage;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->ssmClient = $this->createMock(SsmClient::class);
+
+        $this->systemMessage = new SystemMessage($this->ssmClient);
+    }
+
+    /**
+     * @return void
+     * @throws ExpectationFailedException
+     * @throws IncompatibleReturnValueException
+     */
+    protected function it_returns_system_messages(): void
+    {
+        $mockResponse = [
+            'Parameters' => [
+                ['Name' => '/system-message/use/en', 'Value' => 'English usage message'],
+                ['Name' => '/system-message/use/cy', 'Value' => 'Welsh usage message'],
+            ],
+        ];
+        $this->ssmClient->method('getParameters')
+            ->willReturn($mockResponse);
+
+        $systemMessages = $this->systemMessage->getSystemMessages();
+
+        $expected = [
+            '/system-message/use/en' => 'English usage message',
+            '/system-message/use/cy' => 'Welsh usage message',
+        ];
+        $this->assertEquals($expected, $systemMessages);
+    }
+}
