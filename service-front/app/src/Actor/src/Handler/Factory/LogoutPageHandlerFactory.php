51fa818b67b874c568355c8dba684951feb4b36d
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-03-27 13:41:03 +0000

    Implement logout strategy for onelogin and local

---
+++ service-front/app/src/Actor/src/Handler/Factory/LogoutPageHandlerFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Actor\Handler\Factory;
+
+use Actor\Handler\LogoutPageHandler;
+use Common\Service\Authentication\LocalAccountLogout;
+use Common\Service\Features\FeatureEnabled;
+use Common\Service\OneLogin\OneLoginService;
+use Mezzio\Authentication\AuthenticationInterface;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+class LogoutPageHandlerFactory
+{
+    public function __construct(private FeatureEnabled $featureEnabled)
+    {
+    }
+
+    public function __invoke(ContainerInterface $container): LogoutPageHandler
+    {
+        $logoutStrategy = $container->get(
+            ($this->featureEnabled)('allow_gov_one_login')
+                ? OneLoginService::class
+                : LocalAccountLogout::class,
+        );
+
+        return new LogoutPageHandler(
+            $container->get(TemplateRendererInterface::class),
+            $container->get(UrlHelper::class),
+            $container->get(AuthenticationInterface::class),
+            $container->get(LoggerInterface::class),
+            $logoutStrategy,
+        );
+    }
+}
