40ac95715a65e2edeee50dacb464e9bb5b9da73d
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-03-27 13:38:41 +0000

    Implement json serialisation of User entity.
    Syntax tidy ups

--- service-front/app/src/Common/src/Entity/User.php
+++ service-front/app/src/Common/src/Entity/User.php
@@ -4,22 +4,25 @@ declare(strict_types=1);
 
 namespace Common\Entity;
 
-use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
 use DateTimeZone;
 use Exception;
+use JsonSerializable;
 use Mezzio\Authentication\UserInterface;
 use RuntimeException;
 
 /**
  * Implements the UserInterface interface from the Zend Expressive authentication library
  */
-class User implements UserInterface
+class User implements UserInterface, JsonSerializable
 {
     protected string $email;
 
     protected bool $needsReset;
-    protected ?DateTime $lastLogin;
+    protected ?DateTimeInterface $lastLogin;
     protected ?string $subject;
+    protected ?string $idToken;
 
     public function __construct(protected string $identity, array $roles, array $details)
     {
@@ -32,6 +35,7 @@ class User implements UserInterface
         $this->email      = $details['Email'];
         $this->needsReset = !empty($details['NeedsReset']);
         $this->subject    = $details['Subject'] ?? null;
+        $this->idToken    = $details['IdToken'] ?? null;
 
         if (!empty($details['LastLogin'])) {
             $this->setLastLogin($details['LastLogin']);
@@ -48,8 +52,6 @@ class User implements UserInterface
 
     /**
      * Get all user roles
-     *
-     * @return Iterable
      */
     public function getRoles(): iterable
     {
@@ -61,8 +63,8 @@ class User implements UserInterface
      * Get a detail $name if present, $default otherwise
      *
      * @param string $name
-     * @param null $default
-     * @return mixed|null
+     * @param null|mixed $default
+     * @return mixed
      */
     public function getDetail(string $name, $default = null): mixed
     {
@@ -77,6 +79,14 @@ class User implements UserInterface
 
     /**
      * Get all the details, if any
+     *
+     * @return array{
+     *     Email: string,
+     *     LastLogin: ?array,
+     *     NeedsReset: bool,
+     *     Subject?: string,
+     *     IdToken?: string,
+     * }
      */
     public function getDetails(): array
     {
@@ -85,9 +95,14 @@ class User implements UserInterface
             'LastLogin'  => $this->lastLogin,
             'NeedsReset' => $this->needsReset,
         ];
+
         if ($this->subject !== null) {
             $array['Subject'] = $this->subject;
         }
+        if ($this->idToken !== null) {
+            $array['IdToken'] = $this->idToken;
+        }
+
         return $array;
     }
 
@@ -102,13 +117,34 @@ class User implements UserInterface
         // if this is being called via a construction from the database it will be an ISO/ATOM
         // format string.
         if (is_string($date)) {
-            $this->lastLogin = new DateTime($date);
+            $this->lastLogin = new DateTimeImmutable($date);
         }
 
         // if this is being called via a reconstruction from the session the DateTime object
         // will have been deconstructed to a key/value array. So build a new DateTime from that.
         if (is_array($date) && array_key_exists('date', $date)) {
-            $this->lastLogin = new DateTime($date['date'], new DateTimeZone($date['timezone']));
+            $this->lastLogin = new DateTimeImmutable($date['date'], new DateTimeZone($date['timezone']));
         }
+    }
+
+    /**
+     * @return array{
+     *      Email: string,
+     *      LastLogin: ?string,
+     *      NeedsReset: bool,
+     *      Subject?: string,
+     *      IdToken?: string,
+     *  }
+     */
+    public function jsonSerialize(): array
+    {
+        $details = $this->getDetails();
+
+        // Encode the last login time as a string
+        if ($this->lastLogin !== null) {
+            $details['LastLogin'] = $this->lastLogin->format(DateTimeInterface::ATOM);
+        }
+
+        return $details;
     }
 }
