53b8b749e98c0171c2a9ba4a16630b165c8d1b5f
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-04-09 15:48:55 +0100

    Add test for factory class

---
+++ service-front/app/test/ActorTest/Handler/Factory/LogoutPageHandlerFactoryTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ActorTest\Handler\Factory;
+
+use Actor\Handler\Factory\LogoutPageHandlerFactory;
+use Common\Service\Authentication\LocalAccountLogout;
+use Common\Service\Features\FeatureEnabled;
+use Common\Service\OneLogin\OneLoginService;
+use Mezzio\Authentication\AuthenticationInterface;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+class LogoutPageHandlerFactoryTest extends TestCase
+{
+    use ProphecyTrait;
+
+    private ContainerInterface|ObjectProphecy $container;
+
+    public function setUp(): void
+    {
+        $this->container = $this->prophesize(ContainerInterface::class);
+
+        $this->container
+            ->get(TemplateRendererInterface::class)
+            ->willReturn($this->prophesize(TemplateRendererInterface::class)->reveal());
+
+        $this->container
+            ->get(UrlHelper::class)
+            ->willReturn($this->prophesize(UrlHelper::class)->reveal());
+
+        $this->container
+            ->get(AuthenticationInterface::class)
+            ->willReturn($this->prophesize(AuthenticationInterface::class)->reveal());
+
+        $this->container
+            ->get(LoggerInterface::class)
+            ->willReturn($this->prophesize(LoggerInterface::class)->reveal());
+    }
+
+    public function featureFlagStrategies(): array
+    {
+        return [
+            'one-login disabled' => [
+                false,
+                LocalAccountLogout::class,
+            ],
+            'one-login enabled' => [
+                true,
+                OneLoginService::class,
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider featureFlagStrategies
+     */
+    public function it_creates_an_appropriate_logout_page_handler(bool $allow_gov_one_login, string $strategyClass): void
+    {
+        $featureProphecy = $this->prophesize(FeatureEnabled::class);
+        $featureProphecy
+            ->__invoke('allow_gov_one_login')
+            ->willReturn($allow_gov_one_login);
+
+        $this->container
+            ->get($strategyClass)
+            ->shouldBeCalled()
+            ->willReturn($this->prophesize($strategyClass)->reveal());
+
+        $this->container
+            ->get(FeatureEnabled::class)
+            ->willReturn(
+                $featureProphecy->reveal(),
+            );
+
+        $factory = new LogoutPageHandlerFactory();
+
+        ($factory)($this->container->reveal());
+    }
+
+}
