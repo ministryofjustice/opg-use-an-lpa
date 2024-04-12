410897b860dfd7a37a96dd3d70bd7e78ad405067
Matt Machell <matt.machell@digital.justice.gov.uk>
2024-04-12 14:40:28 +0100

    Fix npm build in admin

--- service-admin/web/assets/main.js
+++ service-admin/web/assets/main.js
@@ -1,8 +1,8 @@
 import MOJFrontend from '@ministryofjustice/frontend/moj/all.js';
-import GOVUKFrontend from 'govuk-frontend/govuk/all.js';
+import {Tabs, initAll} from 'govuk-frontend/dist/govuk/all.mjs';
 
 // we aren't using the JS tabs, but they try to initialise this will stop them breaking
-GOVUKFrontend.Tabs.prototype.setup = () => { };
+Tabs.prototype.setup = () => { };
 
-GOVUKFrontend.initAll();
+initAll();
 MOJFrontend.initAll();
