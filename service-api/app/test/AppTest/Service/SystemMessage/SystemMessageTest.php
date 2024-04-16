091fb46728df55bc6b62e2189c73ffe572f011c2
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2024-04-16 13:58:25 +0100

    fix test

--- service-api/app/test/AppTest/Service/SystemMessage/SystemMessageTest.php
+++ service-api/app/test/AppTest/Service/SystemMessage/SystemMessageTest.php
@@ -45,37 +45,14 @@ class SystemMessageTest extends TestCase
             ],
         ];
 
-        // TODO the following line does not work. It needs something else mocking. we tried to do this elsewhere but ran into other problems
         // this test can be deleted once the 2cnd version using prophecy below, is working , as it will replace this
-        $this->ssmClient->method('getParameters')
+        $this->ssmClient->method('__call')
+            ->with($this->identicalTo('getParameters'))
             ->willReturn($mockResponse);
+
 
         $systemMessages = $this->systemMessage->getSystemMessages();
 
-
-        $expected = [
-            '/system-message/use/en' => 'English usage message',
-            '/system-message/use/cy' => 'Welsh usage message',
-        ];
-        $this->assertEquals($expected, $systemMessages);
-    }
-
-    public function testReturnsSystemMessages2(): void
-    {
-        // this attempts to do the above test, which doesn't currently work, but using prophecies instead
-        $mockResponse      = [
-            'Parameters' => [
-                ['Name' => '/system-message/use/en', 'Value' => 'English usage message'],
-                ['Name' => '/system-message/use/cy', 'Value' => 'Welsh usage message'],
-            ],
-        ];
-        $ssmClientProphecy = $this->prophesize($this->ssmClient::class);
-        $ssmClientProphecy->getParameters()
-            ->willReturn($mockResponse)->reveal();
-
-        $systemMessageProphecy = $this->prophesize($this->systemMessage::class);
-
-        $systemMessages = $systemMessageProphecy->getSystemMessages();
 
         $expected = [
             '/system-message/use/en' => 'English usage message',
