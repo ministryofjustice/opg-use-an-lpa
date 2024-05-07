8aed4913a6e3dc1d56080d8b82c11ff5c93ed1e1
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-05-07 13:30:17 +0100

    Add certificate for mock-onelogin service to account (#2597)

--- terraform/account/region/certificate.tf
+++ terraform/account/region/certificate.tf
@@ -185,3 +185,39 @@ resource "aws_acm_certificate" "certificate_admin" {
 
   provider = aws.region
 }
+
+
+//------------------------
+// Mock One Login Certificates
+
+resource "aws_route53_record" "certificate_validation_mock_onelogin" {
+  provider = aws.management
+  for_each = {
+    for dvo in aws_acm_certificate.certificate_mock_onelogin.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.opg_service_justice_gov_uk.zone_id
+}
+
+resource "aws_acm_certificate_validation" "certificate_validation_mock_onelogin" {
+  certificate_arn         = aws_acm_certificate.certificate_mock_onelogin.arn
+  validation_record_fqdns = [for record in aws_route53_record.certificate_validation_mock_onelogin : record.fqdn]
+
+  provider = aws.region
+}
+
+resource "aws_acm_certificate" "certificate_mock_onelogin" {
+  domain_name       = "${local.dev_wildcard}mock-onelogin.lastingpowerofattorney.opg.service.justice.gov.uk"
+  validation_method = "DNS"
+
+  provider = aws.region
+}
