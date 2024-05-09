d6cab9c7a0aa3b58f0cd0fd19d07edbd775ea991
Lbagg1 <lowenna.baggaley@digital.justice.gov.uk>
2024-05-09 10:20:43 +0100

    UML-2981 Show system messages on use side (#2577)
    * fetch system messages from api
    
    * show system message on use side
    
    * add tests for system messages
    
    * lint
    
    * fix behat login test
    
    * fix behat tests
    
    * return empty array if api call fails
    
    * fix missing `@test`
    
    * remove comment
    
    * lint

---
+++ service-front/app/test/CommonTest/Service/SystemMessage/SystemMessageServiceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommonTest\Service\SystemMessage;
+
+use Common\Exception\ApiException;
+use Common\Service\ApiClient\Client;
+use Common\Service\SystemMessage\SystemMessageService;
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+class SystemMessageServiceTest extends TestCase
+{
+    use ProphecyTrait;
+
+    /**
+     * @test
+     * @throws Exception
+     */
+    public function get_messages_calls_api(): void
+    {
+        $apiClientProphecy = $this->prophesize(Client::class);
+        $apiClientProphecy->httpGet('/v1/system-message')->shouldBeCalled()->willReturn([
+            'use/en' => 'English',
+            'use/cy' => 'Welsh',
+        ]);
+
+        $systemMessageService = new SystemMessageService($apiClientProphecy->reveal());
+
+        $messages = $systemMessageService->getMessages();
+
+        $this->assertEquals('English', $messages['use/en'] ?? null);
+        $this->assertEquals('Welsh', $messages['use/cy'] ?? null);
+    }
+
+    /**
+     * @test
+     * @throws Exception
+     */
+    public function gets_no_messages_when_api_fails(): void
+    {
+        $apiClientProphecy = $this->prophesize(Client::class);
+        $apiClientProphecy->httpGet('/v1/system-message')->shouldBeCalled()->willThrow(ApiException::class);
+
+        $systemMessageService = new SystemMessageService($apiClientProphecy->reveal());
+
+        $messages = $systemMessageService->getMessages();
+
+        $this->assertEmpty($messages);
+    }
+}
