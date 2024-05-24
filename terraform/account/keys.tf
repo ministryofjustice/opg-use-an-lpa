3c508bbe9020b238b43bb0be6abcf3f8917054c3
Lbagg1 <lowenna.baggaley@digital.justice.gov.uk>
2024-05-24 10:55:23 +0100

    UML-3392: add lpa data secrets to terraform (#2616)
    * add lpa data secrets to terraform
    
    * fix missing secret

--- terraform/account/keys.tf
+++ terraform/account/keys.tf
@@ -2,3 +2,8 @@ resource "tls_private_key" "onelogin_auth_pk" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
+
+resource "tls_private_key" "lpa_data_store_pk" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
