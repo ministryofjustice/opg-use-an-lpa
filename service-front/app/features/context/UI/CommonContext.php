43c526b8a7f006f93af40e50632891de7aa7b101
Allen Annom <allen.annom@digital.justice.gov.uk>
2024-06-17 11:03:34 +0100

    UML-3294 reverted changes of satisfying test in UAT

--- service-front/app/features/context/UI/CommonContext.php
+++ service-front/app/features/context/UI/CommonContext.php
@@ -12,7 +12,6 @@ use Common\Middleware\Session\SessionExpiryMiddleware;
 use Common\Middleware\Session\SessionExpiryMiddlewareFactory;
 use Common\Service\ApiClient\Client;
 use Common\Service\ApiClient\ClientFactory;
-use Common\Service\Features\FeatureEnabled;
 use Common\Service\Lpa\LpaService;
 use Common\Service\Session\EncryptedCookiePersistence;
 use Common\Service\Session\EncryptedCookiePersistenceFactory;
@@ -42,13 +41,7 @@ class CommonContext implements Context
      */
     public function iAccessTheServiceHomepage(): void
     {
-        if (
-            $this->base->container->get('config')['application'] === 'viewer'
-            || (
-                $this->base->container->get('config')['application'] === 'actor' &&
-                !$this->base->container->get(FeatureEnabled::class)('allow_gov_one_login')
-            )
-        ) {
+        if ($this->base->container->get('config')['application'] === 'viewer') {
             $this->apiFixtures->append(
                 ContextUtilities::newResponse(
                     StatusCodeInterface::STATUS_OK,
