94fbe1825b306e5cdd9b71301d103468c7280ab6
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-06-20 18:27:47 +0100

    Update all the pact factories

--- service-api/app/test/BehatTest/DataAccess/ApiGateway/PactActorCodesFactory.php
+++ service-api/app/test/BehatTest/DataAccess/ApiGateway/PactActorCodesFactory.php
@@ -7,25 +7,34 @@ namespace BehatTest\DataAccess\ApiGateway;
 use App\DataAccess\ApiGateway\ActorCodes;
 use App\DataAccess\ApiGateway\RequestSignerFactory;
 use App\Service\Log\RequestTracing;
-use GuzzleHttp\Client as HttpClient;
+use DI\NotFoundException;
+use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 
 class PactActorCodesFactory
 {
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
     public function __invoke(ContainerInterface $container): ActorCodes
     {
         $config = $container->get('config');
 
         if (!isset($config['codes_api']['endpoint'])) {
-            throw new \Exception('Actor codes API Gateway endpoint is not set');
+            throw new NotFoundException('Actor codes API Gateway endpoint is not set');
         }
 
-        $apiHost = parse_url($config['codes_api']['endpoint'], PHP_URL_HOST);
-
         return new ActorCodes(
-            new HttpClient(),
+            $container->get(ClientInterface::class),
+            $container->get(RequestFactoryInterface::class),
+            $container->get(StreamFactoryInterface::class),
             $container->get(RequestSignerFactory::class),
-            $apiHost,
+            parse_url($config['codes_api']['endpoint'], PHP_URL_HOST),
             $container->get(RequestTracing::TRACE_PARAMETER_NAME)
         );
     }
--- service-api/app/test/BehatTest/DataAccess/ApiGateway/PactInstructionsAndPreferencesImagesFactory.php
+++ service-api/app/test/BehatTest/DataAccess/ApiGateway/PactInstructionsAndPreferencesImagesFactory.php
@@ -6,22 +6,23 @@ namespace BehatTest\DataAccess\ApiGateway;
 
 use App\DataAccess\ApiGateway\InstructionsAndPreferencesImages;
 use App\DataAccess\ApiGateway\RequestSignerFactory;
+use App\DataAccess\Repository\InstructionsAndPreferencesImagesInterface;
 use App\Service\Log\RequestTracing;
 use DI\NotFoundException;
-use GuzzleHttp\Client as HttpClient;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 
 class PactInstructionsAndPreferencesImagesFactory
 {
     /**
-     * @param ContainerInterface $container
-     * @return InstructionsAndPreferencesImages
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    public function __invoke(ContainerInterface $container)
+    public function __invoke(ContainerInterface $container): InstructionsAndPreferencesImagesInterface
     {
         $config = $container->get('config');
 
@@ -29,12 +30,12 @@ class PactInstructionsAndPreferencesImagesFactory
             throw new NotFoundException('Instructions and preferences images API Gateway endpoint is not set');
         }
 
-        $apiHost = parse_url($config['iap_images_api']['endpoint'], PHP_URL_HOST);
-
         return new InstructionsAndPreferencesImages(
-            new HttpClient(),
+            $container->get(ClientInterface::class),
+            $container->get(RequestFactoryInterface::class),
+            $container->get(StreamFactoryInterface::class),
             $container->get(RequestSignerFactory::class),
-            $apiHost,
+            parse_url($config['iap_images_api']['endpoint'], PHP_URL_HOST),
             $container->get(RequestTracing::TRACE_PARAMETER_NAME),
         );
     }
