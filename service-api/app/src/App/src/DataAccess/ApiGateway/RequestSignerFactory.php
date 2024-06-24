55b380ea4fb108b1502773f88a6e44010409af7a
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-06-24 14:34:05 +0100

    Add code to signer factory to do JWT handling.

--- service-api/app/src/App/src/DataAccess/ApiGateway/RequestSignerFactory.php
+++ service-api/app/src/App/src/DataAccess/ApiGateway/RequestSignerFactory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\DataAccess\ApiGateway;
 
+use App\DataAccess\ApiGateway\JWSPayload\DataStoreLpas;
+use App\Service\Secrets\LpaDataStoreSecretManager;
 use Aws\Signature\SignatureV4;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
@@ -15,13 +17,23 @@ class RequestSignerFactory
     {
     }
 
-    public function __invoke(?SignatureType $signature = null): RequestSigner
+    /**
+     * Variadic function that passes any additionally provided data to the signature implementation
+     *
+     * @param SignatureType|null $signature
+     * @param mixed ...$additionalSignatureData Additional arguments to be passed to the signature
+     *                                          implementation
+     * @return RequestSigner
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __invoke(?SignatureType $signature = null, ...$additionalSignatureData): RequestSigner
     {
         $config = $this->container->get('config');
 
         $additionalHeaders = match ($signature) {
             SignatureType::ActorCodes => $this->actorCodesHeaders(),
-            SignatureType::DataStoreLpas => $this->dataStoreLpasHeaders(),
+            SignatureType::DataStoreLpas => $this->dataStoreLpasHeaders(...$additionalSignatureData),
             default => [],
         };
 
@@ -50,11 +62,16 @@ class RequestSignerFactory
      * @return array{
      *     X-Jwt-Authorization: string
      * }
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
      */
-    private function dataStoreLpasHeaders(): array
+    private function dataStoreLpasHeaders(LpaDataStoreSecretManager $secretManager, string $userIdentifier): array
     {
+        $jwtGenerator = $this->container->get('GenerateJWT');
+        $jwsPayload   = new DataStoreLpas($userIdentifier);
+
         return [
-            'X-Jwt-Authorization' => null,
+            'X-Jwt-Authorization' => ($jwtGenerator)($secretManager, $jwsPayload),
         ];
     }
 }
