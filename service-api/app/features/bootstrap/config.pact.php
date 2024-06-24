32426ddfbca484dea9cff3443027d33343a33821
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-06-24 15:15:23 +0100

    Ensure an actual client is used for PACT tests

--- service-api/app/features/bootstrap/config.pact.php
+++ service-api/app/features/bootstrap/config.pact.php
@@ -1,10 +1,10 @@
 <?php
 
 declare(strict_types=1);
-
 return [
     'dependencies' => [
         'factories' => [
+            Psr\Http\Client\ClientInterface::class => App\Service\ApiClient\ClientFactory::class,
             App\DataAccess\ApiGateway\ActorCodes::class
                 => BehatTest\DataAccess\ApiGateway\PactActorCodesFactory::class,
             App\DataAccess\ApiGateway\InstructionsAndPreferencesImages::class
