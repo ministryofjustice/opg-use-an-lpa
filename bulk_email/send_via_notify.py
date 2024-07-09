c6efcd6bf227bbfcd6a07e86c2abc4b6cdd20fc2
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2024-07-09 17:21:07 +0100

    writes succeeded and failed files

--- bulk_email/send_via_notify.py
+++ bulk_email/send_via_notify.py
@@ -10,7 +10,7 @@ if not notify_api_key:
 
 notifications_client = NotificationsAPIClient(notify_api_key)
 
-def send_msg(email_address):
+def send_msg(email_address, succeeded_file, failed_file):
     try:
         response = notifications_client.send_email_notification(
             email_address=email_address,
@@ -21,8 +21,10 @@ def send_msg(email_address):
         )
         #print(response)
         print(f"Sending to {email_address} succeeded \n")
+        succeeded_file.write(f"{email_address}\n")
     except HTTPError as e:
         print(f"Sending to {email_address} failed \n")
+        failed_file.write(f"{email_address}\n")
         print(e)
         
 def main():
@@ -35,14 +37,13 @@ def main():
     args = parser.parse_args()
     filename = args.file
 
-    with open(args.file) as f:
-        for line_terminated in f:
-            email_address = line_terminated.rstrip('\n')
-            print(email_address)
-            send_msg(email_address)
-        #with open(filename + '_succeeded', 'w') as succeededfile:
-        #    with open(filename + '_failed', 'w') as failedfile:
-    # TODO with open success file and failed file    
+    with open(filename + '_succeeded', 'w') as succeeded_file:
+        with open(filename + '_failed', 'w') as failed_file:
+            with open(args.file) as f:
+                for line_terminated in f:
+                    email_address = line_terminated.rstrip('\n')
+                    print(email_address)
+                    send_msg(email_address, succeeded_file, failed_file)
     
 if __name__ == "__main__":
     main()
