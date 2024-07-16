1de040c05f5afec8c52d33c0ffdb435480476d58
Lowenna Baggaley <lowenna.baggaley@digital.justice.gov.uk>
2024-07-16 15:32:59 +0100

    lint

--- service-front/app/src/Common/src/Middleware/Authentication/AuthenticationMiddlewareFactory.php
+++ service-front/app/src/Common/src/Middleware/Authentication/AuthenticationMiddlewareFactory.php
@@ -8,22 +8,32 @@ use Common\Middleware\Authentication\AuthenticationMiddleware;
 use Common\Middleware\Authentication\CredentialAuthenticationMiddleware;
 use Common\Middleware\Authentication\ForcedPasswordResetMiddleware;
 use Laminas\Stratigility\MiddlewarePipeInterface;
+use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class AuthenticationMiddlewareFactory
 {
-    public function  __invoke(ContainerInterface $container): AuthenticationMiddleware
+    /**
+     * @param ContainerInterface $container
+     *
+     * @return AuthenticationMiddleware
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __invoke(ContainerInterface $container): AuthenticationMiddleware
     {
-     $pipe = $container->get(MiddlewarePipeInterface::class);
-     $credentialAuthenticationMiddleware = $container->get(CredentialAuthenticationMiddleware::class);
-     $forcedPasswordResetMiddleware = $container->get(ForcedPasswordResetMiddleware::class);
+        $pipe                               = $container->get(MiddlewarePipeInterface::class);
+        $credentialAuthenticationMiddleware = $container->get(CredentialAuthenticationMiddleware::class);
+        $forcedPasswordResetMiddleware      = $container->get(ForcedPasswordResetMiddleware::class);
 
-     $feature_flags = $container->get('config')['feature_flags'];
-     $middlewares = [$credentialAuthenticationMiddleware];
+        $feature_flags = $container->get('config')['feature_flags'];
+        $middlewares = [$credentialAuthenticationMiddleware];
 
-     if (!($feature_flags['allow_gov_one_login'] ?? false)) {
-        $middlewares[] = $forcedPasswordResetMiddleware;
-    }
+        if (!($feature_flags['allow_gov_one_login'] ?? false)) {
+            $middlewares[] = $forcedPasswordResetMiddleware;
+        }
+
         return new AuthenticationMiddleware($pipe, ...$middlewares);
     }
 }
