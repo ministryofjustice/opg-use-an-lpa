8e5d1ffc9721f9c87621750d8ef5714b62956759
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-07-19 15:10:42 +0100

    fix duplicate resource

--- terraform/environment/region/cloudwatch_dashboard.tf
+++ terraform/environment/region/cloudwatch_dashboard.tf
@@ -16,7 +16,7 @@ locals {
   use_alb_arn    = trimprefix(aws_lb.use.arn, "arn:aws:elasticloadbalancing:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:loadbalancer/")
 }
 
-resource "aws_cloudwatch_dashboard" "main" {
+resource "aws_cloudwatch_dashboard" "onelogin" {
   count          = var.create_onelogin_dashboard ? 1 : 0
   dashboard_name = "${var.environment_name}-${data.aws_region.current.name}-dashboard"
   dashboard_body = templatefile("${path.module}/templates/cw_dashboard_onelogin.tftpl", {
