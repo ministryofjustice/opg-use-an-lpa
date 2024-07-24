4a36e8f9b3135cb481db6abf2d863983d34f1cc6
Lowenna Baggaley <lowenna.baggaley@digital.justice.gov.uk>
2024-07-24 16:43:34 +0100

    replaces deletion with single space string

--- service-admin/internal/server/data/system_message.go
+++ service-admin/internal/server/data/system_message.go
@@ -2,12 +2,12 @@ package data
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
 	"github.com/rs/zerolog/log"
+	"strings"
 )
 
 type SystemMessageService struct {
@@ -33,7 +33,7 @@ func (s *SystemMessageService) GetSystemMessages(ctx context.Context) (map[strin
 		}
 
 		if messageText != nil && messageText.Parameter != nil && messageText.Parameter.Value != nil {
-			messages[messageKey] = *messageText.Parameter.Value
+			messages[messageKey] = strings.TrimSpace(*messageText.Parameter.Value)
 		}
 	}
 
@@ -44,30 +44,17 @@ func (s *SystemMessageService) PutSystemMessages(ctx context.Context, messages m
 	deleted := false
 
 	for messageKey, messageValue := range messages {
-		if messageValue != "" {
-			_, err := s.ssmConn.Client.PutParameter(ctx, &ssm.PutParameterInput{
-				Name:      aws.String(s.ssmConn.prefixedParameterName(messageKey)),
-				Value:     aws.String(messageValue),
-				Type:      types.ParameterTypeString,
-				Overwrite: aws.Bool(true),
-			})
-			if err != nil {
-				return false, fmt.Errorf("error writing parameter: %w", err)
-			}
-		} else {
-			_, err := s.ssmConn.Client.DeleteParameter(ctx, &ssm.DeleteParameterInput{
-				Name: aws.String(s.ssmConn.prefixedParameterName(messageKey)),
-			})
-			if err != nil {
-				var pnf *types.ParameterNotFound
-				if errors.As(err, &pnf) {
-					log.Debug().Msg(fmt.Sprintf("not deleting parameter '%s' as it does not exist", messageKey))
-				} else {
-					return false, fmt.Errorf("error deleting parameter: %w", err)
-				}
-			} else {
-				deleted = true
-			}
+		if messageValue == "" {
+			deleted = true
+			messageValue = " "
+		}
+		_, err := s.ssmConn.Client.PutParameter(ctx, &ssm.PutParameterInput{
+			Name:      aws.String(s.ssmConn.prefixedParameterName(messageKey)),
+			Value:     aws.String(messageValue),
+			Type:      types.ParameterTypeString,
+			Overwrite: aws.Bool(true)})
+		if err != nil {
+			return false, fmt.Errorf("error writing parameter: %w", err)
 		}
 	}
 
