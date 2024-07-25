96f341228969abb7507d3447324dd49a5ed2b45a
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-07-25 14:10:41 +0100

    conditionally create output

--- terraform/environment/region/modules/dns/outputs.tf
+++ terraform/environment/region/modules/dns/outputs.tf
@@ -3,6 +3,6 @@ output "fqdn" {
 }
 
 output "health_check_id" {
-  value       = aws_route53_health_check.this[0].id
+  value       = var.create_health_check ? aws_route53_health_check.this[0].id : null
   description = "The ID of the health check"
 }
