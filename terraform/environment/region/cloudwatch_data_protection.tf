750016511999d3e7977488410698c3e83cb76f19
gillettmoj <jason.gillett@digital.justice.gov.uk>
2024-07-31 15:47:25 +0100

    Running terraform fmt on cloud_watch_dataprotection.tf

--- terraform/environment/region/cloudwatch_data_protection.tf
+++ terraform/environment/region/cloudwatch_data_protection.tf
@@ -1,31 +1,31 @@
 resource "aws_cloudwatch_log_data_protection_policy" "production_application_logs" {
-  
+
   log_group_name = "${var.environment_name}_application_logs"
 
   policy_document = jsonencode({
     Name    = "data_protection_${var.environment_name}_application_logs"
     Version = "2021-06-01"
 
-    "Statement": [
+    "Statement" : [
       {
-        "Sid": "audit-policy",
-        "DataIdentifier": [
+        "Sid" : "audit-policy",
+        "DataIdentifier" : [
           "arn:aws:dataprotection::aws:data-identifier/EmailAddress"
         ],
-        "Operation": {
-          "Audit": {
-            "FindingsDestination": {}
+        "Operation" : {
+          "Audit" : {
+            "FindingsDestination" : {}
           }
         }
       },
       {
-        "Sid": "redact-policy",
-        "DataIdentifier": [
+        "Sid" : "redact-policy",
+        "DataIdentifier" : [
           "arn:aws:dataprotection::aws:data-identifier/EmailAddress"
         ],
-        "Operation": {
-          "Deidentify": {
-            "MaskConfig": {}
+        "Operation" : {
+          "Deidentify" : {
+            "MaskConfig" : {}
           }
         }
       }
