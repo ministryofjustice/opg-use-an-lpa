432da0f316dd2adcbb51a45304036fa76141745b
Lowenna <lowenna.baggaley@digital.justice.gov.uk>
2024-08-08 09:39:20 +0100

    unit test for ModerniseLpa

---
+++ service-api/app/test/AppTest/DataAccess/ApiGateway/ModerniseLpasTest.php
@@ -0,0 +1,155 @@
+<?php
+
+use GuzzleHttp\Client as GuzzleClient;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Client;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
+use App\DataAccess\ApiGateway\RequestSignerFactory;
+use App\DataAccess\ApiGateway\SignatureType;
+use App\DataAccess\Repository\ModerniseLpas;
+use App\DataAccess\Repository\Response\Lpa;
+use App\DataAccess\Repository\Response\LpaInterface;
+use App\DataAccess\ApiGateway\SiriusLpas;
+use App\DataAccess\ApiGateway\RequestSigner;
+use App\DataAccess\Repository\DataSanitiserStrategy;
+use App\Exception\ApiException;
+use Fig\Http\Message\StatusCodeInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use Prophecy\Argument;
+use Psr\Log\LoggerInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use \AppTest\DataAccess\ApiGateway\PSR17PropheciesTrait;
+
+class ModerniseLpasTest extends TestCase
+{
+    use ProphecyTrait;
+    use PSR17PropheciesTrait;
+
+    private GuzzleClient|ObjectProphecy $guzzleClientProphecy;
+    private ObjectProphecy $requestSignerProphecy;
+    private ObjectProphecy $requestSignerFactoryProphecy;
+    private ObjectProphecy $sanitiserProphecy;
+
+    protected function setUp(): void
+    {
+        $this->requestSignerFactoryProphecy = $this->prophesize(RequestSignerFactory::class);
+        $this->requestSignerProphecy = $this->prophesize(RequestSigner::class);
+        $this->sanitiserProphecy = $this->prophesize(DataSanitiserStrategy::class);
+        $this->guzzleClientProphecy = $this->prophesize(GuzzleClient::class);
+    }
+
+    public function testCanGetAnLpa(): void
+    {
+        $uid = '700000055554';
+        $apiBaseUri = 'http://localhost';
+        $traceId = 'test-trace-id';
+
+        $responseBodyProphecy = $this->prophesize(StreamInterface::class);
+        $responseBodyProphecy->getContents()->willReturn(json_encode(['uId' => $uid]));
+
+        $responseProphecy = $this->prophesize(ResponseInterface::class);
+        $responseProphecy->getStatusCode()->willReturn(200);
+        $responseProphecy->getBody()->willReturn($responseBodyProphecy->reveal());
+        $responseProphecy->getHeaderLine('Date')->willReturn('Wed, 16 Feb 2022 16:45:46 GMT');
+
+        $this->generatePSR17Prophecies($responseProphecy->reveal(), $traceId, []);
+
+        $this->requestFactoryProphecy->createRequest('GET', $apiBaseUri . "/lpa/$uid")
+            ->willReturn($this->requestProphecy->reveal());
+
+        $this->requestSignerFactoryProphecy->__invoke(SignatureType::DataStoreLpas)
+            ->willReturn($this->requestSignerProphecy->reveal());
+
+        $this->requestSignerProphecy->sign(Argument::type(RequestInterface::class))
+            ->willReturn($this->requestProphecy->reveal());
+
+        $this->sanitiserProphecy->sanitise(Argument::any())->willReturnArgument(0);
+
+        $this->guzzleClientProphecy
+            ->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($responseProphecy->reveal());
+
+        $moderniseLpas = new ModerniseLpas(
+            $this->guzzleClientProphecy->reveal(),
+            $this->requestFactoryProphecy->reveal(),
+            $this->streamFactoryProphecy->reveal(),
+            $this->requestSignerFactoryProphecy->reveal(),
+            $apiBaseUri,
+            $traceId,
+            $this->sanitiserProphecy->reveal()
+        );
+
+        $shouldBeAnLPA = $moderniseLpas->get($uid);
+
+        $this->assertInstanceOf(LpaInterface::class, $shouldBeAnLPA);
+        $this->assertEquals($uid, $shouldBeAnLPA->getData()['uId']);
+    }
+
+    public function testCanLookupLpas(): void
+    {
+        $uids = ['700000055554', '700000055555'];
+        $apiBaseUri = 'http://localhost';
+        $traceId = 'test-trace-id';
+
+        $responseBodyProphecy = $this->prophesize(StreamInterface::class);
+        $responseBodyProphecy->getContents()->willReturn(json_encode(['lpas' => [
+            ['uId' => '700000055554', 'date' => '2022-02-16'],
+            ['uId' => '700000055555', 'date' => '2022-02-16']
+        ]]));
+
+        $responseProphecy = $this->prophesize(ResponseInterface::class);
+        $responseProphecy->getStatusCode()->willReturn(200);
+        $responseProphecy->getBody()->willReturn($responseBodyProphecy->reveal());
+        $responseProphecy->getHeaderLine('Date')->willReturn('Wed, 16 Feb 2022 16:45:46 GMT');
+
+        $this->generatePSR17Prophecies($responseProphecy->reveal(), $traceId, []);
+
+        $this->httpClientProphecy->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($responseProphecy->reveal());
+
+        $this->requestFactoryProphecy->createRequest('POST', $apiBaseUri . "/lpas")
+            ->willReturn($this->requestProphecy->reveal());
+
+        $this->streamFactoryProphecy->createStream(json_encode(['uids' => $uids]))
+            ->willReturn($this->prophesize(StreamInterface::class)->reveal());
+
+        $this->requestSignerFactoryProphecy->__invoke(SignatureType::DataStoreLpas)
+            ->willReturn($this->requestSignerProphecy->reveal());
+
+        $this->requestSignerProphecy->sign(Argument::type(RequestInterface::class))
+            ->willReturn($this->requestProphecy->reveal());
+
+        $this->sanitiserProphecy->sanitise(Argument::any())->willReturnArgument(0);
+
+        $this->guzzleClientProphecy
+            ->sendRequest(Argument::type(RequestInterface::class))
+            ->willReturn($responseProphecy->reveal());
+
+        $moderniseLpas = new ModerniseLpas(
+            $this->guzzleClientProphecy->reveal(),
+            $this->requestFactoryProphecy->reveal(),
+            $this->streamFactoryProphecy->reveal(),
+            $this->requestSignerFactoryProphecy->reveal(),
+            $apiBaseUri,
+            $traceId,
+            $this->sanitiserProphecy->reveal()
+        );
+
+        $lpas = $moderniseLpas->lookup($uids);
+
+        $this->assertCount(2, $lpas);
+        foreach ($lpas as $lpa) {
+            $this->assertInstanceOf(LpaInterface::class, $lpa);
+            $this->assertContains($lpa->getData()['uId'], $uids);
+        }
+    }
+}
