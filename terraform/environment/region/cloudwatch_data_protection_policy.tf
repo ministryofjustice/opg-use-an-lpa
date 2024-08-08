410a6649844a8f1af9e82a82206f588d55238417
gillettmoj <jason.gillett@digital.justice.gov.uk>
2024-08-08 15:49:28 +0100

    Uml 3251 revert dp policy (#2742)
    * Update regex query in data protection policy
    
    * Adding '.' to regex query
    
    * Changing data protection policy back to default with email address identifier

--- terraform/environment/region/cloudwatch_data_protection_policy.tf
+++ terraform/environment/region/cloudwatch_data_protection_policy.tf
@@ -10,8 +10,7 @@ resource "aws_cloudwatch_log_data_protection_policy" "application_logs" {
       {
         "Sid" : "audit-policy",
         "DataIdentifier" : [
-          "arn:aws:dataprotection::aws:data-identifier/EmailAddress",
-          "HTTPGetEmailAddress"
+          "arn:aws:dataprotection::aws:data-identifier/EmailAddress"
         ],
         "Operation" : {
           "Audit" : {
@@ -22,8 +21,7 @@ resource "aws_cloudwatch_log_data_protection_policy" "application_logs" {
       {
         "Sid" : "redact-policy",
         "DataIdentifier" : [
-          "arn:aws:dataprotection::aws:data-identifier/EmailAddress",
-          "HTTPGetEmailAddress"
+          "arn:aws:dataprotection::aws:data-identifier/EmailAddress"
         ],
         "Operation" : {
           "Deidentify" : {
@@ -31,14 +29,6 @@ resource "aws_cloudwatch_log_data_protection_policy" "application_logs" {
           }
         }
       }
-    ],
-    "Configuration" : {
-      "CustomDataIdentifier" : [
-        {
-          "Name" : "HTTPGetEmailAddress",
-          "Regex" : "/create-account-success?email=[a-zA-z0-9._+-]+%40[a-zA-z0-9]+.[a-zA-Z]{2,4}&resend=true"
-        }
-      ]
-    }
+    ]
   })
 }
