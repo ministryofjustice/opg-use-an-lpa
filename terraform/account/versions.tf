49d2e64de2d1f62eba393fe63a255c34d10fbe4e
gillettmoj <jason.gillett@digital.justice.gov.uk>
2024-09-04 14:15:08 +0100

    Update method to get tf ver (#2763)
    * Update method to get tf ver
    
    * Revert changes to get terraform version
    
    * Adding versions file
    
    * Adding tf ver switcher

---
+++ terraform/account/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.64.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5.0"
+    }
+    pagerduty = {
+      source  = "PagerDuty/pagerduty"
+      version = "~> 3.12.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.0"
+    }
+  }
+}
