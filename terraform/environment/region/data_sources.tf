a261614f1ec872ada6b7c30d4290406fd5f0224d
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-09-16 16:11:16 +0100

    Update ECR repo name

--- terraform/environment/region/data_sources.tf
+++ terraform/environment/region/data_sources.tf
@@ -95,11 +95,11 @@ data "aws_ecr_repository" "use_an_lpa_api_web" {
 
 data "aws_ecr_repository" "use_an_lpa_pdf" {
   provider = aws.management
-  name     = "pdf_service"
+  name     = "pdf-service"
 }
 
 data "aws_ecr_image" "pdf_service" {
-  repository_name = "pdf_service"
+  repository_name = "pdf-service"
   image_tag       = var.pdf_container_version
   provider        = aws.management
 }
