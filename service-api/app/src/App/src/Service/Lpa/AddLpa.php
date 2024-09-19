9fddf7a2cb445609b03eb7f9e4d5d6cc8308db00
Seema Menon <seema.menon@digital.justice.gov.uk>
2024-09-19 11:13:55 +0100

    test

--- service-api/app/src/App/src/Service/Lpa/AddLpa.php
+++ service-api/app/src/App/src/Service/Lpa/AddLpa.php
@@ -22,6 +22,7 @@ class AddLpa
      * @param array  $data
      * @param string $userId
      * @return array
+     *
      */
     public function validateAddLpaData(array $data, string $userId): array
     {
