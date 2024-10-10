1770d1138539ef427040f60c3645e0003dc98adb
Seema Menon <seema.menon@digital.justice.gov.uk>
2024-10-10 12:56:27 +0100

    Further corrections and test addition

---
+++ service-front/app/test/CommonTest/Entity/Casters/CanExtractPostcodeFromSiriusLpaTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommonTest\Entity\Casters;
+
+use Common\Entity\Casters\ExtractPostcodeFromSiriusLpa;
+use EventSauce\ObjectHydrator\ObjectMapper;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class CanExtractPostcodeFromSiriusLpaTest extends TestCase
+{
+    private ObjectMapper $mockHydrator;
+    private ExtractPostcodeFromSiriusLpa $extractPostcodeFromSiriusLpa;
+
+    public function setUp(): void
+    {
+        $this->mockHydrator                 = $this->createMock(ObjectMapper::class);
+        $this->extractPostcodeFromSiriusLpa = new ExtractPostcodeFromSiriusLpa();
+    }
+
+    #[Test]
+    public function can_extract_postcode_from_sirius(): void
+    {
+        $addresses = [
+            [
+                'addressLine1' => '81 Front Street',
+                'addressLine2' => 'LACEBY',
+                'addressLine3' => 'Street 3',
+                'country'      => 'GB',
+                'county'       => 'London',
+                'postcode'     => 'DN37 5SH',
+                'town'         => 'Town',
+                'type'         => 'Primary',
+            ],
+        ];
+
+        $expectedPostcode = 'DN37 5SH';
+
+        $result = $this->extractPostcodeFromSiriusLpa->cast($addresses, $this->mockHydrator);
+
+        $this->assertEquals($expectedPostcode, $result);
+    }
+
+    #[Test]
+    public function cannot_extract_postcode_from_sirius(): void
+    {
+        $addresses = [
+            [
+                'addressLine1' => '81 Front Street',
+                'addressLine2' => 'LACEBY',
+                'addressLine3' => 'Street 3',
+                'country'      => 'GB',
+                'county'       => 'London',
+                'town'         => 'Town',
+                'type'         => 'Primary',
+            ],
+        ];
+
+        $result = $this->extractPostcodeFromSiriusLpa->cast($addresses, $this->mockHydrator);
+
+        $this->assertEquals(null, $result);
+    }
+}
