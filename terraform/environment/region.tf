41cbe2df72ea7acdad961fea0c52a31d497d10cf
gillettmoj <jason.gillett@digital.justice.gov.uk>
2024-10-28 13:41:55 +0000

    Update cloudwatch var

--- terraform/environment/region.tf
+++ terraform/environment/region.tf
@@ -9,7 +9,7 @@ module "eu_west_1" {
   autoscaling                                      = local.environment.autoscaling
   associate_alb_with_waf_web_acl_enabled           = local.environment.associate_alb_with_waf_web_acl_enabled
   capacity_provider                                = local.capacity_provider
-  cloudwatch_application_insights_enabled          = local.environment.cloudwatch_application_insights_enabled
+  cloudwatch_application_insights_enabled          = var.environments.cloudwatch_application_insights_enabled
   container_version                                = var.container_version
   cookie_expires_use                               = local.environment.cookie_expires_use
   cookie_expires_view                              = local.environment.cookie_expires_view
