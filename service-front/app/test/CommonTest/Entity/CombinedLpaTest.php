6ab02eef0c0afe88949fa4809d5db9e67415cba6
Allen Annom <allen.annom@digital.justice.gov.uk>
2024-11-04 17:04:14 +0000

    added tests for getters in the CombinedLpaTest.php

---
+++ service-front/app/test/CommonTest/Entity/CombinedLpaTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommonTest\Entity;
+
+use Common\Service\Lpa\Factory\LpaDataFormatter;
+use DateTimeImmutable;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+class CombinedLpaTest extends TestCase
+{
+    use ProphecyTrait;
+
+    private LpaDataFormatter $lpaDataFormatter;
+
+    public function setUp(): void
+    {
+        $this->lpaDataFormatter = new LpaDataFormatter();
+    }
+
+    #[Test]
+    public function can_test_getters()
+    {
+        $lpa         = json_decode(file_get_contents(__DIR__ . '../../../../test/fixtures/test_lpa.json'), true);
+        $combinedLpa = ($this->lpaDataFormatter)($lpa);
+
+        $expectedUid                     = '700000000047';
+        $expectedApplicationHasGuidance  = false;
+        $expectedHasRestrictions         = false;
+
+        $this->assertEquals($expectedUid, $combinedLpa->getUId());
+        $this->assertEquals($expectedApplicationHasGuidance, $combinedLpa->getApplicationHasGuidance());
+        $this->assertEquals($expectedHasRestrictions, $combinedLpa->getApplicationHasRestrictions());
+    }
+}
