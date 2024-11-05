f53c9339a7d547bb0498d99ab9a5ac51297fe0d8
Alex Saunders <acsauk@gmail.com>
2024-11-05 15:40:33 +0000

    fix brackets

--- service-front/app/src/Common/src/Enum/HowAttorneysMakeDecisions.php
+++ service-front/app/src/Common/src/Enum/HowAttorneysMakeDecisions.php
@@ -11,19 +11,23 @@ enum HowAttorneysMakeDecisions: string
     case JOINTLY_AND_SEVERALLY                 = 'jointly-and-severally';
     case JOINTLY_FOR_SOME_SEVERALLY_FOR_OTHERS = 'jointly-for-some-severally-for-others';
 
-    public function isSingular(): bool {
+    public function isSingular(): bool
+    {
         return $this === HowAttorneysMakeDecisions::SINGULAR;
     }
 
-    public function isJointly(): bool {
+    public function isJointly(): bool
+    {
         return $this === HowAttorneysMakeDecisions::JOINTLY;
     }
 
-    public function isJointlyAndSeverally(): bool {
+    public function isJointlyAndSeverally(): bool
+    {
         return $this === HowAttorneysMakeDecisions::JOINTLY_AND_SEVERALLY;
     }
 
-    public function isJointlyForSomeSeverallyForOthers(): bool {
+    public function isJointlyForSomeSeverallyForOthers(): bool
+    {
         return $this === HowAttorneysMakeDecisions::JOINTLY_FOR_SOME_SEVERALLY_FOR_OTHERS;
     }
 }
