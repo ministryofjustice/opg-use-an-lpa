c39f26f5392864d4fea021d1b57c5db57d8dfa79
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2024-11-26 09:27:17 +0000

    enhance test, see if coverage recognises this

--- service-admin/internal/server/templates_test.go
+++ service-admin/internal/server/templates_test.go
@@ -25,7 +25,7 @@ func testFS() (fs.FS, error) {
 		return nil, err
 	}
 
-	err = afero.WriteFile(fs, "test.partial.gohtml", []byte("{{ define \"partial\" }}World{{ add 2 3}}{{ end }}"), 0644)
+	err = afero.WriteFile(fs, "test.partial.gohtml", []byte("{{ define \"partial\" }}World{{ $test_var := add 2 3}}{{ printf \"%.0f\" $test_var }}{{ end }}"), 0644)
 	if err != nil {
 		return nil, err
 	}
