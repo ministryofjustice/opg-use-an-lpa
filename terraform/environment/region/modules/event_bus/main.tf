148eb6da2efb05cc47fce6e605a82105dda0e2c7
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-11-28 11:48:08 +0000

    add missing resource instance key

--- terraform/environment/region/modules/event_bus/main.tf
+++ terraform/environment/region/modules/event_bus/main.tf
@@ -7,6 +7,6 @@ resource "aws_cloudwatch_event_bus" "main" {
 resource "aws_cloudwatch_event_archive" "main" {
   count            = var.event_bus_enabled ? 1 : 0
   name             = var.environment_name
-  event_source_arn = aws_cloudwatch_event_bus.main.arn
+  event_source_arn = aws_cloudwatch_event_bus.main[0].arn
   provider         = aws.region
 }
