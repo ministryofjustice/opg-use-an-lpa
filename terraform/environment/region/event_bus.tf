2bfa7bd95131711cdf46e55070cd5d9e62e6dcb7
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-11-29 15:18:02 +0000

    add missing region

--- terraform/environment/region/event_bus.tf
+++ terraform/environment/region/event_bus.tf
@@ -2,6 +2,7 @@ module "event_bus" {
   source            = "./modules/event_bus"
   environment_name  = var.environment_name
   event_bus_enabled = var.event_bus_enabled
+  current_region    = data.aws_region.current.name
   providers = {
     aws.region = aws.region
   }
