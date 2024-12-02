251a9ddc5a4c0a1bd3a20cb3c2adf86a4757c991
gillettmoj <jason.gillett@digital.justice.gov.uk>
2024-12-02 15:40:39 +0000

    Update data call

--- terraform/account/region/lambda_functions.tf
+++ terraform/account/region/lambda_functions.tf
@@ -36,7 +36,7 @@ data "aws_iam_policy_document" "ingestion_lambda_function_policy" {
   statement {
     sid       = "AllowSQSAccess"
     effect    = "Allow"
-    resources = [aws_sqs_queue.env.arn]
+    resources = [data.aws_sqs_queue.env.arn]
     actions = [
       "sqs:SendMessage",
       "sqs:ReceiveMessage",
