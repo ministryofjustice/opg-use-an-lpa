914b1f2727a1497425b2e3dfbb7bb6caee440663
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-12-11 16:25:37 +0000

    Move enum value deciphering to the casters rather than being in the enums themselves.

--- service-front/app/src/Common/src/Entity/Casters/CastToCaseSubtype.php
+++ service-front/app/src/Common/src/Entity/Casters/CastToCaseSubtype.php
@@ -8,6 +8,7 @@ use Common\Enum\LpaType;
 use Attribute;
 use EventSauce\ObjectHydrator\ObjectMapper;
 use EventSauce\ObjectHydrator\PropertyCaster;
+use InvalidArgumentException;
 
 #[Attribute(Attribute::TARGET_PARAMETER)]
 class CastToCaseSubtype implements PropertyCaster
@@ -15,7 +16,11 @@ class CastToCaseSubtype implements PropertyCaster
     public function cast(mixed $value, ObjectMapper $hydrator): ?string
     {
         if (is_null(LpaType::tryFrom($value))) {
-            return LpaType::fromShortName($value)->value;
+            $value = match ($value) {
+                'personal-welfare' => LpaType::PERSONAL_WELFARE,
+                'property-and-affairs' => LpaType::PROPERTY_AND_AFFAIRS,
+                default => throw new InvalidArgumentException('Invalid shorthand name: ' . $value),
+            };
         }
 
         return LpaType::from($value)->value;
