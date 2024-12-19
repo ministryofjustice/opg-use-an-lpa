3b61d35f2b280523c8aec4e27a7bfc3649355efe
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2024-12-19 11:06:56 +0000

    Start of work on CombinedLpaManagerTest

--- service-api/app/test/AppTest/Service/Lpa/CombinedLpaManagerTest.php
+++ service-api/app/test/AppTest/Service/Lpa/CombinedLpaManagerTest.php
@@ -1,77 +1,196 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AppTest\Service\Lpa;
 
 use App\DataAccess\ApiGateway\DataStoreLpas;
 use App\DataAccess\ApiGateway\SiriusLpas;
+use App\DataAccess\Repository\Response\Lpa;
 use App\DataAccess\Repository\UserLpaActorMapInterface;
+use App\Entity\LpaStore\LpaStore;
+use App\Entity\Sirius\SiriusLpa;
 use App\Service\Lpa\Combined\FilterActiveActors;
 use App\Service\Lpa\Combined\ResolveLpaTypes;
 use App\Service\Lpa\CombinedLpaManager;
 use App\Service\Lpa\IsValidLpa;
+use App\Service\Lpa\LpaDataFormatter;
 use App\Service\Lpa\ResolveActor;
+use DateTimeImmutable;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Prophecy\ObjectProphecy;
+use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class CombinedLpaManagerTest extends TestCase
 {
     use ProphecyTrait;
 
     private CombinedLpaManager $combinedLpaManager;
-    private UserLpaActorMapInterface|ObjectProphecy $userLpaActorMapRepository;
-    private ResolveLpaTypes|ObjectProphecy $resolveLpaTypes;
-    private SiriusLpas|ObjectProphecy $siriusLpas;
-    private DataStoreLpas|ObjectProphecy $dataStoreLpas;
-    private ResolveActor|ObjectProphecy $resolveActor;
-    private IsValidLpa|ObjectProphecy $isValidLpa;
-    private FilterActiveActors|ObjectProphecy $filterActiveActors;
+    private DataStoreLpas|ObjectProphecy $dataStoreLpasProphecy;
+    private FilterActiveActors|ObjectProphecy $filterActiveActorsProphecy;
+    private IsValidLpa|ObjectProphecy $isValidLpaProphecy;
+    private ResolveActor|ObjectProphecy $resolveActorProphecy;
+    private ResolveLpaTypes|ObjectProphecy $resolveLpaTypesProphecy;
+    private SiriusLpas|ObjectProphecy $siriusLpasProphecy;
+    private UserLpaActorMapInterface|ObjectProphecy $userLpaActorMapInterfaceProphecy;
+
     public function setUp(): void
     {
-        $this->userLpaActorMapInterfaceProphecy    = $this->prophesize(UserLpaActorMapInterface::class);
-        $this->resolveLpaTypesProphecy    = $this->prophesize(ResolveLpaTypes::class);
-        $this->siriusLpasProphecy    = $this->prophesize(SiriusLpas::class);
-        $this->dataStoreLpasProphecy    = $this->prophesize(DataStoreLpas::class);
-        $this->resolveActorProphecy    = $this->prophesize(ResolveActor::class);
-        $this->isValidLpaProphecy    = $this->prophesize(IsValidLpa::class);
-        $this->filterActiveActorsProphecy    = $this->prophesize(FilterActiveActors::class);
+        $this->userLpaActorMapInterfaceProphecy = $this->prophesize(UserLpaActorMapInterface::class);
+        $this->resolveLpaTypesProphecy          = $this->prophesize(ResolveLpaTypes::class);
+        $this->siriusLpasProphecy               = $this->prophesize(SiriusLpas::class);
+        $this->dataStoreLpasProphecy            = $this->prophesize(DataStoreLpas::class);
+        $this->resolveActorProphecy             = $this->prophesize(ResolveActor::class);
+        $this->isValidLpaProphecy               = $this->prophesize(IsValidLpa::class);
+        $this->filterActiveActorsProphecy       = $this->prophesize(FilterActiveActors::class);
     }
 
-      private function getLpaService(): CombinedLpaManager
-      {
-          return new CombinedLpaManager(
-              $this->userLpaActorMapInterfaceProphecy->reveal(),
-              $this->resolveLpaTypesProphecy->reveal(),
-              $this->siriusLpasProphecy->reveal(),
-              $this->dataStoreLpasProphecy->reveal(),
-              $this->resolveActorProphecy->reveal(),
-              $this->isValidLpaProphecy->reveal(),
-              $this->filterActiveActorsProphecy->reveal(),
-          );
-      }
+    #[Test]
+    public function can_get_all_active_for_user()
+    {
+    }
 
     #[Test]
-    public function can_get_by_uid_test() {
-        // TODO lpamanager needs to have a lpa
-        // check when we get it by uid that we get that lpa with that uid
+    public function can_get_all_for_user()
+    {
+    }
+
+    #[Test]
+    public function can_get_by_sirius_uid()
+    {
+        $testUid = '700012349874';
+
+        $lpaResponse = new Lpa(
+            $this->loadTestSiriusLpaFixture(
+                overwrite: [
+                    'attorneys'         => [
+                        ['id' => 1, 'firstname' => 'A', 'surname' => 'B', 'systemStatus' => true],
+                        ['id' => 2, 'firstname' => 'A', 'surname' => 'B', 'systemStatus' => false], // not active
+                        ['id' => 3, 'firstname' => 'A', 'systemStatus' => true],
+                        ['id' => 4, 'surname' => 'B', 'systemStatus' => true],
+                        ['id' => 5, 'systemStatus' => true], // ghost
+                    ],
+                    'trustCorporations' => [
+                        [
+                            'id'           => 6,
+                            'companyName'  => 'XYZ Ltd',
+                            'systemStatus' => true,
+                        ],
+                    ],
+                ],
+            ),
+            new DateTimeImmutable('now'),
+        );
+
+        $filteredLpa = $this->loadTestSiriusLpaFixture(
+            overwrite: [
+                'attorneys'         => [
+                    ['id' => 1, 'firstname' => 'A', 'surname' => 'B', 'systemStatus' => true],
+                    ['id' => 3, 'firstname' => 'A', 'systemStatus' => true],
+                    ['id' => 4, 'surname' => 'B', 'systemStatus' => true],
+                ],
+                'trustCorporations' => [
+                    [
+                        'id'           => 6,
+                        'companyName'  => 'XYZ Ltd',
+                        'systemStatus' => true,
+                    ],
+                ],
+            ],
+        );
+
+        $this->siriusLpasProphecy->get($testUid)->willReturn($lpaResponse);
+        $this->dataStoreLpasProphecy->get(Argument::any())->shouldNotBeCalled();
+        $this->filterActiveActorsProphecy->__invoke($lpaResponse->getData())->willReturn($filteredLpa);
+
         $service = $this->getLpaService();
-        $this->assertInstanceOf(CombinedLpaManager::class, $service);
+
+        $result = $service->getByUid($testUid);
+
+        $this->assertEquals($filteredLpa, $result->getData());
     }
 
     #[Test]
-    public function can_get_by_user_lpa_actor_token_test() {
+    public function can_get_by_lpastore_uid()
+    {
+        $testUid = 'M-789Q-P4DF-4UX3';
+
+        $lpaResponse = new Lpa(
+            $this->loadTestLpaStoreLpaFixture(
+                overwrite: [
+                    'attorneys' => [
+                        [
+                            'uid'        => '9ac5cb7c-fc75-40c7-8e53-059f36dbbe3d',
+                            'firstNames' => 'Herman',
+                            'lastName'   => 'Seakrest',
+                            'status'     => 'active',
+                        ],
+                        [   // replacement
+                            'uid'        => '6bbb8221-eded-4835-a1ba-dacdf5ac139c',
+                            'firstNames' => 'Test',
+                            'lastName'   => 'Testerson',
+                            'status'     => 'replacement',
+                        ],
+                    ],
+                ],
+            ),
+            new DateTimeImmutable('now'),
+        );
+
+        // stripping the replacement should match the default then we need to swap out the attorneys
+        // as there will *not* be a replacement in there. This is done because there is no way to update
+        // the replacement attorneys in the object manually.
+        $filteredLpa = $this->loadTestLpaStoreLpaFixture();
+        $filteredLpa = $lpaResponse->getData()->withAttorneys($filteredLpa->attorneys);
+
+        $this->dataStoreLpasProphecy->get($testUid)->willReturn($lpaResponse);
+        $this->siriusLpasProphecy->get(Argument::any())->shouldNotBeCalled();
+        $this->filterActiveActorsProphecy->__invoke($lpaResponse->getData())->willReturn($filteredLpa);
+
+        $service = $this->getLpaService();
+        $result  = $service->getByUid($testUid);
+
+        $this->assertEquals($filteredLpa, $result->getData());
     }
 
     #[Test]
-    public function can_get_all_active_for_user_test() {
+    public function can_get_by_user_lpa_actor_token()
+    {
     }
 
     #[Test]
-    public function can_get_all_for_user_test() {
+    public function can_get_by_viewer_code()
+    {
     }
 
-    #[Test]
-    public function can_get_by_viewer_code_test() {
+    private function getLpaService(): CombinedLpaManager
+    {
+        return new CombinedLpaManager(
+            $this->userLpaActorMapInterfaceProphecy->reveal(),
+            $this->resolveLpaTypesProphecy->reveal(),
+            $this->siriusLpasProphecy->reveal(),
+            $this->dataStoreLpasProphecy->reveal(),
+            $this->resolveActorProphecy->reveal(),
+            $this->isValidLpaProphecy->reveal(),
+            $this->filterActiveActorsProphecy->reveal(),
+        );
+    }
+
+    private function loadTestSiriusLpaFixture(array $overwrite = []): SiriusLpa
+    {
+        $lpaData = json_decode(file_get_contents('../../../fixtures/test_lpa.json'), true);
+        $lpaData = array_merge($lpaData, $overwrite);
+
+        return (new LpaDataFormatter())->hydrateObject($lpaData);
+    }
+
+    private function loadTestLpaStoreLpaFixture(array $overwrite = []): LpaStore
+    {
+        $lpaData = json_decode(file_get_contents('../../../fixtures/4UX3.json'), true);
+        $lpaData = array_merge($lpaData, $overwrite);
+
+        return (new LpaDataFormatter())->hydrateObject($lpaData);
     }
 }
