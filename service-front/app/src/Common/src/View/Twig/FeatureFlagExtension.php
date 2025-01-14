9bcb8ba3aa8429bed34c6c99a6ba6ddde815ed32
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2025-01-14 15:03:27 +0000

    Use a Twig runtime loader to bring in the FeatureFlagExtension.
    This stops it from being loaded at app initialisation and therefore locking in configuration values that might later change.

--- service-front/app/src/Common/src/View/Twig/FeatureFlagExtension.php
+++ service-front/app/src/Common/src/View/Twig/FeatureFlagExtension.php
@@ -4,38 +4,18 @@ declare(strict_types=1);
 
 namespace Common\View\Twig;
 
-use Common\Service\Features\FeatureEnabled;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class FeatureFlagExtension extends AbstractExtension
 {
     /**
-     * @param FeatureEnabled $featureEnabled
-     * @codeCoverageIgnore
-     */
-    public function __construct(private FeatureEnabled $featureEnabled)
-    {
-    }
-
-    /**
      * @return array<TwigFunction>
      */
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('feature_enabled', [$this, 'featureEnabled']),
+            new TwigFunction('feature_enabled', FeatureFlagRuntime::class . '::featureEnabled'),
         ];
-    }
-
-    /**
-     * Returns the enabled state of a feature
-     *
-     * @param string $featureName The name of a feature configured in features.php
-     * @return bool
-     */
-    public function featureEnabled(string $featureName): bool
-    {
-        return ($this->featureEnabled)($featureName);
     }
 }
