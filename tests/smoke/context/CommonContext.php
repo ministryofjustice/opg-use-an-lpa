8642bf8c033ad179fc67265d75c69f58bc788a82
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2025-01-23 10:26:42 +0000

    Add smoke test suite whilst I'm at it.
    Rector run and phpcs pass.

--- tests/smoke/context/CommonContext.php
+++ tests/smoke/context/CommonContext.php
@@ -6,6 +6,8 @@ namespace Test\Context;
 
 use Behat\Behat\Context\Context;
 use Behat\Mink\Exception\ExpectationException;
+use Behat\Step\Given;
+use Behat\Step\Then;
 use DMore\ChromeDriver\ChromeDriver;
 use Fig\Http\Message\StatusCodeInterface;
 use PHPUnit\Framework\Assert;
@@ -14,90 +16,77 @@ class CommonContext implements Context
 {
     use BaseContextTrait;
 
+    /** @var mixed[] $responseJson */
     public array $responseJson;
 
-    /**
-     * @Given I access the service homepage
-     * @Given I access the viewer service
-     * @Given I access the actor service
-     */
+    #[Given('I access the service homepage')]
+    #[Given('I access the viewer service')]
+    #[Given('I access the actor service')]
     public function iAccessTheServiceHomepage(): void
     {
         $this->ui->visit('/home');
     }
 
-    /**
-     * @Given I access the service homepage insecurely
-     * @Given I access the viewer service insecurely
-     * @Given I access the actor service insecurely
-     */
+    #[Given('I access the service homepage insecurely')]
+    #[Given('I access the viewer service insecurely')]
+    #[Given('I access the actor service insecurely')]
     public function iAccessTheViewerServiceInsecurely(): void
     {
-        $urlParts = parse_url($this->ui->getMinkParameter('base_url'));
+        $urlParts = parse_url((string) $this->ui->getMinkParameter('base_url'));
 
         $insecureUrl = sprintf(
             'http://%s/home',
-            $urlParts['host'] . (! empty($urlParts['port']) ? ':' . $urlParts['port'] : '')
+            $urlParts['host'] . (empty($urlParts['port']) ? '' : ':' . $urlParts['port'])
         );
 
         $this->ui->visit($insecureUrl);
     }
 
-    /**
-     * @Given I access the service root path
-     * @Given I access the viewer root path
-     * @Given I access the actor root path
-     */
+    #[Given('I access the service root path')]
+    #[Given('I access the viewer root path')]
+    #[Given('I access the actor root path')]
     public function iAccessTheServiceRoot(): void
     {
-        $urlParts = parse_url($this->ui->getMinkParameter('base_url'));
+        $urlParts = parse_url((string) $this->ui->getMinkParameter('base_url'));
 
         $url = sprintf(
             '%s://%s/',
             $urlParts['scheme'],
-            $urlParts['host'] . (! empty($urlParts['port']) ? ':' . $urlParts['port'] : '')
+            $urlParts['host'] . (empty($urlParts['port']) ? '' : ':' . $urlParts['port'])
         );
 
         $this->ui->visit($url);
     }
 
-    /**
-     * @Given I access the service with the old web address
-     */
+    #[Given('I access the service with the old web address')]
     public function iAccessTheOldServiceUrl(): void
     {
-        $urlParts = parse_url($this->ui->getMinkParameter('old_base_url'));
+        $urlParts = parse_url((string) $this->ui->getMinkParameter('old_base_url'));
 
         $url = sprintf(
             '%s://%s/home',
             $urlParts['scheme'],
-            $urlParts['host'] . (! empty($urlParts['port']) ? ':' . $urlParts['port'] : '')
+            $urlParts['host'] . (empty($urlParts['port']) ? '' : ':' . $urlParts['port'])
         );
 
         $this->ui->visit($url);
     }
 
-    /**
-     * @Given I access the Welsh service homepage
-     * @Given I access the Welsh viewer service
-     * @Given I access the Welsh actor service
-     */
+    #[Given('I access the Welsh service homepage')]
+    #[Given('I access the Welsh viewer service')]
+    #[Given('I access the Welsh actor service')]
     public function iAccessTheWelshServiceHomepage(): void
     {
         $this->ui->visit('/cy/home');
     }
 
-    /**
-     * @Then I can see English text
-     */
+    #[Then('I can see English text')]
     public function iCanSeeEnglishText(): void
     {
         $this->ui->assertPageContainsText('a lasting power of attorney');
     }
 
-    /**
-     * @Then /^I can see that the lpa has instructions and preferences images in summary$/
-     */
+    #[Then('/^I can see that the lpa has instructions and preferences images in summary$/')]
     public function iCanSeeThatTheLpaHasInstructionsAndPreferencesImagesInSummary(): void
     {
         $this->ui->assertResponseStatus(StatusCodeInterface::STATUS_OK);
@@ -107,25 +96,19 @@ class CommonContext implements Context
         Assert::assertNotNull($preferencesElement);
     }
 
-    /**
-     * @Then /^I can see Welsh text$/
-     */
+    #[Then('/^I can see Welsh text$/')]
     public function iCanSeeWelshText(): void
     {
         $this->ui->assertPageContainsText('atwrneiaeth arhosol');
     }
 
-    /**
-     * @Given I fetch the healthcheck endpoint
-     */
+    #[Given('I fetch the healthcheck endpoint')]
     public function iFetchTheHealthcheckEndpoint(): void
     {
         $this->ui->visit('/healthcheck');
     }
 
-    /**
-     * @Then /^I receive headers that describe a content security policy$/
-     */
+    #[Then('/^I receive headers that describe a content security policy$/')]
     public function iReceiveHeadersThatDescribeAContentSecurityPolicy(): void
     {
         $session         = $this->ui->getSession();
@@ -155,12 +138,10 @@ class CommonContext implements Context
             );
         }
 
-        Assert::assertMatchesRegularExpression('/script-src[^;]*\'nonce-' . $nonce . '\'[^;]*;/', $header);
+        Assert::assertMatchesRegularExpression("/script-src[^;]*'nonce-" . $nonce . "'[^;]*;/", $header);
     }
 
-    /**
-     * @Given /^the documents language is set to English$/
-     */
+    #[Given('/^the documents language is set to English$/')]
     public function theDocumentsLanguageIsSetToEnglish(): void
     {
         $htmlElement = $this->ui->getMink()->getSession()->getPage()->find('css', 'html');
@@ -179,9 +160,7 @@ class CommonContext implements Context
         }
     }
 
-    /**
-     * @Given the documents language is set to Welsh
-     */
+    #[Given('the documents language is set to Welsh')]
     public function theDocumentsLanguageIsSetToWelsh(): void
     {
         $htmlElement = $this->ui->getMink()->getSession()->getPage()->find('css', 'html');
@@ -200,11 +179,9 @@ class CommonContext implements Context
         }
     }
 
-    /**
-     * @Then the service homepage should be shown securely
-     * @Then the viewer service homepage should be shown securely
-     * @Then the actor service homepage should be shown securely
-     */
+    #[Then('the service homepage should be shown securely')]
+    #[Then('the viewer service homepage should be shown securely')]
+    #[Then('the actor service homepage should be shown securely')]
     public function theViewerServiceHomepageShouldBeShownSecurely(): void
     {
         $this->ui->assertResponseStatus(StatusCodeInterface::STATUS_OK);
@@ -212,9 +189,7 @@ class CommonContext implements Context
         $this->assertHttps();
     }
 
-    /**
-     * @Then the service should redirect me to :startpage
-     */
+    #[Then('the service should redirect me to :startpage')]
     public function theServiceShouldRedirectToGovUk(string $startpage): void
     {
         $this->ui->assertResponseStatus(StatusCodeInterface::STATUS_OK);
@@ -222,17 +197,13 @@ class CommonContext implements Context
         $this->assertExactUrl($startpage);
     }
 
-    /**
-     * @Then I see JSON output
-     */
+    #[Then('I see JSON output')]
     public function iSeeJsonOutput(): void
     {
         $this->responseJson = $this->assertJsonResponse();
     }
 
-    /**
-     * @Then it contains a :key key\/value pair
-     */
+    #[Then('it contains a :key key\/value pair')]
     public function itContainsAKeyValuePair(string $key): void
     {
         if (!array_key_exists($key, $this->responseJson)) {
@@ -243,9 +214,7 @@ class CommonContext implements Context
         }
     }
 
-    /**
-     * @Then the session cookie is marked secure and httponly
-     */
+    #[Then('the session cookie is marked secure and httponly')]
     public function theSessionCookiesIsMarkedSecureAndHttponly(): void
     {
         $this->ui->assertSession()->cookieExists('__Host-session');
@@ -255,7 +224,7 @@ class CommonContext implements Context
         $driver  = $this->ui->getSession()->getDriver();
         $cookies = $driver->getCookies();
 
-        array_walk($cookies, function (array $cookie) {
+        array_walk($cookies, function (array $cookie): void {
             if ($cookie['name'] === '__Host-session' && !$cookie['httpOnly']) {
                 throw new ExpectationException(
                     'Unable to verify that the session cookie is "httpOnly"',
@@ -272,9 +241,7 @@ class CommonContext implements Context
         });
     }
 
-    /**
-     * @Then /^I receive headers that block external indexing$/
-     */
+    #[Then('/^I receive headers that block external indexing$/')]
     public function iReceiveHeadersThatBlockExternalIndexing(): void
     {
         $session    = $this->ui->getSession();
@@ -285,9 +252,7 @@ class CommonContext implements Context
         Assert::assertStringContainsString('noindex', $xrobotstag);
     }
 
-    /**
-     * @Then /^I receive headers that cause the browser to not inform the destination site any URL information$/
-     */
+    #[Then('/^I receive headers that cause the browser to not inform the destination site any URL information$/')]
     public function iReceiveHeadersThatBlockURLInformation(): void
     {
         $session           = $this->ui->getSession();
@@ -297,17 +262,13 @@ class CommonContext implements Context
         Assert::assertStringContainsString('same-origin', $referrerPolicyTag);
     }
 
-    /**
-     * @Then I am given a PDF file of the summary
-     */
+    #[Then('I am given a PDF file of the summary')]
     public function IAmGivenAPDFFileOfTheSummary(): void
     {
         $this->ui->assertResponseStatus(StatusCodeInterface::STATUS_OK);
     }
 
-    /**
-     * @Then I receive headers that block external iframe embedding
-     */
+    #[Then('I receive headers that block external iframe embedding')]
     public function IAmNotAllowedToViewThisPageInAnIframe(): void
     {
         $session       = $this->ui->getSession();
