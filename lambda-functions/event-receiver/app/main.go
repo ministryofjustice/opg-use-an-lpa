7600213925441a0c37f77ed8da097fb545640f2f
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2025-02-04 16:39:43 +0000

    bits in docker compose needed to run lambda in localstack

--- lambda-functions/event-receiver/app/main.go
+++ lambda-functions/event-receiver/app/main.go
@@ -1,21 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(event events.SQSEvent) error {
+type Response struct {
+  StatusCode  int       `json:"statusCode"`
+  Headers     map[string]string  `json:"headers"`
+  Body        string    `json:"body"`
+}
+
+func handler(event events.SQSEvent) (interface{}, error) {
+	res := &Response{
+      StatusCode: 1,
+      Headers: map[string]string{"Content-Type": "application/json"},
+      Body: "Hello World",
+    }
 	for _, record := range event.Records {
 		err := processMessage(record)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 	fmt.Println("done")
-	return nil
+    content, _ := json.Marshal(res)
+    return string(content), nil
 }
 
 func processMessage(record events.SQSMessage) error {
