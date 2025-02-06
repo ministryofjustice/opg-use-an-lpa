e5209d9d94cb4f18eac332f432ae3a904ec1d410
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2025-02-06 10:52:01 +0000

    undo json response from lambda as no longer needed as we can see print out in cloudwatch results

--- lambda-functions/event-receiver/app/main.go
+++ lambda-functions/event-receiver/app/main.go
@@ -1,34 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Response struct {
-  StatusCode  int       `json:"statusCode"`
-  Headers     map[string]string  `json:"headers"`
-  Body        string    `json:"body"`
-}
-
-func handler(event events.SQSEvent) (interface{}, error) {
-	res := &Response{
-      StatusCode: 1,
-      Headers: map[string]string{"Content-Type": "application/json"},
-      Body: "Hello World",
-    }
+func handler(event events.SQSEvent) error {
 	for _, record := range event.Records {
 		err := processMessage(record)
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
 	fmt.Println("done")
-    content, _ := json.Marshal(res)
-    return string(content), nil
+	return nil
 }
 
 func processMessage(record events.SQSMessage) error {
