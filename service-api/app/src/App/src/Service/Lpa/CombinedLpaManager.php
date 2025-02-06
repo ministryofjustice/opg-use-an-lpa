01af6c11b3e9b14a2a8209aae8ee3150382bc346
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2025-02-06 17:36:22 +0000

    Refactor the exception throwing and handling around viewer code access.

--- service-api/app/src/App/src/Service/Lpa/CombinedLpaManager.php
+++ service-api/app/src/App/src/Service/Lpa/CombinedLpaManager.php
@@ -4,18 +4,19 @@ declare(strict_types=1);
 
 namespace App\Service\Lpa;
 
-use App\DataAccess\ApiGateway\DataStoreLpas;
-use App\DataAccess\ApiGateway\SiriusLpas;
-use App\DataAccess\Repository\Response\Lpa;
-use App\DataAccess\Repository\Response\LpaInterface;
-use App\DataAccess\Repository\UserLpaActorMapInterface;
-use App\Exception\ApiException;
-use App\Service\Lpa\Combined\FilterActiveActors;
-use App\Service\Lpa\Combined\ResolveLpaTypes;
+use App\DataAccess\ApiGateway\{DataStoreLpas, SiriusLpas};
+use App\DataAccess\Repository\{InstructionsAndPreferencesImagesInterface,
+    Response\Lpa,
+    Response\LpaInterface,
+    UserLpaActorMapInterface,
+    ViewerCodeActivityInterface,
+    ViewerCodesInterface};
+use App\Exception\{ApiException, MissingCodeExpiryException, NotFoundException};
+use App\Service\Lpa\Combined\{FilterActiveActors, RejectInvalidLpa, ResolveLpaTypes};
+use App\Service\Lpa\IsValid\IsValidInterface;
+use App\Service\Lpa\ResolveActor\HasActorInterface;
 use DateTimeInterface;
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
-use RuntimeException;
+use Psr\Log\LoggerInterface;
 
 /**
  * @psalm-import-type UserLpaActorMap from UserLpaActorMapInterface
@@ -23,16 +24,24 @@ use RuntimeException;
 class CombinedLpaManager implements LpaManagerInterface
 {
     public function __construct(
-        private readonly UserLpaActorMapInterface $userLpaActorMapRepository,
-        private readonly ResolveLpaTypes $resolveLpaTypes,
+        private readonly UserLpaActorMapInterface $userLpaActorMap,
         private readonly SiriusLpas $siriusLpas,
         private readonly DataStoreLpas $dataStoreLpas,
+        private readonly ViewerCodesInterface $viewerCodes,
+        private readonly ViewerCodeActivityInterface $viewerCodeActivity,
+        private readonly InstructionsAndPreferencesImagesInterface $instructionsAndPreferencesImages,
+        private readonly ResolveLpaTypes $resolveLpaTypes,
         private readonly ResolveActor $resolveActor,
         private readonly IsValidLpa $isValidLpa,
         private readonly FilterActiveActors $filterActiveActors,
+        private readonly RejectInvalidLpa $rejectInvalidLpa,
+        private readonly LoggerInterface $logger,
     ) {
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getByUid(string $uid): ?LpaInterface
     {
         $lpa = str_starts_with($uid, '7')
@@ -51,12 +60,15 @@ class CombinedLpaManager implements LpaManagerInterface
         );
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getByUserLpaActorToken(string $token, string $userId): ?array
     {
-        $lpaActorMap = $this->userLpaActorMapRepository->get($token);
+        $lpaActorMap = $this->userLpaActorMap->get($token);
 
         // Ensure the passed userId matches the passed token
-        if ($userId !== $lpaActorMap['UserId']) {
+        if ($lpaActorMap === null || $userId !== $lpaActorMap['UserId']) {
             return null;
         }
 
@@ -81,7 +93,7 @@ class CombinedLpaManager implements LpaManagerInterface
             $result['actor'] = ($this->resolveActor)($lpaData, (string) $lpaActorMap['ActorId']);
         }
 
-        // Extract and return only LPA's where status is Registered or Cancelled
+        // Extract and return only LPAs where status is Registered or Cancelled
         if (($this->isValidLpa)($lpaData)) {
             return $result;
         }
@@ -91,22 +103,28 @@ class CombinedLpaManager implements LpaManagerInterface
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getAllActiveForUser(string $userId): array
     {
         // Returns an array of all the LPAs Ids (plus other metadata) in the user's account.
-        $lpaActorMaps = $this->userLpaActorMapRepository->getByUserId($userId);
+        $lpaActorMaps = $this->userLpaActorMap->getByUserId($userId);
 
-        $lpaActorMaps = array_filter($lpaActorMaps, function ($item) {
+        $lpaActorMaps = array_filter($lpaActorMaps, function (array $item) {
             return !array_key_exists('ActivateBy', $item);
         });
 
         return $this->lookupAndFormatLpas($lpaActorMaps);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getAllForUser(string $userId): array
     {
         // Returns an array of all the LPAs Ids (plus other metadata) in the user's account.
-        $lpaActorMaps = $this->userLpaActorMapRepository->getByUserId($userId);
+        $lpaActorMaps = $this->userLpaActorMap->getByUserId($userId);
 
         return $this->lookupAndFormatLpas($lpaActorMaps);
     }
@@ -117,13 +135,10 @@ class CombinedLpaManager implements LpaManagerInterface
     }
 
     /**
-     * @param $lpaActorMaps array Map of LPAs from Dynamo
+     * @param       $lpaActorMaps array Map of LPAs from Dynamo
      * @psalm-param $lpaActorMaps UserLpaActorMap[]
      * @return array an array with formatted LPA results
      * @throws ApiException
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     * @throws RuntimeException
      */
     private function lookupAndFormatLpas(array $lpaActorMaps): array
     {
@@ -143,6 +158,7 @@ class CombinedLpaManager implements LpaManagerInterface
                 continue;
             }
 
+            /** @var HasActorInterface&IsValidInterface $lpaData */
             $lpaData = $lpa->getData();
 
             $actor = ($this->resolveActor)($lpaData, (string) $item['ActorId']);
@@ -163,13 +179,10 @@ class CombinedLpaManager implements LpaManagerInterface
     }
 
     /**
-     * @param array $lpaActorMap
+     * @param array                 $lpaActorMap
      * @psalm-param UserLpaActorMap $lpaActorMap
      * @return LpaInterface|null
      * @throws ApiException
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     * @throws RuntimeException
      */
     private function lookupLpa(array $lpaActorMap): ?LpaInterface
     {
@@ -182,12 +195,8 @@ class CombinedLpaManager implements LpaManagerInterface
 
     /**
      * @param array $lpaActorMaps
-     * @psalm-param UserLpaActorMap[] $lpaActorMaps
      * @return LpaInterface[]
      * @throws ApiException
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     * @throws RuntimeException
      */
     private function lookupLpas(array $lpaActorMaps): array
     {
