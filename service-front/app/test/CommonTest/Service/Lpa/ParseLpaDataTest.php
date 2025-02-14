6a51edab0fedd90270dcdb3022b634f20c718aaa
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2025-02-14 16:10:55 +0000

    Refactor of ParseLpaData test file to better use the entity helpers

--- service-front/app/test/CommonTest/Service/Lpa/ParseLpaDataTest.php
+++ service-front/app/test/CommonTest/Service/Lpa/ParseLpaDataTest.php
@@ -4,50 +4,33 @@ declare(strict_types=1);
 
 namespace CommonTest\Service\Lpa;
 
-use Common\Entity\CombinedLpa;
-use Common\Entity\LpaStore\LpaStore;
-use Common\Entity\LpaStore\LpaStoreAttorney;
-use Common\Entity\LpaStore\LpaStoreDonor;
-use Common\Entity\LpaStore\LpaStoreTrustCorporations;
-use Common\Entity\Person;
-use Common\Entity\Sirius\SiriusLpa;
-use Common\Entity\Sirius\SiriusLpaAttorney;
-use Common\Entity\Sirius\SiriusLpaDonor;
-use Common\Entity\Sirius\SiriusLpaTrustCorporations;
+use Common\Entity\{CaseActor,
+    CombinedLpa,
+    InstructionsAndPreferences\Images,
+    InstructionsAndPreferences\ImagesStatus,
+    Lpa as SiriusLpa,
+    Person};
 use Common\Service\Features\FeatureEnabled;
-use Common\Service\Lpa\Factory\PersonDataFormatter;
+use Common\Service\Lpa\{Factory\LpaDataFormatter,
+    Factory\PersonDataFormatter,
+    InstAndPrefImagesFactory,
+    LpaFactory,
+    ParseLpaData};
 use CommonTest\Helper\EntityTestHelper;
-use DateTimeImmutable;
-use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\Attributes\Test;
-use Common\Entity\CaseActor;
-use Common\Entity\InstructionsAndPreferences\Images;
-use Common\Entity\InstructionsAndPreferences\ImagesStatus;
-use Common\Entity\Lpa;
-use Common\Service\Lpa\InstAndPrefImagesFactory;
-use Common\Service\Lpa\LpaFactory;
-use Common\Service\Lpa\ParseLpaData;
-use Exception;
+use PHPUnit\Framework\Attributes\{CoversClass, Test};
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Prophecy\Prophecy\ObjectProphecy;
-use Common\Service\Lpa\Factory\LpaDataFormatter;
 
-/**
- * @property array     lpaData
- * @property string    actorToken
- * @property string    lpaId
- * @property string    actorId
- * @property Lpa       lpa
- * @property CaseActor actor
- * @property Images    iapImages
- */
 #[CoversClass(ParseLpaData::class)]
 class ParseLpaDataTest extends TestCase
 {
     use ProphecyTrait;
 
+    private string $actorToken;
+    private array $apiResponse;
+    private string $lpaId;
     private ObjectProphecy|LpaFactory $lpaFactory;
     private ObjectProphecy|InstAndPrefImagesFactory $instAndPrefImagesFactory;
 
@@ -58,38 +41,16 @@ class ParseLpaDataTest extends TestCase
     public function setUp(): void
     {
         $this->actorToken = '34-3-3-3-3456';
-        $this->actorId    = '56-5-5-5-5678';
         $this->lpaId      = '78-7-7-7-7891';
 
-        $this->lpaData = [
-            'user-lpa-actor-token' => $this->actorToken,
+        $this->apiResponse = [
+            'user-actor-lpa-token' => $this->actorToken,
+            'lpa'                  => ['uId' => $this->lpaId],
             'actor'                => [
-                'type'    => 'attorney',
-                'details' => [
-                    'uId' => $this->actorId,
-                ],
-            ],
-            'lpa'                  => [
-                'uId' => $this->lpaId,
-            ],
-            'iap'                  => [
-                'uId'        => $this->lpaId,
-                'status'     => 'COLLECTION_COMPLETE',
-                'signedUrls' => [],
+                'type'    => 'primary-attorney',
+                'details' => ['uId' => '56-5-5-5-5678'],
             ],
         ];
-
-        $this->lpa = new Lpa();
-        $this->lpa->setUId($this->lpaId);
-
-        $this->actor = new CaseActor();
-        $this->actor->setUId($this->actorId);
-
-        $this->iapImages = new Images(
-            (int) $this->lpaData['iap']['uId'],
-            ImagesStatus::from($this->lpaData['iap']['status']),
-            $this->lpaData['iap']['signedUrls'],
-        );
 
         $this->lpaFactory               = $this->prophesize(LpaFactory::class);
         $this->instAndPrefImagesFactory = $this->prophesize(InstAndPrefImagesFactory::class);
@@ -98,16 +59,28 @@ class ParseLpaDataTest extends TestCase
         $this->featureEnabled           = $this->prophesize(FeatureEnabled::class);
     }
 
-    /**
-     * @throws Exception
-     */
     #[Test]
     public function it_correctly_parses_an_lpa_api_response(): void
     {
-        $this->lpaFactory->createLpaFromData($this->lpaData['lpa'])->willReturn($this->lpa);
-        $this->lpaFactory->createCaseActorFromData($this->lpaData['actor']['details'])->willReturn($this->actor);
+        $this->lpaFactory
+            ->createLpaFromData(Argument::type('array'))
+            ->willReturn(EntityTestHelper::makeSiriusLpa());
+        $this->lpaFactory
+            ->createCaseActorFromData(Argument::type('array'))
+            ->willReturn(EntityTestHelper::makeCaseActor());
 
-        $this->instAndPrefImagesFactory->createFromData($this->lpaData['iap'])->willReturn($this->iapImages);
+        $this->instAndPrefImagesFactory
+            ->createFromData(Argument::type('array'))
+            ->willReturn(
+                new Images(
+                    700000000047,
+                    ImagesStatus::COLLECTION_COMPLETE,
+                    [],
+                ),
+            );
+
+        $apiResponse        = $this->apiResponse;
+        $apiResponse['iap'] = ['uId' => $this->lpaId];
 
         $sut = new ParseLpaData(
             $this->lpaFactory->reveal(),
@@ -123,31 +96,28 @@ class ParseLpaDataTest extends TestCase
 
         $result = $sut(
             [
-                $this->lpaId => $this->lpaData,
+                $this->actorToken => $apiResponse,
             ]
         );
 
-        $this->assertObjectHasProperty($this->lpaId, $result);
-        $this->assertEquals($this->lpa, $result->{$this->lpaId}->lpa);
-        $this->assertEquals($this->actor, $result->{$this->lpaId}->actor['details']);
-        $this->assertEquals($this->iapImages, $result->{$this->lpaId}->iap);
+        $this->assertObjectHasProperty($this->actorToken, $result);
+        $this->assertInstanceOf(SiriusLpa::class, $result->{$this->actorToken}->lpa);
+        $this->assertInstanceOf(CaseActor::class, $result->{$this->actorToken}->actor['details']);
+        $this->assertInstanceOf(Images::class, $result->{$this->actorToken}->iap);
     }
 
     #[Test]
     public function it_correctly_parses_an_combined_lpa_api_response(): void
     {
-        $combinedFormat = json_decode(file_get_contents(__DIR__ . '../../../../fixtures/combined_lpa.json'), true);
-
-        $this->lpaData['actor']['details'] = $combinedFormat['donor'];
-
-        $this->lpaFactory->createLpaFromData($this->lpaData['lpa'])->willReturn($combinedFormat);
-        $this->lpaFactory->createCaseActorFromData($this->lpaData['actor']['details'])->willReturn($this->actor);
-        $this->instAndPrefImagesFactory->createFromData($this->lpaData['iap'])->willReturn($this->iapImages);
-        $this->lpaDataFormatter->__invoke($combinedFormat)->willReturn($this->expectedLpa());
+        $this->featureEnabled
+            ->__invoke('support_datastore_lpas')
+            ->willReturn(true);
+        $this->lpaDataFormatter
+            ->__invoke(Argument::type('array'))
+            ->willReturn(EntityTestHelper::makeCombinedLpa());
         $this->personDataFormatter
-            ->__invoke($combinedFormat['donor'])
-            ->shouldBeCalled()
-            ->willReturn($this->getExpectedDonor());
+            ->__invoke(Argument::type('array'))
+            ->willReturn(EntityTestHelper::makePerson());
 
         $sut = new ParseLpaData(
             $this->lpaFactory->reveal(),
@@ -157,118 +127,14 @@ class ParseLpaDataTest extends TestCase
             $this->featureEnabled->reveal()
         );
 
-        $this->featureEnabled
-            ->__invoke('support_datastore_lpas')
-            ->willReturn(true);
-
-        $this->lpaData['lpa'] = $combinedFormat;
-        $result               = $sut($this->lpaData);
-
-        $this->assertEquals($this->expectedLpa(), $result->lpa);
-    }
-
-    public function expectedLpa(): CombinedLpa
-    {
-        $attorneys = [
-            new Person(
-                addressLine1 : '9 high street',
-                addressLine2 : '',
-                addressLine3 : '',
-                country      : '',
-                county       : '',
-                dob          : new DateTimeImmutable('1990-05-04'),
-                email        : '',
-                firstnames   : null,
-                name         : null,
-                otherNames   : null,
-                postcode     : 'DN37 5SH',
-                surname      : 'sanderson',
-                systemStatus : '1',
-                town         : '',
-                uId          : '700000000815'
-            ),
-            new Person(
-                addressLine1       : '',
-                addressLine2       : '',
-                addressLine3       : '',
-                country            : '',
-                county             : '',
-                dob                : new DateTimeImmutable('1975-10-05'),
-                email              : 'XXXXX',
-                firstnames         : null,
-                name               : null,
-                otherNames         : null,
-                postcode           : '',
-                surname            : 'Summers',
-                systemStatus       : '1',
-                town               : '',
-                uId                : '7000-0000-0849'
-            ),
-        ];
-
-        $donor = new Person(
-            addressLine1 : '81 Front Street',
-            addressLine2 : 'LACEBY',
-            addressLine3 : '',
-            country      : '',
-            county       : '',
-            dob          : new DateTimeImmutable('1948-11-01'),
-            email        : 'RachelSanderson@opgtest.com',
-            firstnames   : null,
-            name         : null,
-            otherNames   : null,
-            postcode     : 'DN37 5SH',
-            surname      : 'Sanderson',
-            systemStatus : null,
-            town         : '',
-            uId          : '700000000799'
+        $result = $sut(
+            [
+                $this->actorToken => $this->apiResponse,
+            ]
         );
 
-        $trustCorporations = [
-            new Person(
-                addressLine1 : 'Street 1',
-                addressLine2 : 'Street 2',
-                addressLine3 : 'Street 3',
-                country      : 'GB',
-                county       : 'County',
-                dob          : null,
-                email        : null,
-                firstnames   : null,
-                name         : null,
-                otherNames   : null,
-                postcode     : 'ABC 123',
-                surname      : 'test',
-                systemStatus : '1',
-                town         : 'Town',
-                uId          : '7000-0015-1998',
-            ),
-        ];
-
-        return EntityTestHelper::makeSiriusLpa(
-            attorneys:         $attorneys,
-            donor:             $donor,
-            trustCorporations: $trustCorporations,
-        );
-    }
-
-    public function getExpectedDonor(): Person
-    {
-        return new Person(
-            addressLine1 : '81 Front Street',
-            addressLine2 : 'LACEBY',
-            addressLine3 : '',
-            country      : '',
-            county       : '',
-            dob          : new DateTimeImmutable('1948-11-01'),
-            email        : 'RachelSanderson@opgtest.com',
-            firstnames   : null,
-            name         : null,
-            otherNames   : null,
-            postcode     : 'DN37 5SH',
-            surname      : 'Sanderson',
-            systemStatus : null,
-            town         : '',
-            uId          : '700000000799'
-        );
+        $this->assertObjectHasProperty($this->actorToken, $result);
+        $this->assertInstanceOf(CombinedLpa::class, $result->{$this->actorToken}->lpa);
+        $this->assertInstanceOf(Person::class, $result->{$this->actorToken}->actor['details']);
     }
 }
