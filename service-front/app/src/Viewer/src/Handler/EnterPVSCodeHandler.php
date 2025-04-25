293be3cb01b905faf368e2d0b9a51f8621fbfa2f
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2025-04-25 17:23:26 +0100

    Rework current start page to be a part of the workflow and add new template

---
+++ service-front/app/src/Viewer/src/Handler/EnterPVSCodeHandler.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Viewer\Handler;
+
+use Common\Service\Features\FeatureEnabled;
+use Common\Service\SystemMessage\SystemMessageService;
+use Common\Workflow\WorkflowState;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Viewer\Form\ShareCode;
+
+/**
+ * @codeCoverageIgnore
+ */
+class EnterPVSCodeHandler extends AbstractPVSCodeHandler
+{
+    private ShareCode $form;
+
+    /**
+     * @var array{
+     *   "view/en"?: string,
+     *   "view/cy"?: string
+     * }
+     */
+    private array $systemMessages;
+
+    public function __construct(
+        TemplateRendererInterface $renderer,
+        UrlHelper $urlHelper,
+        private FeatureEnabled $featureEnabled,
+        private SystemMessageService $systemMessageService,
+    ) {
+        parent::__construct($renderer, $urlHelper);
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->form           = new ShareCode($this->getCsrfGuard($request));
+        $this->systemMessages = $this->systemMessageService->getMessages();
+
+        return parent::handle($request);
+    }
+
+    public function handleGet(ServerRequestInterface $request): ResponseInterface
+    {
+        // reset the state on a new visit.
+        $this->state($request)->reset();
+
+        $template = ($this->featureEnabled)('paper_verification')
+            ? 'viewer::enter-code-pv'
+            : 'viewer::enter-code';
+
+        return new HtmlResponse($this->renderer->render($template, [
+            'form'       => $this->form->prepare(),
+            'en_message' => $this->systemMessages['view/en'] ?? null,
+            'cy_message' => $this->systemMessages['view/cy'] ?? null,
+        ]));
+    }
+
+    public function handlePost(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->form->setData($request->getParsedBody());
+
+        if ($this->form->isValid()) {
+            // TODO for now set values in session AND state. Once state is implemented on the check
+            //      answers page for share codes then we can remove.
+            $this->session->set('code', $this->form->getData()['lpa_code']);
+            $this->session->set('surname', $this->form->getData()['donor_surname']);
+
+            $this->state($request)->code     = $this->form->getData()['lpa_code'];
+            $this->state($request)->lastName = $this->form->getData()['donor_surname'];
+
+            return $this->redirectToRoute($this->nextPage($this->state($request)));
+        }
+
+        $template = ($this->featureEnabled)('paper_verification')
+            ? 'viewer::enter-code-pv'
+            : 'viewer::enter-code';
+
+        return new HtmlResponse($this->renderer->render($template, [
+            'form'       => $this->form->prepare(),
+            'en_message' => $this->systemMessages['view/en'] ?? null,
+            'cy_message' => $this->systemMessages['view/cy'] ?? null,
+        ]));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isMissingPrerequisite(ServerRequestInterface $request): bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function nextPage(WorkflowState $state): string
+    {
+        return 'check-code';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function lastPage(WorkflowState $state): string
+    {
+        return 'home';
+    }
+}
