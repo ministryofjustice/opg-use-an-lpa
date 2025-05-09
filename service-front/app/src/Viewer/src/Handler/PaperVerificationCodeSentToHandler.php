159d6032193930969694efa3f3bb548728b9595a
Seema Menon <seema.menon@digital.justice.gov.uk>
2025-05-10 00:33:53 +0100

    new form and handler added in for a new page in the PV journey

---
+++ service-front/app/src/Viewer/src/Handler/PaperVerificationCodeSentToHandler.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Viewer\Handler;
+
+use Common\Handler\AbstractHandler;
+use Common\Handler\CsrfGuardAware;
+use Common\Handler\Traits\CsrfGuard;
+use Common\Handler\Traits\Session as SessionTrait;
+use Common\Service\Features\FeatureEnabled;
+use Common\Service\SystemMessage\SystemMessageService;
+use Common\Workflow\WorkflowState;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Viewer\Form\VerificationCode;
+
+
+/**
+ * @codeCoverageIgnore
+ */
+class PaperVerificationCodeSentToHandler extends AbstractPVSCodeHandler
+{
+    private VerificationCode $form;
+
+    //use CsrfGuard;
+    //use SessionTrait;
+
+    public function __construct(
+        TemplateRendererInterface $renderer,
+        UrlHelper $urlHelper,
+        private FeatureEnabled $featureEnabled,
+        private SystemMessageService $systemMessageService,
+    ) {
+        parent::__construct($renderer, $urlHelper);
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->form = new VerificationCode($this->getCsrfGuard($request));
+        $this->systemMessages = $this->systemMessageService->getMessages();
+
+        return parent::handle($request);
+    }
+
+    public function handleGet(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->state($request)->reset();
+
+        $template = ($this->featureEnabled)('paper_verification')
+            ? 'viewer::paper-verification-code-sent-to'
+            : 'viewer::enter-code';
+
+        return new HtmlResponse($this->renderer->render($template, [
+            'form'       => $this->form->prepare(),
+            'en_message' => $this->systemMessages['view/en'] ?? null,
+            'cy_message' => $this->systemMessages['view/cy'] ?? null,
+        ]));
+    }
+
+    public function handlePost(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->form->setData($request->getParsedBody());
+
+        if ($this->form->isValid()) {
+            $this->session->set('code_receiver', $this->form->getData()['verification_code_receiver']);
+
+           // $this->state($request)->code_receiver = $this->form->getData()['verification_code_receiver'];
+            return $this->redirectToRoute($this->nextPage($this->state($request)));
+        }
+
+        $template = ($this->featureEnabled)('paper_verification')
+            ? 'viewer::paper-verification-code-sent-to'
+            : 'viewer::enter-code';
+
+        return new HtmlResponse($this->renderer->render($template, [
+            'form'       => $this->form->prepare(),
+            'en_message' => $this->systemMessages['view/en'] ?? null,
+            'cy_message' => $this->systemMessages['view/cy'] ?? null,
+        ]));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isMissingPrerequisite(ServerRequestInterface $request): bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function nextPage(WorkflowState $state): string
+    {
+        return 'check-code';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function lastPage(WorkflowState $state): string
+    {
+        return 'home';
+    }
+}
