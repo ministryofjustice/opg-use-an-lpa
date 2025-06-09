53e4c980be6c836e7e7f4c2eed2c8e1618aa9d2c
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-06-09 16:52:24 +0100

    Change viewercode summary and full endpoints to use new objects and validation

---
+++ service-api/app/src/App/src/Request/ViewerCodeFull.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Request;
+
+use Laminas\Form\Annotation as Input;
+use Laminas\Validator\Regex;
+
+class ViewerCodeFull implements InputFilteredRequest
+{
+    public function __construct(
+        #[Input\Required]
+        public readonly string $name,
+        #[Input\Validator(Regex::class, ['pattern' => '/^[[:alnum:]]{12}$/'])]
+        public readonly string $code,
+        #[Input\Required]
+        public readonly string $organisation,
+    ) {
+    }
+}
