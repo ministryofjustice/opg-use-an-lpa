a321bc3b8b5251fe1f025c79655d5ac93bbef089
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-06-09 16:54:21 +0100

    Add initial paper verification code request types

---
+++ service-api/app/src/App/src/Request/PaperVerificationCodeUsable.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Request;
+
+use App\Value\CastToValueObject;
+use App\Value\PaperVerificationCode;
+use Laminas\Form\Annotation as Input;
+use Laminas\Validator\Regex;
+
+class PaperVerificationCodeUsable implements InputFilteredRequest
+{
+    public function __construct(
+        #[Input\Required]
+        public readonly string $name,
+        #[Input\Validator(Regex::class, ['pattern' => '/^P(-[[:alnum:]]{4}){3}-[[:alnum:]]{2}$/'])]
+        #[CastToValueObject(PaperVerificationCode::class)]
+        public readonly PaperVerificationCode $code,
+    ) {
+    }
+}
---
+++ service-api/app/src/App/src/Request/PaperVerificationCodeView.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Request;
+
+use App\Value\CastToValueObject;
+use App\Value\LpaUid;
+use App\Value\PaperVerificationCode;
+use DateTimeInterface;
+use Laminas\Form\Annotation as Input;
+use Laminas\Validator\Regex;
+
+class PaperVerificationCodeView implements InputFilteredRequest
+{
+    public function __construct(
+        #[Input\Required]
+        public readonly string $name,
+        #[Input\Validator(Regex::class, ['pattern' => '/^P(-[[:alnum:]]{4}){3}-[[:alnum:]]{2}$/'])]
+        #[CastToValueObject(PaperVerificationCode::class)]
+        public readonly PaperVerificationCode $code,
+        #[Input\Required]
+        #[CastToValueObject(LpaUid::class)]
+        public readonly LpaUid $lpaUid,
+        #[Input\Required]
+        public readonly bool $sentToDonor,
+        #[Input\Required]
+        public readonly string $attorneyName,
+        #[Input\Required]
+        public readonly DateTimeInterface $dateOfBirth,
+        #[Input\Required]
+        public readonly int $noOfAttorneys,
+        #[Input\Required]
+        public readonly string $organisation,
+    ) {
+    }
+}
