cc24b39d517ca6872b841fd6b7d73e64fcd5bec3
Allen Annom <allen.annom@digital.justice.gov.uk>
2025-06-12 16:26:42 +0100

    updated translations

--- service-front/app/src/Viewer/src/Handler/PaperVerification/CheckAnswersHandler.php
+++ service-front/app/src/Viewer/src/Handler/PaperVerification/CheckAnswersHandler.php
@@ -69,7 +69,6 @@ class CheckAnswersHandler extends AbstractPVSCodeHandler
      */
     public function isMissingPrerequisite(ServerRequestInterface $request): bool
     {
-        return false;
         return $this->state($request)->lastName === null
             || $this->state($request)->code === null
             || $this->state($request)->lpaUid === null
