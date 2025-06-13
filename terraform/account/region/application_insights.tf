40bbe52a27b1722bf2c6aa512b6b8ea746f59e9d
jay-whitwell <jay.whitwell@publicguardian.gov.uk>
2025-06-13 16:12:56 +0100

    remove all app insights configuration

--- terraform/account/region/application_insights.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_sns_topic" "cloudwatch_application_insights" {
-  name              = "CloudWatch-Application-Insights-to-PagerDuty-${var.environment_name}"
-  kms_master_key_id = aws_kms_alias.pagerduty_sns.target_key_arn
-
-  provider = aws.region
-}
