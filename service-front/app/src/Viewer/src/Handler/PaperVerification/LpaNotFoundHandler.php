681387c7dbfa010dc3c83da388a50c389c346c64
Allen Annom <allen.annom@digital.justice.gov.uk>
2025-06-16 11:58:32 +0100

    reverted uat change

--- service-front/app/src/Viewer/src/Handler/PaperVerification/LpaNotFoundHandler.php
+++ service-front/app/src/Viewer/src/Handler/PaperVerification/LpaNotFoundHandler.php
@@ -83,7 +83,6 @@ class LpaNotFoundHandler extends AbstractPVSCodeHandler
      */
     public function isMissingPrerequisite(ServerRequestInterface $request): bool
     {
-        return false;
         return $this->state($request)->lastName === null
             || $this->state($request)->code === null
             || $this->state($request)->lpaUid === null
