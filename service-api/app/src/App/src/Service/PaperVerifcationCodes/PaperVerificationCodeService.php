35e3737538e200afa02cbc1ae8be1e1cd3d78c0f
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-06-16 16:23:01 +0100

    Empty PVC service file

---
+++ service-api/app/src/App/src/Service/PaperVerifcationCodes/PaperVerificationCodeService.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\PaperVerificationCodes;
+
+use App\Request\PaperVerificationCodeUsable;
+use App\Request\PaperVerificationCodeValidate;
+use App\Request\PaperVerificationCodeView;
+
+class PaperVerificationCodeService
+{
+    public function usable(PaperVerificationCodeUsable $params): void
+    {
+    }
+
+    public function validate(PaperVerificationCodeValidate $params): void
+    {
+    }
+
+    public function view(PaperVerificationCodeView $params): void
+    {
+    }
+}
