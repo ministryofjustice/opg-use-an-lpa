3b1b7595148e495c045081115c6a4aee368d8409
jay-whitwell <jay.whitwell@publicguardian.gov.uk>
2025-06-17 16:27:59 +0100

    UML-3962: implement app insights

--- terraform/environment/region/variables.tf
+++ terraform/environment/region/variables.tf
@@ -20,6 +20,12 @@ variable "admin_container_version" {
   type        = string
 }
 
+variable "cloudwatch_application_insights_enabled" {
+  description = "Whether to turn on Application Insights for the environment."
+  type        = bool
+  default     = false
+}
+
 variable "associate_alb_with_waf_web_acl_enabled" {
   description = "Whether or not to associate the ALBs with the WAF web ACL."
   type        = bool
