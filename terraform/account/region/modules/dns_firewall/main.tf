dfc72b63777ed75254ba3dd7544075f1d00dffe0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-07-03 13:41:50 +0100

    use new network in development

--- terraform/account/region/modules/dns_firewall/main.tf
+++ terraform/account/region/modules/dns_firewall/main.tf
@@ -3,16 +3,6 @@ locals {
   block_action   = var.enable_block ? "BLOCK" : "ALERT"
 }
 
-data "aws_region" "current" {
-  provider = aws.region
-}
-
-data "aws_vpc" "default" {
-  default = true
-
-  provider = aws.region
-}
-
 resource "aws_cloudwatch_log_group" "aws_route53_resolver_query_log" {
   name              = "use-an-lpa-aws-route53-resolver-query-log-config"
   retention_in_days = 400
@@ -33,9 +23,8 @@ resource "aws_route53_resolver_query_log_config" "egress" {
 
 resource "aws_route53_resolver_query_log_config_association" "egress" {
   resolver_query_log_config_id = aws_route53_resolver_query_log_config.egress.id
-  resource_id                  = data.aws_vpc.default.id
-
-  provider = aws.region
+  resource_id                  = data.aws_default_tags.current.tags.environment-name == "development" ? data.aws_vpc.main.id : data.aws_vpc.default.id
+  provider                     = aws.region
 }
 
 
@@ -48,14 +37,6 @@ locals {
     "secretsmanager",
     "ecr.api",
   ]
-}
-
-data "aws_service" "services" {
-  for_each   = toset(local.service_id)
-  region     = data.aws_region.current.name
-  service_id = each.value
-
-  provider = aws.region
 }
 
 locals {
@@ -118,11 +99,10 @@ resource "aws_route53_resolver_firewall_rule_group_association" "egress" {
   name                   = "egress"
   firewall_rule_group_id = aws_route53_resolver_firewall_rule_group.egress.id
   priority               = 500
-  vpc_id                 = data.aws_vpc.default.id
+  vpc_id                 = data.aws_default_tags.current.tags.environment-name == "development" ? data.aws_vpc.main.id : data.aws_vpc.default.id
 
   provider = aws.region
 }
-
 
 resource "aws_cloudwatch_query_definition" "dns_firewall_statistics" {
   name = "DNS Firewall Queries/DNS Firewall Statistics"
