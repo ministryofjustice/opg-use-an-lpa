b9db288c8ddb673205caa82d75fc483b3099bf9c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-07-07 14:21:35 +0100

    old vpce still should use old network

--- terraform/account/region/vpc_endpoints.tf
+++ terraform/account/region/vpc_endpoints.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "vpc_endpoints_private" {
   name_prefix = "vpc-endpoint-access-private-subnets-${data.aws_region.current.name}"
   description = "vpc endpoint private sg"
-  vpc_id      = data.aws_default_tags.current.tags.environment-name == "development" ? module.network.vpc.id : aws_default_vpc.default.id
+  vpc_id      = aws_default_vpc.default.id
   tags        = { Name = "vpc-endpoint-access-private-subnets-${data.aws_region.current.name}" }
   lifecycle {
     create_before_destroy = true
@@ -16,7 +16,7 @@ resource "aws_security_group_rule" "vpc_endpoints_private_subnet_ingress" {
   protocol          = "tcp"
   security_group_id = aws_security_group.vpc_endpoints_private.id
   type              = "ingress"
-  cidr_blocks       = data.aws_default_tags.current.tags.environment-name == "development" ? module.network.application_subnets[*].cidr_block : aws_subnet.private[*].cidr_block
+  cidr_blocks       = aws_subnet.private[*].cidr_block
   description       = "Allow Services in Private Subnets of ${data.aws_region.current.name} to connect to VPC Interface Endpoints"
 
   provider = aws.region
@@ -28,7 +28,7 @@ resource "aws_security_group_rule" "vpc_endpoints_public_subnet_ingress" {
   protocol          = "tcp"
   security_group_id = aws_security_group.vpc_endpoints_private.id
   type              = "ingress"
-  cidr_blocks       = data.aws_default_tags.current.tags.environment-name == "development" ? module.network.public_subnets[*].cidr_block : aws_default_subnet.public[*].cidr_block
+  cidr_blocks       = aws_default_subnet.public[*].cidr_block
   description       = "Allow Services in Public Subnets of ${data.aws_region.current.name} to connect to VPC Interface Endpoints"
 
   provider = aws.region
@@ -56,12 +56,12 @@ locals {
 resource "aws_vpc_endpoint" "private" {
   for_each = var.environment_name == "development" ? local.interface_endpoint_dev : local.interface_endpoint
 
-  vpc_id              = data.aws_default_tags.current.tags.environment-name == "development" ? module.network.vpc.id : aws_default_vpc.default.id
+  vpc_id              = aws_default_vpc.default.id
   service_name        = "com.amazonaws.${data.aws_region.current.name}.${each.value}"
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
   security_group_ids  = aws_security_group.vpc_endpoints_private[*].id
-  subnet_ids          = data.aws_default_tags.current.tags.environment-name == "development" ? module.network.application_subnets[*].id : aws_subnet.private[*].id
+  subnet_ids          = aws_subnet.private[*].id
   tags                = { Name = "${each.value}-private-${data.aws_region.current.name}" }
 
   provider = aws.region
