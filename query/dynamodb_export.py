cd3735be5127f1817613124844befc5f5ce37e12
Seema Menon <seema.menon@digital.justice.gov.uk>
2025-08-04 12:42:22 +0100

    undid the query change, not part of this PR

--- query/dynamodb_export.py
+++ query/dynamodb_export.py
@@ -331,26 +331,6 @@ class DynamoDBExporterAndQuerier:
             outputFileName="CountOfUsersWithNoLpas",
         )
 
-    def get_duplicate_users_ids_with_same_email(self):
-                sql_string = f"SELECT a.Item.id.S as User_Id,a.Item.'Identity'.S as One_Login_id,a.Item.email.S as User_Email FROM actor_users a LEFT JOIN user_lpa_actor_map b ON a.Item.id.S = b.Item.UserId.S WHERE a.Item.email.S IN (SELECT Item.email.S FROM actor_users GROUP BY Item.email.S HAVING COUNT(*) > 1) GROUP BY a.Item.email.S, a.Item.id.S, a.Item.'Identity'.S"
-                self.run_athena_query(
-                    sql_string,
-                    outputFileName="DuplicateUsersIDsWithSameEmail",
-                )
-
-    def get_duplicate_users_with_same_email(self):
-            sql_string = f"SELECT a.Item.id.S as User_Id,a.Item.email.S as User_Email,COUNT(b.Item.SiriusUid) AS Lpa_Count FROM actor_users a LEFT JOIN user_lpa_actor_map b ON a.Item.id.S = b.Item.UserId.S WHERE a.Item.email.S IN (SELECT Item.email.S FROM actor_users GROUP BY Item.email.S HAVING COUNT(*) > 1) GROUP BY a.Item.email.S, a.Item.id.S ORDER BY a.Item.email.S"
-            self.run_athena_query(
-                sql_string,
-                outputFileName="DuplicateUsersWithSameEmail",
-            )
-
-    def get_lpa_duplicate_users_with_same_email(self):
-        sql_string = f"SELECT  a.Item.id.S as User_Id, a.Item.email.S as User_Email,b.Item.SiriusUid.S as Lpa_Id FROM actor_users a LEFT JOIN user_lpa_actor_map b ON a.Item.id.S = b.Item.UserId.S WHERE a.Item.email.S IN (SELECT Item.email.S FROM actor_users GROUP BY Item.email.S HAVING COUNT(*) > 1) GROUP BY a. Item.email.S, a.Item.id.S,b.Item.SiriusUid.S ORDER BY a.Item.email.S"
-        self.run_athena_query(
-            sql_string,
-            outputFileName="LpasForDuplicateUsersWithSameEmail",
-        )
 
 def main():
     parser = argparse.ArgumentParser(description="Exports DynamoDB tables to S3.")
@@ -411,17 +391,14 @@ def main():
         work.check_dynamo_export_status()
         work.create_athena_tables()
 
-#     work.get_expired_viewed_access_codes()
-#     work.get_expired_unviewed_access_codes()
-#     work.get_count_of_viewed_access_codes()
-#     work.get_count_of_created_access_codes()
-#     work.get_count_of_expired_access_codes()
-#     work.get_organisations_field()
-#     work.get_count_of_lpas_for_users()
-#     work.get_count_of_users_with_no_lpas()
-    work.get_duplicate_users_ids_with_same_email()
-#     work.get_duplicate_users_with_same_email()
-#     work.get_lpa_duplicate_users_with_same_email()
+    work.get_expired_viewed_access_codes()
+    work.get_expired_unviewed_access_codes()
+    work.get_count_of_viewed_access_codes()
+    work.get_count_of_created_access_codes()
+    work.get_count_of_expired_access_codes()
+    work.get_organisations_field()
+    work.get_count_of_lpas_for_users()
+    work.get_count_of_users_with_no_lpas()
 
 
 if __name__ == "__main__":
