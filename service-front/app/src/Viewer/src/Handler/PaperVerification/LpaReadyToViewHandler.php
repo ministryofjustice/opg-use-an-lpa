7bb11c6eeb65e5bce9e319976efaef59c263d63e
SeemaMenon <seema.menon@digital.justice.gov.uk>
2025-08-15 14:49:09 +0100

    UML-3881: Enter organisation name for pv journey (#3293)
    * new page to enter organisation name, form is the same existing
    
    * linting issues
    
    * psalm breakage fix
    
    * resolved a conflict on this manually

---
+++ service-front/app/src/Viewer/src/Handler/PaperVerification/LpaReadyToViewHandler.php
@@ -0,0 +1,136 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Viewer\Handler\PaperVerification;
+
+use Common\Service\SystemMessage\SystemMessageService;
+use Common\Workflow\WorkflowState;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Viewer\Form\Organisation;
+use Viewer\Handler\AbstractPVSCodeHandler;
+use Common\Service\Lpa\Factory\{LpaDataFormatter};
+
+/**
+ * @codeCoverageIgnore
+ */
+class LpaReadyToViewHandler extends AbstractPVSCodeHandler
+{
+    private Organisation $form;
+
+    /**
+     * @var array{
+     *     "view/en": string,
+     *     "view/cy": string,
+     * }
+     */
+    private array $systemMessages;
+
+    public const TEMPLATE = 'viewer::paper-verification/enter-organisation-name';
+
+    public function __construct(
+        TemplateRendererInterface $renderer,
+        UrlHelper $urlHelper,
+        LoggerInterface $logger,
+        private SystemMessageService $systemMessageService,
+        private LpaDataFormatter $lpaDataFormatter,
+    ) {
+        parent::__construct($renderer, $urlHelper, $logger);
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->form           = new Organisation($this->getCsrfGuard($request));
+        $this->systemMessages = $this->systemMessageService->getMessages();
+
+        return parent::handle($request);
+    }
+
+    public function handleGet(ServerRequestInterface $request): ResponseInterface
+    {
+        $code = $this->state($request)->lpaCode ?? 'P-AB12-CD34-EF56-G7';
+        //$surname = $this->state($request)->donorSurname ?? 'Babara Gilson';
+
+        if (isset($code)) {
+            // TODO - LPA service call to check lpa match
+            //$lpa = $this->lpaService->getLpaByLpaCode($this->code, $surname, null);
+
+            // mocking Lpa data for testing page
+            $lpa = json_decode(
+                file_get_contents(
+                    __DIR__ . '../../../../../../test/fixtures/combined_lpa.json'
+                ),
+                true
+            );
+
+            $combinedSiriusLpa = ($this->lpaDataFormatter)($lpa);
+            $donor             = $combinedSiriusLpa->getDonor()->getFirstname() .
+                ' ' . $combinedSiriusLpa->getDonor()->getSurname();
+        }
+
+        return new HtmlResponse($this->renderer->render(self::TEMPLATE, [
+            'form'       => $this->form->prepare(),
+            'donor_name' => $donor,
+            'lpa_type'   => $combinedSiriusLpa->getLpaType(),
+            'back'       => $this->lastPage($this->state($request)),
+            'en_message' => $this->systemMessages['view/en'] ?? null,
+            'cy_message' => $this->systemMessages['view/cy'] ?? null,
+        ]));
+    }
+
+    public function handlePost(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->form->setData($request->getParsedBody());
+
+        if ($this->form->isValid()) {
+            //$this->session->set('organisation', $this->form->getData()['organisation']);
+
+            $this->state($request)->organisation = $this->form->getData()['organisation'];
+            return $this->redirectToRoute($this->nextPage($this->state($request)));
+        }
+
+        return new HtmlResponse($this->renderer->render(self::TEMPLATE, [
+            'organisation' => $this->state($request)->organisation,
+            'form'         => $this->form->prepare(),
+            'en_message'   => $this->systemMessages['view/en'] ?? null,
+            'cy_message'   => $this->systemMessages['view/cy'] ?? null,
+        ]));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isMissingPrerequisite(ServerRequestInterface $request): bool
+    {
+        return $this->state($request)->lastName === null
+            || $this->state($request)->code === null
+            || $this->state($request)->lpaUid === null
+            || $this->state($request)->sentToDonor === false
+            || $this->state($request)->attorneyName === null
+            || $this->state($request)->noOfAttorneys === 0
+            || $this->state($request)->noOfAttorneys === null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function nextPage(WorkflowState $state): string
+    {
+        //needs changing when next page ready
+        return 'home';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function lastPage(WorkflowState $state): string
+    {
+        //needs changing when next page ready
+        return 'home';
+    }
+}
