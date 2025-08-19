e5a89d16999383c741418c9af0b3752ccd8216f7
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-08-19 18:22:35 +0100

    Cover missing Halite cookie creation code

--- service-front/app/test/CommonTest/Service/Session/Encryption/HaliteEncryptedCookieTest.php
+++ service-front/app/test/CommonTest/Service/Session/Encryption/HaliteEncryptedCookieTest.php
@@ -8,10 +8,13 @@ use Common\Service\Session\Encryption\HaliteCrypto;
 use Common\Service\Session\Encryption\HaliteEncryptedCookie;
 use Common\Service\Session\KeyManager\Key;
 use Common\Service\Session\KeyManager\KeyManagerInterface;
+use Common\Service\Session\KeyManager\KeyNotFoundException;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
 
+#[CoversClass(HaliteEncryptedCookie::class)]
 class HaliteEncryptedCookieTest extends TestCase
 {
     #[Test]
@@ -32,7 +35,7 @@ class HaliteEncryptedCookieTest extends TestCase
             'session' => 'data',
         ];
 
-        $keyMock = $this->createMock(Key::class);
+        $keyMock = $this->createStub(Key::class);
         $keyMock->method('getId')->willReturn('1');
 
         $keyManagerStub = $this->createStub(KeyManagerInterface::class);
@@ -52,20 +55,71 @@ class HaliteEncryptedCookieTest extends TestCase
     #[Test]
     public function it_encodes_an_empty_array_of_data_to_a_blank_string(): void
     {
+        $data = [];
+
+        $keyManagerStub = $this->createStub(KeyManagerInterface::class);
+        $cryptoStub     = $this->createStub(HaliteCrypto::class);
+        $loggerStub     = $this->createStub(LoggerInterface::class);
+
+        $sut = new HaliteEncryptedCookie($keyManagerStub, $cryptoStub, $loggerStub);
+
+        $encoded = $sut->encodeCookieValue($data);
+        $this->assertEquals('', $encoded);
     }
 
     #[Test]
     public function it_decodes_a_string_into_session_data(): void
     {
+        $payload = '1.ZW5jcnlwdGVkU3RyaW5n';
+
+        $keyStub = $this->createStub(Key::class);
+
+        $keyManagerStub = $this->createStub(KeyManagerInterface::class);
+        $keyManagerStub->method('getDecryptionKey')->willReturn($keyStub);
+
+        $cryptoMock = $this->createMock(HaliteCrypto::class);
+        $cryptoMock
+            ->method('decrypt')
+            ->with('encryptedString', $keyStub)
+            ->willReturn('{"session":"data"}');
+
+        $loggerStub = $this->createStub(LoggerInterface::class);
+
+        $sut = new HaliteEncryptedCookie($keyManagerStub, $cryptoMock, $loggerStub);
+
+        $data = $sut->decodeCookieValue($payload);
+        $this->assertEquals('data', $data['session']);
     }
 
     #[Test]
     public function it_throws_an_exception_when_key_id_not_matched_and_returns_new_session(): void
     {
+        $payload = '1.ZW5jcnlwdGVkU3RyaW5n';
+
+        $keyManagerStub = $this->createStub(KeyManagerInterface::class);
+        $keyManagerStub->method('getDecryptionKey')->willThrowException(new KeyNotFoundException());
+
+        $cryptoMock = $this->createMock(HaliteCrypto::class);
+        $loggerStub = $this->createStub(LoggerInterface::class);
+
+        $sut = new HaliteEncryptedCookie($keyManagerStub, $cryptoMock, $loggerStub);
+
+        $data = $sut->decodeCookieValue($payload);
+        $this->assertEquals([], $data);
     }
 
     #[Test]
     public function it_decodes_an_empty_string_into_an_empty_array(): void
     {
+        $data = '';
+
+        $keyManagerStub = $this->createStub(KeyManagerInterface::class);
+        $cryptoStub     = $this->createStub(HaliteCrypto::class);
+        $loggerStub     = $this->createStub(LoggerInterface::class);
+
+        $sut = new HaliteEncryptedCookie($keyManagerStub, $cryptoStub, $loggerStub);
+
+        $encoded = $sut->decodeCookieValue($data);
+        $this->assertEquals([], $encoded);
     }
 }
