8bbed0d43f09b2a22bd76eb748a21f3611407785
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-08-20 17:03:49 +0100

    Add coverage for the fallback cookie factory

---
+++ service-front/app/test/CommonTest/Service/Session/Encryption/EncryptionFallbackCookieFactoryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommonTest\Service\Session\Encryption;
+
+use Common\Service\Session\Encryption\BlockCipherEncryptedCookie;
+use Common\Service\Session\Encryption\EncryptInterface;
+use Common\Service\Session\Encryption\EncryptionFallbackCookieFactory;
+use Common\Service\Session\Encryption\HaliteEncryptedCookie;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+#[CoversClass(EncryptionFallbackCookieFactory::class)]
+class EncryptionFallbackCookieFactoryTest extends TestCase
+{
+    #[Test]
+    public function it_creates_a_halite_primary_fallback_cookie(): void
+    {
+        $matcher       = $this->exactly(2);
+        $containerMock = $this->createMock(ContainerInterface::class);
+        $containerMock
+            ->expects($matcher)
+            ->method('get')
+            ->willReturnCallback(function (string $class) use ($matcher) {
+                match ($matcher->numberOfInvocations()) {
+                    1 => $this->assertEquals(HaliteEncryptedCookie::class, $class),
+                    2 => $this->assertEquals(BlockCipherEncryptedCookie::class, $class),
+                };
+
+                return $this->createStub(EncryptInterface::class);
+            });
+
+        $sut = new EncryptionFallbackCookieFactory();
+
+        ($sut)($containerMock);
+    }
+}
