c32e94a848b74df3b7d2b22ff11b9045c555d732
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2025-09-30 16:25:21 +0100

    linting

--- service-api/app/src/App/src/Enum/VerificationCodeExpiryReason.php
+++ service-api/app/src/App/src/Enum/VerificationCodeExpiryReason.php
@@ -6,7 +6,7 @@ namespace App\Enum;
 
 enum VerificationCodeExpiryReason: string
 {
-    case PAPER_TO_DIGITAL ='paper_to_digital';
-    case FIRST_TIME_USE = 'first_time_use';
-    case CANCELLED = 'cancelled';
+    case PAPER_TO_DIGITAL = 'paper_to_digital';
+    case FIRST_TIME_USE   = 'first_time_use';
+    case CANCELLED        = 'cancelled';
 }
