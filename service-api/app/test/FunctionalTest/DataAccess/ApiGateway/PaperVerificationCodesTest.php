d019dfdf0e3ebb745da31e1894cf83aa86f5dce3
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-10-27 12:18:49 +0000

    Move pact storage out of /tmp

--- service-api/app/test/FunctionalTest/DataAccess/ApiGateway/PaperVerificationCodesTest.php
+++ service-api/app/test/FunctionalTest/DataAccess/ApiGateway/PaperVerificationCodesTest.php
@@ -40,7 +40,7 @@ class PaperVerificationCodesTest extends AbstractFunctionalTestCase
         $mockServer->setPort(80);
         $mockServer->setConsumer('use-an-lpa');
         $mockServer->setProvider('data-lpa-codes');
-        $mockServer->setPactDir('/tmp/pacts');
+        $mockServer->setPactDir('./build/pacts');
         $mockServer->setPactFileWriteMode('merge');
 
         $this->builder = new InteractionBuilder($mockServer);
