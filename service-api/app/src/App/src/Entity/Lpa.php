74e9fea3bcfe54dd332126a835e09a2ea09f731e
Seema Menon <seema.menon@digital.justice.gov.uk>
2025-12-19 17:08:20 +0000

    entity inclusions

--- service-api/app/src/App/src/Entity/Lpa.php
+++ service-api/app/src/App/src/Entity/Lpa.php
@@ -49,6 +49,7 @@ abstract class Lpa implements
         public readonly ?Person $donor,
         public readonly ?bool $hasSeveranceWarning,
         public readonly ?HowAttorneysMakeDecisions $howAttorneysMakeDecisions,
+        public readonly ?string $howAttorneysMakeDecisionsDetails,
         public readonly ?DateTimeImmutable $invalidDate,
         public readonly ?LifeSustainingTreatment $lifeSustainingTreatment,
         public readonly ?DateTimeImmutable $lpaDonorSignatureDate,
