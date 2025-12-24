44608715fe0bf40ab009a375ebf6d5ff05fba606
Seema Menon <seema.menon@digital.justice.gov.uk>
2025-12-24 10:42:45 +0000

    testing the breakge

--- service-api/app/src/App/src/Entity/Lpa.php
+++ service-api/app/src/App/src/Entity/Lpa.php
@@ -49,6 +49,7 @@ abstract class Lpa implements
         public readonly ?Person $donor,
         public readonly ?bool $hasSeveranceWarning,
         public readonly ?HowAttorneysMakeDecisions $howAttorneysMakeDecisions,
+        public readonly ?string $howAttorneysMakeDecisionsDetails,
         public readonly ?DateTimeImmutable $invalidDate,
         public readonly ?LifeSustainingTreatment $lifeSustainingTreatment,
         public readonly ?DateTimeImmutable $lpaDonorSignatureDate,
@@ -144,4 +145,9 @@ abstract class Lpa implements
     }
 
     abstract public function getDonor(): Person;
+
+    public function getHowAttorneysMakeDecisionsDetails(): string
+    {
+        return $this->howAttorneysMakeDecisionsDetails ?? '';
+    }
 }
