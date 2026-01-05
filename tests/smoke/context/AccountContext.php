ea7179c968a5795e72d440c93126b8e6f502657b
Seema Menon <seema.menon@digital.justice.gov.uk>
2026-01-05 15:27:48 +0000

    changes to try test working

--- tests/smoke/context/AccountContext.php
+++ tests/smoke/context/AccountContext.php
@@ -118,7 +118,9 @@ class AccountContext implements Context
     #[Then('I am signed in')]
     public function iAmSignedIn(): void
     {
-        $this->ui->assertElementOnPage('nav.one-login-header__nav');
+        //$this->ui->assertElementOnPage('nav.one-login-header__nav');
+        $this->ui->assertPageContainsText('GOV.UK One Login');
+        $this->ui->assertPageContainsText('Sign out');
     }
 
     #[Then('the javascript is working')]
