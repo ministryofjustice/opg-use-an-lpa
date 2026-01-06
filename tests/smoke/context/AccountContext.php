2b900a629de9f94dc6d8677678784d9c2ac40400
Seema Menon <seema.menon@digital.justice.gov.uk>
2026-01-06 00:57:32 +0000

    wip

--- tests/smoke/context/AccountContext.php
+++ tests/smoke/context/AccountContext.php
@@ -118,18 +118,18 @@ class AccountContext implements Context
     #[Then('I am signed in')]
     public function iAmSignedIn(): void
     {
-        $this->ui->assertElementOnPage('nav.one-login-header__nav');
+        //$this->ui->assertElementOnPage('nav.one-login-header__nav');
 //        $this->ui->assertPageContainsText('GOV.UK One Login');
 //        $this->ui->assertPageContainsText('Sign out');
 
-//        $url = $this->ui->getSession()->getDriver()->getCurrentUrl();
-//
-//        if ($url !== null) {
-//            throw new ExpectationException(
-//                sprintf('Current page is "%s" ', $url),
-//                $this->ui->getSession()->getDriver()
-//            );
-//        }
+        $url = $this->ui->getSession()->getDriver()->getCurrentUrl();
+
+        if ($url !== null) {
+            throw new ExpectationException(
+                sprintf('Current page is "%s" ', $url),
+                $this->ui->getSession()->getDriver()
+            );
+        }
     }
 
     #[Then('the javascript is working')]
