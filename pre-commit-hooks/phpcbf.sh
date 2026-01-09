fc18183d376f485f06126be92b4492e60bda0e0a
Seema Menon <seema.menon@digital.justice.gov.uk>
2026-01-09 11:54:29 +0000

    Removed interactive flag as breaking commit hooks in new docker

--- pre-commit-hooks/phpcbf.sh
+++ pre-commit-hooks/phpcbf.sh
@@ -11,14 +11,14 @@ if [ $MODE = 'front' ]; then
         FILES+="${path#service-front/app/} "
     done
 
-    docker compose run --rm -i --no-deps actor-app php /app/vendor/bin/phpcbf $FILES
+    docker compose run --rm --no-deps actor-app php /app/vendor/bin/phpcbf $FILES
 elif [ $MODE = 'api' ]; then
     FILES=""
     for path in "$@"; do
         FILES+="${path#service-api/app/} "
     done
 
-    docker compose run --rm -i --no-deps api-app php /app/vendor/bin/phpcbf $FILES
+    docker compose run --rm --no-deps api-app php /app/vendor/bin/phpcbf $FILES
 else
     exit 1
 fi
