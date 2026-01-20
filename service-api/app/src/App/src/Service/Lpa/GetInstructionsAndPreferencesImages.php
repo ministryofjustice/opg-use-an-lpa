a9855b169e980e979f7fefd7dff46104b30ac40c
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2026-01-20 12:51:18 +0000

    Log any image request coming through that is not "complete"
    This means we capture extraction requests as well as in-progress and failures.

--- service-api/app/src/App/src/Service/Lpa/GetInstructionsAndPreferencesImages.php
+++ service-api/app/src/App/src/Service/Lpa/GetInstructionsAndPreferencesImages.php
@@ -6,15 +6,39 @@ namespace App\Service\Lpa;
 
 use App\DataAccess\Repository\InstructionsAndPreferencesImagesInterface;
 use App\DataAccess\Repository\Response\InstructionsAndPreferencesImages;
+use App\Enum\InstructionsAndPreferencesImagesResult;
+use App\Service\Log\EventCodes;
+use Psr\Log\LoggerInterface;
 
 class GetInstructionsAndPreferencesImages
 {
-    public function __construct(private InstructionsAndPreferencesImagesInterface $apiGateway)
-    {
+    public function __construct(
+        private InstructionsAndPreferencesImagesInterface $apiGateway,
+        private LoggerInterface $logger,
+    ) {
     }
 
     public function __invoke(int $lpaId): InstructionsAndPreferencesImages
     {
-        return $this->apiGateway->getInstructionsAndPreferencesImages($lpaId);
+        $images = $this->apiGateway->getInstructionsAndPreferencesImages($lpaId);
+
+        if ($images->status !== InstructionsAndPreferencesImagesResult::COLLECTION_COMPLETE) {
+            $this->logger->notice(
+                'I&P images for Lpa with id {lpa_id} have status {status}',
+                [
+                    'lpa_id'     => $images->uId,
+                    'status'     => $images->status->value,
+                    'event_code' => match ($images->status) {
+                        InstructionsAndPreferencesImagesResult::COLLECTION_NOT_STARTED
+                            => EventCodes::INSTRUCTIONS_PREFERENCES_IMAGES_NEW_EXTRACT,
+                        InstructionsAndPreferencesImagesResult::COLLECTION_ERROR
+                            => EventCodes::INSTRUCTIONS_PREFERENCES_IMAGES_ERROR,
+                        default => ''
+                    },
+                ]
+            );
+        }
+
+        return $images;
     }
 }
