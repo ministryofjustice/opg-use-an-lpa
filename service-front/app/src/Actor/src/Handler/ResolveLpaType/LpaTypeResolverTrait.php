2eeb7c25fc6ca17289d5cbbba506b710aeb85152
Seema Menon <seema.menon@digital.justice.gov.uk>
2026-02-26 08:47:16 +0000

    added a trait, removed the service class

---
+++ service-front/app/src/Actor/src/Handler/ResolveLpaType/LpaTypeResolverTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Actor\Handler\ResolveLpaType;
+
+use Common\Service\Log\EventCodes;
+
+trait LpaTypeResolverTrait
+{
+    public function resolveLabel(string $subtype, string $lpaUid): string
+    {
+        $caseSubtype = strtolower($subtype);
+        $lpaUid      = strtoupper($lpaUid);
+
+        $isDigital = str_starts_with($lpaUid, 'M');
+
+        if ($caseSubtype === 'hw') {
+            return $isDigital
+                ? $this->translator->translate('personal welfare')
+                : $this->translator->translate('health and welfare');
+        }
+
+        return $isDigital
+            ? $this->translator->translate('property and affairs')
+            : $this->translator->translate('property and finance');
+    }
+
+    public function resolveEventCode(string $subtype): string
+    {
+        return strtolower($subtype) === 'hw'
+            ? EventCodes::ADDED_LPA_TYPE_HW
+            : EventCodes::ADDED_LPA_TYPE_PFA;
+    }
+}
