98185af04f573d676ec729370ba7ac650d94b297
Josh Murray <jhm1999@hotmail.co.uk>
2026-03-26 11:27:32 +0000

    remove tfsec

--- terraform/environment/region/admin_ecs.tf
+++ terraform/environment/region/admin_ecs.tf
@@ -101,7 +101,7 @@ resource "aws_security_group_rule" "admin_ecs_service_egress" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-egress-sgr - open egress for ECR access
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.admin_ecs_service.id
   lifecycle {
     create_before_destroy = true
