6e588f82e68b95fdfb6796540af7c226b53a18a4
Joshua Hawxwell <m@hawx.me>
2026-05-06 11:07:51 +0100

    Update uses of govuk-frontend

--- service-admin/web/assets/main.js
+++ service-admin/web/assets/main.js
@@ -1,5 +1,5 @@
-import MOJFrontend from '@ministryofjustice/frontend/moj/all.js';
-import {Tabs, initAll} from 'govuk-frontend/dist/govuk/all.mjs';
+import * as MOJFrontend from '@ministryofjustice/frontend/moj/all.mjs';
+import { Tabs, initAll } from 'govuk-frontend/dist/govuk/all.mjs';
 
 // we aren't using the JS tabs, but they try to initialise this will stop them breaking
 Tabs.prototype.setup = () => { };
