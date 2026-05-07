c159af2d94210afb817be13ea1a5dd768ab42b71
jay-whitwell <jay.whitwell@publicguardian.gov.uk>
2026-05-07 16:37:09 +0100

    use kms key in replica region

--- terraform/environment/modules/dynamodb_backup/vault.tf
+++ terraform/environment/modules/dynamodb_backup/vault.tf
@@ -4,7 +4,7 @@ resource "aws_backup_vault" "primary" {
 }
 
 resource "aws_backup_vault" "replica" {
-  name        = "${var.environment_name}_${data.aws_region.current.region}_dynamodb_vault_replica"
-  kms_key_arn = data.aws_kms_key.source_key.arn
+  name        = "${var.environment_name}_${data.aws_region.eu_west_2.region}_dynamodb_vault_replica"
+  kms_key_arn = data.aws_kms_key.source_key_replica.arn
   provider    = aws.eu_west_2
 }
