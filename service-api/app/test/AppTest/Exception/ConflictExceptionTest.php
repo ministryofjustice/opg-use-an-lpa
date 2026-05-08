225c49a186eac7002fb0ff4cb1a9b842f46b4283
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2026-05-08 17:32:30 +0100

    Test logging scope of conflict exception

--- service-api/app/test/AppTest/Exception/ConflictExceptionTest.php
+++ service-api/app/test/AppTest/Exception/ConflictExceptionTest.php
@@ -28,4 +28,26 @@ class ConflictExceptionTest extends TestCase
         $this->assertEquals($message, $ce->getMessage());
         $this->assertEquals(409, $ce->getCode());
     }
+
+    #[Test]
+    public function it_narrows_scope_of_logged_data(): void
+    {
+        $additionalData = [
+            'identity' => 'identity',
+            'email'    => 'test@example.com',
+            'some'     => 'additional',
+            'data'     => 'here,',
+        ];
+
+        $sut = new ConflictException('', $additionalData);
+
+        $this->assertCount(2, $sut->getAdditionalDataForLogging());
+        $this->assertArrayHasKey('identity', $sut->getAdditionalDataForLogging());
+        $this->assertEquals('identity', $sut->getAdditionalDataForLogging()['identity']);
+        $this->assertArrayHasKey('email', $sut->getAdditionalDataForLogging());
+        $this->assertEquals(
+            '973dfe463ec85785f5f95af5ba3906eedb2d931c24e69824a89ea65dba4e813b', // pragma: allowlist secret
+            (string) $sut->getAdditionalDataForLogging()['email']
+        );
+    }
 }
