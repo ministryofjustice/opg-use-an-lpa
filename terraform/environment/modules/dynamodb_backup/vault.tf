6ea9e330157ad148463e4cac7679a87c579871cf
jay-whitwell <jay.whitwell@publicguardian.gov.uk>
2026-05-29 10:18:57 +0100

    UML-4164: Create out of account backups

--- terraform/environment/modules/dynamodb_backup/vault.tf
+++ terraform/environment/modules/dynamodb_backup/vault.tf
@@ -9,3 +9,9 @@ resource "aws_backup_vault" "replica" {
   kms_key_arn = data.aws_kms_key.source_key_replica.arn
   region      = var.replica_region
 }
+
+resource "aws_backup_vault" "cross_account" {
+  count       = var.cross_account_backup_enabled ? 1 : 0
+  name        = "opg_use_an_lpa_${var.environment_name}_${data.aws_region.current.region}_backup"
+  kms_key_arn = data.aws_kms_key.source_key.arn
+}
