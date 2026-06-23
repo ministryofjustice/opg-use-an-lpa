7de1917959478ab96aeacca78fdbe3329e9945d2
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-06-23 16:41:51 +0100

    use correct tag for VPC Name

--- terraform/environment/region/network.tf
+++ terraform/environment/region/network.tf
@@ -9,7 +9,7 @@ data "aws_vpc" "main" {
     values = [data.aws_default_tags.current.tags.application]
   }
   filter {
-    name   = "tag:name"
+    name   = "tag:Name"
     values = ["${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.account-name}-vpc"]
   }
   provider = aws.region
