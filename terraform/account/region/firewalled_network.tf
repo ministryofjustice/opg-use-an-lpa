86d47dbfd1756a6ed3aecd5dbca130403b30cc5a
Andrew Pearce <6545556+andrewpearce-digital@users.noreply.github.com>
2026-06-23 17:27:34 +0100

    UML-4371 - upgrade firewalled network module (#3779)
    * upgrade firewalled network module
    
    * use correct tag for VPC Name

--- terraform/account/region/firewalled_network.tf
+++ terraform/account/region/firewalled_network.tf
@@ -1,11 +1,12 @@
 module "network" {
-  source                              = "github.com/ministryofjustice/opg-terraform-aws-firewalled-network?ref=v0.2.15"
+  source                              = "github.com/ministryofjustice/opg-terraform-aws-firewalled-network?ref=v1.3.1"
   cidr                                = var.network_cidr_block
   enable_dns_hostnames                = true
   enable_dns_support                  = true
   default_security_group_ingress      = []
   default_security_group_egress       = []
   aws_networkfirewall_firewall_policy = aws_networkfirewall_firewall_policy.main
+  network_firewall_enabled            = true
   providers = {
     aws = aws.region
   }
