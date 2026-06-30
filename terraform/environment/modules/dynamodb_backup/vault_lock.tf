91c9c7232cec437a16ebf77ad0d5d2dda32ea7f7
jay-whitwell <jay.whitwell@publicguardian.gov.uk>
2026-06-30 16:01:27 +0100

    UML-4464: Enable vault lock on preprod

---
+++ terraform/environment/modules/dynamodb_backup/vault_lock.tf
@@ -0,0 +1,11 @@
+resource "aws_backup_vault_lock_configuration" "primary" {
+  count              = var.enable_vault_lock ? 1 : 0
+  backup_vault_name  = aws_backup_vault.primary.name
+  min_retention_days = var.vault_lock_min_retention_days
+}
+
+resource "aws_backup_vault_lock_configuration" "replica" {
+  count              = var.enable_vault_lock && var.region_replication_enabled ? 1 : 0
+  backup_vault_name  = aws_backup_vault.replica[0].name
+  min_retention_days = var.vault_lock_min_retention_days
+}
