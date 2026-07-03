17c7e86d9a75d4af84df6d45df557b3cc5f94f79
jay-whitwell <jay.whitwell@publicguardian.gov.uk>
2026-07-03 09:06:18 +0100

    add replica region to vault lock

--- terraform/environment/modules/dynamodb_backup/vault_lock.tf
+++ terraform/environment/modules/dynamodb_backup/vault_lock.tf
@@ -8,4 +8,5 @@ resource "aws_backup_vault_lock_configuration" "replica" {
   count              = var.enable_vault_lock && var.region_replication_enabled ? 1 : 0
   backup_vault_name  = aws_backup_vault.replica[0].name
   min_retention_days = var.vault_lock_min_retention_days
+  region             = var.replica_region
 }
