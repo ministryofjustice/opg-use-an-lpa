a5cd761407382ce0d7acbb29c3f50a76633d8e97
jay-whitwell <jay.whitwell@publicguardian.gov.uk>
2026-07-10 13:56:49 +0100

    UML-4474: use region in ecr data source

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -7,7 +7,7 @@ locals {
 
   active_region                                           = [for k, v in local.environment.regions : k if v["is_active"] == true][0]
   mock_onelogin_load_balancer_security_group_name_enabled = local.active_region == "eu-west-1" ? module.eu_west_1[0].security_group_names.mock_onelogin_loadbalancer : module.eu_west_2[0].security_group_names.mock_onelogin_loadbalancer
-  mock_onelogin_load_balancer_security_group_id_enabled   = local.active_region == "eu-west-1" ? module.eu_west_1[0].security_group_ids.mock_onelogin_loadbalancer : module.eu_west_2[0].security_group_id.mock_onelogin_loadbalancer
+  mock_onelogin_load_balancer_security_group_id_enabled   = local.active_region == "eu-west-1" ? module.eu_west_1[0].security_group_ids.mock_onelogin_loadbalancer : module.eu_west_2[0].security_group_ids.mock_onelogin_loadbalancer
 
 
 
@@ -29,8 +29,8 @@ locals {
     viewer_load_balancer_security_group_name        = local.active_region == "eu-west-1" ? module.eu_west_1[0].security_group_names.viewer_loadbalancer : module.eu_west_2[0].security_group_names.viewer_loadbalancer
     actor_load_balancer_security_group_name         = local.active_region == "eu-west-1" ? module.eu_west_1[0].security_group_names.actor_loadbalancer : module.eu_west_2[0].security_group_names.actor_loadbalancer
     mock_onelogin_load_balancer_security_group_name = local.environment.mock_onelogin_enabled ? local.mock_onelogin_load_balancer_security_group_name_enabled : null
-    viewer_load_balancer_security_group_id          = local.active_region == "eu-west-1" ? module.eu_west_1[0].security_group_ids.viewer_loadbalancer : module.eu_west_2[0].security_group_id.viewer_loadbalancer
-    actor_load_balancer_security_group_id           = local.active_region == "eu-west-1" ? module.eu_west_1[0].security_group_ids.actor_loadbalancer : module.eu_west_2[0].security_group_id.actor_loadbalancer
+    viewer_load_balancer_security_group_id          = local.active_region == "eu-west-1" ? module.eu_west_1[0].security_group_ids.viewer_loadbalancer : module.eu_west_2[0].security_group_ids.viewer_loadbalancer
+    actor_load_balancer_security_group_id           = local.active_region == "eu-west-1" ? module.eu_west_1[0].security_group_ids.actor_loadbalancer : module.eu_west_2[0].security_group_ids.actor_loadbalancer
     mock_onelogin_load_balancer_security_group_id   = local.environment.mock_onelogin_enabled ? local.mock_onelogin_load_balancer_security_group_id_enabled : null
     active_region                                   = local.active_region
     vpc_id                                          = local.active_region == "eu-west-1" ? module.eu_west_1[0].vpc_id : module.eu_west_2[0].vpc_id
