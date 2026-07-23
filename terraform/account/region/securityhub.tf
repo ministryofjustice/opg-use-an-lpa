d8447dbd9aed065f4eb312d0f6395665b9c78cbe
Josh Murray <jhm1999@hotmail.co.uk>
2026-07-23 11:56:07 +0100

    UML-4489 - Security Hub- SSM.7 - SSM documents should have the block public sharing setting enabled

--- terraform/account/region/securityhub.tf
+++ terraform/account/region/securityhub.tf
@@ -1,3 +1,12 @@
 resource "aws_ebs_snapshot_block_public_access" "this" {
   state = "block-all-sharing"
+
+  provider = aws.region
+}
+
+resource "aws_ssm_service_setting" "public_sharing_permission" {
+  setting_id    = "arn:aws:ssm:${data.aws_region.current.region}:${var.account.account_id}:servicesetting/ssm/documents/console/public-sharing-permission"
+  setting_value = "Disable"
+
+  provider = aws.region
 }
