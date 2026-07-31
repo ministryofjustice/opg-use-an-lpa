bd5740cf8361309f7143e9c085702ecf64f55669
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-07-31 09:51:09 +0100

    replace local default role with variable

--- terraform/environment/terraform.tf
+++ terraform/environment/terraform.tf
@@ -29,16 +29,22 @@ variable "management_role" {
   default = "opg-use-an-lpa-ci"
 }
 
-locals {
-  default_role = local.environment.permissions_boundary_enabled ? "opg-use-an-lpa-ci-boundary" : "opg-use-an-lpa-ci"
+variable "default_role" {
+  type    = string
+  default = "opg-use-an-lpa-ci-boundary"
 }
+
+# locals {
+#   default_role = local.environment.permissions_boundary_enabled ? "opg-use-an-lpa-ci-boundary" : "opg-use-an-lpa-ci"
+# }
+
 provider "aws" {
   region = "eu-west-1"
   default_tags {
     tags = local.default_tags
   }
   assume_role {
-    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${local.default_role}"
+    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${var.default_role}"
     session_name = "terraform-session"
   }
 }
@@ -50,7 +56,7 @@ provider "aws" {
     tags = local.default_tags
   }
   assume_role {
-    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${local.default_role}"
+    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${var.default_role}"
     session_name = "terraform-session"
   }
 }
@@ -62,7 +68,7 @@ provider "aws" {
     tags = local.default_tags
   }
   assume_role {
-    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${local.default_role}"
+    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${var.default_role}"
     session_name = "terraform-session"
   }
 }
@@ -75,7 +81,7 @@ provider "aws" {
     tags = local.default_tags
   }
   assume_role {
-    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${local.default_role}"
+    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${var.default_role}"
     session_name = "terraform-session"
   }
 }
