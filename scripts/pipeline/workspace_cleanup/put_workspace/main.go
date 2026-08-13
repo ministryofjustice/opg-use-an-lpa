e004f96d619c28619d723baebf2548e797154716
Seema Menon <seema.menon@digital.justice.gov.uk>
2026-08-13 10:33:26 +0100

    switched the ci role to a new boundaried ci role and was missed here

--- scripts/pipeline/workspace_cleanup/put_workspace/main.go
+++ scripts/pipeline/workspace_cleanup/put_workspace/main.go
@@ -39,7 +39,7 @@ func main() {
 	}
 	RoleArn := ""
 	if len(os.Getenv("CI")) > 0 {
-		RoleArn = "arn:aws:iam::367815980639:role/opg-use-an-lpa-ci-boundary"
+		RoleArn = "arn:aws:iam::367815980639:role/opg-use-an-lpa-ci"
 	} else {
 		RoleArn = "arn:aws:iam::367815980639:role/operator"
 	}
