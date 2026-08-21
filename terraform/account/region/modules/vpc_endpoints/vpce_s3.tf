e35696b008d34caf4fc8991b3fb19aa682bbc846
Josh Murray <jhm1999@hotmail.co.uk>
2026-08-21 11:01:04 +0100

    refactor s3 vpce policy

--- terraform/account/region/modules/vpc_endpoints/vpce_s3.tf
+++ terraform/account/region/modules/vpc_endpoints/vpce_s3.tf
@@ -10,28 +10,20 @@ resource "aws_vpc_endpoint" "s3" {
 resource "aws_vpc_endpoint_policy" "s3" {
   provider        = aws.region
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
-  policy          = data.aws_iam_policy_document.s3.json
-}
-
-data "aws_iam_policy_document" "s3" {
-  source_policy_documents = [
-    data.aws_iam_policy_document.allow_account_access.json,
-    data.aws_iam_policy_document.s3_bucket_access.json,
-  ]
-}
-
-data "aws_iam_policy_document" "s3_bucket_access" {
-  statement {
-    sid     = "Access-to-specific-bucket-only"
-    effect  = "Allow"
-    actions = ["s3:GetObject"]
-    resources = concat([
-      "arn:aws:s3:::prod-${var.region_name}-starport-layer-bucket/*",
-
-    ], var.permitted_s3_buckets)
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-  }
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "Access-to-specific-bucket-only",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        "Action" : ["s3:GetObject"],
+        "Resource" : concat([
+          "arn:aws:s3:::prod-${var.region_name}-starport-layer-bucket/*",
+        ], var.permitted_s3_buckets)
+      }
+    ]
+  })
 }
