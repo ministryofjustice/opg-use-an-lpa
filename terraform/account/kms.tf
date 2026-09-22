ac40622484f196d1206dc998ea9119806fbb6046
jay-whitwell <jay.whitwell@publicguardian.gov.uk>
2026-09-22 16:37:56 +0100

    add the arn role part to address comment

--- terraform/account/kms.tf
+++ terraform/account/kms.tf
@@ -242,7 +242,7 @@ module "dynamodb_encryption_key" {
       "-admin-task-role",
       "arn:aws:iam::${data.aws_caller_identity.backup.account_id}:role/aws-service-role/backup.amazonaws.com/AWSServiceRoleForBackup",
       "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/data-access",
-      "event-receiver-"
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/event-receiver-"
     ],
     local.environment != "production" ? ["use-a-lpa-github-actions-dynamodb-seeding-"] : [],
     local.environment == "production" ? ["use-a-lpa-github-actions-get-statistics"] : []
